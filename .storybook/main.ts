c62dd8158f1cea40d3dcd60a30726b8a6ec00b08
Niranjith Perumal <niranjithperumal@cunet.carleton.ca>
2023-02-28 16:33:11 -0500

    refactor(packages): updated packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,18 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "docs": {
-    "autodocs": "tag"
-  }
-};
-export default config;
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
