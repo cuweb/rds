ab24685f7d5846eca6bb73aef50bffc34f142595
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-01 16:17:38 -0500

    feat: added base component files for layouts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-export { Toast } from './components/Toast/Toast'
+// Components
+// -----------------
 export { Alert } from './components/Alert/Alert'
-export { Card } from './components/Card/Card'
-export { Link } from './components/Link/Link'
-export { Icon } from './components/Icon/Icon'
 export { Button } from './components/Button/Button'
+export { Card } from './components/Card/Card'
+export { Icon } from './components/Icon/Icon'
+export { Link } from './components/Link/Link'
+export { Toast } from './components/Toast/Toast'
+
+// Layouts
+// -----------------
+
+export { Article } from './layouts/Article/Article'
+export { Aside } from './layouts/Aside/Aside'
+export { Body } from './layouts/Body/Body'
+export { Column } from './layouts/Column/Column'
+export { Container } from './layouts/Container/Container'
+export { Main } from './layouts/Main/Main'
+export { Panel } from './layouts/Panel/Panel'
+export { StackedList } from './layouts/StackedList/StackedList'
