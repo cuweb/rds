ed67ea6df8a152297514bd389fbbb4c3d40cb689
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-01 12:20:53 -0500

    feat: added docs folder with changelog import

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,31 @@
-import './index.css';
+import './index.css'
 export const parameters = {
   backgrounds: {
     default: 'light',
   },
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      method: 'alphabetical',
+      order: [
+        'Get Started',
+        ['Introduction', 'Changelog', ['Basic Markup', 'Grey Section', 'Alternating Sections', 'Grids And Columns']],
+        'Components',
+        'Cards & Lists',
+        'Headers',
+        'Footers',
+        'Layouts',
+        ['Main', 'Container', 'Article', 'Column', 'Aside', 'Panel', 'Stacked List'],
+        'Forms',
+        'Loading Screens',
+        'Project Templates',
+      ],
     },
   },
 }
