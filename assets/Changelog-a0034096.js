95599600a3d2e120d1d9bfaf46bb4b6f9f4a7335
troychaplin <troy.chaplin@gmail.com>
2023-03-01 21:41:44 +0000

    Deploying to gh-pages from @ cuweb/rds@c0bbd43a1fa51545cd37953618ef4be86c3d9a00 🚀

---
+++ assets/Changelog-a0034096.js
@@ -0,0 +1,40 @@
+import{j as n,a as i,F as t}from"./jsx-runtime-670450c2.js";import"./chunk-G4YQS2SV-2daa0f97.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{M as c}from"./index-cd32d5e9.js";import{u as d}from"./index-4fb8b842.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-bd0626ce.js";import"../sb-preview/runtime.mjs";import"./react-18-75f22a57.js";import"./index-2d4beb60.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";function h(o={}){const{wrapper:r}=Object.assign({},d(),o.components);return r?n(r,Object.assign({},o,{children:n(l,{})})):l();function l(){const e=Object.assign({h1:"h1",p:"p",a:"a",ul:"ul",li:"li",em:"em",h2:"h2",h3:"h3",code:"code"},d(),o.components);return i(t,{children:[n(e.h1,{children:"Changelog"}),`
+`,n(e.p,{children:"All notable changes to this project will be documented in this file."}),`
+`,i(e.p,{children:["The format is based on ",n(e.a,{href:"https://keepachangelog.com/en/1.0.0/",children:"Keep a Changelog"}),`,
+and this project adheres to `,n(e.a,{href:"https://semver.org/spec/v2.0.0.html",children:"Semantic Versioning"}),"."]}),`
+`,n(e.p,{children:"Prefix the change with one of these keywords:"}),`
+`,i(e.ul,{children:[`
+`,i(e.li,{children:[n(e.em,{children:"Added"}),": for new features."]}),`
+`,i(e.li,{children:[n(e.em,{children:"Changed"}),": for changes in existing functionality."]}),`
+`,i(e.li,{children:[n(e.em,{children:"Deprecated"}),": for soon-to-be removed features."]}),`
+`,i(e.li,{children:[n(e.em,{children:"Removed"}),": for now removed features."]}),`
+`,i(e.li,{children:[n(e.em,{children:"Fixed"}),": for any bug fixes."]}),`
+`,i(e.li,{children:[n(e.em,{children:"Security"}),": in case of vulnerabilities."]}),`
+`]}),`
+`,n(e.h2,{children:"[Unreleased]"}),`
+`,n(e.h3,{children:"Added"}),`
+`,i(e.ul,{children:[`
+`,i(e.li,{children:[n(e.code,{children:"Article"})," layout component"]}),`
+`,i(e.li,{children:[n(e.code,{children:"Aside"})," layout component"]}),`
+`,i(e.li,{children:[n(e.code,{children:"Body"})," layout component"]}),`
+`,i(e.li,{children:[n(e.code,{children:"Column"})," layout component"]}),`
+`,i(e.li,{children:[n(e.code,{children:"Container"})," layout component"]}),`
+`,i(e.li,{children:[n(e.code,{children:"Main"})," layout component"]}),`
+`,i(e.li,{children:[n(e.code,{children:"Panel"})," layout component"]}),`
+`,i(e.li,{children:[n(e.code,{children:"StackedList"})," layout component"]}),`
+`]}),`
+`,n(e.h2,{children:"[0.0.4]"}),`
+`,n(e.h3,{children:"Added"}),`
+`,i(e.ul,{children:[`
+`,i(e.li,{children:["Added ",n(e.code,{children:"publish.yml"})," git workflow"]}),`
+`,n(e.li,{children:"Added a11y add-on to Storybook"}),`
+`,i(e.li,{children:["Added ",n(e.code,{children:"CHANGELOG.mdx"})," and imported into a Changelog story doc file"]}),`
+`,i(e.li,{children:["Initial commit from a clone of ",n(e.a,{href:"https://github.com/troychaplin/design-ui",children:"Design UI"})]}),`
+`]}),`
+`,n(e.h3,{children:n(e.em,{children:"Changed"})}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"Updated secret keys for Google Maps API"}),`
+`,n(e.li,{children:"Renamed README.md to README.mdx"}),`
+`]})]})}}function w(o={}){const{wrapper:r}=Object.assign({},d(),o.components);return r?n(r,Object.assign({},o,{children:n(l,{})})):l();function l(){return i(t,{children:[n(c,{title:"Get Started/Changelog"}),`
+`,n(h,{})]})}}export{w as default};
+//# sourceMappingURL=Changelog-a0034096.js.map
---
+++ assets/WithTooltip-5WWCYCAA-c8e3361f.js
@@ -0,0 +1,2 @@
+import{b as l,b as s,d as W}from"./index-cd32d5e9.js";import"./iframe-bd0626ce.js";import"../sb-preview/runtime.mjs";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-d475d2ea.js";import"./index-2d4beb60.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{l as WithToolTipState,s as WithTooltip,W as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-5WWCYCAA-c8e3361f.js.map
