313c4846a193a23e16b9b23ab77467441ed2826c
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-01 09:12:44 -0500

    feat: setup docs folder, added changelog as doc page, removed forms to avoid commit warnings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,33 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        transcludeMarkdown: true,
+      },
+    },
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        outline: false,
+        toolbars: false,
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "docs": {
-    "autodocs": "tag"
-  }
-};
-export default config;
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-export { Toast } from './components/Toast/Toast'
 export { Alert } from './components/Alert/Alert'
-export { Card } from './components/Card/Card'
-export { Link } from './components/Link/Link'
-export { Icon } from './components/Icon/Icon'
 export { Button } from './components/Button/Button'
-
-export { Form } from './components/Forms/Form/Form'
+export { Card } from './components/Card/Card'
+export { Toast } from './components/Toast/Toast'
+export { Icon } from './components/Icon/Icon'
+export { Link } from './components/Link/Link'
