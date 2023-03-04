ab42c11c752882c8668258d81dd16a009098ec13
troychaplin <troy.chaplin@gmail.com>
2023-03-04 03:53:04 +0000

    Deploying to gh-pages from @ cuweb/rds@e39ebc3a0dcbdda70079007b27abab42ce790a7b 🚀

---
+++ layouts/Body/Body.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+export interface BodyProps {
+    children: React.ReactNode;
+    className?: string;
+}
+export declare const Body: ({ children, className }: BodyProps) => JSX.Element;
+//# sourceMappingURL=Body.d.ts.map
---
+++ stories/Body.stories.d.ts
@@ -0,0 +1,8 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Body } from '../layouts/Body/Body';
+declare const meta: Meta<typeof Body>;
+export default meta;
+type Story = StoryObj<typeof Body>;
+export declare const Primary: Story;
+export declare const UseTailwind: Story;
+//# sourceMappingURL=Body.stories.d.ts.map
