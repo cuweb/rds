c1d1d7970041c03a054e803fd4ed2c759b09a6f6
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-04 12:59:47 -0500

    added: panel and stackedlist components

--- src/layouts/Panel/Panel.tsx
+++ src/layouts/Panel/Panel.tsx
@@ -4,17 +4,20 @@ const styles = {
   wrapper: `cu-panel not-prose overflow-hidden rounded-lg bg-white`,
   border: `border border-cu-black-100`,
   shadow: `shadow-lg`,
+  spacing: `p-6`,
 }
 
 export interface PanelProps {
   children: React.ReactNode
   hasBorder?: boolean
   hasShadow?: boolean
+  hasSpacing?: boolean
 }
 
-export const Panel = ({ children, hasBorder, hasShadow }: PanelProps) => {
+export const Panel = ({ children, hasBorder, hasShadow, hasSpacing }: PanelProps) => {
   const borderStyle = hasBorder ? styles.border : ''
   const shadowStyle = hasShadow ? styles.shadow : ''
+  const spacingStyle = hasSpacing ? styles.spacing : ''
 
-  return <div className={`${styles.wrapper} ${borderStyle} ${shadowStyle}`}>{children}</div>
+  return <div className={`${styles.wrapper} ${borderStyle} ${shadowStyle} ${spacingStyle}`}>{children}</div>
 }
