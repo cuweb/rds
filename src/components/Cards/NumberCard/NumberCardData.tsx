8451562ffa7f57276566dfdedf6455981d3e7987
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-06 11:45:28 -0500

    add: number card component

---
+++ src/components/Cards/NumberCard/NumberCardData.tsx
@@ -0,0 +1,47 @@
+export const NumberCardData = [
+  {
+    id: 1,
+    title: 'Carleton Alumni',
+    content: '177,000+',
+  },
+  {
+    id: 2,
+    title: 'Faculty Members',
+    content: '1,006',
+  },
+  {
+    id: 3,
+    title: 'Staff Members',
+    content: '1,403',
+  },
+  {
+    id: 4,
+    title: 'Library Staff',
+    content: '105',
+  },
+  {
+    id: 5,
+    title: 'Graduate Teaching Assistants',
+    content: '2020',
+  },
+  {
+    id: 6,
+    title: 'Contract Instructors',
+    content: '893',
+  },
+  {
+    id: 7,
+    title: 'Research Funding (2020-21)',
+    content: '$97.4 M',
+  },
+  {
+    id: 8,
+    title: 'Scholarships and Bursaries awarded',
+    content: '$97.4 M',
+  },
+  {
+    id: 9,
+    title: 'Students received OSAP',
+    content: '35.8%',
+  },
+];
