e7f68d432a8e9dcf1cdd685896a2765eb71bb423
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-06 12:36:06 -0500

    add: added the rest of the list item components

---
+++ src/components/ListingItems/FileItem/FileItemData.tsx
@@ -0,0 +1,37 @@
+export const FileItemData = [
+  {
+    id: 1,
+    title: 'Monthly Leave Report',
+    link: '#',
+    filesize: '175KB',
+    date: 'January 7, 2020',
+  },
+  {
+    id: 2,
+    title: 'Dental Claim Form',
+    link: '#',
+    filesize: '175KB',
+    date: 'January 7, 2020',
+  },
+  {
+    id: 3,
+    title: 'Healthcare Expenses Statement',
+    link: '#',
+    filesize: '175KB',
+    date: 'January 7, 2020',
+  },
+  {
+    id: 4,
+    title: 'CUPE 3778 Collective Agreement',
+    link: '#',
+    filesize: '175KB',
+    date: 'January 7, 2020',
+  },
+  {
+    id: 5,
+    title: 'Accident Report',
+    link: '#',
+    filesize: '175KB',
+    date: 'January 7, 2020',
+  },
+]
