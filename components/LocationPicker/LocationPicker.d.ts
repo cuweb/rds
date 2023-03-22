ef74e91c53a979d6c2e1ec7ee304d91b82904e76
troychaplin <troy.chaplin@gmail.com>
2023-03-22 20:37:38 +0000

    Deploying to gh-pages from @ cuweb/rds@4af30d3826bd26df868118c94f716f656fd228b2 🚀

---
+++ components/LocationPicker/LocationPicker.d.ts
@@ -0,0 +1,2 @@
+export declare const LocationPicker: () => JSX.Element;
+//# sourceMappingURL=LocationPicker.d.ts.map
---
+++ stories/LocationPicker.stories.d.ts
@@ -0,0 +1,7 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LocationPicker } from '../components/LocationPicker/LocationPicker';
+declare const meta: Meta<typeof LocationPicker>;
+export default meta;
+type Story = StoryObj<typeof LocationPicker>;
+export declare const Default: Story;
+//# sourceMappingURL=LocationPicker.stories.d.ts.map
