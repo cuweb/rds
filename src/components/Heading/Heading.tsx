d2a36ed552906f182be1ecabd25b765d90fef7a9
Troy Chaplin <troy.chaplin@gmail.com>
2023-03-24 07:48:25 -0400

    change: heading spacing

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -10,7 +10,7 @@ export interface HeadingProps {
 const styles = {
   prose: `prose prose-lg md:prose-xl`,
   spacing: `pt-10 pb-2.5 md:pt-14`,
-  heading: `prose-headings:font-semibold prose-headings:text-cu-black-800 prose-headings:mb-6 prose-headings:pb-4`,
+  heading: `prose-headings:font-semibold prose-headings:text-cu-black-800 prose-headings:mb-6 prose-headings:pb-6`,
   underline: `after:absolute after:w-10 after:h-px after:bottom-0 after:bg-cu-red`,
   paragraph: `prose-p:md:text-[22px] prose-p:leading-9 prose-p:text-cu-black-800`,
 }
