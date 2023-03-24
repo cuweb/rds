688c8ba4bea205015163c9e344729831aaafda1f
troychaplin <troy.chaplin@gmail.com>
2023-03-24 15:39:10 +0000

    Deploying to gh-pages from @ cuweb/rds@0b018ca16fa57e0d1ee6a208cb9e514871d7c064 🚀

---
+++ assets/Checkbox.stories-61d0d022.js
@@ -0,0 +1,9 @@
+import{j as a}from"./jsx-runtime-9d2cd5de.js";import{F as o}from"./Form-97f6ffdf.js";import"./index-580b8df0.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-e08e05bb.js";import"./Button-40091c81.js";import"./Icon-98794453.js";import"./optionClasses-9f612ec7.js";import"./ChevronDownIcon-4154b4bc.js";import"./Link-44b0899b.js";import"./Calendar-aa9e3dfb.js";import"./index-0ba33c7b.js";import"./index-e2ec86ea.js";import"./index-ae1f7f2c.js";import"./index-34bd24b7.js";import"./index-4d501b15.js";import"./Location-2b638b33.js";import"./index-1eb27dd0.js";import"./combobox-00d1fd7e.js";import"./open-closed-6ab898d8.js";import"./keyboard-46ff6cb9.js";import"./use-resolve-button-type-6b4df908.js";import"./use-tracked-pointer-511c0c47.js";import"./platform-ae94fb3f.js";const W={title:"Forms/Checkbox",component:o,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},r={render:()=>a(o,{onSubmit:()=>{},schema:{},initialValues:m,children:a(o.Checkbox,{name:"favcharacters",label:"Who are your favourite 90s sitcom characters?",options:[{name:"name",label:"name"}]})})},m={firstname:"ish",description:" sample description",regularviewer:"2",location:"Carleton University, Colonel By Drive, Ottawa, ON, Canada",favsitcom:"familymatter",favcharacters:["kellykapowski","carltonbanks"]};var t,e,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <Form onSubmit={() => undefined} schema={{}} initialValues={InitialValues}>
+      <Form.Checkbox name="favcharacters" label="Who are your favourite 90s sitcom characters?" options={[{
+      name: 'name',
+      label: 'name'
+    }]} />
+    </Form>
+}`,...(i=(e=r.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,W as default};
+//# sourceMappingURL=Checkbox.stories-61d0d022.js.map
