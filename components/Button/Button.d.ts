d7f3554698651c2744fc4d8ed59aa7ee9cea6386
troychaplin <troy.chaplin@gmail.com>
2023-04-11 18:21:12 +0000

    Deploying to gh-pages from @ cuweb/rds@4947c11456afb64ea6eb497340b58af0d8ac9e24 🚀

--- components/Button/Button.d.ts
+++ components/Button/Button.d.ts
@@ -2,14 +2,13 @@ import React from 'react';
 export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
     title?: string;
     icon?: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>;
-    isType?: 'default' | 'ghost' | 'grey' | 'dark-grey' | 'white' | 'disabled';
-    size?: 'sm' | 'base';
+    isType?: 'solid' | 'outline' | 'disabled';
+    color?: 'red' | 'grey' | 'dark-grey' | 'white';
+    isSmall?: boolean;
     hasShadow?: boolean;
     isFull?: boolean;
     isCenter?: boolean;
-    disabled?: boolean;
     hasDropDown?: boolean;
-    url?: string | undefined;
 }
-export declare const Button: ({ isType, size, title, icon, hasShadow, isFull, isCenter, url, hasDropDown, ...rest }: ButtonProps) => JSX.Element;
+export declare const Button: ({ isType, color, title, icon, isSmall, hasShadow, isFull, isCenter, hasDropDown, ...rest }: ButtonProps) => JSX.Element;
 //# sourceMappingURL=Button.d.ts.map
---
+++ utils/buttonClasses.d.ts
@@ -0,0 +1,21 @@
+export declare const buttonStyles: {
+    core: string;
+    solid: string;
+    outline: string;
+    shadow: string;
+    small: string;
+    disabled: string;
+};
+export declare const solidStyles: {
+    red: string;
+    'dark-grey': string;
+    grey: string;
+    white: string;
+};
+export declare const outlineStyles: {
+    red: string;
+    'dark-grey': string;
+    grey: string;
+    white: string;
+};
+//# sourceMappingURL=buttonClasses.d.ts.map
