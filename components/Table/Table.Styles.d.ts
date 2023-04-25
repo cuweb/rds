e740db1b7685e8ddb3531a78b35d7abe7870c035
troychaplin <troy.chaplin@gmail.com>
2023-04-25 17:00:51 +0000

    Deploying to gh-pages from @ cuweb/rds@b56fd8a184a58ab4c551a34f19a8cfe5b8796a50 🚀

---
+++ components/Table/Table.Styles.d.ts
@@ -0,0 +1,10 @@
+export declare const styles: {
+    table: string;
+    shadow: string;
+    border: string;
+    thead: string;
+    tbody: string;
+    td: string;
+    striped: string;
+};
+//# sourceMappingURL=Table.Styles.d.ts.map
--- components/Table/Table.styles.d.ts
+++
@@ -1,7 +0,0 @@
-declare const styles: {
-    core: string;
-    shadow: string;
-    border: string;
-};
-export default styles;
-//# sourceMappingURL=Table.styles.d.ts.map
