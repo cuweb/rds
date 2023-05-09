d6c987417ebc6b20f4b1ac68011e8e0f604f59b6
troychaplin <troy.chaplin@gmail.com>
2023-05-09 17:56:31 +0000

    Deploying to gh-pages from @ cuweb/rds@ff877ddc37f4dd2c623dd0213a621299d1c56aac 🚀

---
+++ assets/FooterBasic-1089ebf0.js
@@ -0,0 +1,2 @@
+import{j as o}from"./jsx-runtime-daf202a7.js";import{F as e}from"./FooterWave-9f2988f8.js";const t=()=>o.jsxs("footer",{className:"not-prose bg-cu-black-900 pt-8 sm:pt-12","aria-labelledby":"footer-heading",children:[o.jsx("h2",{id:"footer-heading",className:"sr-only",children:"Footer"}),o.jsx(e,{})]});export{t as F};
+//# sourceMappingURL=FooterBasic-1089ebf0.js.map
