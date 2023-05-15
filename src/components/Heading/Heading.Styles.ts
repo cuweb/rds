a7b17a482c9cae2140102755f8780335577d04c9
Troy Chaplin <troy.chaplin@gmail.com>
2023-05-15 14:45:31 -0400

    change: spacing on header and global spacing styles for container and section

---
+++ src/components/Heading/Heading.Styles.ts
@@ -0,0 +1,7 @@
+export const styles = {
+  prose: `prose prose-lg md:prose-xl`,
+  spacing: `pt-10 pb-2.5 md:pt-14`,
+  heading: `prose-headings:font-semibold prose-headings:text-cu-black-800 prose-headings:mb-6 prose-headings:pb-6 mb-5 lg:mb-8`,
+  underline: `after:absolute after:w-10 after:h-px after:bottom-0 after:bg-cu-red`,
+  paragraph: `prose-p:md:text-[22px] prose-p:leading-9 prose-p:text-cu-black-800`,
+}
