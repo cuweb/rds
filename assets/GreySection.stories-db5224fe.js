31ed74a2f0517038967da62680a1082ed67e77cf
troychaplin <troy.chaplin@gmail.com>
2023-05-15 19:02:23 +0000

    Deploying to gh-pages from @ cuweb/rds@4949c8a87c11081ec29e63a3c6ddcdb9a219bc82 🚀

---
+++ assets/GreySection.stories-db5224fe.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-daf202a7.js";import{M as o}from"./Main-ffb90dea.js";import{S as n}from"./Section-41118051.js";import{C as u}from"./Container-de90f290.js";import{T as m}from"./TopNav-0efb9330.js";import{B as p}from"./Banner-075a2cd7.js";import{F as l}from"./FooterBasic-b338f8bb.js";import"./index-ec8b93d8.js";import"./_commonjsHelpers-042e6b4d.js";import"./globalClasses-044468c0.js";import"./index-12b82ffd.js";import"./Avatar-80b16ccb.js";import"./optionClasses-767b8be6.js";import"./DropDown-65a81d84.js";import"./ChevronDownIcon-bf1ca2d9.js";import"./Icon-78ba648b.js";import"./Link-0eedc7fc.js";import"./iframe-28ef3b1a.js";import"../sb-preview/runtime.js";import"./popover-509b60ee.js";import"./open-closed-aa2934ca.js";import"./keyboard-815d6eba.js";import"./use-resolve-button-type-b6561d60.js";import"./use-event-listener-e14079c9.js";import"./transition-4429edf0.js";import"./Search-276954f3.js";import"./dialog-12422483.js";import"./platform-42294b71.js";import"./index-73f8b247.js";import"./combobox-b5286f3e.js";import"./use-tracked-pointer-b81bf215.js";import"./TopNavLoader-2924a9dd.js";import"./disclosure-dd40256e.js";import"./FooterWave-99e34901.js";const K={title:"Examples/Templates/Grey Section"},i=()=>e.jsx(e.Fragment,{children:e.jsxs("p",{children:[e.jsx("strong",{children:"Single Paragraph:"})," Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."]})}),c=()=>e.jsxs(e.Fragment,{children:[e.jsxs("p",{children:[e.jsx("strong",{children:"Double Paragraph:"})," Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."]}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),t={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Carleton University"}),e.jsx(p,{title:"Grey Section Container",paragraph:"This template replicates the basic markup template by using simple headings and paragraphs but is modifying the section container to feature a grey background that extends to the full width of the browser"}),e.jsx(o,{children:e.jsx(n,{children:e.jsxs(u,{hasProse:!0,children:[e.jsx(i,{}),e.jsx("h2",{children:"This is a header two"}),e.jsx(c,{}),e.jsx("h3",{children:"This is a header three"}),e.jsx(i,{})]})})}),e.jsx(l,{})]})};var r,a,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <>
+      <TopNav title="Carleton University" />
+      <Banner title="Grey Section Container" paragraph="This template replicates the basic markup template by using simple headings and paragraphs but is modifying the section container to feature a grey background that extends to the full width of the browser" />
+
+      <Main>
+        <Section>
+          <Container hasProse>
+            <SinglePara />
+            <h2>This is a header two</h2>
+            <DoublePara />
+            <h3>This is a header three</h3>
+            <SinglePara />
+          </Container>
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(s=(a=t.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const Q=["GreySection"];export{t as GreySection,Q as __namedExportsOrder,K as default};
+//# sourceMappingURL=GreySection.stories-db5224fe.js.map
