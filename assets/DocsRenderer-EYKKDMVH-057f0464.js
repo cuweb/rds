3e19b45b23d563f418de3e0194c202e25d0bb977
troychaplin <troy.chaplin@gmail.com>
2023-05-24 16:52:35 +0000

    Deploying to gh-pages from @ cuweb/rds@8e0a31332844fb6386a55cb026d440bf483bb097 🚀

---
+++ assets/DocsRenderer-EYKKDMVH-057f0464.js
@@ -0,0 +1,2 @@
+import{D as l,d as u}from"./chunk-PCJTTTQV-8a505ab6.js";import"./iframe-71df0706.js";import"../sb-preview/runtime.js";import"./index-ec8b93d8.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-18-2839251f.js";import"./index-73f8b247.js";import"./index-5ceb3f84.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-c2b08d85.js";import"./_baseIsEqual-782858e5.js";import"./index-356e4a49.js";export{l as DocsRenderer,u as defaultComponents};
+//# sourceMappingURL=DocsRenderer-EYKKDMVH-057f0464.js.map
---
+++ assets/NewsListing.stories-e2e062dd.js
@@ -0,0 +1,193 @@
+import{j as t}from"./jsx-runtime-daf202a7.js";import{M as B}from"./Main-ffb90dea.js";import{S as C}from"./Section-399033d9.js";import{C as l}from"./Container-42eb7417.js";import{S as h}from"./StackedList-4165f68e.js";import{T as k}from"./TopNav-5e6965c3.js";import{B as S}from"./Banner-2ef25b74.js";import{F as v}from"./FooterBasic-195e86e7.js";import{L as e}from"./Listing-909a9373.js";import{a as c}from"./NewsData-8fa2e9af.js";import{H as x}from"./Heading-8a8f6ed6.js";import"./index-ec8b93d8.js";import"./_commonjsHelpers-042e6b4d.js";import"./globalClasses-74916236.js";import"./Panel-7a739c47.js";import"./optionClasses-767b8be6.js";import"./index-12b82ffd.js";import"./Avatar-80b16ccb.js";import"./DropDown-4bb1a361.js";import"./ChevronDownIcon-bf1ca2d9.js";import"./Icon-78ba648b.js";import"./Link-09979d81.js";import"./iframe-71df0706.js";import"../sb-preview/runtime.js";import"./popover-509b60ee.js";import"./open-closed-aa2934ca.js";import"./keyboard-815d6eba.js";import"./use-resolve-button-type-b6561d60.js";import"./use-event-listener-e14079c9.js";import"./transition-4429edf0.js";import"./Search-ca06f108.js";import"./dialog-12422483.js";import"./platform-42294b71.js";import"./index-73f8b247.js";import"./combobox-b5286f3e.js";import"./use-tracked-pointer-b81bf215.js";import"./TopNavLoader-2924a9dd.js";import"./disclosure-dd40256e.js";import"./FooterWave-7e169af4.js";import"./index-e4ae510c.js";import"./index-f3821225.js";import"./index-9180d5e9.js";import"./index-ea76b77d.js";import"./index-3d4b39e4.js";import"./Badge-4cc04c38.js";import"./index-ac9ed659.js";const jt={title:"Examples/Projects/cutheme/Listing Blocks",tags:["autodocs"]},m=()=>t.jsx(t.Fragment,{children:t.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."})}),p=()=>t.jsxs(t.Fragment,{children:[t.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),t.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),u={render:()=>t.jsxs(t.Fragment,{children:[t.jsx(k,{title:"Carleton University"}),t.jsx(S,{title:"cutheme news card testing",isType:"dark-wave",align:"left"}),t.jsx(B,{children:t.jsxs(C,{hasProse:!0,children:[t.jsx(p,{}),t.jsx("h2",{children:"News Listing Block"}),t.jsx(m,{}),t.jsxs(l,{bgColor:"white",maxWidth:"7xl",hasProse:!0,children:[t.jsx(x,{text:"Basic News Listing Block"}),t.jsx(h,{hasBorder:!0,hasShadow:!0,children:c.map(({id:i,title:n,link:s,image:r,alt:a,date:o,excerpt:g,tags:d})=>t.jsx(e,{children:t.jsxs("a",{href:s,children:[t.jsx(e.Figure,{children:t.jsx("img",{src:r,alt:a,width:"400",height:"266"})}),t.jsxs(e.Content,{children:[t.jsx(e.PostMeta,{date:o}),t.jsx(e.Header,{text:n}),t.jsx(e.Excerpt,{text:g}),t.jsx(e.Badges,{tags:d})]})]})},i))})]}),t.jsx(p,{}),t.jsx("h2",{children:"News Listing Block with Grey Bg"}),t.jsx(m,{}),t.jsxs(l,{bgColor:"grey",hasProse:!0,children:[t.jsx(x,{text:"News Listing Block with Grey Bg"}),t.jsx(h,{hasBorder:!0,hasShadow:!0,children:c.map(({id:i,title:n,link:s,image:r,alt:a,date:o,excerpt:g,tags:d})=>t.jsx(e,{children:t.jsxs("a",{href:s,children:[t.jsx(e.Figure,{children:t.jsx("img",{src:r,alt:a,width:"400",height:"266"})}),t.jsxs(e.Content,{children:[t.jsx(e.PostMeta,{date:o}),t.jsx(e.Header,{text:n}),t.jsx(e.Excerpt,{text:g}),t.jsx(e.Badges,{tags:d})]})]})},i))})]}),t.jsx("h2",{children:"Consecutive News Listing Block"}),t.jsx(m,{}),t.jsx(l,{bgColor:"white",hasProse:!0,children:t.jsx(h,{hasBorder:!0,hasShadow:!0,children:c.map(({id:i,title:n,link:s,image:r,alt:a,date:o,excerpt:g,tags:d})=>t.jsx(e,{children:t.jsxs("a",{href:s,children:[t.jsx(e.Figure,{children:t.jsx("img",{src:r,alt:a,width:"400",height:"266"})}),t.jsxs(e.Content,{children:[t.jsx(e.PostMeta,{date:o}),t.jsx(e.Header,{text:n}),t.jsx(e.Excerpt,{text:g}),t.jsx(e.Badges,{tags:d})]})]})},i))})}),t.jsx(l,{bgColor:"white",hasProse:!0,children:t.jsx(h,{hasBorder:!0,hasShadow:!0,children:c.map(({id:i,title:n,link:s,image:r,alt:a,date:o,excerpt:g,tags:d})=>t.jsx(e,{children:t.jsxs("a",{href:s,children:[t.jsx(e.Figure,{children:t.jsx("img",{src:r,alt:a,width:"400",height:"266"})}),t.jsxs(e.Content,{children:[t.jsx(e.PostMeta,{date:o}),t.jsx(e.Header,{text:n}),t.jsx(e.Excerpt,{text:g}),t.jsx(e.Badges,{tags:d})]})]})},i))})}),t.jsx("h2",{children:"Consecutive News Listing Block with Grey Bg"}),t.jsx(m,{}),t.jsx(l,{bgColor:"grey",hasProse:!0,children:t.jsx(h,{hasBorder:!0,hasShadow:!0,children:c.map(({id:i,title:n,link:s,image:r,alt:a,date:o,excerpt:g,tags:d})=>t.jsx(e,{children:t.jsxs("a",{href:s,children:[t.jsx(e.Figure,{children:t.jsx("img",{src:r,alt:a,width:"400",height:"266"})}),t.jsxs(e.Content,{children:[t.jsx(e.PostMeta,{date:o}),t.jsx(e.Header,{text:n}),t.jsx(e.Excerpt,{text:g}),t.jsx(e.Badges,{tags:d})]})]})},i))})}),t.jsx(l,{bgColor:"grey",hasProse:!0,children:t.jsx(h,{hasBorder:!0,hasShadow:!0,children:c.map(({id:i,title:n,link:s,image:r,alt:a,date:o,excerpt:g,tags:d})=>t.jsx(e,{children:t.jsxs("a",{href:s,children:[t.jsx(e.Figure,{children:t.jsx("img",{src:r,alt:a,width:"400",height:"266"})}),t.jsxs(e.Content,{children:[t.jsx(e.PostMeta,{date:o}),t.jsx(e.Header,{text:n}),t.jsx(e.Excerpt,{text:g}),t.jsx(e.Badges,{tags:d})]})]})},i))})})]})}),t.jsx(v,{})]})};var j,L,w;u.parameters={...u.parameters,docs:{...(j=u.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <>
+      <TopNav title="Carleton University" />
+      <Banner title="cutheme news card testing" isType="dark-wave" align="left" />
+
+      <Main>
+        <Section hasProse>
+          <DoublePara />
+
+          <h2>News Listing Block</h2>
+          <SinglePara />
+
+          <Container bgColor="white" maxWidth="7xl" hasProse>
+            <Heading text="Basic News Listing Block" />
+            <StackedList hasBorder hasShadow>
+              {data.map(({
+              id,
+              title,
+              link,
+              image,
+              alt,
+              date,
+              excerpt,
+              tags
+            }) => <Listing key={id}>
+                  <a href={link}>
+                    <Listing.Figure>
+                      <img src={image} alt={alt} width="400" height="266" />
+                    </Listing.Figure>
+                    <Listing.Content>
+                      <Listing.PostMeta date={date} />
+                      <Listing.Header text={title} />
+                      <Listing.Excerpt text={excerpt} />
+                      <Listing.Badges tags={tags} />
+                    </Listing.Content>
+                  </a>
+                </Listing>)}
+            </StackedList>
+          </Container>
+
+          <DoublePara />
+
+          <h2>News Listing Block with Grey Bg</h2>
+          <SinglePara />
+
+          <Container bgColor="grey" hasProse>
+            <Heading text="News Listing Block with Grey Bg" />
+            <StackedList hasBorder hasShadow>
+              {data.map(({
+              id,
+              title,
+              link,
+              image,
+              alt,
+              date,
+              excerpt,
+              tags
+            }) => <Listing key={id}>
+                  <a href={link}>
+                    <Listing.Figure>
+                      <img src={image} alt={alt} width="400" height="266" />
+                    </Listing.Figure>
+                    <Listing.Content>
+                      <Listing.PostMeta date={date} />
+                      <Listing.Header text={title} />
+                      <Listing.Excerpt text={excerpt} />
+                      <Listing.Badges tags={tags} />
+                    </Listing.Content>
+                  </a>
+                </Listing>)}
+            </StackedList>
+          </Container>
+
+          <h2>Consecutive News Listing Block</h2>
+          <SinglePara />
+
+          <Container bgColor="white" hasProse>
+            <StackedList hasBorder hasShadow>
+              {data.map(({
+              id,
+              title,
+              link,
+              image,
+              alt,
+              date,
+              excerpt,
+              tags
+            }) => <Listing key={id}>
+                  <a href={link}>
+                    <Listing.Figure>
+                      <img src={image} alt={alt} width="400" height="266" />
+                    </Listing.Figure>
+                    <Listing.Content>
+                      <Listing.PostMeta date={date} />
+                      <Listing.Header text={title} />
+                      <Listing.Excerpt text={excerpt} />
+                      <Listing.Badges tags={tags} />
+                    </Listing.Content>
+                  </a>
+                </Listing>)}
+            </StackedList>
+          </Container>
+
+          <Container bgColor="white" hasProse>
+            <StackedList hasBorder hasShadow>
+              {data.map(({
+              id,
+              title,
+              link,
+              image,
+              alt,
+              date,
+              excerpt,
+              tags
+            }) => <Listing key={id}>
+                  <a href={link}>
+                    <Listing.Figure>
+                      <img src={image} alt={alt} width="400" height="266" />
+                    </Listing.Figure>
+                    <Listing.Content>
+                      <Listing.PostMeta date={date} />
+                      <Listing.Header text={title} />
+                      <Listing.Excerpt text={excerpt} />
+                      <Listing.Badges tags={tags} />
+                    </Listing.Content>
+                  </a>
+                </Listing>)}
+            </StackedList>
+          </Container>
+
+          <h2>Consecutive News Listing Block with Grey Bg</h2>
+          <SinglePara />
+
+          <Container bgColor="grey" hasProse>
+            <StackedList hasBorder hasShadow>
+              {data.map(({
+              id,
+              title,
+              link,
+              image,
+              alt,
+              date,
+              excerpt,
+              tags
+            }) => <Listing key={id}>
+                  <a href={link}>
+                    <Listing.Figure>
+                      <img src={image} alt={alt} width="400" height="266" />
+                    </Listing.Figure>
+                    <Listing.Content>
+                      <Listing.PostMeta date={date} />
+                      <Listing.Header text={title} />
+                      <Listing.Excerpt text={excerpt} />
+                      <Listing.Badges tags={tags} />
+                    </Listing.Content>
+                  </a>
+                </Listing>)}
+            </StackedList>
+          </Container>
+
+          <Container bgColor="grey" hasProse>
+            <StackedList hasBorder hasShadow>
+              {data.map(({
+              id,
+              title,
+              link,
+              image,
+              alt,
+              date,
+              excerpt,
+              tags
+            }) => <Listing key={id}>
+                  <a href={link}>
+                    <Listing.Figure>
+                      <img src={image} alt={alt} width="400" height="266" />
+                    </Listing.Figure>
+                    <Listing.Content>
+                      <Listing.PostMeta date={date} />
+                      <Listing.Header text={title} />
+                      <Listing.Excerpt text={excerpt} />
+                      <Listing.Badges tags={tags} />
+                    </Listing.Content>
+                  </a>
+                </Listing>)}
+            </StackedList>
+          </Container>
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(w=(L=u.parameters)==null?void 0:L.docs)==null?void 0:w.source}}};const Lt=["NewsListingBlocks"];export{u as NewsListingBlocks,Lt as __namedExportsOrder,jt as default};
+//# sourceMappingURL=NewsListing.stories-e2e062dd.js.map
