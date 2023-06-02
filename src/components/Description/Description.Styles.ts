1e6ae495114969d64af7425956364e7fa06166af
Troy Chaplin <troy.chaplin@gmail.com>
2023-06-01 22:23:41 -0400

    change: stacked and column descList options now use grid

--- src/components/Description/Description.Styles.ts
+++ src/components/Description/Description.Styles.ts
@@ -1,7 +1,7 @@
 export const styles = {
-  base: `border-b border-cu-black-200 last:border-none py-5 first:pt-0 last-pt-b`,
-  flexRow: `grid md:grid-cols-left260 gap-4`,
-  flexCol: `flex flex-col gap-2`,
+  base: `grid py-5 border-b border-cu-black-200 last:border-none first:pt-0 last-pt-b`,
+  flexRow: `md:grid-cols-left260 gap-6`,
+  flexCol: `grid-row gap-2`,
   button: `flex items-center w-full`,
   chevron: `w-5 h-5 ml-auto text-cu-black-500 transition-transform`,
   term: `font-semibold text-lg md:text-xl`,
