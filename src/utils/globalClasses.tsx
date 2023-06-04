918e9e0fe1e6ed6303be5fb49131a3c3e4d3bb45
Troy Chaplin <troy.chaplin@gmail.com>
2023-06-04 08:42:12 -0400

    change: updated spacing on section component

--- src/utils/globalClasses.tsx
+++ src/utils/globalClasses.tsx
@@ -3,9 +3,9 @@ export const proseStyles = {
 }
 
 export const layoutSpacing = {
-  section: 'px-8 py-8 md:px-10 md:py-12',
+  section: 'py-6 md:py-10 px-8 md:p-10',
   containerWhite: 'py-5 md:py-6 bg-white',
-  containerGrey: 'bg-cu-black-50 rounded-xl py-6 md:py-12 px-8 md:p-10',
+  containerGrey: 'bg-cu-black-50 rounded-xl py-6 md:py-10 px-8 md:p-10',
 }
 
 // ORIGINAL
