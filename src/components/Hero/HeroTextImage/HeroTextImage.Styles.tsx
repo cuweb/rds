29a709d7d5fc91bda3da742d9d0e2654d455c6a9
Troy Chaplin <troy.chaplin@gmail.com>
2023-06-05 21:30:36 -0400

    change: updated hero text image styles

--- src/components/Hero/HeroTextImage/HeroTextImage.Styles.tsx
+++ src/components/Hero/HeroTextImage/HeroTextImage.Styles.tsx
@@ -1,10 +1,10 @@
 export const styles = {
   base: 'cu-herotextimage not-contained not-prose mx-auto lg:items-center mb-12',
-  bottom: 'pb-6 border-b border-cu-black-50',
+  bottom: 'pb-6 border-b border-cu-black-50 shadow-hero-text-image',
   bottomSpace: 'md:pb-10',
   grid: 'grid gap-6 md:gap-10 mx-auto ',
-  gridWithImage: 'md:grid-cols-hero-txtimg-40',
-  gridNoImage: 'md:grid-cols-hero-txtimg-20',
+  gridWithImage: 'md:grid-cols-hero-text-image-40',
+  gridNoImage: 'md:grid-cols-hero-text-image-20',
   content: 'grid gap-5 italic order-2 md:order-1',
   header: 'font-semibold md:leading-10 text-2xl sm:text-3xl md:text-4xl not-italic',
   image: 'bg-center bg-cover rounded order-1 md:order-2 md:min-h-[260px] h-full',
