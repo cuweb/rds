2834b242cd20dc3fa67a6db5bb692ed7d7fdeb81
Troy Chaplin <troy.chaplin@gmail.com>
2023-06-05 21:23:06 -0400

    change: updated styles

--- src/components/Hero/HeroTextImage/HeroTextImage.Styles.tsx
+++ src/components/Hero/HeroTextImage/HeroTextImage.Styles.tsx
@@ -1,11 +1,12 @@
 export const styles = {
-  base: 'cu-herotxtimg not-contained not-prose mx-auto lg:items-center',
-  bottom: 'pb-2 md:pb-6',
+  base: 'cu-herotextimage not-contained not-prose mx-auto lg:items-center mb-12',
+  bottom: 'pb-6 border-b border-cu-black-50',
+  bottomSpace: 'md:pb-10',
   grid: 'grid gap-6 md:gap-10 mx-auto ',
   gridWithImage: 'md:grid-cols-hero-txtimg-40',
   gridNoImage: 'md:grid-cols-hero-txtimg-20',
   content: 'grid gap-5 italic order-2 md:order-1',
-  header: 'font-semibold md:leading-10 text-2xl sm:text-3xl not-italic',
-  image: 'bg-center bg-cover rounded-lg order-1 md:order-2 md:min-h-[260px] h-full',
+  header: 'font-semibold md:leading-10 text-2xl sm:text-3xl md:text-4xl not-italic',
+  image: 'bg-center bg-cover rounded order-1 md:order-2 md:min-h-[260px] h-full',
   svg: 'hidden lg:block absolute left-0 top-0 z-10 h-full',
 }
