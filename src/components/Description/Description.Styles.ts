a0fd3b8761eb1338be2e96e6015d49dd31182753
Troy Chaplin <troy.chaplin@gmail.com>
2023-06-16 19:27:17 -0400

    change: updated styles for description list

--- src/components/Description/Description.Styles.ts
+++ src/components/Description/Description.Styles.ts
@@ -1,10 +1,10 @@
 export const styles = {
-  base: `grid py-5 border-b border-cu-black-200 last:border-none first:pt-0 last-pt-b`,
+  base: `grid border-b border-cu-black-200 last:border-none first:pt-0 pt-8 pb-8 md:pt-8 md:pb-8`,
   flexRow: `md:grid-cols-left260 gap-6`,
   flexCol: `grid-row gap-2`,
   button: `flex items-center w-full`,
   chevron: `w-5 h-5 ml-auto text-cu-black-500 transition-transform`,
-  term: `font-semibold text-lg md:text-xl`,
+  term: `font-semibold text-lg md:text-xl pb-3`,
   def: `text-base md:text-lg`,
   accordion: `flex flex-col gap-2`,
 }
