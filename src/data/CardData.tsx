5e3e54e4a9dbd1085efc415eefec471a483c8eb3
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2023-06-16 15:07:54 -0400

    add(Loader story): add more data for card and listing

---
+++ src/data/CardData.tsx
@@ -0,0 +1,370 @@
+export const content = {
+  title: 'End-of-Year Reflections and Resolutions for Our Web Services Team',
+  link: 'https://carleton.ca/webservices',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2022-12-16',
+  excerpt:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a est vel mi finibus tempor. Etiam sagittis eros ac venenatis pretium. Donec bibendum eget ante quis maximus. Nullam ultrices erat sagittis luctus volutpat. Etiam nec arcu sodales, convallis justo quis, aliquam lorem. Curabitur feugiat accumsan felis, placerat eleifend ipsum auctor.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content2 = {
+  title: 'Animating Accessibility: The Art of Inclusion',
+  link: 'https://carleton.ca/webservices/2023/animating-accessibility-the-art-of-inclusion/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-25',
+  excerpt:
+    'If you have been keeping up with our YouTube channel or following us on Instagram, you’ve probably seen some of the animated videos we’ve created on various web-related topics! Animation is a captivating and powerful medium that has become a significant part of our education landscape. However, to truly harness the potential of animation, it is crucial to ensure that it is accessible to individuals of all abilities.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Anyone',
+        slug: 'anyone',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content3 = {
+  title: 'Visual Accessibility',
+  link: 'https://carleton.ca/webservices/2023/animating-accessibility-the-art-of-inclusion/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-24',
+  excerpt: 'Here are some key considerations for ensuring visual accessibility in animation:',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content4 = {
+  title: 'Auditory Accessibility',
+  link: 'https://carleton.ca/webservices/2023/animating-accessibility-the-art-of-inclusion/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-23',
+  excerpt:
+    'Audio plays a vital role in conveying emotions, dialogue, and sound effects in animations. However, individuals with hearing impairments or those who are deaf may struggle to fully comprehend the auditory elements. To address this, it is essential to include accurate closed captions or subtitles that effectively represent the dialogue and sound effects. These captions should be synchronized with the animation and presented in a clear and easily readable format, ensuring accessibility for all viewers. To learn more about the differencen between captions and subtitles and why we use them, check out our blogpost titled Captivating Captions: Why We Use Captions on Video. Additionally, we have a great tutorial on Generating Transcripts and Closed Captioning from YouTube Videos.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content5 = {
+  title: 'Cognitive Accessibility',
+  link: 'https://carleton.ca/webservices/2023/animating-accessibility-the-art-of-inclusion/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-22',
+  excerpt: 'Here are some key considerations for ensuring cognitive accessibility in animation:',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content6 = {
+  title: 'Media Player and Assistive Technologies',
+  link: 'https://carleton.ca/webservices/2023/animating-accessibility-the-art-of-inclusion/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-21',
+  excerpt:
+    'Captions and audio description support: The video player should have the capability to display captions and support audio description tracks. This ensures that individuals with hearing impairments or those who benefit from additional audio narration can access the content effectively.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content7 = {
+  title: 'Guide to WordPress Built-In Image Editor',
+  link: 'https://carleton.ca/webservices/2023/guide-to-wordpress-built-in-image-editor/#access',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-20',
+  excerpt:
+    'Did you know that WordPress has a built-in image editor? Equipped with an intuitive interface, this tool empowers you to effortlessly enhance your website’s visual content without the need for external software. In this article, we will explore all the editing options offered by WordPress’ built-in image editor, providing you with a step-by-step guide on how to make the most of this incredible feature!',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content8 = {
+  title: 'Basic Image Editing',
+  link: 'https://carleton.ca/webservices/2023/guide-to-wordpress-built-in-image-editor/#basic',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-19',
+  excerpt:
+    'WordPress built-in image editor offers various editing options. In the toolbar, you can choose to crop, rotate, or flip your images. As well, you have the option to undo or redo your edits. Let’s go over these in more detail:',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content9 = {
+  title: 'Saving an Image',
+  link: 'https://carleton.ca/webservices/2023/guide-to-wordpress-built-in-image-editor/#save',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-18',
+  excerpt: 'After editing your image, click the Save button to apply the changes.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content10 = {
+  title: 'Restoring an Image',
+  link: 'https://carleton.ca/webservices/2023/guide-to-wordpress-built-in-image-editor/#restore',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-17',
+  excerpt: 'WordPress itself gives you the option to restore your original image even after saving the edited image.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content11 = {
+  title: 'Web Wednesday Recap – Crafting a Winning Instagram Pilot',
+  link: 'https://carleton.ca/webservices/2023/web-wednesday-recap-instagram/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-16',
+  excerpt:
+    'If you were unable to attend our most recent Web Wednesday event, no worries. We’ve written a recap of the highlights and shared the video and slides below. Enjoy!',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content12 = {
+  title: '5 Common Web Accessibility Issues',
+  link: 'https://carleton.ca/webservices/2023/5-common-web-accessibility-issues/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-12',
+  excerpt:
+    'Accessibility! Accessibility! Accessibility! Here at Web Services, you may notice we chat often on this topic, though that is only because it plays such a key role in the web world. Across the Carleton domain, we want to ensure everyone has the same user experience. Below are five common accessibility issues found on websites. Have a look at this list and see if your site is free of these issues.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content13 = {
+  title: 'Google Analytics is Changing',
+  link: 'https://carleton.ca/webservices/2023/google-analytics-is-changing/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-13',
+  excerpt:
+    'We use Google Analytics to track how our Carleton websites are performing. If you have logged in to analytics recently, you may have noticed a pop-up warning. It talks about the switch to Google Analytics 4 (GA4) occurring on July 1.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content14 = {
+  title: 'How to Add Alt Text to Social Media Posts',
+  link: 'https://carleton.ca/webservices/2023/how-to-add-alt-text-to-social-media-posts/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-14',
+  excerpt:
+    'Alternative text, also known as alt text, is a short description of an image that is read by screen readers. With social media becoming an increasingly integral part of our daily lives, it’s important to consider the accessibility of our content.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
+
+export const content15 = {
+  title: 'cuTheme Teasers: Video Block Sneak Peek',
+  link: 'https://carleton.ca/webservices/2023/cutheme-teasers-video-block-sneak-peek/',
+  video: 'https://www.youtube.com/watch?v=gtEJtKwUGiU',
+  image: './sample-imgs/news-img.jpg',
+  alt: 'Required alt text',
+  date: '2023-06-15',
+  excerpt:
+    'Welcome to the next post of our cuTheme teaser series! Today we give you a sneak peek into the video listing block. The video block lets you display your videos in a card format that creates a nice gallery on your page.',
+  tags: {
+    category: [
+      {
+        id: 1,
+        name: 'Breaking News',
+        slug: 'breaking-news',
+      },
+      {
+        id: 2,
+        name: 'Headline',
+        slug: 'headline',
+      },
+    ],
+  },
+}
