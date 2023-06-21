1260c356ee007df0f8fbd65494932e4260528b63
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2023-06-21 10:36:55 -0400

    add(Loader story): add cookie component with click

--- src/components/Cookie/Cookie.tsx
+++ src/components/Cookie/Cookie.tsx
@@ -1,7 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from 'react'
+
 export const Cookie = () => {
+  const [isVisible, setIsVisible] = useState(true)
+
+  const handleButtonClick = () => {
+    setIsVisible(false)
+  }
+
   return (
-    <div className="pointer-events-none fixed inset-x-0 bottom-0 px-6 pb-6">
+    <div className={`pointer-events-none fixed inset-x-0 bottom-0 px-6 pb-6 ${isVisible ? '' : 'hidden'}`}>
       <div className="pointer-events-auto mx-auto w-2/3 rounded-xl bg-white p-6 shadow-lg ring-1 ring-gray-900/10">
         <div className="mb-4 flex items-center justify-between">
           <p className="text-base leading-6 text-gray-900">
@@ -13,6 +21,7 @@ export const Cookie = () => {
           <button
             type="button"
             className="rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+            onClick={handleButtonClick}
           >
             Ok, Got it.
           </button>
