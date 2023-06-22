2c4e224616b69d31c19c949892adff30f484d721
troychaplin <troy.chaplin@gmail.com>
2023-06-22 18:38:56 +0000

    Deploying to gh-pages from @ cuweb/rds@31d695c85a60b2591baa179f7f4fb77826da182d 🚀

---
+++ components/Cookie/Cookie.d.ts
@@ -0,0 +1,2 @@
+export declare const Cookie: () => import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=Cookie.d.ts.map
---
+++ components/Cookie/Cookie.stories.d.ts
@@ -0,0 +1,7 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Cookie } from './Cookie';
+declare const meta: Meta<typeof Cookie>;
+export default meta;
+type Story = StoryObj<typeof Cookie>;
+export declare const Default: Story;
+//# sourceMappingURL=Cookie.stories.d.ts.map
