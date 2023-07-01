0bbc4d85aa43f4b75301c5d5e7457db2afba6808
troychaplin <troy.chaplin@gmail.com>
2023-07-01 20:15:55 +0000

    Deploying to gh-pages from @ cuweb/rds@551ad503312cfbbff23137f7c809f5da4b5faeb7 🚀

---
+++ components/ButtonGroup/ButtonGroup.stories.d.ts
@@ -0,0 +1,7 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ButtonGroup } from './ButtonGroup';
+declare const meta: Meta<typeof ButtonGroup>;
+export default meta;
+type Story = StoryObj<typeof ButtonGroup>;
+export declare const Primary: Story;
+//# sourceMappingURL=ButtonGroup.stories.d.ts.map
