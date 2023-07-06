9378615d76b019c07d1d3b01da368bb6fcb67967
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2023-07-06 19:41:41 -0400

    add(Loader story): add multiday calendar

---
+++ src/components/MultiDayCalendar/MultiDayCalendar.styles.tsx
@@ -0,0 +1,5 @@
+const styles = {
+  prevNextArrows: `flex items-center justify-center flex-none p-2 text-cu-black-800 hover:text-cu-red`,
+  calendarGrid: `grid grid-cols-7 gap-px mt-4 text-center`,
+}
+export default styles
