5677b3bf8672f32e204e58dd4ba4d1dd34f1fd63
Troy Chaplin <troy.chaplin@gmail.com>
2023-07-18 10:33:47 -0300

    change: wrapping embed in figure with maxWidth prop

--- src/components/Embed/Embed.tsx
+++ src/components/Embed/Embed.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { EmbedPowerBi } from './Embed.PowerBi'
+import { rdsMaxWidth } from '../../utils/optionClasses'
 import { styles } from './Embed.Styles'
 
 export interface EmbedProps {
   children: React.ReactNode
+  maxWidth?: 'full' | '5xl' | '7xl'
 }
 
-export const EmbedWrapper = ({ children }: EmbedProps) => {
-  return <div className={`${styles.base}`}>{children}</div>
+export const EmbedWrapper = ({ children, maxWidth = '5xl' }: EmbedProps) => {
+  return (
+    <figure className={rdsMaxWidth[maxWidth]}>
+      <div className={`${styles.base}`}>{children}</div>
+    </figure>
+  )
 }
 
 export const Embed = Object.assign(EmbedWrapper, {
