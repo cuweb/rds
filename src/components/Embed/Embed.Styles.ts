dc93b9d8e562d7896761fd4d9c12f106b64e1eae
Troy Chaplin <troy.chaplin@gmail.com>
2023-07-18 14:24:11 -0300

    add: kaltura subcomponent

--- src/components/Embed/Embed.Styles.ts
+++ src/components/Embed/Embed.Styles.ts
@@ -1,5 +1,5 @@
 export const styles = {
   base: `relative w-full pb-[56.25%] overflow-hidden border border-cu-black-100`,
   powerBi: `absolute top-0 left-0 w-full h-full`,
-  kaltura: `absolute top-0 left-0 w-full h-full`
+  kaltura: `absolute top-0 left-0 w-full h-full`,
 }
