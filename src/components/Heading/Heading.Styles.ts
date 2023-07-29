dbb9d2f0dd1d6285735a53866316bfb55f24538c
Troy Chaplin <troy.chaplin@gmail.com>
2023-07-28 23:09:13 -0300

    change: updated heading style for para and spacing

--- src/components/Heading/Heading.Styles.ts
+++ src/components/Heading/Heading.Styles.ts
@@ -1,15 +1,7 @@
 export const styles = {
   core: `mx-auto`,
-  header: `text-2xl md:text-3xl lg:text-4xl font-semibold text-cu-black-800 pb-4`,
+  header: `text-2xl md:text-3xl lg:text-4xl pb-6 font-semibold text-cu-black-800 pb-4`,
   underline: `after:absolute after:w-10 after:h-px after:bottom-0 after:bg-cu-red`,
   spacing: `mt-6 mb-6 md:mt-14 md:mb-10`,
-  paragraph: ``,
+  paragraph: `text-lg md:text-xl md:leading-8 mt-8 font-light italic text-cu-black-600 mb-2`,
 }
-
-// export const styles2 = {
-//   prose: `prose prose-lg md:prose-xl`,
-//   spacing: `pt-10 pb-2.5 md:pt-14`,
-//   heading: `prose-headings:font-semibold prose-headings:text-cu-black-800 prose-headings:mb-6 prose-headings:pb-6 mb-5 lg:mb-8`,
-//   underline: `after:absolute after:w-10 after:h-px after:bottom-0 after:bg-cu-red`,
-//   paragraph: `prose-p:md:text-[22px] prose-p:leading-9 prose-p:text-cu-black-800`,
-// }
