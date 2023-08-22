0d1d7fbe5f03ec56cd00baf22a61549d3c0891cc
Troy Chaplin <troy.chaplin@gmail.com>
2023-08-21 23:38:47 -0400

    change: updated dependencies

--- src/components/TopBar/TopBarSubMenu.tsx
+++ src/components/TopBar/TopBarSubMenu.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, PropsWithChildren } from 'react'
+import { Fragment, PropsWithChildren } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { navItemStyles } from './TopBar.Styles'
