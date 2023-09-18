77ce2b0faffd6c771e70d937300025ac946ebdce
Troy Chaplin <troy.chaplin@gmail.com>
2023-09-18 10:56:49 -0400

    change: spacing

--- src/components/Loaders/ListingLoader/ListingLoader.tsx
+++ src/components/Loaders/ListingLoader/ListingLoader.tsx
@@ -3,7 +3,7 @@ import { Listing } from '../../../../src/components/Listing/Listing'
 export const ListingLoader = () => {
   return (
     <Listing>
-      <div className="relative flex flex-col @lg:md:flex-row items-start h-full gap-8 p-8 animate-pulse">
+      <div className="relative flex flex-col @lg:md:flex-row items-start h-full gap-4 @lg:md:gap-8 p-8 animate-pulse">
         <div className="cu-figure flex-none rounded-md min-w-[35%] max-w-[50%] @lg:md:max-w-[35%]">
           <div className="object-cover w-full h-[100px] @lg:md:h-[180px] bg-cu-black-100 rounded-md"></div>
         </div>
