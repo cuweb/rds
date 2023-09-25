bc817bfd3f7c0791eaf8e63b190cf7d07fcb4e61
Troy Chaplin <troy.chaplin@gmail.com>
2023-09-25 15:27:39 -0400

    change: updated prose styles and global classes

--- src/examples/templates/ProseExamples.stories.tsx
+++ src/examples/templates/ProseExamples.stories.tsx
@@ -78,28 +78,6 @@ export const ProseExamples: Story = {
             sapien in urna.
           </p>
 
-          <h5>Heading Five</h5>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus
-            vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse
-            condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida
-            volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et
-            malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus
-            sapien in urna.
-          </p>
-
-          <h6>Heading Six</h6>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus
-            vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse
-            condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida
-            volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et
-            malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus
-            sapien in urna.
-          </p>
-
           <h2>Basic Content Styles</h2>
 
           <p>
