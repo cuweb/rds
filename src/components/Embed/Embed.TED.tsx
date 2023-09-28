7d8a947ebec5bf6e35c68c8544e9c40087cf1eaa
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-09-28 10:16:24 -0400

    add: add new embed blocks

---
+++ src/components/Embed/Embed.TED.tsx
@@ -0,0 +1,31 @@
+import { styles } from './Embed.Styles'
+export interface EmbedTEDProps {
+  title: string
+  url: string
+}
+
+export const EmbedTED = ({ title, url }: EmbedTEDProps) => {
+  let embedURL = ''
+  let lastPiece = ''
+
+  if (url) {
+    const urlParts = url.split('/')
+    lastPiece = urlParts[urlParts.length - 1]
+    embedURL = `https://embed.ted.com/talks/lang/en/${lastPiece}`
+  }
+
+  return (
+    <iframe
+      id={`kmsembed-${lastPiece}`}
+      title={title}
+      src={embedURL}
+      className={`kmsembed ${styles.video}`}
+      allowFullScreen
+      allow="autoplay *; fullscreen *; encrypted-media *"
+      referrerPolicy="no-referrer-when-downgrade"
+      sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation"
+    ></iframe>
+  )
+}
+
+EmbedTED.displayName = 'Embed.TED'
