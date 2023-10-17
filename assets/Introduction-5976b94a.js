c18299b964b38f1632400afd9373dfa953311c9e
troychaplin <troy.chaplin@gmail.com>
2023-10-17 23:22:28 +0000

    Deploying to gh-pages from @ cuweb/rds@a8a4639d2907c8c0123dea0828360643b619e586 🚀

---
+++ assets/Introduction-5976b94a.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-7ce7b810.js";import{M as s}from"./index-bfaf91a9.js";import{u as i}from"./index-375787cf.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-97ff28eb.js";import"../sb-preview/runtime.js";import"./index-4da2af8c.js";import"./index-d37d4223.js";import"./isNativeReflectConstruct-81b4d0cb.js";import"./index-2bcb8ae6.js";import"./_baseIsEqual-93bb5583.js";import"./index-356e4a49.js";function o(n){const e=Object.assign({h1:"h1",p:"p",strong:"strong",h2:"h2",code:"code",a:"a"},i(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Get Started/Introduction"}),`
+`,t.jsx(e.h1,{id:"an-introduction-to-raven-design-system",children:"An Introduction to Raven Design System"}),`
+`,t.jsx(e.p,{children:"Raven Design System, better known as RDS, is Carleton University’s design system, is a living digital organism uniting campus wide teams around a common visual language."}),`
+`,t.jsxs(e.p,{children:[`Storybook helps you build UI components in isolation from your app's business logic, data, and context.
+That makes it easy to develop hard-to-reach states. Save these UI states as `,t.jsx(e.strong,{children:"stories"})," to revisit during development, testing, or QA."]}),`
+`,t.jsx(e.h2,{id:"getting-started-with-rds",children:"Getting Started with RDS"}),`
+`,t.jsxs(e.p,{children:[`Browse example stories now by navigating to them in the sidebar.
+View their code in the `,t.jsx(e.code,{children:"stories"}),` directory to learn how they work.
+We recommend building UIs with a `,t.jsx(e.a,{href:"https://componentdriven.org",target:"_blank",rel:"nofollow noopener noreferrer",children:t.jsx(e.strong,{children:"component-driven"})})," process starting with atomic components and ending with pages."]})]})}function v(n={}){const{wrapper:e}=Object.assign({},i(),n.components);return e?t.jsx(e,Object.assign({},n,{children:t.jsx(o,n)})):o(n)}export{v as default};
+//# sourceMappingURL=Introduction-5976b94a.js.map
---
+++ assets/WithTooltip-4HIR6TLV-a3cd4957.js
@@ -0,0 +1,2 @@
+import{W as s,W as f,a as u}from"./index-bfaf91a9.js";import"./iframe-97ff28eb.js";import"../sb-preview/runtime.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./index-4da2af8c.js";import"./index-d37d4223.js";import"./isNativeReflectConstruct-81b4d0cb.js";import"./index-2bcb8ae6.js";import"./_baseIsEqual-93bb5583.js";import"./index-356e4a49.js";export{s as WithToolTipState,f as WithTooltip,u as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-4HIR6TLV-a3cd4957.js.map
