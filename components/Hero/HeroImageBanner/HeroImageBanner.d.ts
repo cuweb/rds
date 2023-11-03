2938305e3316291cda84348428c408c7c36487f9
troychaplin <troy.chaplin@gmail.com>
2023-11-03 02:18:34 +0000

    Deploying to gh-pages from @ cuweb/rds@19986e7f495726568480847e74d271d416f6aaf7 🚀

---
+++ components/Hero/HeroImageBanner/HeroImageBanner.d.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+export interface HeroImageBannerProps {
+    children?: React.ReactNode;
+    title?: string;
+    image?: string;
+    focalPointX?: string;
+    focalPointY?: string;
+}
+export declare const HeroImageBanner: ({ children, title, image, focalPointX, focalPointY, }: HeroImageBannerProps) => import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=HeroImageBanner.d.ts.map
---
+++ components/Hero/HeroImageBanner/HeroImageBanner.stories.d.ts
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { HeroImageBanner } from './HeroImageBanner';
+declare const meta: Meta<typeof HeroImageBanner>;
+export default meta;
+type Story = StoryObj<typeof HeroImageBanner>;
+export declare const Default: Story;
+export declare const WithButton: Story;
+export declare const HasImage: Story;
+//# sourceMappingURL=HeroImageBanner.stories.d.ts.map
