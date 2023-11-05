8cd6c0c8341c22c23bd60405fe828274e63d2a18
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-11-05 16:21:16 -0500

    change: update alert componenet

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,111 +1,60 @@
-import React, { PropsWithChildren, useState } from 'react'
-import { Transition } from '@headlessui/react'
+import { PropsWithChildren } from 'react'
+import { rdsTextColor, rdsBgColor, rdsBgOpacity } from '../../utils/optionClasses'
+import { Icon } from '../../components/Icon/Icon'
 import {
   CheckCircleIcon,
   ExclamationCircleIcon,
   InformationCircleIcon,
   ShieldExclamationIcon,
 } from '@heroicons/react/24/outline'
-import { XMarkIcon } from '@heroicons/react/20/solid'
 
-export interface AlertBaseProps {
+export interface AlertProps {
+  title: string
+  content?: string
   type: 'success' | 'error' | 'warning' | 'info'
   textSize?: 'small' | 'large'
-  isPersistent?: boolean
 }
 
-const Alerter = ({ children }: PropsWithChildren) => {
-  return (
-    <div aria-live="assertive" className="cu-alert not-prose top-20">
-      <div className="w-full items-center space-y-4 sm:items-end">{children}</div>
-    </div>
-  )
-}
-
-const Title = ({ children }: PropsWithChildren) => {
-  return <h3 className="font-medium ">{children}</h3>
-}
-
-const Content = ({ children }: PropsWithChildren) => {
-  return <div className="mt-2 ">{children}</div>
-}
-
-const AlertBase = ({ children, type, textSize = 'small', isPersistent = false }: PropsWithChildren<AlertBaseProps>) => {
-  const [showAlert, setShowAlert] = useState(true)
-
+const Alert = ({ title, content, type, textSize = 'small' }: PropsWithChildren<AlertProps>) => {
   const alertTextSize = textSize === 'small' ? 'text-sm' : 'text-lg'
-  const alertIconSize = textSize === 'small' ? 'h-5 w-5' : 'h-8 w-8'
+  const alertIconSize = textSize === 'small' ? '5' : '8'
 
   const AlertTypes = {
     success: {
       icon: CheckCircleIcon,
-      text: 'text-green-600',
-      bg: 'bg-green-50',
+      color: 'green',
     },
     warning: {
       icon: ShieldExclamationIcon,
-      text: 'text-yellow-600',
-      bg: 'bg-yellow-50',
+      color: 'yellow',
     },
     error: {
       icon: ExclamationCircleIcon,
-      text: 'text-cu-red-600',
-      bg: 'bg-cu-red-50',
+      color: 'red',
     },
     info: {
       icon: InformationCircleIcon,
-      text: 'text-blue-600',
-      bg: 'bg-blue-50',
+      color: 'blue',
     },
   }
 
+  const colorClass = type ? AlertTypes[type].color : ''
+
   return (
-    <Transition
-      show={showAlert}
-      enter="transform ease-out duration-300 transition"
-      enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-      enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-      leave="transition ease-in duration-100"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
+    <div
+      className={`flex rounded-md p-4 not-prose ${rdsBgColor[colorClass as keyof typeof rdsBgColor]} ${
+        rdsBgOpacity[20]
+      }`}
     >
-      <div className={`rounded-md ${AlertTypes[type].bg} p-4`}>
-        <div className="flex">
-          <div className="flex-shrink-0">
-            {React.createElement(AlertTypes[type].icon, {
-              className: `${alertIconSize} ${AlertTypes[type].text}`,
-              'aria-hidden': 'true',
-            })}
-          </div>
-          <div className={`ml-3 ${AlertTypes[type].text} ${alertTextSize}`}>{children}</div>
-          <div className="ml-auto pl-3">
-            <div className="-mx-1.5 -my-1.5">
-              {!isPersistent && (
-                <button
-                  type="button"
-                  className={`inline-flex rounded-md ${AlertTypes[type].bg} p-1.5 ${AlertTypes[type].text} hover:${AlertTypes[type].bg} focus:outline-none focus:ring-2 focus:ring-offset-2`}
-                  onClick={() => {
-                    setShowAlert(false)
-                  }}
-                >
-                  <span className="sr-only">Dismiss</span>
-                  <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
+      <Icon icon={AlertTypes[type].icon} color={AlertTypes[type].color} size={alertIconSize}></Icon>
+      <div className={`${textSize == 'large' ? 'mt-0.5' : null} ml-3`}>
+        <p className={`${alertTextSize} ${rdsTextColor[colorClass as keyof typeof rdsTextColor]} my-0 font-semibold`}>
+          {title}
+        </p>
+        <p className={`${alertTextSize} ${rdsTextColor[colorClass as keyof typeof rdsTextColor]} mt-2`}>{content}</p>
       </div>
-    </Transition>
+    </div>
   )
 }
 
-AlertBase.displayName = 'Alert'
-Title.displayName = 'Alert.Title'
-Content.displayName = 'Alert.Content'
-
-export const Alert = Object.assign(AlertBase, {
-  Alerter,
-  Title,
-  Content,
-})
+export { Alert as Alert }
