5fd21ff234f33456348b99f9c1dc92ab6ae320eb
Troy Chaplin <troy.chaplin@gmail.com>
2023-11-05 09:30:53 -0500

    refactor: text image

--- src/components/TextImage/TextImage.stories.tsx
+++ src/components/TextImage/TextImage.stories.tsx
@@ -58,11 +58,7 @@ export const HeaderOne: Story = {
 export const ImageLeft: Story = {
   render: () => (
     <TextImage hasBorder reverse>
-      <TextImage.Content
-        title="Website and Application Development"
-        headerType="h1"
-        imageUrl="https://picsum.photos/400/266"
-      >
+      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266">
         <p>
           Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
           reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
