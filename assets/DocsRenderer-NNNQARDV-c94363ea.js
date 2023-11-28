5d26e14af2f02bda9b0452b54ea389ce616e539c
troychaplin <troy.chaplin@gmail.com>
2023-11-28 23:07:16 +0000

    Deploying to gh-pages from @ cuweb/rds@cb578112da224ebfb86f4ded27747a7dec023631 🚀

--- assets/DocsRenderer-NNNQARDV-c94363ea.js
+++
@@ -1,2 +0,0 @@
-import{D as l,d as u}from"./chunk-HLWAVYOI-438d743d.js";import"./iframe-8bd67882.js";import"../sb-preview/runtime.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./react-18-aafee9b3.js";import"./index-4da2af8c.js";import"./index-bcdbd7b1.js";import"./index-d37d4223.js";import"./isNativeReflectConstruct-81b4d0cb.js";import"./index-2bcb8ae6.js";import"./_baseIsEqual-93bb5583.js";import"./index-356e4a49.js";export{l as DocsRenderer,u as defaultComponents};
-//# sourceMappingURL=DocsRenderer-NNNQARDV-c94363ea.js.map
---
+++ assets/WideImage.stories-fb049f4e.js
@@ -0,0 +1,95 @@
+import{j as e}from"./jsx-runtime-7ce7b810.js";import{M as g}from"./Main-6cf1ae56.js";import{S as h}from"./Section-0b894742.js";import{T as o}from"./TopBar-78f0bb84.js";import{F as x}from"./FooterBasic-8e156281.js";import{B as a}from"./Button-c1d196ed.js";import{H as t}from"./HeroTextImage-8fd2230d.js";import{T as j}from"./TopBarData-62ad3a12.js";import{W as v}from"./WideImage-d8423cc4.js";import{B as b}from"./ButtonGroup-1dfe70f1.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./globalClasses-ae4f1421.js";import"./menu-8eae4fbd.js";import"./transition-271b59c1.js";import"./use-root-containers-8cc2decd.js";import"./index-4da2af8c.js";import"./use-tracked-pointer-7dc0618c.js";import"./use-resolve-button-type-967b1172.js";import"./ChevronDownIcon-2780c6ad.js";import"./disclosure-3192912c.js";import"./ChevronRightIcon-8ad439f2.js";import"./MagnifyingGlassIcon-97d62ca3.js";import"./popover-3ff197fb.js";import"./FooterLogoLinks-02c1cc9c.js";import"./Link-83e9abc7.js";import"./iframe-724b37f9.js";import"../sb-preview/runtime.js";import"./Icon-857a786a.js";import"./optionClasses-8b995f37.js";const X={title:"Examples/Projects/cutheme"},s=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus
+vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse
+condimentum magna vel orci vulputate, eget vulputate neque porttitor.`,i=()=>e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),n={render:()=>e.jsxs(e.Fragment,{children:[e.jsxs(o,{children:[e.jsxs(o.Primary,{children:[e.jsx(o.Logo,{title:"Raven Design System",link:"https://carleton.ca/webservices",children:e.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[e.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),e.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),e.jsx(o.Aside,{})]}),e.jsx(o.Secondary,{children:e.jsx(o.Menu,{children:j.map((r,c)=>e.jsxs(e.Fragment,{children:[!r.submenu&&e.jsx("a",{href:r.href,className:"cu-topbar--parent-link",children:r.title},c),r.submenu&&e.jsx(o.SubMenu,{label:r.title,children:r.submenu.map((l,d)=>e.jsx("a",{href:l.href,className:"cu-topbar--more-nav",children:l.title},d))})]}))})})]}),e.jsx(g,{children:e.jsxs(h,{hasProse:!0,children:[e.jsx(v,{headerType:"h1",image:"https://picsum.photos/1600/700",title:"Wide image block with buttons and background image",children:e.jsxs(b,{children:[e.jsx(a,{title:"Apply Now"}),e.jsx(a,{color:"white",isType:"outline",title:"Request Information"})]})}),e.jsx(i,{}),e.jsxs(t,{children:[e.jsx(t.Content,{title:"With image right and left angle",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"left"})]}),e.jsx(i,{}),e.jsxs(t,{reverse:!0,children:[e.jsx(t.Content,{title:"Website and Application Development",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"left"})]}),e.jsx(i,{}),e.jsxs(t,{children:[e.jsx(t.Content,{title:"Website and Application Development",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"dual"})]}),e.jsx(i,{}),e.jsxs(t,{children:[e.jsx(t.Content,{title:"Website and Application Development",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"none"})]}),e.jsx(i,{})]})}),e.jsx(x,{})]})};var p,m,u;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo title="Raven Design System" link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+        <TopBar.Secondary>
+          <TopBar.Menu>
+            {TopBarDataSingle.map((topBarLink, i) => <>
+                {!topBarLink.submenu && <a key={i} href={topBarLink.href} className="cu-topbar--parent-link">
+                    {topBarLink.title}
+                  </a>}
+                {topBarLink.submenu && <TopBar.SubMenu label={topBarLink.title}>
+                    {topBarLink.submenu.map((topBarSubLink, s) => <a key={s} href={topBarSubLink.href} className="cu-topbar--more-nav">
+                        {topBarSubLink.title}
+                      </a>)}
+                  </TopBar.SubMenu>}
+              </>)}
+          </TopBar.Menu>
+        </TopBar.Secondary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <WideImage headerType="h1" image="https://picsum.photos/1600/700" title="Wide image block with buttons and background image">
+            <ButtonGroup>
+              <Button title="Apply Now" />
+              <Button color="white" isType="outline" title="Request Information" />
+            </ButtonGroup>
+          </WideImage>
+
+          <DoublePara />
+
+          <HeroTextImage>
+            <HeroTextImage.Content title="With image right and left angle" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="left" />
+          </HeroTextImage>
+
+          <DoublePara />
+
+          <HeroTextImage reverse>
+            <HeroTextImage.Content title="Website and Application Development" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="left" />
+          </HeroTextImage>
+
+          <DoublePara />
+
+          <HeroTextImage>
+            <HeroTextImage.Content title="Website and Application Development" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="dual" />
+          </HeroTextImage>
+
+          <DoublePara />
+
+          <HeroTextImage>
+            <HeroTextImage.Content title="Website and Application Development" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="none" />
+          </HeroTextImage>
+
+          <DoublePara />
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(u=(m=n.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const Y=["WideImageBanner"];export{n as WideImageBanner,Y as __namedExportsOrder,X as default};
+//# sourceMappingURL=WideImage.stories-fb049f4e.js.map
