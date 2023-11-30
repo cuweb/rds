3ad1fc0e7a54bf3fd405c479a6eb57e7ae015067
troychaplin <troy.chaplin@gmail.com>
2023-11-30 14:43:53 +0000

    Deploying to gh-pages from @ cuweb/rds@a9872fb29e49b3f9105382bdbec81993d93c61fe 🚀

---
+++ assets/WithContainers.stories-be94fc19.js
@@ -0,0 +1,140 @@
+import{j as e}from"./jsx-runtime-7ce7b810.js";import{S as d}from"./Section-0b894742.js";import{C as s}from"./Container-15c90b25.js";import{M as m}from"./Main-6cf1ae56.js";import{F as h}from"./FooterBasic-191b186a.js";import{H as u}from"./HeroTextImage-8fd2230d.js";import{T as r}from"./TopBar-78f0bb84.js";import{D as n,a}from"./Description-03715ebf.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./globalClasses-ae4f1421.js";import"./FooterLogoLinks-73bf291e.js";import"./Link-20dcfb97.js";import"./iframe-18bf86e4.js";import"../sb-preview/runtime.js";import"./ButtonGroup-1dfe70f1.js";import"./Button-c1d196ed.js";import"./Icon-857a786a.js";import"./optionClasses-8b995f37.js";import"./ChevronDownIcon-2780c6ad.js";import"./menu-8eae4fbd.js";import"./transition-271b59c1.js";import"./use-root-containers-8cc2decd.js";import"./index-4da2af8c.js";import"./use-tracked-pointer-7dc0618c.js";import"./use-resolve-button-type-967b1172.js";import"./disclosure-3192912c.js";import"./ChevronRightIcon-8ad439f2.js";import"./MagnifyingGlassIcon-97d62ca3.js";import"./popover-3ff197fb.js";import"./ChevronRightIcon-0ed75403.js";const K={title:"Examples/Templates"},g=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})}),t=()=>e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),o={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{children:e.jsxs(r.Primary,{children:[e.jsx(r.Logo,{link:"https://carleton.ca/webservices",children:e.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[e.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),e.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),e.jsx(r.Aside,{})]})}),e.jsx(m,{children:e.jsxs(d,{hasProse:!0,children:[e.jsx(u,{maxWidth:"5xl",hasBorder:!0,children:e.jsx(u.Content,{title:"With Containers",headerType:"h1"})}),e.jsx("h2",{children:"Heading Two"}),e.jsx(t,{}),e.jsx("h3",{children:"Heading Three"}),e.jsx(g,{}),e.jsxs(s,{isGrey:!0,children:[e.jsx("h2",{children:"Heading Two"}),e.jsx(t,{})]}),e.jsxs(s,{isGrey:!0,children:[e.jsx("h2",{children:"Heading Two"}),e.jsx(t,{})]}),e.jsx("h2",{children:"Heading Two"}),e.jsx(t,{}),e.jsx(s,{children:e.jsx(n,{children:a.map(i=>e.jsxs(n.Meta,{term:i==null?void 0:i.term,children:[e.jsx("p",{children:"Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non."}),e.jsx("p",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("p",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor."})]},i==null?void 0:i.id))})}),e.jsx(t,{}),e.jsx(s,{isGrey:!0,children:e.jsx(n,{children:a.map(i=>e.jsxs(n.Meta,{term:i==null?void 0:i.term,children:[e.jsx("p",{children:"Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non."}),e.jsx("p",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("p",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor."})]},i==null?void 0:i.id))})}),e.jsx(t,{}),e.jsx(s,{children:e.jsx(n,{children:a.map(i=>e.jsxs(n.Meta,{term:i==null?void 0:i.term,useColumns:!0,children:[e.jsx("p",{children:"Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non."}),e.jsx("p",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("p",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor."})]},i==null?void 0:i.id))})}),e.jsx(t,{}),e.jsx(s,{isGrey:!0,children:e.jsx(n,{children:a.map(i=>e.jsxs(n.Meta,{term:i==null?void 0:i.term,useColumns:!0,children:[e.jsx("p",{children:"Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non."}),e.jsx("p",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("p",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor."})]},i==null?void 0:i.id))})}),e.jsxs(s,{isGrey:!0,children:[e.jsx("h3",{children:"Heading Three"}),e.jsx(t,{})]}),e.jsxs(s,{children:[e.jsx("h3",{children:"Heading Three"}),e.jsx(t,{})]}),e.jsxs(s,{children:[e.jsx("h3",{children:"Heading Three"}),e.jsx(t,{})]})]})}),e.jsx(h,{})]})};var p,l,c;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <HeroTextImage maxWidth="5xl" hasBorder>
+            <HeroTextImage.Content title="With Containers" headerType="h1" />
+          </HeroTextImage>
+          <h2>Heading Two</h2>
+          <DoublePara />
+          <h3>Heading Three</h3>
+          <SinglePara />
+
+          <Container isGrey>
+            <h2>Heading Two</h2>
+            <DoublePara />
+          </Container>
+
+          <Container isGrey>
+            <h2>Heading Two</h2>
+            <DoublePara />
+          </Container>
+
+          <h2>Heading Two</h2>
+          <DoublePara />
+
+          <Container>
+            <Description>
+              {descMeta.map(item => <Description.Meta key={item?.id} term={item?.term}>
+                  <p>
+                    Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil
+                    voluptatem non.
+                  </p>
+                  <p>
+                    Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis
+                    nec elit ultricies, sed tempus diam dignissim.
+                  </p>
+                  <p>
+                    Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum
+                    magna vel orci vulputate, eget vulputate neque porttitor.
+                  </p>
+                </Description.Meta>)}
+            </Description>
+          </Container>
+
+          <DoublePara />
+
+          <Container isGrey>
+            <Description>
+              {descMeta.map(item => <Description.Meta key={item?.id} term={item?.term}>
+                  <p>
+                    Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil
+                    voluptatem non.
+                  </p>
+                  <p>
+                    Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis
+                    nec elit ultricies, sed tempus diam dignissim.
+                  </p>
+                  <p>
+                    Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum
+                    magna vel orci vulputate, eget vulputate neque porttitor.
+                  </p>
+                </Description.Meta>)}
+            </Description>
+          </Container>
+
+          <DoublePara />
+
+          <Container>
+            <Description>
+              {descMeta.map(item => <Description.Meta key={item?.id} term={item?.term} useColumns>
+                  <p>
+                    Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil
+                    voluptatem non.
+                  </p>
+                  <p>
+                    Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis
+                    nec elit ultricies, sed tempus diam dignissim.
+                  </p>
+                  <p>
+                    Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum
+                    magna vel orci vulputate, eget vulputate neque porttitor.
+                  </p>
+                </Description.Meta>)}
+            </Description>
+          </Container>
+
+          <DoublePara />
+
+          <Container isGrey>
+            <Description>
+              {descMeta.map(item => <Description.Meta key={item?.id} term={item?.term} useColumns>
+                  <p>
+                    Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil
+                    voluptatem non.
+                  </p>
+                  <p>
+                    Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis
+                    nec elit ultricies, sed tempus diam dignissim.
+                  </p>
+                  <p>
+                    Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum
+                    magna vel orci vulputate, eget vulputate neque porttitor.
+                  </p>
+                </Description.Meta>)}
+            </Description>
+          </Container>
+
+          <Container isGrey>
+            <h3>Heading Three</h3>
+            <DoublePara />
+          </Container>
+
+          <Container>
+            <h3>Heading Three</h3>
+            <DoublePara />
+          </Container>
+
+          <Container>
+            <h3>Heading Three</h3>
+            <DoublePara />
+          </Container>
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const Q=["WithContainers"];export{o as WithContainers,Q as __namedExportsOrder,K as default};
+//# sourceMappingURL=WithContainers.stories-be94fc19.js.map
