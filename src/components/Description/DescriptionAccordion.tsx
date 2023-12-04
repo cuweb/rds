aa8171b07d00b9ba2364e756d65c0c0a6b9a2d4f
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2023-12-04 14:14:42 -0500

    fix: indentation

--- src/components/Description/DescriptionAccordion.tsx
+++ src/components/Description/DescriptionAccordion.tsx
@@ -1,15 +1,14 @@
 import { styles } from './Description.Styles'
 import { ChevronRightIcon } from '@heroicons/react/24/outline'
 import { proseStyles } from '../../utils/globalClasses'
-import {Helmet} from "react-helmet";
+import { Helmet } from 'react-helmet'
 export interface DescriptionAccordionProps {
   term: string
   children: React.ReactNode
 }
 
 export const DescriptionAccordion = ({ term, children }: DescriptionAccordionProps) => {
-
-  const termLabel = term && typeof term === 'string' ? 'accordion-' + term.toLowerCase().replace(/ +/g, '-') : '';
+  const termLabel = term && typeof term === 'string' ? 'accordion-' + term.toLowerCase().replace(/ +/g, '-') : ''
 
   return (
     <>
@@ -18,7 +17,7 @@ export const DescriptionAccordion = ({ term, children }: DescriptionAccordionPro
       </Helmet>
       <div className={`accordion ${styles.base} ${styles.accordion}`}>
         <dt className={`${styles.term} ${proseStyles.base}`}>
-          <button className={`accordion__button ${styles.button}`} aria-expanded='false' aria-controls={termLabel}>
+          <button className={`accordion__button ${styles.button}`} aria-expanded="false" aria-controls={termLabel}>
             {term}
             <ChevronRightIcon className={`accordion__icon ${styles.chevron} rotate-0}`} />
           </button>
--- src/components/Description/script.js
+++ src/components/Description/script.js
@@ -1,27 +1,25 @@
-var buttons = document.querySelectorAll('.accordion__button');
+var buttons = document.querySelectorAll('.accordion__button')
 
 if (buttons) {
+  buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const target = event.currentTarget,
+        icon = target.querySelector('.accordion__icon'),
+        accordion = target.closest('.accordion'),
+        content = accordion.querySelector('.accordion__content'),
+        hidden = content.hidden
 
-    buttons.forEach((button) => {
+      if (hidden) {
+        target.setAttribute('aria-expanded', 'true')
+        icon.classList.remove('rotate-0')
+        icon.classList.add('rotate-90')
+      } else {
+        target.setAttribute('aria-expanded', 'false')
+        icon.classList.add('rotate-0')
+        icon.classList.remove('rotate-90')
+      }
 
-        button.addEventListener('click', (event) => {
-            const target     = event.currentTarget,
-                icon         = target.querySelector('.accordion__icon'),
-                accordion    = target.closest('.accordion'),
-                content      = accordion.querySelector('.accordion__content'),
-                hidden       = content.hidden;
-
-            if( hidden ){
-                target.setAttribute('aria-expanded', 'true')
-                icon.classList.remove('rotate-0');
-                icon.classList.add('rotate-90');
-            } else {
-                target.setAttribute('aria-expanded', 'false')
-                icon.classList.add('rotate-0');
-                icon.classList.remove('rotate-90');
-            }
-
-            content.hidden = !hidden
-        })
+      content.hidden = !hidden
     })
+  })
 }
