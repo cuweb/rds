00932236b59c67032ef51447a94f7b1b120fc552
troychaplin <troy.chaplin@gmail.com>
2023-12-09 14:50:05 +0000

    Deploying to gh-pages from @ cuweb/rds@a80f432b45890f5b803d653e55501d36cd7c8afe 🚀

---
+++ assets/TextImage.stories-J5pL7bA7.js
@@ -0,0 +1,125 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{T as t}from"./TextImage-RgT2Tytv.js";import{B as a}from"./Button-jLOfSZVe.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./globalClasses-EYtDdY2O.js";import"./ButtonGroup-34C6VEr-.js";import"./optionClasses-BVitpQvL.js";import"./Icon-MkcnZ0Qb.js";import"./ChevronDownIcon-qyckNURu.js";const K={title:"Components/Text Image",component:t,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},r={render:()=>e.jsx(t,{hasBorder:!0,children:e.jsxs(t.Content,{title:"Website and Application Development",imageUrl:"https://picsum.photos/400/266",children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},i={render:()=>e.jsx(t,{hasBorder:!0,maxWidth:"7xl",children:e.jsxs(t.Content,{title:"Website and Application Development",headerType:"h1",imageUrl:"https://picsum.photos/400/266",children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},o={render:()=>e.jsx(t,{hasBorder:!0,flipX:!0,children:e.jsxs(t.Content,{title:"Website and Application Development",imageUrl:"https://picsum.photos/400/266",hasMobileImage:!0,children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},s={render:()=>e.jsx(t,{maxWidth:"7xl",hasBorder:!0,children:e.jsxs(t.Content,{title:"Website and Application Development",imageUrl:"https://picsum.photos/400/266",imageZoom:50,angle:"left",hasMobileImage:!0,children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},n={render:()=>e.jsx(t,{maxWidth:"7xl",hasBorder:!0,flipX:!0,children:e.jsxs(t.Content,{title:"Website and Application Development",imageUrl:"https://picsum.photos/400/266",angle:"left",flipX:!0,children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},l={render:()=>e.jsx(t,{maxWidth:"7xl",hasBorder:!0,children:e.jsxs(t.Content,{title:"Website and Application Development",imageUrl:"https://picsum.photos/400/266",angle:"dual",children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},m={render:()=>e.jsx(t,{maxWidth:"7xl",hasBorder:!0,flipX:!0,children:e.jsxs(t.Content,{title:"Website and Application Development",imageUrl:"https://picsum.photos/400/266",angle:"dual",flipX:!0,children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},u={render:()=>e.jsx(t,{children:e.jsxs(t.Content,{title:"Website and Application Development",children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs("div",{className:"flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})]})})},d={render:()=>e.jsx(t,{flexRow:"md",flipYsm:!0,children:e.jsx(t.People,{designation:"Mr. ",pronoun:"he/him",name:"Greely Wrinkle",jobTitle:"Jedi Master & Jedi Council Member",degrees:"Certificate, Padawan Academy of Jedi Arts",building:"Onraed",room:"Sector A29",email:"greely@thejediyouseek.com",phone:"613-520-2600",phoneExt:"1234",resume:"#",linkedin:"#",twitter:"#",facebook:"#",children:e.jsx("img",{src:"https://fastly.picsum.photos/id/1062/400/400.jpg?hmac=zaTGri35k94fGnPFBesQ7tRVfjy6BUCtXDFQdWQ3r-k",alt:"People profile graphic",width:"350",height:"350"})})})},c={render:()=>e.jsx(t,{children:e.jsx(t.Event,{title:"Capital Hoops Classic",eventType:"Hybrid",virtualType:"Teams",virtualUrl:"https://events.carleton.ca",startDate:"March 21st, 2023 at 6:00pm",location:"Raven's Nest, 1125 Colonel By Drive",cost:"$20 per adult, $15 for youth/senior",contactName:"John Doe",contactEmail:"johndoe@test.com",contactPhone:"613-520-2600 x1234",primaryButtonUrl:"https://carleton.ca",secondaryButtonUrl:"https://carleton.ca/webservices",secondaryButtonText:"More Information",children:e.jsx("img",{src:"https://cu-production.s3.amazonaws.com/rds/sample-imgs/event-img.jpg",alt:"Event supporting graphic",width:"350",height:"153"})})})};var p,g,h;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <TextImage hasBorder>
+      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266">
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(h=(g=r.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var x,v,f;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <TextImage hasBorder maxWidth="7xl">
+      <TextImage.Content title="Website and Application Development" headerType="h1" imageUrl="https://picsum.photos/400/266">
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(f=(v=i.parameters)==null?void 0:v.docs)==null?void 0:f.source}}};var P,y,j;o.parameters={...o.parameters,docs:{...(P=o.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: () => <TextImage hasBorder flipX>
+      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266" hasMobileImage>
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(j=(y=o.parameters)==null?void 0:y.docs)==null?void 0:j.source}}};var q,b,I;s.parameters={...s.parameters,docs:{...(q=s.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: () => <TextImage maxWidth="7xl" hasBorder>
+      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266" imageZoom={50} angle="left" hasMobileImage>
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(I=(b=s.parameters)==null?void 0:b.docs)==null?void 0:I.source}}};var T,B,N;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: () => <TextImage maxWidth="7xl" hasBorder flipX>
+      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266" angle="left" flipX>
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(N=(B=n.parameters)==null?void 0:B.docs)==null?void 0:N.source}}};var C,D,w;l.parameters={...l.parameters,docs:{...(C=l.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: () => <TextImage maxWidth="7xl" hasBorder>
+      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266" angle="dual">
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(w=(D=l.parameters)==null?void 0:D.docs)==null?void 0:w.source}}};var A,M,W;m.parameters={...m.parameters,docs:{...(A=m.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => <TextImage maxWidth="7xl" hasBorder flipX>
+      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266" angle="dual" flipX>
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(W=(M=m.parameters)==null?void 0:M.docs)==null?void 0:W.source}}};var S,U,E;u.parameters={...u.parameters,docs:{...(S=u.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: () => <TextImage>
+      <TextImage.Content title="Website and Application Development">
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+          <Button title="Primary" />
+          <Button title="Secondary" color="grey" />
+        </div>
+      </TextImage.Content>
+    </TextImage>
+}`,...(E=(U=u.parameters)==null?void 0:U.docs)==null?void 0:E.source}}};var k,X,R;d.parameters={...d.parameters,docs:{...(k=d.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: () => <TextImage flexRow="md" flipYsm>
+      <TextImage.People designation="Mr. " pronoun="he/him" name="Greely Wrinkle" jobTitle="Jedi Master & Jedi Council Member" degrees="Certificate, Padawan Academy of Jedi Arts" building="Onraed" room="Sector A29" email="greely@thejediyouseek.com" phone="613-520-2600" phoneExt="1234" resume="#" linkedin="#" twitter="#" facebook="#">
+        <img src="https://fastly.picsum.photos/id/1062/400/400.jpg?hmac=zaTGri35k94fGnPFBesQ7tRVfjy6BUCtXDFQdWQ3r-k" alt="People profile graphic" width="350" height="350" />
+      </TextImage.People>
+    </TextImage>
+}`,...(R=(X=d.parameters)==null?void 0:X.docs)==null?void 0:R.source}}};var J,G,H;c.parameters={...c.parameters,docs:{...(J=c.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  render: () => <TextImage>
+      <TextImage.Event title="Capital Hoops Classic" eventType="Hybrid" virtualType="Teams" virtualUrl="https://events.carleton.ca" startDate="March 21st, 2023 at 6:00pm" location="Raven's Nest, 1125 Colonel By Drive" cost="$20 per adult, $15 for youth/senior" contactName="John Doe" contactEmail="johndoe@test.com" contactPhone="613-520-2600 x1234" primaryButtonUrl="https://carleton.ca" secondaryButtonUrl="https://carleton.ca/webservices" secondaryButtonText="More Information">
+        <img src="https://cu-production.s3.amazonaws.com/rds/sample-imgs/event-img.jpg" alt="Event supporting graphic" width="350" height="153" />
+      </TextImage.Event>
+    </TextImage>
+}`,...(H=(G=c.parameters)==null?void 0:G.docs)==null?void 0:H.source}}};const ee=["DefaultTemplate","HeaderOne","ImageLeft","ImageAngle","ImageAngleReverse","ImageDualAngle","ImageDualAngleReverse","NoImage","PeopleProfile","EventDetails"];export{r as DefaultTemplate,c as EventDetails,i as HeaderOne,s as ImageAngle,n as ImageAngleReverse,l as ImageDualAngle,m as ImageDualAngleReverse,o as ImageLeft,u as NoImage,d as PeopleProfile,ee as __namedExportsOrder,K as default};
