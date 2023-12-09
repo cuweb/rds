2fe65260bc1190a8531405793f4602f6826c4e05
troychaplin <troy.chaplin@gmail.com>
2023-12-09 14:43:25 +0000

    Deploying to gh-pages from @ cuweb/rds@9b1616cbffe7a1027ade524d06fbb57085c5974a 🚀

--- assets/Radio.stories-nuzKt0CI.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{F as r}from"./Form-P23XC_J0.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./XMarkIcon-ABUcTHhH.js";import"./Button-hdG8B1q3.js";import"./Icon-XxUdtgof.js";import"./optionClasses-BVitpQvL.js";import"./ChevronDownIcon-qyckNURu.js";import"./Calendar-Kpq_f_UU.js";import"./index-0f3231Fw.js";import"./index-bkt57PaO.js";import"./isNativeReflectConstruct-3LOYyi5T.js";const g={title:"Components/Forms/Radio",component:r,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},e={render:()=>o.jsx(r,{onSubmit:()=>{},schema:{},initialValues:s,children:o.jsx(r.Radio,{label:"How many of these shows did you watch regularly?",name:"regularviewer",options:[{name:"1",label:"1"}]})})},s={firstname:"ish",description:" sample description",regularviewer:"2",location:"Carleton University, Colonel By Drive, Ottawa, ON, Canada",favsitcom:"familymatter",favcharacters:["kellykapowski","carltonbanks"]};var a,t,i;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: () => <Form onSubmit={() => undefined} schema={{}} initialValues={InitialValues}>
-      <Form.Radio label="How many of these shows did you watch regularly?" name="regularviewer" options={[{
-      name: '1',
-      label: '1'
-    }]} />
-    </Form>
-}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const F=["Default"];export{e as Default,F as __namedExportsOrder,g as default};
