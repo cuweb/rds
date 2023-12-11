4b7c828bfdbeff8da56a8bcd7a34582161dc9326
troychaplin <troy.chaplin@gmail.com>
2023-12-11 13:25:36 +0000

    Deploying to gh-pages from @ cuweb/rds@e6980f444fe3ba59c4615c95cd8752bee7caa591 🚀

---
+++ assets/BasicMarkup.stories-A5r5XEIh.js
@@ -0,0 +1,154 @@
+import{j as i}from"./jsx-runtime-vNq4Oc-g.js";import{S as a}from"./Section-273f7e_u.js";import{M as o}from"./Main-6yTdoHTQ.js";import{F as c}from"./FooterDept-HaRA3pRH.js";import{T as e}from"./TopBar-GlvJDD4t.js";import{H as n}from"./HeroTextImage--NfxzvOn.js";import{F as m}from"./FooterStandard-HAQGF_lf.js";import{F as d}from"./FooterCookie-33bLNuIo.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./globalClasses-EYtDdY2O.js";import"./Button-Mc4YbIAv.js";import"./Icon-JTOhHsqE.js";import"./optionClasses-BVitpQvL.js";import"./ChevronDownIcon-qyckNURu.js";import"./ChevronRightIcon-U-mlqZiw.js";import"./menu-oFeCtKpM.js";import"./transition-8qPHkook.js";import"./use-root-containers-7zw4BiR7.js";import"./index-jmm5gWkb.js";import"./use-tracked-pointer-EZY5uaCR.js";import"./use-resolve-button-type-kFzmZbAv.js";import"./disclosure-LLQX-sEi.js";import"./MagnifyingGlassIcon-5GODM8Pf.js";import"./popover-tP4paLqk.js";import"./ButtonGroup-34C6VEr-.js";import"./FooterLogoLinks-UWwIltOF.js";import"./Link-SVD8XN2N.js";import"./iframe-jNd_mSzu.js";import"../sb-preview/runtime.js";const _={title:"Examples/Templates"},p=()=>i.jsx(i.Fragment,{children:i.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})}),s=()=>i.jsxs(i.Fragment,{children:[i.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),i.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),l={render:()=>i.jsxs(i.Fragment,{children:[i.jsx(e,{children:i.jsxs(e.Primary,{children:[i.jsx(e.Logo,{link:"https://carleton.ca/webservices",children:i.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[i.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),i.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),i.jsx(e.Aside,{})]})}),i.jsx(o,{children:i.jsxs(a,{hasProse:!0,children:[i.jsx(n,{maxWidth:"5xl",children:i.jsx(n.Content,{title:"Basic Markup",headerType:"h1"})}),i.jsx("h2",{children:"Heading Two"}),i.jsx(p,{}),i.jsx("h3",{children:"Heading Three"}),i.jsx(s,{}),i.jsxs("ul",{children:[i.jsx("li",{children:"Donec ac suscipit nunc. Fusce egestas risus eu libero fermentum congue."}),i.jsxs("li",{children:["In congue felis id mollis accumsan.",i.jsxs("ul",{children:[i.jsxs("li",{children:["Proin sollicitudin tellus congue venenatis semper.",i.jsxs("ul",{children:[i.jsx("li",{children:"Nullam mollis in velit sed pulvinar."}),i.jsx("li",{children:"Curabitur fermentum nisi dui, id blandit nisi pellentesque at."})]})]}),i.jsx("li",{children:"Ut id felis quis mi fringilla faucibus."}),i.jsx("li",{children:"Curabitur elementum velit ex, eu interdum ante suscipit sit amet."})]})]}),i.jsx("li",{children:"Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue."}),i.jsx("li",{children:"Etiam et convallis elit. Donec ac suscipit nunc."}),i.jsxs("li",{children:["Fusce egestas risus eu libero fermentum congue.",i.jsxs("ul",{children:[i.jsxs("li",{children:["In congue felis id mollis accumsan.",i.jsxs("ul",{children:[i.jsx("li",{children:"Proin sollicitudin tellus congue venenatis semper."}),i.jsxs("li",{children:["Nullam mollis in velit sed pulvinar.",i.jsxs("ul",{children:[i.jsx("li",{children:"Curabitur fermentum nisi dui, id blandit nisi pellentesque at."}),i.jsx("li",{children:"Ut id felis quis mi fringilla faucibus."})]})]}),i.jsx("li",{children:"Curabitur elementum velit ex, eu interdum ante suscipit sit amet."})]})]}),i.jsx("li",{children:"Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue."})]})]}),i.jsx("li",{children:"Etiam et convallis elit. Donec ac suscipit nunc."}),i.jsx("li",{children:"Fusce egestas risus eu libero fermentum congue."})]}),i.jsx(s,{}),i.jsxs("ol",{children:[i.jsx("li",{children:"Donec ac suscipit nunc. Fusce egestas risus eu libero fermentum congue."}),i.jsxs("li",{children:["In congue felis id mollis accumsan.",i.jsxs("ol",{children:[i.jsxs("li",{children:["Proin sollicitudin tellus congue venenatis semper.",i.jsxs("ol",{children:[i.jsx("li",{children:"Nullam mollis in velit sed pulvinar."}),i.jsx("li",{children:"Curabitur fermentum nisi dui, id blandit nisi pellentesque at."})]})]}),i.jsx("li",{children:"Ut id felis quis mi fringilla faucibus."}),i.jsx("li",{children:"Curabitur elementum velit ex, eu interdum ante suscipit sit amet."})]})]}),i.jsx("li",{children:"Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue."}),i.jsx("li",{children:"Etiam et convallis elit. Donec ac suscipit nunc."}),i.jsxs("li",{children:["Fusce egestas risus eu libero fermentum congue.",i.jsxs("ol",{children:[i.jsxs("li",{children:["In congue felis id mollis accumsan.",i.jsxs("ol",{children:[i.jsx("li",{children:"Proin sollicitudin tellus congue venenatis semper."}),i.jsxs("li",{children:["Nullam mollis in velit sed pulvinar.",i.jsxs("ol",{children:[i.jsx("li",{children:"Curabitur fermentum nisi dui, id blandit nisi pellentesque at."}),i.jsx("li",{children:"Ut id felis quis mi fringilla faucibus."})]})]}),i.jsx("li",{children:"Curabitur elementum velit ex, eu interdum ante suscipit sit amet."})]})]}),i.jsx("li",{children:"Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue."})]})]}),i.jsx("li",{children:"Etiam et convallis elit. Donec ac suscipit nunc."}),i.jsx("li",{children:"Fusce egestas risus eu libero fermentum congue."})]}),i.jsx(s,{})]})}),i.jsx(c,{buildingName:"Pigiarvik (ᐱᒋᐊᕐᕕᒃ)",deptName:"Information Technology Services",email:"noreply@carleton.ca",footerButtons:[{id:1,title:"Contact support",url:"#"},{id:2,title:"Recent alerts",url:"#"}],officeNumber:"400",phone:"613-520-2600"}),i.jsx(m,{}),i.jsx(d,{})]})};var t,u,r;l.parameters={...l.parameters,docs:{...(t=l.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <HeroTextImage maxWidth="5xl">
+            <HeroTextImage.Content title="Basic Markup" headerType="h1" />
+          </HeroTextImage>
+          <h2>Heading Two</h2>
+          <SinglePara />
+          <h3>Heading Three</h3>
+          <DoublePara />
+
+          <ul>
+            <li>Donec ac suscipit nunc. Fusce egestas risus eu libero fermentum congue.</li>
+
+            <li>
+              In congue felis id mollis accumsan.
+              <ul>
+                <li>
+                  Proin sollicitudin tellus congue venenatis semper.
+                  <ul>
+                    <li>Nullam mollis in velit sed pulvinar.</li>
+
+                    <li>Curabitur fermentum nisi dui, id blandit nisi pellentesque at.</li>
+                  </ul>
+                </li>
+
+                <li>Ut id felis quis mi fringilla faucibus.</li>
+
+                <li>Curabitur elementum velit ex, eu interdum ante suscipit sit amet.</li>
+              </ul>
+            </li>
+
+            <li>Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue.</li>
+
+            <li>Etiam et convallis elit. Donec ac suscipit nunc.</li>
+
+            <li>
+              Fusce egestas risus eu libero fermentum congue.
+              <ul>
+                <li>
+                  In congue felis id mollis accumsan.
+                  <ul>
+                    <li>Proin sollicitudin tellus congue venenatis semper.</li>
+
+                    <li>
+                      Nullam mollis in velit sed pulvinar.
+                      <ul>
+                        <li>Curabitur fermentum nisi dui, id blandit nisi pellentesque at.</li>
+
+                        <li>Ut id felis quis mi fringilla faucibus.</li>
+                      </ul>
+                    </li>
+
+                    <li>Curabitur elementum velit ex, eu interdum ante suscipit sit amet.</li>
+                  </ul>
+                </li>
+
+                <li>Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue.</li>
+              </ul>
+            </li>
+
+            <li>Etiam et convallis elit. Donec ac suscipit nunc.</li>
+
+            <li>Fusce egestas risus eu libero fermentum congue.</li>
+          </ul>
+
+          <DoublePara />
+
+          <ol>
+            <li>Donec ac suscipit nunc. Fusce egestas risus eu libero fermentum congue.</li>
+
+            <li>
+              In congue felis id mollis accumsan.
+              <ol>
+                <li>
+                  Proin sollicitudin tellus congue venenatis semper.
+                  <ol>
+                    <li>Nullam mollis in velit sed pulvinar.</li>
+
+                    <li>Curabitur fermentum nisi dui, id blandit nisi pellentesque at.</li>
+                  </ol>
+                </li>
+
+                <li>Ut id felis quis mi fringilla faucibus.</li>
+
+                <li>Curabitur elementum velit ex, eu interdum ante suscipit sit amet.</li>
+              </ol>
+            </li>
+
+            <li>Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue.</li>
+
+            <li>Etiam et convallis elit. Donec ac suscipit nunc.</li>
+
+            <li>
+              Fusce egestas risus eu libero fermentum congue.
+              <ol>
+                <li>
+                  In congue felis id mollis accumsan.
+                  <ol>
+                    <li>Proin sollicitudin tellus congue venenatis semper.</li>
+
+                    <li>
+                      Nullam mollis in velit sed pulvinar.
+                      <ol>
+                        <li>Curabitur fermentum nisi dui, id blandit nisi pellentesque at.</li>
+
+                        <li>Ut id felis quis mi fringilla faucibus.</li>
+                      </ol>
+                    </li>
+
+                    <li>Curabitur elementum velit ex, eu interdum ante suscipit sit amet.</li>
+                  </ol>
+                </li>
+
+                <li>Vestibulum est lorem, placerat ac libero vitae, placerat suscipit augue.</li>
+              </ol>
+            </li>
+
+            <li>Etiam et convallis elit. Donec ac suscipit nunc.</li>
+
+            <li>Fusce egestas risus eu libero fermentum congue.</li>
+          </ol>
+
+          <DoublePara />
+        </Section>
+      </Main>
+
+      <FooterDept buildingName="Pigiarvik (ᐱᒋᐊᕐᕕᒃ)" deptName="Information Technology Services" email="noreply@carleton.ca" footerButtons={[{
+      id: 1,
+      title: 'Contact support',
+      url: '#'
+    }, {
+      id: 2,
+      title: 'Recent alerts',
+      url: '#'
+    }]} officeNumber="400" phone="613-520-2600" />
+
+      <FooterStandard />
+      <FooterCookie />
+    </>
+}`,...(r=(u=l.parameters)==null?void 0:u.docs)==null?void 0:r.source}}};const O=["BasicMarkup"];export{l as BasicMarkup,O as __namedExportsOrder,_ as default};
