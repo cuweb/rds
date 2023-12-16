ba8a4cde686c4662b33c4ce66fdf34911c78cb4e
Troy Chaplin <troy.chaplin@gmail.com>
2023-12-15 22:03:49 -0500

    change: stackedlist to use ul or div

--- src/layouts/StackedList/StackedList.styles.tsx
+++ src/layouts/StackedList/StackedList.styles.tsx
@@ -1,10 +1,8 @@
-const styles = {
+export const styles = {
   title: `px-6 py-4 text-base md:text-xl font-semibold border-b rounded-t-lg bg-gray-50 text-cu-black-800`,
-  grid: `grid overflow-hidden`,
-}
-export default styles
-
-export const gridStyles = {
-  oneCol: `md:grid-cols-1`,
-  twoCol: `md:grid-cols-2`,
+  grid: `overflow-hidden`,
+  oneCol: `grid md:grid-cols-1`,
+  twoCol: `grid md:grid-cols-2`,
+  div: `p-6`,
+  ul: ``,
 }
--- src/layouts/StackedList/StackedList.tsx
+++ src/layouts/StackedList/StackedList.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
-import styles, { gridStyles } from './StackedList.styles'
+import { styles } from './StackedList.styles'
 import { Panel } from '../Panel/Panel'
 
 export interface StackedListProps {
   children: React.ReactNode
+  as?: 'ul' | 'div'
   cols?: '1' | '2'
+  listType?: 'posts' | 'toc'
   header?: string
   hasBorder?: boolean
   hasShadow?: boolean
 }
 
-export const StackedList = ({ children, cols = '1', header, hasBorder, hasShadow }: StackedListProps) => {
-  const gridColumns = cols === '1' ? gridStyles.oneCol : gridStyles.twoCol
+export const StackedList = ({
+  children,
+  as = 'ul',
+  cols = '2',
+  listType = 'posts',
+  header,
+  hasBorder,
+  hasShadow,
+}: StackedListProps) => {
+  const gridColumns = cols === '1' ? styles.oneCol : styles.twoCol
+  const ListComponent = as
 
   return (
     <Panel hasShadow={hasShadow} hasBorder={hasBorder}>
       {header && <h2 className={`${styles.title}`}>{header}</h2>}
-      <ul className={`cu-stackedlist ${styles.grid} ${gridColumns}`}>{children}</ul>
+      <ListComponent className={`cu-stackedlist-${listType} ${styles[as]} ${gridColumns}`}>{children}</ListComponent>
     </Panel>
   )
 }
