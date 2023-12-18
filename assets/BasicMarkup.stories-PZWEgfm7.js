329d5aed7bf29379584cdfb331fa2f135e883090
troychaplin <troy.chaplin@gmail.com>
2023-12-18 19:27:30 +0000

    Deploying to gh-pages from @ cuweb/rds@3e374831b832f523c0c82c4b326ac56f72761dea 🚀

---
+++ assets/BasicMarkup.stories-PZWEgfm7.js
@@ -0,0 +1,173 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{S as j}from"./Section-273f7e_u.js";import{M as f}from"./Main-6yTdoHTQ.js";import{F as L}from"./FooterDept-yI6XPsWe.js";import{T as d}from"./TopBar-GlvJDD4t.js";import{H as u}from"./HeroTextImage-e9ESeqMy.js";import{F as T}from"./FooterStandard-rSL9dxVv.js";import{F as S}from"./FooterCookie-U2meHEqX.js";import{S as o}from"./StackedList-9DnVSzTy.js";import{L as t}from"./Listing-_elQVZD2.js";import{a as c}from"./NewsData-IVt9b4UK.js";import{H as P}from"./Heading-eeuRkPD0.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./globalClasses-EYtDdY2O.js";import"./Button-rCx4NyoA.js";import"./Icon-JTOhHsqE.js";import"./optionClasses-BVitpQvL.js";import"./ChevronDownIcon-qyckNURu.js";import"./ChevronRightIcon-U-mlqZiw.js";import"./menu-oFeCtKpM.js";import"./transition-8qPHkook.js";import"./use-root-containers-7zw4BiR7.js";import"./index-jmm5gWkb.js";import"./use-tracked-pointer-EZY5uaCR.js";import"./use-resolve-button-type-kFzmZbAv.js";import"./disclosure-LLQX-sEi.js";import"./MagnifyingGlassIcon-5GODM8Pf.js";import"./popover-tP4paLqk.js";import"./ButtonGroup-34C6VEr-.js";import"./FooterLogoLinks-oOJB4Qik.js";import"./Link-xI0Swr3F.js";import"./iframe-lTem1UyJ.js";import"../sb-preview/runtime.js";import"./index-bkt57PaO.js";import"./isNativeReflectConstruct-3LOYyi5T.js";import"./index-9aHumogv.js";import"./Badge-Zsxsg5ql.js";import"./ChevronRightIcon-slJogYDx.js";const le={title:"Examples/Layouts"},l=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."})}),i=()=>e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),p=()=>e.jsx(e.Fragment,{children:e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Lorem ipsum dolor"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Nunc viverra lorem arcu"})}),e.jsxs("li",{children:[e.jsx("a",{href:"http://carleton.ca",children:"Consectetur elit"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Suspendisse risus."})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Nullam ut nunc"})}),e.jsxs("li",{children:[e.jsx("a",{href:"http://carleton.ca",children:"Integer mattis"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Phasellus porttitor"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Praesent ante massa"})})]})]}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Etiam non convallis"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Vestibulum"})})]})]}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Turpis placerat"})}),e.jsxs("li",{children:[e.jsx("a",{href:"http://carleton.ca",children:"Consectetur elit"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Mauris feugiat interdum"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Etiam non convallis"})})]})]})]})}),h={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(d,{children:e.jsxs(d.Primary,{children:[e.jsx(d.Logo,{link:"https://carleton.ca/webservices",children:e.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[e.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),e.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),e.jsx(d.Aside,{})]})}),e.jsx(f,{children:e.jsxs(j,{hasProse:!0,children:[e.jsx(u,{maxWidth:"5xl",children:e.jsx(u.Content,{title:"Basic Markup",headerType:"h1"})}),e.jsx("h2",{children:"Heading Two"}),e.jsx(l,{}),e.jsx(o,{header:"Table of Contents",as:"div",cols:"1",listType:"toc",offset:"right",hasShadow:!0,children:e.jsx(p,{})}),e.jsx("h3",{children:"Heading Three"}),e.jsx(l,{}),e.jsx(i,{}),e.jsx(o,{header:"Table of Contents",as:"div",cols:"1",listType:"toc",offset:"left",hasShadow:!0,children:e.jsx(p,{})}),e.jsx("h3",{children:"Heading Three"}),e.jsx(l,{}),e.jsx(i,{}),e.jsx(o,{header:"Recent News",as:"ul",cols:"1",listType:"posts",offset:"right",hasShadow:!0,children:c.slice(0,3).map(({id:s,title:n,link:a,date:r})=>e.jsx(t,{children:e.jsx("a",{href:a,children:e.jsxs(t.Content,{children:[e.jsx(t.PostMeta,{date:r}),e.jsx(t.Header,{text:n})]})})},s))}),e.jsx("h3",{children:"Heading Three"}),e.jsx(l,{}),e.jsx(i,{}),e.jsx(o,{header:"Recent News",as:"ul",cols:"1",listType:"posts",offset:"left",hasShadow:!0,children:c.slice(0,3).map(({id:s,title:n,link:a,date:r})=>e.jsx(t,{children:e.jsx("a",{href:a,children:e.jsxs(t.Content,{children:[e.jsx(t.PostMeta,{date:r}),e.jsx(t.Header,{text:n})]})})},s))}),e.jsx("h3",{children:"Heading Three"}),e.jsx(l,{}),e.jsx(i,{}),e.jsx(o,{header:"Recent News",as:"ul",cols:"1",listType:"posts",offset:"right",size:"lg",hasShadow:!0,children:c.slice(0,3).map(({id:s,title:n,link:a,date:r})=>e.jsx(t,{children:e.jsx("a",{href:a,children:e.jsxs(t.Content,{children:[e.jsx(t.PostMeta,{date:r}),e.jsx(t.Header,{text:n})]})})},s))}),e.jsx("h3",{children:"Heading Three"}),e.jsx(l,{}),e.jsx(i,{}),e.jsx(o,{header:"Recent News",as:"ul",cols:"1",listType:"posts",offset:"left",size:"lg",hasShadow:!0,children:c.slice(0,3).map(({id:s,title:n,link:a,date:r})=>e.jsx(t,{children:e.jsx("a",{href:a,children:e.jsxs(t.Content,{children:[e.jsx(t.PostMeta,{date:r}),e.jsx(t.Header,{text:n})]})})},s))}),e.jsx("h3",{children:"Heading Three"}),e.jsx(l,{}),e.jsx(i,{}),e.jsx(P,{text:"Recent News"}),e.jsx(o,{as:"ul",cols:"2",listType:"posts",hasShadow:!0,children:c.map(({id:s,title:n,link:a,date:r})=>e.jsx(t,{children:e.jsx("a",{href:a,children:e.jsxs(t.Content,{children:[e.jsx(t.PostMeta,{date:r}),e.jsx(t.Header,{text:n})]})})},s))}),e.jsx("h3",{children:"Heading Three"}),e.jsx(i,{}),e.jsx(i,{}),e.jsx(i,{}),e.jsx(i,{})]})}),e.jsx(L,{buildingName:"Pigiarvik (ᐱᒋᐊᕐᕕᒃ)",deptName:"Information Technology Services",email:"noreply@carleton.ca",footerButtons:[{id:1,title:"Contact support",url:"#"},{id:2,title:"Recent alerts",url:"#"}],officeNumber:"400",phone:"613-520-2600"}),e.jsx(T,{}),e.jsx(S,{})]})};var m,g,x;h.parameters={...h.parameters,docs:{...(m=h.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <HeroTextImage maxWidth="5xl">
+            <HeroTextImage.Content title="Basic Markup" headerType="h1" />
+          </HeroTextImage>
+          <h2>Heading Two</h2>
+          <SinglePara />
+
+          <StackedList header="Table of Contents" as="div" cols="1" listType="toc" offset="right" hasShadow>
+            <ToC />
+          </StackedList>
+
+          <h3>Heading Three</h3>
+          <SinglePara />
+          <DoublePara />
+
+          <StackedList header="Table of Contents" as="div" cols="1" listType="toc" offset="left" hasShadow>
+            <ToC />
+          </StackedList>
+
+          <h3>Heading Three</h3>
+          <SinglePara />
+          <DoublePara />
+
+          <StackedList header="Recent News" as="ul" cols="1" listType="posts" offset="right" hasShadow>
+            {data.slice(0, 3).map(({
+            id,
+            title,
+            link,
+            date
+          }) => <Listing key={id}>
+                <a href={link}>
+                  {/* <Listing.Figure>
+                    <img src={image} alt={alt} width="400" height="266" />
+                   </Listing.Figure> */}
+                  <Listing.Content>
+                    <Listing.PostMeta date={date} />
+                    <Listing.Header text={title} />
+                  </Listing.Content>
+                </a>
+              </Listing>)}
+          </StackedList>
+
+          <h3>Heading Three</h3>
+          <SinglePara />
+          <DoublePara />
+
+          <StackedList header="Recent News" as="ul" cols="1" listType="posts" offset="left" hasShadow>
+            {data.slice(0, 3).map(({
+            id,
+            title,
+            link,
+            date
+          }) => <Listing key={id}>
+                <a href={link}>
+                  {/* <Listing.Figure>
+                    <img src={image} alt={alt} width="400" height="266" />
+                   </Listing.Figure> */}
+                  <Listing.Content>
+                    <Listing.PostMeta date={date} />
+                    <Listing.Header text={title} />
+                  </Listing.Content>
+                </a>
+              </Listing>)}
+          </StackedList>
+
+          <h3>Heading Three</h3>
+          <SinglePara />
+          <DoublePara />
+
+          <StackedList header="Recent News" as="ul" cols="1" listType="posts" offset="right" size="lg" hasShadow>
+            {data.slice(0, 3).map(({
+            id,
+            title,
+            link,
+            date
+          }) => <Listing key={id}>
+                <a href={link}>
+                  {/* <Listing.Figure>
+                    <img src={image} alt={alt} width="400" height="266" />
+                   </Listing.Figure> */}
+                  <Listing.Content>
+                    <Listing.PostMeta date={date} />
+                    <Listing.Header text={title} />
+                  </Listing.Content>
+                </a>
+              </Listing>)}
+          </StackedList>
+
+          <h3>Heading Three</h3>
+          <SinglePara />
+          <DoublePara />
+
+          <StackedList header="Recent News" as="ul" cols="1" listType="posts" offset="left" size="lg" hasShadow>
+            {data.slice(0, 3).map(({
+            id,
+            title,
+            link,
+            date
+          }) => <Listing key={id}>
+                <a href={link}>
+                  {/* <Listing.Figure>
+                    <img src={image} alt={alt} width="400" height="266" />
+                   </Listing.Figure> */}
+                  <Listing.Content>
+                    <Listing.PostMeta date={date} />
+                    <Listing.Header text={title} />
+                  </Listing.Content>
+                </a>
+              </Listing>)}
+          </StackedList>
+
+          <h3>Heading Three</h3>
+          <SinglePara />
+          <DoublePara />
+
+          <Heading text="Recent News" />
+          <StackedList as="ul" cols="2" listType="posts" hasShadow>
+            {data.map(({
+            id,
+            title,
+            link,
+            date
+          }) => <Listing key={id}>
+                <a href={link}>
+                  {/* <Listing.Figure>
+                    <img src={image} alt={alt} width="400" height="266" />
+                   </Listing.Figure> */}
+                  <Listing.Content>
+                    <Listing.PostMeta date={date} />
+                    <Listing.Header text={title} />
+                  </Listing.Content>
+                </a>
+              </Listing>)}
+          </StackedList>
+
+          <h3>Heading Three</h3>
+          <DoublePara />
+          <DoublePara />
+
+          <DoublePara />
+          <DoublePara />
+        </Section>
+      </Main>
+
+      <FooterDept buildingName="Pigiarvik (ᐱᒋᐊᕐᕕᒃ)" deptName="Information Technology Services" email="noreply@carleton.ca" footerButtons={[{
+      id: 1,
+      title: 'Contact support',
+      url: '#'
+    }, {
+      id: 2,
+      title: 'Recent alerts',
+      url: '#'
+    }]} officeNumber="400" phone="613-520-2600" />
+
+      <FooterStandard />
+      <FooterCookie />
+    </>
+}`,...(x=(g=h.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const ce=["BasicMarkup"];export{h as BasicMarkup,ce as __namedExportsOrder,le as default};
