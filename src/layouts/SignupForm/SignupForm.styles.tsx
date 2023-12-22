4d26d096549f8c6a83ba4caebb8ddaa46e9d7251
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-12-22 16:13:56 -0500

    change: remove old signup components

--- src/layouts/SignupForm/SignupForm.styles.tsx
+++
@@ -1,9 +0,0 @@
-const styles = {
-  wrapper: `cu-signupform not-contained not-prose mx-auto overflow-hidden rounded-lg bg-white w-full`,
-  type: {
-    light: `bg-zinc-200`,
-    dark: ``,
-    image: ``,
-  },
-}
-export default styles
