98ce39aea663e88c3b32075a91948b11e88d12ce
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-12-22 16:18:53 -0500

    change: update component to accomodate new types

--- src/components/WideImage/WideImage.Styles.tsx
+++ src/components/WideImage/WideImage.Styles.tsx
@@ -1,10 +1,11 @@
 export const styles = {
-  baseBg: `flex items-center justify-center mx-auto px-4 mb-6 overflow-hidden md:px-8 md:mb-12 rounded-xl not-contained not-prose`,
+  baseBg: `relative flex items-center justify-center mx-auto px-4 mb-6 overflow-hidden md:px-8 md:mb-12 rounded-xl not-contained not-prose`,
   noImage: `py-20 bg-cu-black-50`,
   imageBg: `relative py-24 bg-opacity-50 bg-cover bg-cu-black-50 md:py-28 lg:py-36 xl:py-48`,
+  darkBg: `py-20 bg-gray-900`,
   imageOverlay: `absolute w-full h-full bg-black`,
-  content: `relative z-10 flex flex-col items-center w-full gap-5 max-w-7xl md:gap-10`,
-  header: 'font-semibold text-center',
+  content: `relative z-10 flex flex-col items-center w-full gap-5 max-w-7xl md:gap-10 text-center`,
+  header: 'font-semibold',
   headerOne: `text-3xl md:text-4xl lg:text-5xl lg:leading-[3.5rem]`,
   headerTwo: `text-2xl md:text-3xl lg:text-4xl lg:leading-[3rem]`,
 }
--- src/components/WideImage/WideImage.tsx
+++ src/components/WideImage/WideImage.tsx
@@ -13,9 +13,10 @@ export interface WideImageProps {
   opacity?: 40 | 50 | 60 | 70 | 80
   focalPointX?: string
   focalPointY?: string
+  isType?: 'light' | 'dark' | 'image'
 }
 
-export const WideImage = ({
+export const WideImageWrapper = ({
   children,
   title,
   image,
@@ -24,24 +25,73 @@ export const WideImage = ({
   opacity = 70,
   focalPointX = '50',
   focalPointY = '50',
+  isType = 'light',
 }: WideImageProps) => {
   const inlineStyle = {
     backgroundImage: `url(${image})`,
     backgroundPosition: `${focalPointX}% ${focalPointY}%`,
   }
 
-  const hasImageStyles = image ? styles.imageBg : styles.noImage
-  const imageTextStyles = image ? 'text-white' : 'text-cu-black-700'
+  let hasImageStyles
+  let imageTextStyles
+
+  if (image) {
+    isType = 'image'
+  }
+
+  switch (isType) {
+    case 'light':
+      imageTextStyles = 'text-cu-black-700'
+      hasImageStyles = styles.noImage
+      break
+    case 'image':
+      imageTextStyles = 'text-white'
+      hasImageStyles = styles.imageBg
+      break
+    case 'dark':
+      imageTextStyles = 'text-white'
+      hasImageStyles = styles.darkBg
+      break
+    default:
+      imageTextStyles = 'text-cu-black-700'
+      hasImageStyles = styles.noImage
+  }
 
   return (
     <div style={inlineStyle} className={`${styles.baseBg} ${rdsMaxWidth[maxWidth]} ${hasImageStyles}`}>
       {image && <div className={`${styles.imageOverlay} ${rdsOpacity[opacity]}`}></div>}
-
-      <div className={styles.content}>
-        {headerType === 'h1' && <h1 className={`${styles.header} ${styles.headerOne} ${imageTextStyles}`}>{title}</h1>}
-        {headerType === 'h2' && <h2 className={`${styles.header} ${styles.headerTwo} ${imageTextStyles}`}>{title}</h2>}
+      <div className={`${styles.content} ${imageTextStyles} cu-wideimage-input-${isType}`}>
+        {headerType === 'h1' && <h1 className={`${styles.header} ${styles.headerOne}`}>{title}</h1>}
+        {headerType === 'h2' && <h2 className={`${styles.header} ${styles.headerTwo}`}>{title}</h2>}
         {children}
+        {isType === 'dark' && (
+          <svg
+            viewBox="0 0 1024 1024"
+            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
+            aria-hidden="true"
+          >
+            <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+            <defs>
+              <radialGradient
+                id="759c1415-0410-454c-8f7c-9a820de03641"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(512 512) rotate(90) scale(512)"
+              >
+                <stop stopColor="#cfcfd9" />
+                <stop offset="1" stopColor="#eeeeee" stopOpacity="0" />
+              </radialGradient>
+            </defs>
+          </svg>
+        )}
       </div>
     </div>
   )
 }
+
+export const WideImage = Object.assign(WideImageWrapper, {
+  MailChimp: WideImageMailChimp,
+  CampaignMonitor: WideImageCampaignMonitor,
+})
