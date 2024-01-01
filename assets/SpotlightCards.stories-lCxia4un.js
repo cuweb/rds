352a54ea86b567202000a8fad4eea3a01e8ac263
troychaplin <troy.chaplin@gmail.com>
2024-01-01 21:46:52 +0000

    Deploying to gh-pages from @ cuweb/rds@d308774f8238419f51d1e7631b3f6ceeab44334c 🚀

---
+++ assets/SpotlightCards.stories-lCxia4un.js
@@ -0,0 +1,127 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{S as C}from"./Section-273f7e_u.js";import{C as s}from"./Container-gVUWVoze.js";import{M as j}from"./Main-6yTdoHTQ.js";import{F as b}from"./FooterBasic-CwKUxWBt.js";import{T as d}from"./TopBar-xywG3lse.js";import{H as l}from"./Heading-e9K0UJNj.js";import{T as p}from"./TextImage-vRdqDVlO.js";import{C as c}from"./Column-YPGoCv6X.js";import{a as m}from"./NewsData-coxflhgV.js";import{C as e}from"./Card-kOuMcx93.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./globalClasses-EYtDdY2O.js";import"./FooterLogoLinks-_-vT5TZm.js";import"./Link-7QyOLBcG.js";import"./iframe-ilMUvCIZ.js";import"../sb-preview/runtime.js";import"./ChevronDownIcon-WJjzl-Lh.js";import"./ChevronRightIcon-XyhEQFMv.js";import"./menu-oFeCtKpM.js";import"./transition-8qPHkook.js";import"./use-root-containers-7zw4BiR7.js";import"./index-jmm5gWkb.js";import"./use-tracked-pointer-EZY5uaCR.js";import"./use-resolve-button-type-kFzmZbAv.js";import"./disclosure-LLQX-sEi.js";import"./MagnifyingGlassIcon-vfrtgMO8.js";import"./popover-tP4paLqk.js";import"./optionClasses-ccrz1VfO.js";import"./Button-vGHki37U.js";import"./Icon-WcGhYRcv.js";import"./ButtonGroup-34C6VEr-.js";import"./index-bkt57PaO.js";import"./isNativeReflectConstruct-3LOYyi5T.js";import"./index-J1scOhqY.js";import"./index-ep2Jkdwr.js";import"./index-VFMbf6KQ.js";const ot={title:"Examples/Blocks"},h={render:()=>t.jsxs(t.Fragment,{children:[t.jsx(d,{children:t.jsxs(d.Primary,{children:[t.jsx(d.Logo,{link:"https://carleton.ca/webservices",children:t.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[t.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),t.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),t.jsx(d.Aside,{})]})}),t.jsx(j,{children:t.jsxs(C,{hasProse:!0,children:[t.jsx(p,{children:t.jsx(p.Content,{headerType:"h1",title:"Spotlight Cards",children:t.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})}),t.jsxs(s,{children:[t.jsx(l,{text:"Spotlight Cards: Shadow on Static & Hover"}),t.jsx(c,{cols:"3",children:m.slice(0,6).map(({id:r,link:a,title:o,image:i,alt:n})=>t.jsxs(e,{hasShadow:"onCard",noLink:!0,children:[t.jsx(e.ImageThumb,{children:t.jsx("img",{src:i,alt:n,width:"400",height:"266"})}),t.jsx(e.Header,{children:o}),t.jsx(e.Body,{children:t.jsx(e.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),t.jsx(e.Footer,{isType:"button",buttonStyle:"red",children:t.jsx("a",{href:a,children:"More info"})})]},r))})]}),t.jsxs(s,{children:[t.jsx(l,{text:"Spotlight Cards: Shadow on Static & Hover with Grey Bg"}),t.jsx(c,{cols:"3",children:m.slice(0,6).map(({id:r,link:a,title:o,image:i,alt:n})=>t.jsxs(e,{isGrey:!0,noLink:!0,children:[t.jsx(e.ImageThumb,{children:t.jsx("img",{src:i,alt:n,width:"400",height:"266"})}),t.jsx(e.Header,{children:o}),t.jsx(e.Body,{children:t.jsx(e.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),t.jsx(e.Footer,{isType:"button",buttonStyle:"red",children:t.jsx("a",{href:a,children:"More info"})})]},r))})]}),t.jsxs(s,{isGrey:!0,children:[t.jsx(l,{text:"Spotlight Cards: Shadow on Static & Hover"}),t.jsx(c,{cols:"3",children:m.slice(0,6).map(({id:r,link:a,title:o,image:i,alt:n})=>t.jsxs(e,{noLink:!0,children:[t.jsx(e.ImageThumb,{children:t.jsx("img",{src:i,alt:n,width:"400",height:"266"})}),t.jsx(e.Header,{children:o}),t.jsx(e.Body,{children:t.jsx(e.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),t.jsx(e.Footer,{isType:"button",buttonStyle:"red",children:t.jsx("a",{href:a,children:"More info"})})]},r))})]}),t.jsxs(s,{isGrey:!0,children:[t.jsx(l,{text:"Spotlight Cards: Shadow on Static & Hover with Grey Bg"}),t.jsx(c,{cols:"3",children:m.slice(0,6).map(({id:r,link:a,title:o,image:i,alt:n})=>t.jsxs(e,{isGrey:!0,hasShadow:"onCard",noLink:!0,children:[t.jsx(e.ImageThumb,{children:t.jsx("img",{src:i,alt:n,width:"400",height:"266"})}),t.jsx(e.Header,{children:o}),t.jsx(e.Body,{children:t.jsx(e.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),t.jsx(e.Footer,{isType:"button",buttonStyle:"red",children:t.jsx("a",{href:a,children:"More info"})})]},r))})]})]})}),t.jsx(b,{})]})};var u,g,x;h.parameters={...h.parameters,docs:{...(u=h.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <TextImage>
+            <TextImage.Content headerType="h1" title="Spotlight Cards">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+
+          <Container>
+            <Heading text="Spotlight Cards: Shadow on Static & Hover" />
+            <Column cols="3">
+              {NewsData.slice(0, 6).map(({
+              id,
+              link,
+              title,
+              image,
+              alt
+            }) => <Card key={id} hasShadow="onCard" noLink>
+                  <Card.ImageThumb>
+                    <img src={image} alt={alt} width="400" height="266" />
+                  </Card.ImageThumb>
+                  <Card.Header>{title}</Card.Header>
+                  <Card.Body>
+                    <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+                  </Card.Body>
+                  <Card.Footer isType="button" buttonStyle="red">
+                    <a href={link}>More info</a>
+                  </Card.Footer>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container>
+            <Heading text="Spotlight Cards: Shadow on Static & Hover with Grey Bg" />
+            <Column cols="3">
+              {NewsData.slice(0, 6).map(({
+              id,
+              link,
+              title,
+              image,
+              alt
+            }) => <Card key={id} isGrey noLink>
+                  <Card.ImageThumb>
+                    <img src={image} alt={alt} width="400" height="266" />
+                  </Card.ImageThumb>
+                  <Card.Header>{title}</Card.Header>
+                  <Card.Body>
+                    <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+                  </Card.Body>
+                  <Card.Footer isType="button" buttonStyle="red">
+                    <a href={link}>More info</a>
+                  </Card.Footer>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Spotlight Cards: Shadow on Static & Hover" />
+            <Column cols="3">
+              {NewsData.slice(0, 6).map(({
+              id,
+              link,
+              title,
+              image,
+              alt
+            }) => <Card key={id} noLink>
+                  <Card.ImageThumb>
+                    <img src={image} alt={alt} width="400" height="266" />
+                  </Card.ImageThumb>
+                  <Card.Header>{title}</Card.Header>
+                  <Card.Body>
+                    <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+                  </Card.Body>
+                  <Card.Footer isType="button" buttonStyle="red">
+                    <a href={link}>More info</a>
+                  </Card.Footer>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Spotlight Cards: Shadow on Static & Hover with Grey Bg" />
+            <Column cols="3">
+              {NewsData.slice(0, 6).map(({
+              id,
+              link,
+              title,
+              image,
+              alt
+            }) => <Card key={id} isGrey hasShadow="onCard" noLink>
+                  <Card.ImageThumb>
+                    <img src={image} alt={alt} width="400" height="266" />
+                  </Card.ImageThumb>
+                  <Card.Header>{title}</Card.Header>
+                  <Card.Body>
+                    <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+                  </Card.Body>
+                  <Card.Footer isType="button" buttonStyle="red">
+                    <a href={link}>More info</a>
+                  </Card.Footer>
+                </Card>)}
+            </Column>
+          </Container>
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(x=(g=h.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const it=["SpotlightCardOptions"];export{h as SpotlightCardOptions,it as __namedExportsOrder,ot as default};
