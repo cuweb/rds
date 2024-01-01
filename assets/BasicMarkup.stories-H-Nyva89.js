4305ad0baabc36f186674e7565dbc80eef040006
troychaplin <troy.chaplin@gmail.com>
2024-01-01 21:42:48 +0000

    Deploying to gh-pages from @ cuweb/rds@a39a895fb77ad1f6c39537f0b3cdd7f79ff03aff 🚀

---
+++ assets/BasicMarkup.stories-H-Nyva89.js
@@ -0,0 +1,161 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{S as c}from"./Section-273f7e_u.js";import{M as m}from"./Main-6yTdoHTQ.js";import{F as d}from"./FooterDept-6og8OxZg.js";import{T as t}from"./TopBar-xywG3lse.js";import{F as p}from"./FooterStandard-vo3aCiQg.js";import{F as h}from"./FooterCookie-UX1Mn_Bd.js";import{S as g}from"./StackedList-sQ1MOSVA.js";import{T as l}from"./TextImage-vRdqDVlO.js";import{Q as s}from"./Quote-pXFYqMay.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./globalClasses-EYtDdY2O.js";import"./Button-vGHki37U.js";import"./Icon-WcGhYRcv.js";import"./optionClasses-ccrz1VfO.js";import"./ChevronDownIcon-WJjzl-Lh.js";import"./ChevronRightIcon-XyhEQFMv.js";import"./menu-oFeCtKpM.js";import"./transition-8qPHkook.js";import"./use-root-containers-7zw4BiR7.js";import"./index-jmm5gWkb.js";import"./use-tracked-pointer-EZY5uaCR.js";import"./use-resolve-button-type-kFzmZbAv.js";import"./disclosure-LLQX-sEi.js";import"./MagnifyingGlassIcon-vfrtgMO8.js";import"./popover-tP4paLqk.js";import"./FooterLogoLinks-OsYLam8F.js";import"./Link-B65QTJUa.js";import"./iframe-x8eSWZYk.js";import"../sb-preview/runtime.js";import"./ButtonGroup-34C6VEr-.js";const W={title:"Examples/Layouts"},r=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."})}),i=()=>e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),j=()=>e.jsx(e.Fragment,{children:e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Lorem ipsum dolor"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Nunc viverra lorem arcu"})}),e.jsxs("li",{children:[e.jsx("a",{href:"http://carleton.ca",children:"Consectetur elit"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Suspendisse risus."})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Nullam ut nunc"})}),e.jsxs("li",{children:[e.jsx("a",{href:"http://carleton.ca",children:"Integer mattis"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Phasellus porttitor"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Praesent ante massa"})})]})]}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Etiam non convallis"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Vestibulum"})})]})]}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Turpis placerat"})}),e.jsxs("li",{children:[e.jsx("a",{href:"http://carleton.ca",children:"Consectetur elit"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Mauris feugiat interdum"})}),e.jsx("li",{children:e.jsx("a",{href:"http://carleton.ca",children:"Etiam non convallis"})})]})]})]})}),n={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(t,{children:e.jsxs(t.Primary,{children:[e.jsx(t.Logo,{link:"https://carleton.ca/webservices",children:e.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[e.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),e.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),e.jsx(t.Aside,{})]})}),e.jsx(m,{children:e.jsxs(c,{hasProse:!0,children:[e.jsx(l,{children:e.jsx(l.Content,{headerType:"h1",title:"Basic Markup",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})}),e.jsx("h2",{children:"Heading Two"}),e.jsx(r,{}),e.jsx(g,{header:"Table of Contents",as:"div",cols:"1",listType:"toc",offset:"right",hasShadow:!0,children:e.jsx(j,{})}),e.jsx(r,{}),e.jsx(r,{}),e.jsx("h3",{children:"Heading Three"}),e.jsx(r,{}),e.jsx("h4",{children:"Heading Four"}),e.jsx(i,{}),e.jsxs("ul",{children:[e.jsx("li",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsxs("li",{children:["Suspendisse ",e.jsx("a",{href:"https://cuweb.github.io/rds/",children:"velit eget suscipit tincidunt"})," vel orci vulputate, eget vulputate neque porttitor."]}),e.jsxs("li",{children:["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",e.jsxs("ul",{children:[e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsxs("li",{children:["Aenean sit amet tortor ",e.jsx("a",{href:"https://cuweb.github.io/rds/",children:"velit eget suscipit tincidunt"})," vitae, sagittis justo.",e.jsxs("ul",{children:[e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."})]})]}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."})]})]}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("li",{children:"Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."})]}),e.jsx(i,{}),e.jsxs("ol",{children:[e.jsx("li",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsx("li",{children:"Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor."}),e.jsxs("li",{children:["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",e.jsxs("ol",{children:[e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsxs("li",{children:["Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.",e.jsxs("ol",{children:[e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."})]})]}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("li",{children:"Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo."})]})]}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."}),e.jsx("li",{children:"Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor."}),e.jsx("li",{children:"Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim."})]}),e.jsx(i,{}),e.jsx(s,{cite:"John Doe",children:e.jsx("p",{children:"Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsx(i,{}),e.jsx(s,{cite:"John Doe",graphic:"quote",children:e.jsx("p",{children:"Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsx(i,{}),e.jsx(s,{cite:"John Doe",isCenter:!0,children:e.jsx("p",{children:"Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsx(i,{}),e.jsx(s,{cite:"John Doe",graphic:"quote",isCenter:!0,children:e.jsx("p",{children:"Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsx(i,{})]})}),e.jsx(d,{buildingName:"Pigiarvik (ᐱᒋᐊᕐᕕᒃ)",deptName:"Information Technology Services",email:"noreply@carleton.ca",footerButtons:[{id:1,title:"Contact support",url:"#"},{id:2,title:"Recent alerts",url:"#"}],officeNumber:"400",phone:"613-520-2600"}),e.jsx(p,{}),e.jsx(h,{})]})};var u,a,o;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <TextImage>
+            <TextImage.Content headerType="h1" title="Basic Markup">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+
+          <h2>Heading Two</h2>
+          <SinglePara />
+
+          <StackedList header="Table of Contents" as="div" cols="1" listType="toc" offset="right" hasShadow>
+            <ToC />
+          </StackedList>
+
+          <SinglePara />
+
+          <SinglePara />
+
+          <h3>Heading Three</h3>
+          <SinglePara />
+
+          <h4>Heading Four</h4>
+          <DoublePara />
+
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere
+              tellus vitae, sagittis justo.
+            </li>
+            <li>
+              Suspendisse <a href="https://cuweb.github.io/rds/">velit eget suscipit tincidunt</a> vel orci vulputate,
+              eget vulputate neque porttitor.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              <ul>
+                <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+                <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                <li>
+                  Aenean sit amet tortor <a href="https://cuweb.github.io/rds/">velit eget suscipit tincidunt</a> vitae,
+                  sagittis justo.
+                  <ul>
+                    <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+                    <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                    <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+                    <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                  </ul>
+                </li>
+                <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+              </ul>
+            </li>
+            <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+            <li>Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor.</li>
+            <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+          </ul>
+
+          <DoublePara />
+
+          <ol>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere
+              tellus vitae, sagittis justo.
+            </li>
+            <li>Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor.</li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              <ol>
+                <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+                <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                <li>
+                  Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.
+                  <ol>
+                    <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+                    <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                    <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+                    <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                  </ol>
+                </li>
+                <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+                <li>Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo.</li>
+              </ol>
+            </li>
+            <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+            <li>Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor.</li>
+            <li>Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim.</li>
+          </ol>
+
+          <DoublePara />
+
+          <Quote cite="John Doe">
+            <p>
+              Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+              reprehenderit ducimus.
+            </p>
+          </Quote>
+
+          <DoublePara />
+
+          <Quote cite="John Doe" graphic="quote">
+            <p>
+              Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+              reprehenderit ducimus.
+            </p>
+          </Quote>
+
+          <DoublePara />
+
+          <Quote cite="John Doe" isCenter>
+            <p>
+              Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+              reprehenderit ducimus.
+            </p>
+          </Quote>
+
+          <DoublePara />
+
+          <Quote cite="John Doe" graphic="quote" isCenter>
+            <p>
+              Obis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+              reprehenderit ducimus.
+            </p>
+          </Quote>
+
+          <DoublePara />
+        </Section>
+      </Main>
+
+      <FooterDept buildingName="Pigiarvik (ᐱᒋᐊᕐᕕᒃ)" deptName="Information Technology Services" email="noreply@carleton.ca" footerButtons={[{
+      id: 1,
+      title: 'Contact support',
+      url: '#'
+    }, {
+      id: 2,
+      title: 'Recent alerts',
+      url: '#'
+    }]} officeNumber="400" phone="613-520-2600" />
+
+      <FooterStandard />
+      <FooterCookie />
+    </>
+}`,...(o=(a=n.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const X=["BasicMarkup"];export{n as BasicMarkup,X as __namedExportsOrder,W as default};
---
+++ assets/VideoCards.stories-BqbSKYV0.js
@@ -0,0 +1,143 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{S as p}from"./Section-273f7e_u.js";import{C as a}from"./Container-gVUWVoze.js";import{M as x}from"./Main-6yTdoHTQ.js";import{F as j}from"./FooterBasic-AOkxO_81.js";import{C as s}from"./Column-YPGoCv6X.js";import{T as c}from"./TopBar-xywG3lse.js";import{H as t}from"./Heading-e9K0UJNj.js";import{C as r}from"./Card-f0EwnAjj.js";import{a as d}from"./VideoData-ZDqbeZG0.js";import{T as m}from"./TextImage-vRdqDVlO.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./globalClasses-EYtDdY2O.js";import"./FooterLogoLinks-OsYLam8F.js";import"./Link-B65QTJUa.js";import"./iframe-x8eSWZYk.js";import"../sb-preview/runtime.js";import"./optionClasses-ccrz1VfO.js";import"./ChevronDownIcon-WJjzl-Lh.js";import"./ChevronRightIcon-XyhEQFMv.js";import"./menu-oFeCtKpM.js";import"./transition-8qPHkook.js";import"./use-root-containers-7zw4BiR7.js";import"./index-jmm5gWkb.js";import"./use-tracked-pointer-EZY5uaCR.js";import"./use-resolve-button-type-kFzmZbAv.js";import"./disclosure-LLQX-sEi.js";import"./MagnifyingGlassIcon-vfrtgMO8.js";import"./popover-tP4paLqk.js";import"./index-bkt57PaO.js";import"./isNativeReflectConstruct-3LOYyi5T.js";import"./index-J1scOhqY.js";import"./index-t850Cm8s.js";import"./index-VFMbf6KQ.js";import"./Button-vGHki37U.js";import"./Icon-WcGhYRcv.js";import"./ButtonGroup-34C6VEr-.js";const ne={title:"Examples/Blocks"},l={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(c,{children:e.jsxs(c.Primary,{children:[e.jsx(c.Logo,{link:"https://carleton.ca/webservices",children:e.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[e.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),e.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),e.jsx(c.Aside,{})]})}),e.jsx(x,{children:e.jsxs(p,{hasProse:!0,children:[e.jsx(m,{hasBorder:!0,children:e.jsx(m.Content,{headerType:"h1",title:"Video Cards",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})}),e.jsxs(a,{children:[e.jsx(t,{text:"Video Cards: Default"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{noLink:!0,children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]}),e.jsxs(a,{children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{noLink:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]}),e.jsxs(a,{children:[e.jsx(t,{text:"Video Cards: Default with Grey Bg"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{isGrey:!0,noLink:!0,children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]}),e.jsxs(a,{children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{isGrey:!0,noLink:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]}),e.jsxs(a,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Default"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{noLink:!0,children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]}),e.jsxs(a,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{noLink:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]}),e.jsxs(a,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Default with Grey Bg"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{isGrey:!0,noLink:!0,children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]}),e.jsxs(a,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:n,source:o,title:i})=>e.jsxs(r,{isGrey:!0,noLink:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:o}),e.jsx(r.Header,{children:i})]},n))})]})]})}),e.jsx(j,{})]})};var u,h,C;l.parameters={...l.parameters,docs:{...(u=l.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <TextImage hasBorder>
+            <TextImage.Content headerType="h1" title="Video Cards">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+
+          <Container>
+            <Heading text="Video Cards: Default" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noLink>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noLink hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container>
+            <Heading text="Video Cards: Default with Grey Bg" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noLink>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noLink hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Default" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noLink>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noLink hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Default with Grey Bg" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noLink>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noLink hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(C=(h=l.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};const oe=["VideoCardOptions"];export{l as VideoCardOptions,oe as __namedExportsOrder,ne as default};
