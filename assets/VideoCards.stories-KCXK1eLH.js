808fe655dc3933995e8b2054fbbee506be322cb2
troychaplin <troy.chaplin@gmail.com>
2024-01-03 03:33:43 +0000

    Deploying to gh-pages from @ cuweb/rds@8a8f205a6025fd8dd88f3aed359681bf24e414f4 🚀

---
+++ assets/VideoCards.stories-KCXK1eLH.js
@@ -0,0 +1,143 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{S as p}from"./Section-273f7e_u.js";import{C as i}from"./Container-gVUWVoze.js";import{M as x}from"./Main-6yTdoHTQ.js";import{F as j}from"./FooterBasic-EGOz7aMM.js";import{C as s}from"./Column-IeRseL1O.js";import{T as c}from"./TopBar-xywG3lse.js";import{H as t}from"./Heading-3VgvRISV.js";import{C as r}from"./Card-TJG89dGg.js";import{a as d}from"./VideoData-ZDqbeZG0.js";import{T as m}from"./TextImage-SerwNjlw.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./globalClasses-EYtDdY2O.js";import"./FooterLogoLinks-uIwJxS_i.js";import"./Link-1eaFKy-m.js";import"./iframe-DpMmBQfO.js";import"../sb-preview/runtime.js";import"./optionClasses-BVitpQvL.js";import"./ChevronDownIcon-WJjzl-Lh.js";import"./ChevronRightIcon-XyhEQFMv.js";import"./menu-oFeCtKpM.js";import"./transition-8qPHkook.js";import"./use-root-containers-7zw4BiR7.js";import"./index-jmm5gWkb.js";import"./use-tracked-pointer-EZY5uaCR.js";import"./use-resolve-button-type-kFzmZbAv.js";import"./disclosure-LLQX-sEi.js";import"./MagnifyingGlassIcon-vfrtgMO8.js";import"./popover-tP4paLqk.js";import"./index-bkt57PaO.js";import"./isNativeReflectConstruct-3LOYyi5T.js";import"./index-J1scOhqY.js";import"./index-YmVCX52A.js";import"./index-VFMbf6KQ.js";import"./Button-NlEATDV2.js";import"./Icon-JTOhHsqE.js";import"./ButtonGroup-34C6VEr-.js";const oe={title:"Examples/Blocks"},l={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(c,{children:e.jsxs(c.Primary,{children:[e.jsx(c.Logo,{link:"https://carleton.ca/webservices",children:e.jsxs("a",{href:"https://carleton.ca",className:"cu-topbar--logo",children:[e.jsx("img",{className:"culogo",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg",width:"130",height:"35",alt:"Logo"}),e.jsx("img",{className:"cushield",src:"https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg",width:"28",height:"35",alt:"Logo"})]})}),e.jsx(c.Aside,{})]})}),e.jsx(x,{children:e.jsxs(p,{hasProse:!0,children:[e.jsx(m,{hasBorder:!0,children:e.jsx(m.Content,{headerType:"h1",title:"Video Cards",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})}),e.jsxs(i,{children:[e.jsx(t,{text:"Video Cards: Default"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{noHover:!0,children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]}),e.jsxs(i,{children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{noHover:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]}),e.jsxs(i,{children:[e.jsx(t,{text:"Video Cards: Default with Grey Bg"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{isGrey:!0,noHover:!0,children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]}),e.jsxs(i,{children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{isGrey:!0,noHover:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]}),e.jsxs(i,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Default"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{noHover:!0,children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]}),e.jsxs(i,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{noHover:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]}),e.jsxs(i,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Default with Grey Bg"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{isGrey:!0,noHover:!0,children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]}),e.jsxs(i,{isGrey:!0,children:[e.jsx(t,{text:"Video Cards: Shadow on Static"}),e.jsx(s,{cols:"3",children:d.slice(0,3).map(({id:o,source:n,title:a})=>e.jsxs(r,{isGrey:!0,noHover:!0,hasShadow:"onCard",children:[e.jsx(r.Video,{source:n}),e.jsx(r.Header,{children:a})]},o))})]})]})}),e.jsx(j,{})]})};var u,h,C;l.parameters={...l.parameters,docs:{...(u=l.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <>
+      <TopBar>
+        <TopBar.Primary>
+          <TopBar.Logo link="https://carleton.ca/webservices">
+            <a href="https://carleton.ca" className="cu-topbar--logo">
+              <img className="culogo" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-logo-color-right-horiztonal.svg" width="130" height="35" alt="Logo" />
+              <img className="cushield" src="https://cu-production.s3.amazonaws.com/rds/assets/cu-logos/cu-shield-color.svg" width="28" height="35" alt="Logo" />
+            </a>
+          </TopBar.Logo>
+          <TopBar.Aside />
+        </TopBar.Primary>
+      </TopBar>
+
+      <Main>
+        <Section hasProse>
+          <TextImage hasBorder>
+            <TextImage.Content headerType="h1" title="Video Cards">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+
+          <Container>
+            <Heading text="Video Cards: Default" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noHover>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noHover hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container>
+            <Heading text="Video Cards: Default with Grey Bg" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noHover>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noHover hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Default" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noHover>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} noHover hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Default with Grey Bg" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noHover>
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Video Cards: Shadow on Static" />
+            <Column cols="3">
+              {VideoData.slice(0, 3).map(({
+              id,
+              source,
+              title
+            }) => <Card key={id} isGrey noHover hasShadow="onCard">
+                  <Card.Video source={source} />
+                  <Card.Header>{title}</Card.Header>
+                </Card>)}
+            </Column>
+          </Container>
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(C=(h=l.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};const ne=["VideoCardOptions"];export{l as VideoCardOptions,ne as __namedExportsOrder,oe as default};
