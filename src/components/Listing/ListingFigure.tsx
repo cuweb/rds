ecb684f9e281c24408d6549f702725640ca6e9d6
Troy Chaplin <troy.chaplin@gmail.com>
2024-01-05 11:05:17 -0500

    add: square image option to listings

--- src/components/Listing/ListingFigure.tsx
+++ src/components/Listing/ListingFigure.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export interface ListingFigureProps {
   children: React.ReactNode
+  isSquare?: boolean
 }
 
-export const ListingFigure = ({ children }: ListingFigureProps) => {
-  return <figure className="flex-none max-w-[45%] @xl:md:max-w-[25%] aspect-[4/2.7]">{children}</figure>
+export const ListingFigure = ({ children, isSquare }: ListingFigureProps) => {
+  const imageStyles = isSquare ? 'aspect-square' : 'aspect-square'
+
+  return <figure className={`flex-none max-w-[45%] @xl:md:max-w-[25%] ${imageStyles}`}>{children}</figure>
 }
 
 ListingFigure.displayName = 'Listing.Figure'
