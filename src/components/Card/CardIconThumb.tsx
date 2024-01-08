18e3fc2c2031a1f1673d9c05943db69fc5837366
Troy Chaplin <troy.chaplin@gmail.com>
2024-01-08 16:50:55 -0500

    change: icon thumb alt

--- src/components/Card/CardIconThumb.tsx
+++ src/components/Card/CardIconThumb.tsx
@@ -9,14 +9,6 @@ export const CardIconThumb = ({ icon, bgType = 'red', hasShadow }: CardIconThumb
   const iconPath = `${cdnPath}${icon}.svg`
   const iconShadow = hasShadow ? 'shadow-md' : ''
 
-  const whiteIcon = {
-    filter: 'invert(100%) sepia(5%) saturate(0%) hue-rotate(29deg) brightness(106%) contrast(107%)',
-  }
-
-  const redIcon = {
-    filter: 'invert(20%) sepia(50%) saturate(7177%) hue-rotate(348deg) brightness(91%) contrast(100%)',
-  }
-
   // Set classes on icon and wrapping div
   let iconDiv
 
@@ -35,11 +27,24 @@ export const CardIconThumb = ({ icon, bgType = 'red', hasShadow }: CardIconThumb
       break
   }
 
+  // Use filter to change svg as image to white
+  const whiteIcon = {
+    filter: 'invert(100%) sepia(5%) saturate(0%) hue-rotate(29deg) brightness(106%) contrast(107%)',
+  }
+
+  // Use filter to change svg as image to red
+  const redIcon = {
+    filter: 'invert(20%) sepia(50%) saturate(7177%) hue-rotate(348deg) brightness(91%) contrast(100%)',
+  }
+
+  // Remove dashes from icon name
+  const iconAlt = icon ? icon.replace(/-/g, ' ') : ''
+
   return (
     <figure className={`flex items-center justify-center mx-6 mt-6 mb-2 overflow-hidden ${iconDiv} ${iconShadow}`}>
       <img
         src={iconPath}
-        alt={icon}
+        alt={`An icon of a ${iconAlt}`}
         className={`cu-icon-thumb ${bgType === 'none' ? '' : 'p-3'}`}
         style={bgType === 'red' ? whiteIcon : redIcon}
       />
