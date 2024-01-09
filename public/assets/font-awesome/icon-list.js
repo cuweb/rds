a98a814965bb79ee9a567a1e0e013e3e34fbf572
Troy Chaplin <troy.chaplin@gmail.com>
2024-01-08 23:31:47 -0500

    change: icon list

--- public/assets/font-awesome/icon-list.js
+++ public/assets/font-awesome/icon-list.js
@@ -1,8 +1,18 @@
 export const iconNames = [
   {"value":"album-collection","label":"Album collection"},
   {"value":"boombox","label":"Boombox"},
+  {"value":"facebook","label":"Facebook"},
   {"value":"headphones","label":"Headphones"},
+  {"value":"instagram","label":"Instagram"},
+  {"value":"linkedin-in","label":"Linkedin in"},
+  {"value":"linkedin","label":"Linkedin"},
   {"value":"saxophone","label":"Saxophone"},
+  {"value":"square-facebook","label":"Square facebook"},
+  {"value":"square-instagram","label":"Square instagram"},
+  {"value":"square-twitter","label":"Square twitter"},
+  {"value":"square-youtube","label":"Square youtube"},
   {"value":"trumpet","label":"Trumpet"},
-  {"value":"turntable","label":"Turntable"}
+  {"value":"turntable","label":"Turntable"},
+  {"value":"twitter","label":"Twitter"},
+  {"value":"youtube","label":"Youtube"}
 ];
