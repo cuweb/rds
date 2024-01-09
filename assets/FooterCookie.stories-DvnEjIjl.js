3bfee3f486a9913938a83ed192b5734f99707bfc
troychaplin <troy.chaplin@gmail.com>
2024-01-09 16:03:57 +0000

    Deploying to gh-pages from @ cuweb/rds@e11ceb1c216497dcfeda894e597ce2d17007e08f 🚀

---
+++ assets/FooterCookie.stories-DvnEjIjl.js
@@ -0,0 +1,3 @@
+import{j as a}from"./jsx-runtime-vNq4Oc-g.js";import{F as s}from"./FooterCookie-PtA_lY_b.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./Button-NlEATDV2.js";import"./Icon-JTOhHsqE.js";import"./optionClasses-BVitpQvL.js";import"./ChevronDownIcon-WJjzl-Lh.js";const f={title:"Components/Footers/Cookie",component:s,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},o=()=>a.jsx(s,{});o.storyName="Default FooterCookie Banner";var r,t,e;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  return <FooterCookie />;
+}`,...(e=(t=o.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const F=["Default"];export{o as Default,F as __namedExportsOrder,f as default};
