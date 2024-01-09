ee73ab61ab42c9518c6da9d06de2a16a3cc2eb3f
Troy Chaplin <troy.chaplin@gmail.com>
2024-01-08 23:04:00 -0500

    add: icon list

--- public/assets/font-awesome/icon-list.js
+++ public/assets/font-awesome/icon-list.js
@@ -1,8 +1,8 @@
 export const iconNames = [
-  { "value": "album-collection", "label": "Album collection" },
-  { "value": "boombox", "label": "Boombox" },
-  { "value": "headphones", "label": "Headphones" },
-  { "value": "saxophone", "label": "Saxophone" },
-  { "value": "trumpet", "label": "Trumpet" },
-  { "value": "turntable", "label": "Turntable" }
+  {"value":"album-collection","label":"Album collection"},
+  {"value":"boombox","label":"Boombox"},
+  {"value":"headphones","label":"Headphones"},
+  {"value":"saxophone","label":"Saxophone"},
+  {"value":"trumpet","label":"Trumpet"},
+  {"value":"turntable","label":"Turntable"}
 ];
