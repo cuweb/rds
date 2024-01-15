07308514f856e5b8b9974526539557d5dd5e7317
Troy Chaplin <troy.chaplin@gmail.com>
2024-01-15 12:31:01 -0500

    change: privacy statement and link

--- src/components/WideImage/WideImageSignup.tsx
+++ src/components/WideImage/WideImageSignup.tsx
@@ -10,13 +10,13 @@ export const WideImageSignup = ({ buttonText = 'Submit Email' }: WideImageSignup
   return (
     <div className="w-full max-w-xl space-y-5 cu-wideimage-signup">
       <div className="flex gap-2.5">
-        <label htmlFor="optin" className="sr-only">
+        <label htmlFor="email" className="sr-only">
           Enter you email address
         </label>
         <input
           type="text"
-          id="name"
-          name="name"
+          id="email"
+          name="email"
           required
           placeholder="Enter you email address"
           className="flex-1 rounded px-3.5 py-2 focus:ring-1 focus:ring-inset"
@@ -24,18 +24,20 @@ export const WideImageSignup = ({ buttonText = 'Submit Email' }: WideImageSignup
         <Button title={buttonText || 'Subscribe'} />
       </div>
 
-      <div className="flex items-center flex-none gap-2 ml-1 text-left">
-        <input name="favcharacters" id="optin" type="checkbox" className="border rounded-sm" value="name" />
-        <label htmlFor="optin" className="text-sm">
-          I agree to receive email from Carleton University.
-        </label>
-        {/* <label htmlFor="optin" className="text-sm">
+      <div className="flex items-start flex-none gap-2 ml-1 text-left">
+        <input name="favcharacters" id="optin" type="checkbox" className="mt-0.5 border rounded-sm" value="name" />
+        <label htmlFor="optin" className="text-sm font-light">
           I agree to receive email from Carleton University. Read our{' '}
-          <a href="www.carleton.ca" style={{ textDecoration: 'underline' }}>
-            Privacy Policy
-          </a>{' '}
-          to learn more.
-        </label> */}
+          <a
+            className="font-bold border-b border-white border-px"
+            href="https://carleton.ca/privacy/privacy-notices/general-notice-of-collection-use-and-disclosure/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Privacy Statement
+          </a>
+          .
+        </label>
       </div>
     </div>
   )
