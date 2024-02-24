d32f17e4684ddbe592e80506e7e671859f303bab
Troy Chaplin <troy.chaplin@gmail.com>
2024-02-23 20:42:07 -0500

    add: as prop to set h1, h2, h3

--- src/components/PageHeaders/PageHeaders.tsx
+++ src/components/PageHeaders/PageHeaders.tsx
@@ -5,16 +5,19 @@ import { PageHeadersEvent } from './PageHeadersEvent'
 export interface PageHeadersProps {
   children?: React.ReactNode
   header: string
+  as?: 'h1' | 'h2' | 'h3'
   pronoun?: string
 }
 
-export const PageHeadersWrapper = ({ children, header, pronoun }: PageHeadersProps) => {
+export const PageHeadersWrapper = ({ children, header, as = 'h1', pronoun }: PageHeadersProps) => {
+  const HeaderComponent = as
+
   return (
     <>
-      <h1 className="font-semibold text-3xl md:text-4xl lg:text-5xl lg:leading-[3.5rem] text-cu-black-700 not-prose relative mb-6 after:absolute after:w-10 after:h-px after:bottom-0 after:bg-cu-red after:left-px pb-6">
+      <HeaderComponent className="font-semibold text-3xl md:text-4xl lg:text-5xl lg:leading-[3.5rem] text-cu-black-700 not-prose relative mb-6 after:absolute after:w-10 after:h-px after:bottom-0 after:bg-cu-red after:left-px pb-6">
         {header}{' '}
         {pronoun && <span className="text-xl font-light lowercase lg:text-3xl text-cu-black-500">({pronoun})</span>}
-      </h1>
+      </HeaderComponent>
       {children}
     </>
   )
