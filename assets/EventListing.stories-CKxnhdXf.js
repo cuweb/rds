b0a9c82f055361e5d2835b187216418d8b73240d
troychaplin <troy.chaplin@gmail.com>
2024-02-26 17:16:17 +0000

    Deploying to gh-pages from @ cuweb/rds@a05cbb863b332738acc87b744a8eef4a165763ad 🚀

---
+++ assets/EventListing.stories-CKxnhdXf.js
@@ -0,0 +1,207 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{S as D}from"./Section-CVJtbDHu.js";import{C as h}from"./Container-CMmfm5fp.js";import{M as _}from"./Main-CJHjqGYv.js";import{F as j}from"./FooterStandard-BQm2B3eI.js";import{S as d}from"./StackedList-DGrgHzif.js";import{N as c,a as P,b as E}from"./NavData-BKlm5Tye.js";import{H as p}from"./Heading-C_7F1f88.js";import{L as t}from"./Listing-DATyQjq4.js";import{T as x}from"./TextImage-CbXbQHYg.js";import{a as v}from"./EventData-Suzp3opJ.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./globalClasses-GmQrI9Vi.js";import"./FooterLogoLinks-sEvdyT2V.js";import"./useLinkContext-B6ulR7Vg.js";import"./optionClasses-nRtJeGr9.js";import"./Avatar-DjV_X37D.js";import"./parse-BfiLtjV-.js";import"./getDate-BAl1yPiw.js";import"./ChevronRightIcon-CNwURrwD.js";const Q={title:"Examples/Blocks"},g={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(c,{children:e.jsxs(c.Primary,{children:[e.jsx(c.Logo,{title:"Web Services",link:"https://carleton.ca/webservices"}),e.jsx(c.Menu,{menu:P}),e.jsx(c.Aside,{menu:E})]})}),e.jsx(_,{children:e.jsxs(D,{hasProse:!0,children:[e.jsx(x,{hasBorder:!0,children:e.jsx(x.Content,{headerType:"h1",title:"Event Listing",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})}),e.jsxs(h,{children:[e.jsx(p,{text:"Event Listings: Default"}),e.jsx(d,{hasShadow:!0,children:v.slice(0,4).map(({id:s,title:a,link:u,startDate:i,endDate:n,on_campus:r,on_campus_building:o,on_campus_room_number:l,event_address:m})=>e.jsxs(t,{children:[e.jsx(t.DateThumb,{startDate:i,endDate:n}),e.jsxs(t.Body,{children:[e.jsx(t.Header,{children:a}),e.jsx(t.EventMeta,{startDateTime:i,endDateTime:n,onCampus:r,onCampusBuilding:o,onCampusRoomNumber:l,eventAddress:m}),e.jsx(t.Footer,{isType:"button",children:e.jsx("a",{href:u,children:"Event details"})})]})]},s))})]}),e.jsxs(h,{children:[e.jsx(p,{text:"Event Listings: One Column"}),e.jsx(d,{cols:"1",hasShadow:!0,children:v.slice(0,4).map(({id:s,title:a,link:u,startDate:i,endDate:n,on_campus:r,on_campus_building:o,on_campus_room_number:l,event_address:m})=>e.jsxs(t,{children:[e.jsx(t.DateThumb,{startDate:i,endDate:n}),e.jsxs(t.Body,{children:[e.jsx(t.Header,{children:a}),e.jsx(t.EventMeta,{startDateTime:i,endDateTime:n,onCampus:r,onCampusBuilding:o,onCampusRoomNumber:l,eventAddress:m}),e.jsx(t.Footer,{isType:"button",children:e.jsx("a",{href:u,children:"Event details"})})]})]},s))})]}),e.jsxs(h,{isGrey:!0,children:[e.jsx(p,{text:"Event Listings: Default with Grey Bg"}),e.jsx(d,{hasShadow:!0,children:v.slice(0,4).map(({id:s,title:a,link:u,startDate:i,endDate:n,on_campus:r,on_campus_building:o,on_campus_room_number:l,event_address:m})=>e.jsxs(t,{children:[e.jsx(t.DateThumb,{startDate:i,endDate:n}),e.jsxs(t.Body,{children:[e.jsx(t.Header,{children:a}),e.jsx(t.EventMeta,{startDateTime:i,endDateTime:n,onCampus:r,onCampusBuilding:o,onCampusRoomNumber:l,eventAddress:m}),e.jsx(t.Footer,{isType:"button",children:e.jsx("a",{href:u,children:"Event details"})})]})]},s))})]}),e.jsxs(h,{isGrey:!0,children:[e.jsx(p,{text:"Event Listings: One Column with Grey Bg"}),e.jsx(d,{cols:"1",hasShadow:!0,children:v.slice(0,4).map(({id:s,title:a,link:u,startDate:i,endDate:n,on_campus:r,on_campus_building:o,on_campus_room_number:l,event_address:m})=>e.jsxs(t,{children:[e.jsx(t.DateThumb,{startDate:i,endDate:n}),e.jsxs(t.Body,{children:[e.jsx(t.Header,{children:a}),e.jsx(t.EventMeta,{startDateTime:i,endDateTime:n,onCampus:r,onCampusBuilding:o,onCampusRoomNumber:l,eventAddress:m}),e.jsx(t.Footer,{isType:"button",children:e.jsx("a",{href:u,children:"Event details"})})]})]},s))})]}),e.jsx(p,{text:"Event Listings: Offset"}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx(d,{header:"Recent Event",as:"ul",cols:"1",listType:"posts",offset:"right",hasShadow:!0,children:v.slice(0,3).map(({id:s,title:a,link:u,startDate:i,endDate:n,on_campus:r,on_campus_building:o,on_campus_room_number:l,event_address:m})=>e.jsxs(t,{children:[e.jsx(t.DateThumb,{startDate:i,endDate:n}),e.jsxs(t.Body,{children:[e.jsx(t.Header,{children:a}),e.jsx(t.EventMeta,{startDateTime:i,endDateTime:n,onCampus:r,onCampusBuilding:o,onCampusRoomNumber:l,eventAddress:m}),e.jsx(t.Footer,{isType:"button",children:e.jsx("a",{href:u,children:"Event details"})})]})]},s))}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})]})}),e.jsx(j,{})]})};var q,L,b;g.parameters={...g.parameters,docs:{...(q=g.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Primary>
+          <Nav.Logo title="Web Services" link="https://carleton.ca/webservices" />
+          <Nav.Menu menu={NavDataSingle} />
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Primary>
+      </Nav>
+
+      <Main>
+        <Section hasProse>
+          <TextImage hasBorder>
+            <TextImage.Content headerType="h1" title="Event Listing">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+
+          <Container>
+            <Heading text="Event Listings: Default" />
+            <StackedList hasShadow>
+              {EventData.slice(0, 4).map(({
+              id,
+              title,
+              link,
+              startDate,
+              endDate,
+              on_campus,
+              on_campus_building,
+              on_campus_room_number,
+              event_address
+            }) => <Listing key={id}>
+                    <Listing.DateThumb startDate={startDate} endDate={endDate} />
+                    <Listing.Body>
+                      <Listing.Header>{title}</Listing.Header>
+                      <Listing.EventMeta startDateTime={startDate} endDateTime={endDate} onCampus={on_campus} onCampusBuilding={on_campus_building} onCampusRoomNumber={on_campus_room_number} eventAddress={event_address} />
+                      <Listing.Footer isType="button">
+                        <a href={link}>Event details</a>
+                      </Listing.Footer>
+                    </Listing.Body>
+                  </Listing>)}
+            </StackedList>
+          </Container>
+
+          <Container>
+            <Heading text="Event Listings: One Column" />
+            <StackedList cols="1" hasShadow>
+              {EventData.slice(0, 4).map(({
+              id,
+              title,
+              link,
+              startDate,
+              endDate,
+              on_campus,
+              on_campus_building,
+              on_campus_room_number,
+              event_address
+            }) => <Listing key={id}>
+                    <Listing.DateThumb startDate={startDate} endDate={endDate} />
+                    <Listing.Body>
+                      <Listing.Header>{title}</Listing.Header>
+                      <Listing.EventMeta startDateTime={startDate} endDateTime={endDate} onCampus={on_campus} onCampusBuilding={on_campus_building} onCampusRoomNumber={on_campus_room_number} eventAddress={event_address} />
+                      <Listing.Footer isType="button">
+                        <a href={link}>Event details</a>
+                      </Listing.Footer>
+                    </Listing.Body>
+                  </Listing>)}
+            </StackedList>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Event Listings: Default with Grey Bg" />
+            <StackedList hasShadow>
+              {EventData.slice(0, 4).map(({
+              id,
+              title,
+              link,
+              startDate,
+              endDate,
+              on_campus,
+              on_campus_building,
+              on_campus_room_number,
+              event_address
+            }) => <Listing key={id}>
+                    <Listing.DateThumb startDate={startDate} endDate={endDate} />
+                    <Listing.Body>
+                      <Listing.Header>{title}</Listing.Header>
+                      <Listing.EventMeta startDateTime={startDate} endDateTime={endDate} onCampus={on_campus} onCampusBuilding={on_campus_building} onCampusRoomNumber={on_campus_room_number} eventAddress={event_address} />
+                      <Listing.Footer isType="button">
+                        <a href={link}>Event details</a>
+                      </Listing.Footer>
+                    </Listing.Body>
+                  </Listing>)}
+            </StackedList>
+          </Container>
+
+          <Container isGrey>
+            <Heading text="Event Listings: One Column with Grey Bg" />
+            <StackedList cols="1" hasShadow>
+              {EventData.slice(0, 4).map(({
+              id,
+              title,
+              link,
+              startDate,
+              endDate,
+              on_campus,
+              on_campus_building,
+              on_campus_room_number,
+              event_address
+            }) => <Listing key={id}>
+                    <Listing.DateThumb startDate={startDate} endDate={endDate} />
+                    <Listing.Body>
+                      <Listing.Header>{title}</Listing.Header>
+                      <Listing.EventMeta startDateTime={startDate} endDateTime={endDate} onCampus={on_campus} onCampusBuilding={on_campus_building} onCampusRoomNumber={on_campus_room_number} eventAddress={event_address} />
+                      <Listing.Footer isType="button">
+                        <a href={link}>Event details</a>
+                      </Listing.Footer>
+                    </Listing.Body>
+                  </Listing>)}
+            </StackedList>
+          </Container>
+
+          <Heading text="Event Listings: Offset" />
+
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+
+          <StackedList header="Recent Event" as="ul" cols="1" listType="posts" offset="right" hasShadow>
+            {EventData.slice(0, 3).map(({
+            id,
+            title,
+            link,
+            startDate,
+            endDate,
+            on_campus,
+            on_campus_building,
+            on_campus_room_number,
+            event_address
+          }) => <Listing key={id}>
+                  <Listing.DateThumb startDate={startDate} endDate={endDate} />
+                  <Listing.Body>
+                    <Listing.Header>{title}</Listing.Header>
+                    <Listing.EventMeta startDateTime={startDate} endDateTime={endDate} onCampus={on_campus} onCampusBuilding={on_campus_building} onCampusRoomNumber={on_campus_room_number} eventAddress={event_address} />
+                    <Listing.Footer isType="button">
+                      <a href={link}>Event details</a>
+                    </Listing.Footer>
+                  </Listing.Body>
+                </Listing>)}
+          </StackedList>
+
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Nobis voluptatem dolorum et eum doloremque cupiditate
+            velit. Praesentium architecto a distinctio aut reprehenderit ducimus.
+          </p>
+        </Section>
+      </Main>
+
+      <FooterStandard />
+    </>
+}`,...(b=(L=g.parameters)==null?void 0:L.docs)==null?void 0:b.source}}};const U=["EventListing"];export{g as EventListing,U as __namedExportsOrder,Q as default};
--- assets/preview-CV4Hhn1D.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-Cib7kI5_.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DfQ8DoM6.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DfQ8DoM6.js","./chunk-HLWAVYOI-C4nkOerP.js","./iframe-Cib7kI5_.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-Cuzm583n.js","./index-Bw8VTzHM.js","./index-yajTA2Yq.js","./_baseIsEqual-C0VLQfVQ.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
