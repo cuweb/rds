361f13ba9abe6b54eebefd9b1816e86ef6b5d091
troychaplin <troychaplin@users.noreply.github.com>
2024-03-05 20:26:20 +0000

    Deploying to gh-pages from @ cuweb/rds@0d15f3a5df9c8e98d09c67536fd2e18b2c4d8846 🚀

---
+++ lib/components/Card/CardInitials.d.ts
@@ -0,0 +1,7 @@
+export interface CardInitialsProps {
+    initials: string;
+}
+export declare const CardInitials: {
+    ({ initials }: CardInitialsProps): import("react/jsx-runtime").JSX.Element;
+    displayName: string;
+};
