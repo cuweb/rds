52b207c3a8ea8964b3ddd61a2d4700bd32f1a20f
Bhoomi-Pipalia <56330259+Bhoomi-Pipalia@users.noreply.github.com>
2024-03-12 15:50:46 +0000

    Deploying to gh-pages from @ cuweb/rds@8c99197bc6d76c2280e9a1d0e11d62f9f1143474 🚀

--- assets/Checkbox.stories-qvZog-jE.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-CKrituN3.js";import{F as a}from"./Form-Cl68DKVS.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./XMarkIcon-iOOrpGul.js";import"./Button-DZIOirHR.js";import"./Icon-DPQQEofx.js";import"./optionClasses-nRtJeGr9.js";import"./ChevronDownIcon-DCf8Nrl_.js";import"./Calendar-TLP8IKaP.js";import"./startOfToday-C7FXxczV.js";import"./parse-BfiLtjV-.js";const k={title:"Deprecated/Forms/Checkbox",component:a,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},e={render:()=>r.jsx(a,{onSubmit:()=>{},schema:{},initialValues:s,children:r.jsx(a.Checkbox,{name:"favcharacters",label:"Who are your favourite 90s sitcom characters?",options:[{name:"name",label:"name",id:2}]})})},s={firstname:"ish",description:" sample description",regularviewer:"2",location:"Carleton University, Colonel By Drive, Ottawa, ON, Canada",favsitcom:"familymatter",favcharacters:["kellykapowski","carltonbanks"]};var o,t,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => <Form onSubmit={() => undefined} schema={{}} initialValues={InitialValues}>
-      <Form.Checkbox name="favcharacters" label="Who are your favourite 90s sitcom characters?" options={[{
-      name: 'name',
-      label: 'name',
-      id: 2
-    }]} />
-    </Form>
-}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const F=["Default"];export{e as Default,F as __namedExportsOrder,k as default};
