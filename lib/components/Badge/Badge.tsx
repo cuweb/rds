fa037ba5908be76607f8455b6d18720b2080bf38
Troy Chaplin <troy.chaplin@gmail.com>
2024-03-23 18:04:10 -0400

    change: refactored badges to have color, no-wrap and more spacing

--- lib/components/Badge/Badge.tsx
+++ lib/components/Badge/Badge.tsx
@@ -2,16 +2,41 @@ import React from 'react'
 
 export interface BadgeProps {
   children?: React.ReactNode
+  color?: 'green' | 'red' | 'yellow' | 'blue' | 'purple' | 'grey'
+  noWordBreak?: boolean
 }
 
-const BadgeBase = ({ children }: BadgeProps) => {
+export const Badge = ({ children, color = 'grey', noWordBreak = false }: BadgeProps) => {
+  const wordBreakClass = noWordBreak ? 'whitespace-nowrap' : ''
+
+  // Set color classes
+  let badgeColor
+
+  switch (color) {
+    case 'green':
+      badgeColor = 'bg-green-50 text-green-800'
+      break
+    case 'red':
+      badgeColor = 'bg-red-50 text-red-800'
+      break
+    case 'yellow':
+      badgeColor = 'bg-yellow-50 text-yellow-800'
+      break
+    case 'blue':
+      badgeColor = 'bg-blue-50 text-blue-800'
+      break
+    case 'purple':
+      badgeColor = 'bg-purple-50 text-purple-800'
+      break
+    default:
+      badgeColor = 'bg-cu-black-50 text-cu-black-800'
+      break
+  }
   return (
-    <p className="inline-flex px-2 mr-2 text-xs font-semibold cu-badge not-prose rounded-xl bg-cu-black-50 text-cu-black-900 last:mr-0">
+    <p
+      className={`inline-flex px-3 py-1 mr-2 text-xs font-semibold cu-badge not-prose rounded-full last:mr-0 ${badgeColor} ${wordBreakClass}`}
+    >
       {children}
     </p>
   )
 }
-
-BadgeBase.displayName = 'LinkItem'
-
-export const Badge = Object.assign(BadgeBase, {})
