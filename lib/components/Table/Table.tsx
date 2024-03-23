a73a63f97b5ac85e50f8822f30a4f82615a8e25c
Troy Chaplin <troy.chaplin@gmail.com>
2024-03-23 11:40:13 -0400

    add: not prose to table

--- lib/components/Table/Table.tsx
+++ lib/components/Table/Table.tsx
@@ -23,7 +23,7 @@ export const Table = ({ data, columns, hasStripes = false, hasShadow, range = [1
   const shadowStyle = hasShadow ? 'shadow-lg' : ''
 
   return (
-    <div className={`overflow-x-auto rounded-lg ${shadowStyle}`}>
+    <div className={`not-prose overflow-x-auto rounded-lg ${shadowStyle}`}>
       <table className="min-w-full table-auto cu-table">
         <TableHeader columns={columns} sortData={setTableData} />
         <TableRows data={tableData} columns={columns} striped={hasStripes} range={range} />
