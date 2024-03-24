0902fb0789cf8d027fbac93c371cffa267aa71e8
troychaplin <troychaplin@users.noreply.github.com>
2024-03-24 19:11:48 +0000

    Deploying to gh-pages from @ cuweb/rds@719e8ee2788e28482002d740ad9e8d2c238fb4a4 🚀

--- lib/components/PageHeaders/PageHeaders.d.ts
+++ lib/components/PageHeaders/PageHeaders.d.ts
@@ -1,13 +1,15 @@
 import React from '../../../node_modules/react';
 export interface PageHeadersProps {
     children?: React.ReactNode;
-    header: string;
     as?: 'h1' | 'h2' | 'h3';
-    size?: 'sm' | 'md' | 'lg';
+    header: string;
+    content?: string;
+    size?: 'xs' | 'sm' | 'md' | 'lg';
     pronoun?: string;
+    noUnderline?: boolean;
 }
-export declare const PageHeadersWrapper: ({ children, header, as, size, pronoun }: PageHeadersProps) => import('../../../node_modules/react/jsx-runtime').JSX.Element;
-export declare const PageHeaders: (({ children, header, as, size, pronoun }: PageHeadersProps) => import('../../../node_modules/react/jsx-runtime').JSX.Element) & {
+export declare const PageHeadersWrapper: ({ children, as, header, content, size, noUnderline, pronoun, }: PageHeadersProps) => import('../../../node_modules/react/jsx-runtime').JSX.Element;
+export declare const PageHeaders: (({ children, as, header, content, size, noUnderline, pronoun, }: PageHeadersProps) => import('../../../node_modules/react/jsx-runtime').JSX.Element) & {
     People: {
         ({ jobTitle, ...restProps }: import("./PageHeadersPeople").PageHeadersPeopleProps & import("./PageHeadersPeople").PageHeadersSocialProps): import('../../../node_modules/react/jsx-runtime').JSX.Element;
         displayName: string;
