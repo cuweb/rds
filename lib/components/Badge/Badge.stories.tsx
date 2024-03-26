3bec629281350f63733b0e3ccdfa8f7e00271785
Troy Chaplin <troy.chaplin@gmail.com>
2024-03-26 12:40:59 -0400

    add: badge group component

--- lib/components/Badge/Badge.stories.tsx
+++ lib/components/Badge/Badge.stories.tsx
@@ -27,16 +27,3 @@ export const Default: Story = {
     noWordBreak: false,
   },
 }
-
-export const MultiColors: Story = {
-  render: () => (
-    <>
-      <Badge color="green">Green Badge</Badge>
-      <Badge color="red">Red Badge</Badge>
-      <Badge color="yellow">Yellow Badge</Badge>
-      <Badge color="blue">Blue Badge</Badge>
-      <Badge color="purple">Purple Badge</Badge>
-      <Badge color="grey">Grey Badge</Badge>
-    </>
-  ),
-}
