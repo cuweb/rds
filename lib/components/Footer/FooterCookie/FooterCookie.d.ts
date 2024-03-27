2351bef38972ec225286091dd0fbba1cdb630235
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-03-27 14:57:19 +0000

    Deploying to gh-pages from @ cuweb/rds@f97211934c09392f4544b3978a0065e552943154 🚀

--- lib/components/Footer/FooterCookie/FooterCookie.d.ts
+++ lib/components/Footer/FooterCookie/FooterCookie.d.ts
@@ -1,1 +1,3 @@
+export declare const IsCookieExpired: (cookieName: string) => boolean;
+export declare const SetCookie: (cookieName: string) => void;
 export declare const FooterCookie: () => import('../../../../node_modules/react/jsx-runtime').JSX.Element;
