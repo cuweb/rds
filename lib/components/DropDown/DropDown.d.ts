113dbf10927221757bdcd868a2734a195b66d9af
troychaplin <troychaplin@users.noreply.github.com>
2024-04-04 16:11:05 +0000

    Deploying to gh-pages from @ cuweb/rds@690c4d7d2fef74f4ce795afb52b376c69188100a 🚀

--- lib/components/DropDown/DropDown.d.ts
+++ lib/components/DropDown/DropDown.d.ts
@@ -8,8 +8,7 @@ export interface DropDownItemProps {
 export interface DropDownProps {
     children?: React.ReactNode;
     buttonText?: string;
-    renderAs?: 'button' | 'div';
     menuAlign?: 'left' | 'right';
     listItems: DropDownItemProps[];
 }
-export declare const DropDown: ({ children, buttonText, renderAs, listItems, menuAlign }: DropDownProps) => import('../../../node_modules/react/jsx-runtime').JSX.Element;
+export declare const DropDown: ({ children, buttonText, menuAlign, listItems }: DropDownProps) => import('../../../node_modules/react/jsx-runtime').JSX.Element;
