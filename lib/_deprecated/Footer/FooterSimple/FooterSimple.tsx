bc0c81fe6d546e67338469819059882d5ef4fb3e
Troy Chaplin <troy.chaplin@gmail.com>
2024-04-04 12:53:59 -0400

    deprecate: footer simple

--- lib/_deprecated/Footer/FooterSimple/FooterSimple.tsx
+++ lib/_deprecated/Footer/FooterSimple/FooterSimple.tsx
@@ -1,4 +1,4 @@
-import { Button } from '../../Button/Button'
+import { Button } from '../../../components/Button/Button'
 import { footerStyles } from '../../../utils/globalClasses'
 import { SmallLinks } from '../../../data/FooterData'
 
