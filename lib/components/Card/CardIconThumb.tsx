bdea67fadca81a227d35d4ddb8951a57a3e4ffd8
Troy Chaplin <troy.chaplin@gmail.com>
2024-04-05 13:24:57 -0400

    finish: icon card updates

--- lib/components/Card/CardIconThumb.tsx
+++ lib/components/Card/CardIconThumb.tsx
@@ -5,21 +5,18 @@ export interface CardIconThumbProps {
 export const CardIconThumb = ({ icon }: CardIconThumbProps) => {
   const cdnPath = 'https://cdn.carleton.ca/rds/assets/font-awesome/'
   const iconPath = `${cdnPath}${icon}.svg`
-
-  // Remove dashes from icon name
   const iconAlt = icon ? icon.replace(/-/g, ' ') : ''
 
-  // Use filter to change svg as image to red
   const redIcon = {
     filter: 'invert(20%) sepia(50%) saturate(7177%) hue-rotate(348deg) brightness(91%) contrast(100%)',
   }
 
   return (
-    <figure className="flex items-center justify-center w-12 h-12 mx-6 mt-5 mb-1.5 bg-cyan-400 overflow-hidden">
+    <figure className="flex items-center justify-center w-12 h-12 mx-6 mt-5 mb-1.5 overflow-hidden">
       <img
         src={iconPath}
         alt={`An icon of a ${iconAlt}`}
-        className="object-cover cu-icon-thumb max-w-none max-h-full"
+        className="object-cover max-h-full cu-icon-thumb max-w-none"
         style={redIcon}
       />
     </figure>
