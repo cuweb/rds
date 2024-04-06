ce2f0e36eac3af99bcf1ccb138018658f9de5e9f
troychaplin <troychaplin@users.noreply.github.com>
2024-04-06 13:49:35 +0000

    Deploying to gh-pages from @ cuweb/rds@9ad1b069fe23ef5b11c3743c3a0922fd94f208fa 🚀

--- lib/components/TextImage/TextImageContent.d.ts
+++ lib/components/TextImage/TextImageContent.d.ts
@@ -9,13 +9,11 @@ export interface TextImageMediaProps {
     imageUrl?: string;
     contentWidth?: number;
     imageZoom?: number;
-    angle?: 'left' | 'dual' | 'none';
-    flipX?: boolean;
     focalPointX?: string;
     focalPointY?: string;
     hasMobileImage?: boolean;
 }
 export declare const TextImageContent: {
-    ({ children, title, headerType, isCenter, imageUrl, contentWidth, imageZoom, angle, flipX, focalPointX, focalPointY, hasMobileImage, }: TextImageContentProps & TextImageMediaProps): import('../../../node_modules/react/jsx-runtime').JSX.Element;
+    ({ children, title, headerType, isCenter, imageUrl, contentWidth, imageZoom, focalPointX, focalPointY, hasMobileImage, }: TextImageContentProps & TextImageMediaProps): import('../../../node_modules/react/jsx-runtime').JSX.Element;
     displayName: string;
 };
