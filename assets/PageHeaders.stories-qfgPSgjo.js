31a9d8ba3d1ad162be1eb7a8de2eaedfe841aa17
troychaplin <troy.chaplin@gmail.com>
2024-04-08 13:38:21 +0000

    Deploying to gh-pages from @ cuweb/rds@d9c72695842c3acba8829da7f5b4270176ac7f25 🚀

---
+++ assets/PageHeaders.stories-qfgPSgjo.js
@@ -0,0 +1,44 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{P as n}from"./PageHeaders-DSzupGOl.js";import{F}from"./Figure-DEG8RRQV.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./globalClasses-DjGLNohy.js";import"./Button-BgXB67LW.js";import"./Icon-DOs3VExa.js";import"./optionClasses-D-XbMsDd.js";import"./index-D6d4vuQb.js";import"./iframe-xrsHr1Tk.js";import"../sb-preview/runtime.js";import"./index-Dk74W0Oi.js";const J={title:"Components/PageHeaders",component:n,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},t={};t.args={header:"Large page header",size:"lg",as:"h1"};const r={args:{header:"Page header centre",size:"lg",as:"h1",isCenter:!0}},a={args:{header:"Large header with content",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis.",size:"lg",as:"h1"}},s={args:{header:"Large center header with content",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis.",size:"lg",as:"h1",isCenter:!0}},i={args:{header:"Upcoming Event Header",children:e.jsxs(e.Fragment,{children:[e.jsx(F,{size:"sm",align:"right",hasShadow:!0,noMobile:!0,children:e.jsx("img",{src:"https://fastly.picsum.photos/id/1062/400/400.jpg?hmac=zaTGri35k94fGnPFBesQ7tRVfjy6BUCtXDFQdWQ3r-k",alt:"Required alt text",width:"200",height:"133"})}),e.jsx(n.Event,{eventType:"Hybrid",virtualType:"Teams",virtualUrl:"https://events.carleton.ca",startDate:"March 21st, 2023 at 6:00pm",location:"Raven's Nest, 1125 Colonel By Drive",cost:"$20 per adult, $15 for youth/senior",contactName:"John Doe",contactEmail:"johndoe@test.com",contactPhone:"613-520-2600 x1234",primaryButtonUrl:"https://carleton.ca",secondaryButtonUrl:"https://carleton.ca/webservices",secondaryButtonText:"More Information"})]})}},o={args:{header:"People Profile Header",pronoun:"He/Him",children:e.jsxs(e.Fragment,{children:[e.jsx(F,{size:"sm",align:"right",hasShadow:!0,children:e.jsx("img",{src:"https://fastly.picsum.photos/id/1062/400/400.jpg?hmac=zaTGri35k94fGnPFBesQ7tRVfjy6BUCtXDFQdWQ3r-k",alt:"Required alt text",width:"200",height:"133"})}),e.jsx(n.People,{jobTitle:"Jedi Master & Jedi Council Member",degrees:"Certificate, Padawan Academy of Jedi Arts",building:"Onraed",room:"Sector A29",email:"greely@thejediyouseek.com",phone:"613-520-2600",phoneExt:"1234",resume:"#",website:"#",linkedin:"#",twitter:"#",facebook:"#"})]})}};var c,d,m;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(m=(d=t.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var l,u,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    header: 'Page header centre',
+    size: 'lg',
+    as: 'h1',
+    isCenter: true
+  }
+}`,...(p=(u=r.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var h,g,f;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    header: 'Large header with content',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis.',
+    size: 'lg',
+    as: 'h1'
+  }
+}`,...(f=(g=a.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var y,P,v;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    header: 'Large center header with content',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis.',
+    size: 'lg',
+    as: 'h1',
+    isCenter: true
+  }
+}`,...(v=(P=s.parameters)==null?void 0:P.docs)==null?void 0:v.source}}};var j,C,x;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    header: 'Upcoming Event Header',
+    children: <>
+        <Figure size="sm" align="right" hasShadow noMobile>
+          <img src="https://fastly.picsum.photos/id/1062/400/400.jpg?hmac=zaTGri35k94fGnPFBesQ7tRVfjy6BUCtXDFQdWQ3r-k" alt="Required alt text" width="200" height="133" />
+        </Figure>
+        <PageHeaders.Event eventType="Hybrid" virtualType="Teams" virtualUrl="https://events.carleton.ca" startDate="March 21st, 2023 at 6:00pm" location="Raven's Nest, 1125 Colonel By Drive" cost="$20 per adult, $15 for youth/senior" contactName="John Doe" contactEmail="johndoe@test.com" contactPhone="613-520-2600 x1234" primaryButtonUrl="https://carleton.ca" secondaryButtonUrl="https://carleton.ca/webservices" secondaryButtonText="More Information" />
+      </>
+  }
+}`,...(x=(C=i.parameters)==null?void 0:C.docs)==null?void 0:x.source}}};var b,w,H;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    header: 'People Profile Header',
+    pronoun: 'He/Him',
+    children: <>
+        <Figure size="sm" align="right" hasShadow>
+          <img src="https://fastly.picsum.photos/id/1062/400/400.jpg?hmac=zaTGri35k94fGnPFBesQ7tRVfjy6BUCtXDFQdWQ3r-k" alt="Required alt text" width="200" height="133" />
+        </Figure>
+        <PageHeaders.People jobTitle="Jedi Master & Jedi Council Member" degrees="Certificate, Padawan Academy of Jedi Arts" building="Onraed" room="Sector A29" email="greely@thejediyouseek.com" phone="613-520-2600" phoneExt="1234" resume="#" website="#" linkedin="#" twitter="#" facebook="#" />
+      </>
+  }
+}`,...(H=(w=o.parameters)==null?void 0:w.docs)==null?void 0:H.source}}};const N=["Default","PageHeaderCentre","DefaultWithContent","CenterWithContent","EventPageHeader","PeoplePageHeader"];export{s as CenterWithContent,t as Default,a as DefaultWithContent,i as EventPageHeader,r as PageHeaderCentre,o as PeoplePageHeader,N as __namedExportsOrder,J as default};
