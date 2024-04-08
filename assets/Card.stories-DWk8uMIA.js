8edda5c3db32fac623aee9c0a4dd946f4e23a53f
troychaplin <troy.chaplin@gmail.com>
2024-04-08 21:28:41 +0000

    Deploying to gh-pages from @ cuweb/rds@40cd28358f85d8e7bd8f28c4b26cf373928a5d89 🚀

---
+++ assets/Card.stories-DWk8uMIA.js
@@ -0,0 +1,112 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{C as a}from"./Card-DkrMfXG8.js";import{E as t,P as s}from"./PeopleData-DAgl9gKI.js";import{I as h}from"./IconData-CmGgWEcq.js";import{N as r}from"./NewsData-DZeLrR9d.js";import{S as C,V as x}from"./VideoData-DxiIlLz2.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./getDate-BAl1yPiw.js";import"./parse-BfiLtjV-.js";import"./index-DReTa1j3.js";import"./iframe-DdtGQgER.js";import"../sb-preview/runtime.js";import"./index-Dk74W0Oi.js";const se={title:"Components/Card",component:a,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},o={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(a.Header,{title:"How to Write for the Web"}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:"https://carleton.ca/webservices",className:"cu-button cu-button--red",children:"More info"})})]})}},n={render:()=>e.jsxs(a,{children:[e.jsx(a.Figure,{children:e.jsx("img",{src:t.image,alt:t.alt,width:"400",height:"175"})}),e.jsx(a.DateThumb,{startDate:t.startDate,endDate:t.endDate}),e.jsx(a.Header,{title:t.title,as:"h3"}),e.jsx(a.Body,{children:e.jsx(a.EventMeta,{startDateTime:t.startDate,endDateTime:t.endDate,onCampus:t.on_campus,onCampusBuilding:t.on_campus_building,onCampusRoomNumber:t.on_campus_room_number,eventAddress:t.event_address})}),e.jsx(a.Footer,{children:e.jsx("a",{href:t.link,className:"cu-button cu-button--red",children:"Event details"})})]})},i={render:()=>e.jsxs(a,{children:[e.jsx(a.Figure,{children:e.jsx("img",{src:r.image,alt:r.alt,width:"400",height:"266"})}),e.jsx(a.Header,{title:r.title,date:r.date}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:r.link,className:"cu-button cu-button--red",children:"Read more"})})]})},d={render:()=>e.jsxs(a,{noHover:!0,children:[e.jsx(a.IconThumb,{icon:h.icon}),e.jsx(a.Header,{title:h.title}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:h.link,className:"cu-button cu-button--red",children:"Get informed"})})]})},l={render:()=>e.jsxs(a,{children:[e.jsx(a.Header,{title:r.title,date:r.date,datePrefix:"Modified on ",datePosition:"bottom"}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:r.link,className:"cu-button cu-button--red",children:"Read more"})})]})},c={render:()=>e.jsxs(a,{isCenter:!0,children:[e.jsx(a.Figure,{isRound:!0,children:e.jsx("img",{src:s.image,alt:s.alt,width:"280",height:"280"})}),e.jsx(a.Header,{title:`${s.firstName} ${s.lastName}`}),e.jsx(a.Body,{children:e.jsx(a.PeopleMeta,{jobTitle:s.jobTitle,phone:s.phone,children:e.jsx("a",{href:`mailto:${s.email}`,children:s.email})})}),e.jsx(a.Footer,{children:e.jsx("a",{href:s.link,className:"cu-button cu-button--red",children:"View profile"})})]})},m={render:()=>e.jsxs(a,{noHover:!0,children:[e.jsx(a.ImageThumb,{children:e.jsx("img",{src:r.image,alt:r.alt,width:"200",height:"133"})}),e.jsx(a.Header,{title:r.title}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:r.link,className:"cu-button cu-button--red",children:"Get informed"})})]})},u={render:()=>e.jsx(a,{leftBorder:!0,noHover:!0,children:e.jsx(a.Stats,{stat:C.stat,desc:C.desc})})},p={render:()=>e.jsxs(a,{noHover:!0,children:[e.jsx(a.Video,{source:x.source}),e.jsx(a.Header,{title:x.title})]})};var g,b,D;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <Card.Header title="How to Write for the Web" />
+        <Card.Body>
+          <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+        </Card.Body>
+        <Card.Footer>
+          <a href="https://carleton.ca/webservices" className="cu-button cu-button--red">
+            More info
+          </a>
+        </Card.Footer>
+      </>
+  }
+}`,...(D=(b=o.parameters)==null?void 0:b.docs)==null?void 0:D.source}}};var j,v,f;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <Card>
+      <Card.Figure>
+        <img src={eventData.image} alt={eventData.alt} width="400" height="175" />
+      </Card.Figure>
+      <Card.DateThumb startDate={eventData.startDate} endDate={eventData.endDate} />
+      <Card.Header title={eventData.title} as="h3" />
+      <Card.Body>
+        <Card.EventMeta startDateTime={eventData.startDate} endDateTime={eventData.endDate} onCampus={eventData.on_campus} onCampusBuilding={eventData.on_campus_building} onCampusRoomNumber={eventData.on_campus_room_number} eventAddress={eventData.event_address} />
+      </Card.Body>
+      <Card.Footer>
+        <a href={eventData.link} className="cu-button cu-button--red">
+          Event details
+        </a>
+      </Card.Footer>
+    </Card>
+}`,...(f=(v=n.parameters)==null?void 0:v.docs)==null?void 0:f.source}}};var F,N,w;i.parameters={...i.parameters,docs:{...(F=i.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => <Card>
+      <Card.Figure>
+        <img src={newsData.image} alt={newsData.alt} width="400" height="266" />
+      </Card.Figure>
+      <Card.Header title={newsData.title} date={newsData.date} />
+      <Card.Body>
+        <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+      </Card.Body>
+      <Card.Footer>
+        <a href={newsData.link} className="cu-button cu-button--red">
+          Read more
+        </a>
+      </Card.Footer>
+    </Card>
+}`,...(w=(N=i.parameters)==null?void 0:N.docs)==null?void 0:w.source}}};var H,B,S;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: () => <Card noHover>
+      <Card.IconThumb icon={iconData.icon} />
+      <Card.Header title={iconData.title} />
+      <Card.Body>
+        <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+      </Card.Body>
+      <Card.Footer>
+        <a href={iconData.link} className="cu-button cu-button--red">
+          Get informed
+        </a>
+      </Card.Footer>
+    </Card>
+}`,...(S=(B=d.parameters)==null?void 0:B.docs)==null?void 0:S.source}}};var y,E,I;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => <Card>
+      <Card.Header title={newsData.title} date={newsData.date} datePrefix="Modified on " datePosition="bottom" />
+      <Card.Body>
+        <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+      </Card.Body>
+      <Card.Footer>
+        <a href={newsData.link} className="cu-button cu-button--red">
+          Read more
+        </a>
+      </Card.Footer>
+    </Card>
+}`,...(I=(E=l.parameters)==null?void 0:E.docs)==null?void 0:I.source}}};var _,M,T;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: () => <Card isCenter>
+      <Card.Figure isRound>
+        <img src={peopleData.image} alt={peopleData.alt} width="280" height="280" />
+      </Card.Figure>
+      <Card.Header title={\`\${peopleData.firstName} \${peopleData.lastName}\`} />
+      <Card.Body>
+        <Card.PeopleMeta jobTitle={peopleData.jobTitle} phone={peopleData.phone}>
+          <a href={\`mailto:\${peopleData.email}\`}>{peopleData.email}</a>
+        </Card.PeopleMeta>
+      </Card.Body>
+      <Card.Footer>
+        <a href={peopleData.link} className="cu-button cu-button--red">
+          View profile
+        </a>
+      </Card.Footer>
+    </Card>
+}`,...(T=(M=c.parameters)==null?void 0:M.docs)==null?void 0:T.source}}};var P,k,L;m.parameters={...m.parameters,docs:{...(P=m.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: () => <Card noHover>
+      <Card.ImageThumb>
+        <img src={newsData.image} alt={newsData.alt} width="200" height="133" />
+      </Card.ImageThumb>
+      <Card.Header title={newsData.title} />
+      <Card.Body>
+        <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+      </Card.Body>
+      <Card.Footer>
+        <a href={newsData.link} className="cu-button cu-button--red">
+          Get informed
+        </a>
+      </Card.Footer>
+    </Card>
+}`,...(L=(k=m.parameters)==null?void 0:k.docs)==null?void 0:L.source}}};var U,R,V;u.parameters={...u.parameters,docs:{...(U=u.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: () => <Card leftBorder noHover>
+      <Card.Stats stat={statData.stat} desc={statData.desc} />
+    </Card>
+}`,...(V=(R=u.parameters)==null?void 0:R.docs)==null?void 0:V.source}}};var q,$,G;p.parameters={...p.parameters,docs:{...(q=p.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: () => <Card noHover>
+      <Card.Video source={videoData.source} />
+      <Card.Header title={videoData.title} />
+    </Card>
+}`,...(G=($=p.parameters)==null?void 0:$.docs)==null?void 0:G.source}}};const oe=["Default","EventCard","NewsCard","IconCard","PageCard","PeopleCard","SpotlightCard","StatCard","VideoCard"];export{o as Default,n as EventCard,d as IconCard,i as NewsCard,l as PageCard,c as PeopleCard,m as SpotlightCard,u as StatCard,p as VideoCard,oe as __namedExportsOrder,se as default};
