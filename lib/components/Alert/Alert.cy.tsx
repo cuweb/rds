9a185b1ce685e440c41664fbc5227d3d17747bf2
Ish <ishdeep.711@gmail.com>
2024-04-18 09:43:08 -0400

    change: fixing the mount for componenet and typescript configuration for cypress

--- lib/components/Alert/Alert.cy.tsx
+++ lib/components/Alert/Alert.cy.tsx
@@ -1,9 +1,9 @@
-// import React from 'react'
-// import { Alert } from './Alert'
+import { Alert } from './Alert'
+import { mount } from 'cypress/react'
 
-// describe('<Alert />', () => {
-//   it('renders', () => {
-//     // see: https://on.cypress.io/mounting-react
-//     cy.mount(<Alert title={'Sucess alert '} type="success" />)
-//   })
-// })
+describe('<Alert />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    mount(<Alert title={'Sucess alert '} type="success" />)
+  })
+})
