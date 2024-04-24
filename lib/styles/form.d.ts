d3fabec011bdda12166d259a53a7eb07c031baef
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-04-24 19:06:32 +0000

    Deploying to gh-pages from @ cuweb/rds@6612d7fac9036e8228ec1c9e9a79e019d4d35c3a 🚀

--- lib/styles/form.d.ts
+++ lib/styles/form.d.ts
@@ -1,27 +1,20 @@
 export declare const primaryStyles: {
+    form: string;
     wrapper: string;
-    button: string;
-    buttonDarkgrey: string;
-    required: string;
-    disabled: string;
-    ghostButton: string;
 };
 export declare const textStyles: {
     label: string;
     legend: string;
-    helper: string;
     required: string;
+    helper: string;
     legendHelper: string;
 };
 export declare const fieldStyles: {
     input: string;
-    select: string;
     radioCheck: string;
     horizontalOptions: string;
     verticalOptions: string;
     uploads: string;
-    errorInput: string;
     disabled: string;
     disabledCheckbox: string;
-    disabledUpload: string;
 };
