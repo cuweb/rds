8f3cd450c6d04f34d144ae6a3504717da1f01f91
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2024-04-24 14:00:05 -0400

    upgrade: form button

--- lib/components/Form/FormButton/FormButton.tsx
+++ lib/components/Form/FormButton/FormButton.tsx
@@ -4,8 +4,6 @@ import { Button } from '../../Button/Button'
 export interface FormButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title?: string
   color?: 'red' | 'grey' | 'dark-grey'
-  isOutline?: boolean
-  isType?: boolean
 }
 
 export const FormButton = ({
@@ -13,9 +11,7 @@ export const FormButton = ({
   color = 'red',
   type = 'button',
   disabled = false,
-  isOutline,
   ...rest
 }: FormButtonProps) => {
-  const buttonStyle = isOutline ? 'outline' : 'solid'
-  return <Button title={title} color={color} type={type} isType={disabled ? disabled : buttonStyle} {...rest} />
+  return <Button title={title} color={color} type={type} disabled={disabled} {...rest} />
 }
