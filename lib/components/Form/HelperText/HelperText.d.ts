73fc005672b744a5e8a04e8308e63e7358d986a1
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-04-24 18:30:32 +0000

    Deploying to gh-pages from @ cuweb/rds@ef9336d02010d7ad84b33e3a62d0e45d0c4e328a 🚀

---
+++ lib/components/Form/HelperText/HelperText.d.ts
@@ -0,0 +1,6 @@
+export interface HelperTextProps {
+    children: string;
+    className?: string;
+}
+export declare const HelperText: ({ ...props }: HelperTextProps) => import("react/jsx-runtime").JSX.Element;
+export default HelperText;
--- lib/components/Form/WYSIWYG/WYSIWYG.d.ts
+++ lib/components/Form/WYSIWYG/WYSIWYG.d.ts
@@ -1,9 +1,10 @@
 
-export interface WYSIWYGUploadProps {
+export interface WYSIWYGProps {
     label: string;
     name: string;
     maxWidth?: 'sm' | 'md' | 'lg' | 'xl';
     helper?: string;
     required?: boolean;
+    displayError?: boolean;
 }
-export declare const WYSIWYG: ({ ...props }: WYSIWYGUploadProps) => import("react/jsx-runtime").JSX.Element;
+export declare const WYSIWYG: ({ ...props }: WYSIWYGProps) => import("react/jsx-runtime").JSX.Element;
