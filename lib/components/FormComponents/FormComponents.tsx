31eaaecd8f18f04a85f6cfd1ad053158db53a6eb
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2024-05-02 22:03:58 -0400

    add: formik context to the component

--- lib/components/FormComponents/FormComponents.tsx
+++ lib/components/FormComponents/FormComponents.tsx
@@ -1,26 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Formik, Form } from 'formik'
+import { FormikProvider } from 'formik'
 import { FieldControl } from './FieldControl/FieldControl'
 
 export interface FormComponentsWrapperProps {
   children: React.ReactNode
-  initialValues: any
-  validationSchema: any
-  onSubmit: any
-  method?: 'GET' | 'POST'
+  formikProps: any
 }
 
-export const FormComponentsWrapper = ({
-  children,
-  onSubmit,
-  initialValues,
-  validationSchema,
-  method = 'POST',
-}: FormComponentsWrapperProps) => {
+export const FormComponentsWrapper = ({ children, formikProps }: FormComponentsWrapperProps) => {
   return (
-    <Formik initialValues={initialValues} validationSchema={validationSchema} validateOnBlur={true} onSubmit={onSubmit}>
-      <Form method={method}>{children}</Form>
-    </Formik>
+    // <Formik initialValues={initialValues} validationSchema={validationSchema} validateOnBlur={true} onSubmit={onSubmit}>
+    //   <Form method={method}>{children}</Form>
+    // </Formik>
+    <FormikProvider value={formikProps}>{children}</FormikProvider>
   )
 }
 
