de8f404e50268492c5317570d99a6f0e1c02d513
troychaplin <troychaplin@users.noreply.github.com>
2024-05-03 22:13:52 +0000

    Deploying to gh-pages from @ cuweb/rds@cd9c072adfea2b7d59b80f7f1b21bde3e17f5e45 🚀

--- lib/components/Loaders/FormLoader/FormLoader.d.ts
+++
@@ -1,7 +0,0 @@
-import { default as React } from '../../../../node_modules/react';
-
-interface FormLoaderProps {
-    numCol?: number;
-}
-export declare const FormLoader: React.FC<FormLoaderProps>;
-export {};
--- lib/components/Loaders/TableLoader/TableLoader.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const TableLoader: () => import("react/jsx-runtime").JSX.Element;
