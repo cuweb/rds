e9dfa73975fc5c082d986880675babc26d0ac23e
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2024-05-08 15:37:12 -0400

    add: table, with number of rows and columns, different style for odd and even number of row

--- lib/components/Loaders/TableLoader/TableLoader.tsx
+++ lib/components/Loaders/TableLoader/TableLoader.tsx
@@ -1,9 +1,37 @@
-export const TableLoader = () => {
+import React from 'react'
+
+interface TableLoaderProps {
+  numRow?: number
+  numCol?: number
+}
+
+export const TableLoader: React.FC<TableLoaderProps> = ({ numRow = 5, numCol = 5 }) => {
+  // Generate table rows with span elements for each column
+  const rows = Array.from({ length: numRow }, (_, rowIndex) => (
+    <>
+      {rowIndex % 2 === 0 ? (
+        <div key={rowIndex} className="flex space-x-2 mb-2">
+          {' '}
+          {/* Added mb-2 for bottom margin */}
+          {Array.from({ length: numCol }, (_, colIndex) => (
+            <span key={colIndex} className="animate-pulse block w-20 h-4 bg-cu-black-200"></span>
+          ))}
+        </div>
+      ) : (
+        <div key={rowIndex} className="flex space-x-2 mb-2">
+          {' '}
+          {/* Added mb-2 for bottom margin */}
+          {Array.from({ length: numCol }, (_, colIndex) => (
+            <span key={colIndex} className="animate-pulse block w-20 h-4 bg-cu-black-100"></span>
+          ))}
+        </div>
+      )}
+    </>
+  ))
+
   return (
     <>
-      <div className="animate-pulse">
-        <span className="block w-full rounded-t-lg h-60 bg-cu-black-100"></span>
-      </div>
+      {rows}
       <span className="sr-only">Loading...</span>
     </>
   )
