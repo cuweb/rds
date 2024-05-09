e53d345883b22cc2aa9ed15d8c131f23f3a97056
HanqingZhou <HanqingZhou@users.noreply.github.com>
2024-05-09 18:50:37 +0000

    Deploying to gh-pages from @ cuweb/rds@ab633244f25f02e9c6f040cd24011f641970282f 🚀

--- lib/components/Loaders/FormLoader/FormLoader.d.ts
+++ lib/components/Loaders/FormLoader/FormLoader.d.ts
@@ -1,7 +1,11 @@
 import { default as React } from '../../../../node_modules/react';
 
+interface RowLoaderProps {
+    cols: number;
+}
+export declare const RowLoader: React.FC<RowLoaderProps>;
 interface FormLoaderProps {
-    numCol?: number;
+    children: any;
 }
 export declare const FormLoader: React.FC<FormLoaderProps>;
 export {};
