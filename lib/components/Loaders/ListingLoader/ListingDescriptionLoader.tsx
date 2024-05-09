0c5c4deea966ba2451dcc38eb7c1a64f462528f9
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2024-05-09 11:08:23 -0400

    add: make changes to description listing loader, and add multiple option

--- lib/components/Loaders/ListingLoader/ListingDescriptionLoader.tsx
+++ lib/components/Loaders/ListingLoader/ListingDescriptionLoader.tsx
@@ -1,21 +1,25 @@
 import { Listing } from '../../Listing/Listing'
 
-export const ListingDescriptionLoader = () => {
-  return (
-    <Listing>
-      <Listing.Body>
-        <div className="animate-pulse">
-          <div className="flex flex-wrap gap-5">
-            <span className="h-36 w-36 @4xl:lg:h-32 @4xl:lg:w-32 rounded-md bg-cu-black-100"></span>
-            <div className="grid flex-auto w-64 gap-3">
-              <span className="block h-8 rounded-md w-52 bg-cu-black-100"></span>
-              <span className="block h-6 rounded-md w-36 bg-cu-black-100"></span>
-              <span className="block h-6 mb-2 rounded-md w-36 bg-cu-black-100"></span>
-            </div>
+interface ListingDescriptionLoaderProps {
+  numCol?: number
+}
+
+export const ListingDescriptionLoader: React.FC<ListingDescriptionLoaderProps> = ({ numCol = 2 }) => {
+  // Render one or two Listing.Body components based on numCol
+  const listingBodies = Array.from({ length: numCol }, (_, index) => (
+    <Listing.Body key={index}>
+      <div className="animate-pulse">
+        <div className="flex flex-wrap gap-5">
+          <div className="grid flex-auto w-64 gap-3">
+            <span className="block h-8 rounded-md w-52 bg-cu-black-100"></span>
+            <span className="block h-6 rounded-md w-36 bg-cu-black-100"></span>
+            <span className="block h-6 mb-2 rounded-md w-36 bg-cu-black-100"></span>
           </div>
         </div>
-        <span className="sr-only">Loading...</span>
-      </Listing.Body>
-    </Listing>
-  )
+      </div>
+      <span className="sr-only">Loading...</span>
+    </Listing.Body>
+  ))
+
+  return <Listing>{listingBodies}</Listing>
 }
