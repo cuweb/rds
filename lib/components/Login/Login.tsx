673fa62613bcdd57048c9a4179c539fe92ac94be
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2024-05-15 17:18:16 -0400

    change: add button handler

--- lib/components/Login/Login.tsx
+++ lib/components/Login/Login.tsx
@@ -4,10 +4,18 @@ import { ButtonGroup } from '../ButtonGroup/ButtonGroup'
 export interface LoginProps {
   isCenter?: boolean
   useSocial?: boolean
+  onClickHandler?: () => void
 }
 
-export const Login = ({ isCenter = false, useSocial = false }: LoginProps) => {
+export const Login = ({ isCenter = false, useSocial = false, onClickHandler }: LoginProps) => {
   const centerBox = isCenter ? 'mx-auto' : ''
+
+  const handleCarletonClick = () => {
+    if (onClickHandler) {
+      onClickHandler()
+    }
+  }
+
   return (
     <div className={`border border-cu-black-100 max-w-md shadow-md rounded-md ${centerBox}`}>
       <img
@@ -17,12 +25,12 @@ export const Login = ({ isCenter = false, useSocial = false }: LoginProps) => {
       />
       <div className="p-6">
         <ButtonGroup>
-          <Button title="Login with your Carleton account" onClick={() => {}} isFull />
+          <Button title="Login with your Carleton account" onClick={handleCarletonClick} isFull />
         </ButtonGroup>
       </div>
       {useSocial && (
-        <div className="bg-cu-black-50 p-6 rounded-b-md">
-          <p className="mb-6 text-center font-semibold">Or login with one of the following:</p>
+        <div className="p-6 bg-cu-black-50 rounded-b-md">
+          <p className="mb-6 font-semibold text-center">Or login with one of the following:</p>
           <ButtonGroup>
             <Button title="Social Network A" color="white" onClick={() => {}} isFull />
             <Button title="Social Network B" color="white" onClick={() => {}} isFull />
