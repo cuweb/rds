ce830e28f65fabde8ee0749ae46218ef90cfa065
troychaplin <troychaplin@users.noreply.github.com>
2024-05-16 15:18:25 +0000

    Deploying to gh-pages from @ cuweb/rds@e7e6a0222f3e338e64356f11ceff87b2b3c5c3df 🚀

--- lib/components/Loaders/PageHeadLoader/PageHeadLoader.d.ts
+++
@@ -1,6 +0,0 @@
-interface PageHeadLoaderProps {
-    content?: 'small' | 'large';
-    isCenter?: boolean;
-}
-export declare const PageHeadLoader: ({ content, isCenter }: PageHeadLoaderProps) => import("react/jsx-runtime").JSX.Element;
-export {};
