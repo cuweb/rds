b69cc12ea7b958974b3538450fd32845b06d2401
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2024-05-16 12:34:07 -0400

    change: used existing gridcolclass

--- lib/components/Loaders/BlockLoader/BlockLoader.tsx
+++ lib/components/Loaders/BlockLoader/BlockLoader.tsx
@@ -1,19 +1,8 @@
+import { gridColsClass } from '../../../helpers/optionClasses'
+
 interface BlockLoaderProps {
   height: number
-  cols: number | string
-}
-
-interface IGridColsClass {
-  [key: string | number]: string
-}
-
-const gridColsClass: IGridColsClass = {
-  1: 'grid-cols-1',
-  2: 'md:grid-cols-2',
-  3: 'md:grid-cols-3',
-  4: 'md:grid-cols-2 lg:grid-cols-4',
-  '1/3': 'lg:grid-cols-3 lg:[&>*:last-child]:col-span-2',
-  '2/3': 'lg:grid-cols-3 lg:[&>*:first-child]:col-span-2',
+  cols: 1 | 2 | 3 | 4 | '1/3' | '2/3'
 }
 
 export const BlockLoader = ({ height = 100, cols = 1 }: BlockLoaderProps) => {
