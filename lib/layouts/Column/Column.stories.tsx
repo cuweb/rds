d1313a98a709fe383d983fda11d4856837a6021d
Troy Chaplin <troy.chaplin@gmail.com>
2024-05-21 14:51:12 -0400

    add: reverse prop to columns

--- lib/layouts/Column/Column.stories.tsx
+++ lib/layouts/Column/Column.stories.tsx
@@ -1,5 +1,7 @@
+import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { StackedList } from '../StackedList/StackedList'
+import { Container } from '../Container/Container'
 import { Column } from './Column'
 
 const meta: Meta<typeof Column> = {
@@ -19,185 +21,195 @@ type Story = StoryObj<typeof Column>
 export const TwoCol: Story = {
   name: 'Two Columns',
   render: () => (
-    <Column maxWidth="5xl" cols="2" gridGap="10">
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-        <p>
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-      </Column.Content>
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-        <p>
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-      </Column.Content>
-    </Column>
+    <Container>
+      <Column cols="2" gridGap="10" reverse>
+        <Column.Content>
+          <p>
+            Left. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra
+            in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada
+            metus, et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+          <p>
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+        </Column.Content>
+        <Column.Content>
+          <p>
+            Right. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra
+            in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada
+            metus, et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+          <p>
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+        </Column.Content>
+      </Column>
+    </Container>
   ),
 }
 
 export const ThreeCol: Story = {
   name: 'Three Columns',
   render: () => (
-    <Column maxWidth="5xl" cols="3" gridGap="10">
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-        <p>
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-      </Column.Content>
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-        <p>
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-      </Column.Content>
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-        <p>
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-      </Column.Content>
-    </Column>
+    <Container>
+      <Column cols="3" gridGap="10">
+        <Column.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+          <p>
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+        </Column.Content>
+        <Column.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+          <p>
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+        </Column.Content>
+        <Column.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+          <p>
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+        </Column.Content>
+      </Column>
+    </Container>
   ),
 }
 
 export const FourCol: Story = {
   name: 'Four Columns',
   render: () => (
-    <Column maxWidth="7xl" cols="4" gridGap="10">
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-      </Column.Content>
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-      </Column.Content>
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-      </Column.Content>
-      <Column.Content>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-      </Column.Content>
-    </Column>
+    <Container maxWidth="7xl">
+      <Column maxWidth="7xl" cols="4" gridGap="10">
+        <Column.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+        </Column.Content>
+        <Column.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+        </Column.Content>
+        <Column.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+        </Column.Content>
+        <Column.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+        </Column.Content>
+      </Column>
+    </Container>
   ),
 }
 
 export const OneThird: Story = {
   name: '1/3 Columns',
   render: () => (
-    <Column maxWidth="7xl" cols="1/3" gridGap="10">
-      <StackedList hasBorder>
-        <li className="px-6 py-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla.
-        </li>
-        <li className="px-6 py-5">Quisque auctor ultrices mauris, et semper urna aliquam quis.</li>
-        <li className="px-6 py-5">Suspendisse vestibulum hendrerit.</li>
-      </StackedList>
-      <Column.Content>
-        <p className="pb-6 last:pb-0">
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-        <p className="pb-6 last:pb-0">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-        <p className="pb-6 last:pb-0">
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-      </Column.Content>
-    </Column>
+    <Container maxWidth="7xl">
+      <Column maxWidth="7xl" cols="1/3" gridGap="10">
+        <StackedList>
+          <li className="px-6 py-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla.
+          </li>
+          <li className="px-6 py-5">Quisque auctor ultrices mauris, et semper urna aliquam quis.</li>
+          <li className="px-6 py-5">Suspendisse vestibulum hendrerit.</li>
+        </StackedList>
+        <Column.Content>
+          <p className="pb-6 last:pb-0">
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+          <p className="pb-6 last:pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+          <p className="pb-6 last:pb-0">
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+        </Column.Content>
+      </Column>
+    </Container>
   ),
 }
 
 export const TwoThirds: Story = {
   name: '2/3 Columns',
   render: () => (
-    <Column maxWidth="7xl" cols="2/3" gridGap="10">
-      <Column.Content>
-        <p className="pb-6 last:pb-0">
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-        <p className="pb-6 last:pb-0">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et
-          tristique dolor. Suspendisse vestibulum hendrerit.
-        </p>
-        <p className="pb-6 last:pb-0">
-          Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
-          lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus.
-          Phasellus in egestas sapien ac libero.
-        </p>
-      </Column.Content>
-      <StackedList hasShadow>
-        <li className="px-6 py-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
-          egestas vitae nulla.
-        </li>
-        <li className="px-6 py-5">Quisque auctor ultrices mauris, et semper urna aliquam quis.</li>
-        <li className="px-6 py-5">Suspendisse vestibulum hendrerit.</li>
-      </StackedList>
-    </Column>
+    <Container maxWidth="7xl">
+      <Column maxWidth="7xl" cols="2/3" gridGap="10">
+        <Column.Content>
+          <p className="pb-6 last:pb-0">
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+          <p className="pb-6 last:pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+          <p className="pb-6 last:pb-0">
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero.
+          </p>
+        </Column.Content>
+        <StackedList>
+          <li className="px-6 py-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla.
+          </li>
+          <li className="px-6 py-5">Quisque auctor ultrices mauris, et semper urna aliquam quis.</li>
+          <li className="px-6 py-5">Suspendisse vestibulum hendrerit.</li>
+        </StackedList>
+      </Column>
+    </Container>
   ),
 }
 
 export const ConsCols: Story = {
   name: 'Consecutive Columns',
   render: () => (
-    <>
+    <Container maxWidth="7xl">
       <Column maxWidth="7xl" cols="2" gridGap="10">
         <Column.Content>
           <p>
@@ -237,6 +249,6 @@ export const ConsCols: Story = {
           </p>
         </Column.Content>
       </Column>
-    </>
+    </Container>
   ),
 }
--- lib/layouts/Column/Column.tsx
+++ lib/layouts/Column/Column.tsx
@@ -7,19 +7,23 @@ export interface ColumnProps {
   maxWidth?: 'full' | '5xl' | '7xl'
   gridGap?: '0' | '5' | '10'
   cols?: '1' | '2' | '3' | '4' | '1/3' | '2/3'
+  reverse?: boolean
 }
 
 const styles = {
   column: `cu-column cu-block-spacing not-contained mx-auto grid`,
 }
 
-export const ColumnWrapper = ({ children, maxWidth = '5xl', gridGap = '10', cols = '1' }: ColumnProps) => {
+export const ColumnWrapper = ({ children, maxWidth = '5xl', gridGap = '10', cols = '1', reverse }: ColumnProps) => {
+  const reverseGrid = reverse ? 'cu-column--reverse' : ''
+
   return (
     <div
       className={`
         ${styles.column}
         ${rdsGridColumns[cols]}
         ${rdsMaxWidth[maxWidth]} ${rdsGridSpacing[gridGap]}
+        ${reverseGrid}
       `}
     >
       {children}
