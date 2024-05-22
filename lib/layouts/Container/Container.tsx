7aaa627f00ec0ea4b61c6b08e9ffdd2cb18572a3
Troy Chaplin <troy.chaplin@gmail.com>
2024-05-22 15:11:58 -0400

    change: starting to rework container

--- lib/layouts/Container/Container.tsx
+++ lib/layouts/Container/Container.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { layoutSpacing, proseStyles } from '../../utils/globalClasses'
+import { layoutSpacing } from '../../utils/globalClasses'
+// import { layoutSpacing, proseStyles } from '../../utils/globalClasses'
 
 export interface ContainerProps {
   children?: React.ReactNode
+  as?: 'section' | 'div'
   isGrey?: boolean
   maxWidth?: 'none' | 'full' | '5xl' | '7xl'
 }
 
-export const Container = ({ children, isGrey, maxWidth = '5xl' }: ContainerProps) => {
+export const Container = ({ children, as = 'section', isGrey, maxWidth = '5xl' }: ContainerProps) => {
+  const ContainerComponent = as
   const childWidth = maxWidth ? `cu-container-${maxWidth}` : ''
   const bgStyles = isGrey ? layoutSpacing.containerGrey : layoutSpacing.containerWhite
 
-  return <div className={`cu-container not-contained ${proseStyles.base} ${childWidth} ${bgStyles}`}>{children}</div>
+  return (
+    <ContainerComponent className={`cu-container not-contained ${childWidth} ${bgStyles}`}>
+      {children}
+    </ContainerComponent>
+  )
+  // return <div className={`cu-container not-contained ${proseStyles.base} ${childWidth} ${bgStyles}`}>{children}</div>
 }
