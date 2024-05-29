f659601a03c172906d2effedba5d02803440e65e
troychaplin <troychaplin@users.noreply.github.com>
2024-05-29 16:29:07 +0000

    Deploying to gh-pages from @ cuweb/rds@b6ec080a43421554dd1cd6293b0394c3e49e2fff 🚀

---
+++ assets/BlockSpacing.stories-CSKdfbjt.js
@@ -0,0 +1,123 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{S as p}from"./Section-DrmBlyt5.js";import{M as y}from"./Main-CgLTKl-s.js";import{F as S}from"./FooterStandard-DHM6uugp.js";import{C as n}from"./Column-Dw5OiQ1f.js";import{N as a,a as I,b as w}from"./NavData-DRu1MbK0.js";import{T as h}from"./TextImage-B32QZuFd.js";import{W as r}from"./WideImage-YtgM4epR.js";import{B as s}from"./ButtonGroup-D-bd2cm2.js";import{B as o}from"./Button-BbqjxLtd.js";import{C as t}from"./Card-BBXAeKGV.js";import{P as x}from"./PageHeaders-DmchAU6R.js";import{a as g}from"./IconData-CmGgWEcq.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./globalClasses-BlHLKr18.js";import"./FooterLogoLinks-xDaj2ZDi.js";import"./useLinkContext-CZLCmVqQ.js";import"./optionClasses-C1xmOtGn.js";import"./Avatar-BORvOmmK.js";import"./Icon-CS9Mqat_.js";import"./getDate-BfMQ2h7Y.js";import"./format-DUBvZ7C8.js";import"./parse-BFmE_n72.js";import"./MapPinIcon-CRQ4FDTI.js";import"./index-BYS25BBd.js";import"./iframe-DD2pNiYU.js";import"../sb-preview/runtime.js";import"./index-Dk74W0Oi.js";import"./parseISO-CHcXfkHQ.js";const ee={title:"Examples/Layouts"},i=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})}),l={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(a,{children:e.jsxs(a.Primary,{children:[e.jsx(a.Logo,{title:"Web Services",link:"https://carleton.ca/webservices"}),e.jsx(a.Menu,{menu:I}),e.jsx(a.Aside,{menu:w})]})}),e.jsxs(y,{children:[e.jsx(p,{children:e.jsx(h,{children:e.jsx(h.Content,{headerType:"h1",title:"Block Spacing",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})})}),e.jsx("h2",{children:"Header Two"}),e.jsx(i,{}),e.jsx(i,{}),e.jsxs(p,{children:[e.jsx(x,{header:"Icon Cards: Shadow on Static & Hover"}),e.jsx(n,{cols:"3",children:g.slice(0,6).map(({id:d,link:c,title:m,icon:u})=>e.jsxs(t,{hasShadow:"onCard",noHover:!0,children:[e.jsx(t.IconThumb,{icon:u}),e.jsx(t.Header,{children:m}),e.jsx(t.Body,{children:e.jsx(t.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),e.jsx(t.Footer,{isType:"button",buttonStyle:"grey",children:e.jsx("a",{href:c,children:"More info"})})]},d))})]}),e.jsx(r,{title:"Wide image block with buttons and background image",children:e.jsxs(s,{children:[e.jsx(o,{title:"Apply Now"}),e.jsx(o,{color:"white",isType:"outline",title:"Request Information"})]})}),e.jsxs(p,{isGrey:!0,children:[e.jsx(x,{text:"Icon Cards: Shadow on Static & Hover",intro:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."}),e.jsx(n,{cols:"3",children:g.slice(0,6).map(({id:d,link:c,title:m,icon:u})=>e.jsxs(t,{hasShadow:"onCard",noHover:!0,children:[e.jsx(t.IconThumb,{icon:u}),e.jsx(t.Header,{children:m}),e.jsx(t.Body,{children:e.jsx(t.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),e.jsx(t.Footer,{isType:"button",buttonStyle:"grey",children:e.jsx("a",{href:c,children:"More info"})})]},d))})]}),e.jsx(r,{image:"https://picsum.photos/1600/700",title:"Wide image block with buttons and background image",children:e.jsxs(s,{children:[e.jsx(o,{title:"Apply Now"}),e.jsx(o,{color:"white",isType:"outline",title:"Request Information"})]})}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(i,{})}),e.jsx(n.Content,{children:e.jsx(i,{})})]}),e.jsx(r,{image:"https://picsum.photos/1600/700",title:"Wide image block with buttons and background image",children:e.jsxs(s,{children:[e.jsx(o,{title:"Apply Now"}),e.jsx(o,{color:"white",isType:"outline",title:"Request Information"})]})}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(i,{})}),e.jsx(n.Content,{children:e.jsx(i,{})})]}),e.jsx(r,{image:"https://picsum.photos/1600/700",title:"Wide image block with buttons and background image",children:e.jsxs(s,{children:[e.jsx(o,{title:"Apply Now"}),e.jsx(o,{color:"white",isType:"outline",title:"Request Information"})]})}),e.jsx(i,{}),e.jsx(i,{})]}),e.jsx(S,{})]})};var j,C,b;l.parameters={...l.parameters,docs:{...(j=l.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Primary>
+          <Nav.Logo title="Web Services" link="https://carleton.ca/webservices" />
+          <Nav.Menu menu={NavDataSingle} />
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Primary>
+      </Nav>
+
+      <Main>
+        <Section>
+          <TextImage>
+            <TextImage.Content headerType="h1" title="Block Spacing">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+        </Section>
+
+        <h2>Header Two</h2>
+
+        <SinglePara />
+        <SinglePara />
+
+        <Section>
+          <PageHeaders header="Icon Cards: Shadow on Static & Hover" />
+          <Column cols="3">
+            {IconData.slice(0, 6).map(({
+            id,
+            link,
+            title,
+            icon
+          }) => <Card key={id} hasShadow="onCard" noHover>
+                <Card.IconThumb icon={icon} />
+                <Card.Header>{title}</Card.Header>
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+                </Card.Body>
+                <Card.Footer isType="button" buttonStyle="grey">
+                  <a href={link}>More info</a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+        </Section>
+
+        <WideImage title="Wide image block with buttons and background image">
+          <ButtonGroup>
+            <Button title="Apply Now" />
+            <Button color="white" isType="outline" title="Request Information" />
+          </ButtonGroup>
+        </WideImage>
+
+        <Section isGrey>
+          <PageHeaders text="Icon Cards: Shadow on Static & Hover" intro="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+          <Column cols="3">
+            {IconData.slice(0, 6).map(({
+            id,
+            link,
+            title,
+            icon
+          }) => <Card key={id} hasShadow="onCard" noHover>
+                <Card.IconThumb icon={icon} />
+                <Card.Header>{title}</Card.Header>
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+                </Card.Body>
+                <Card.Footer isType="button" buttonStyle="grey">
+                  <a href={link}>More info</a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+        </Section>
+
+        <WideImage image="https://picsum.photos/1600/700" title="Wide image block with buttons and background image">
+          <ButtonGroup>
+            <Button title="Apply Now" />
+            <Button color="white" isType="outline" title="Request Information" />
+          </ButtonGroup>
+        </WideImage>
+
+        <Column cols="2">
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+        </Column>
+
+        <WideImage image="https://picsum.photos/1600/700" title="Wide image block with buttons and background image">
+          <ButtonGroup>
+            <Button title="Apply Now" />
+            <Button color="white" isType="outline" title="Request Information" />
+          </ButtonGroup>
+        </WideImage>
+
+        <Column cols="2">
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+        </Column>
+
+        <WideImage image="https://picsum.photos/1600/700" title="Wide image block with buttons and background image">
+          <ButtonGroup>
+            <Button title="Apply Now" />
+            <Button color="white" isType="outline" title="Request Information" />
+          </ButtonGroup>
+        </WideImage>
+
+        <SinglePara />
+        <SinglePara />
+      </Main>
+
+      <FooterStandard />
+    </>
+}`,...(b=(C=l.parameters)==null?void 0:C.docs)==null?void 0:b.source}}};const te=["BlockSpacing"];export{l as BlockSpacing,te as __namedExportsOrder,ee as default};
