38c918355058a8bfde796ef766266133a3744b7b
troychaplin <troychaplin@users.noreply.github.com>
2024-05-31 15:19:22 +0000

    Deploying to gh-pages from @ cuweb/rds@79191140ec50b8cc5aab7f0fd29b4879163d76ee 🚀

--- lib/components/Modal/Modal.d.ts
+++ lib/components/Modal/Modal.d.ts
@@ -2,10 +2,11 @@ import { default as React } from '../../../node_modules/react';
 
 export interface ModalProps {
     children?: React.ReactNode;
-    maxWidth?: '3xl' | '4xl' | '5xl' | '6xl' | '7xl';
-    content?: string;
-    noProse?: boolean;
+    title?: string;
+    description?: string;
+    noButton?: boolean;
     isOpen: boolean;
     setIsOpen: (k: boolean) => void;
+    hasOverlay?: boolean;
 }
-export declare const Modal: ({ children, content, isOpen, setIsOpen, maxWidth, noProse }: ModalProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Modal: ({ children, title, description, noButton, isOpen, setIsOpen, hasOverlay, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
