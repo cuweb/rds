70a8b9b61b0653002dfe60f91347113f4cf4ee3e
Troy Chaplin <troy.chaplin@gmail.com>
2024-06-07 10:45:46 -0400

    change: modal styles

--- lib/components/Modal/Modal.tsx
+++ lib/components/Modal/Modal.tsx
@@ -73,19 +73,19 @@ export const Modal = ({
   return (
     <dialog
       ref={modalRef}
-      className={`cu-dialog ${positionTop} ${useProse} md:px-8 md:py-6 px-12 py-10 z-10 w-11/12 ${rdsMaxWidth[maxWidth]} shadow-md rounded-md p-3.5`}
+      className={`cu-dialog relative ${positionTop} ${useProse} p-5 md:p-6 z-10 w-11/12 ${rdsMaxWidth[maxWidth]} shadow-md rounded-md`}
       onClick={handleClick}
       aria-labelledby={ariaLabel}
       aria-describedby={ariaDescription}
     >
       <button
-        className="absolute rounded-md top-2 right-2 text-cu-black-300 hover:text-cu-red"
+        className="absolute top-0 right-0 z-50 p-0.5 rounded-bl-md bg-cu-black-50 text-cu-black-500 hover:bg-cu-red hover:text-white"
         onClick={() => setIsOpen(false)}
       >
         <span className="sr-only">Close</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="w-4 h-4"
+          className="w-3.5 h-3.5"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
