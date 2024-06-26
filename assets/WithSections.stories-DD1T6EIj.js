7197f6306e6fcb5620301b8dea335c6c03565242
troychaplin <troychaplin@users.noreply.github.com>
2024-06-26 18:12:05 +0000

    Deploying to gh-pages from @ cuweb/rds@192aeeb82e2f7d20f9a9bcbb8314fbb54e4b7f3e 🚀

---
+++ assets/WithSections.stories-DD1T6EIj.js
@@ -0,0 +1,191 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{S as s}from"./Section-BvwVsgGu.js";import{M as m}from"./Main-B8uQ0PDb.js";import{F as p}from"./FooterStandard-Qr3Pr6Y8.js";import{N as a,a as h,b as x}from"./NavData-EO1sQ0Qs.js";import{C as n}from"./Column-BoA3N6UJ.js";import{T as o}from"./TextImage-DQnBN6U6.js";import{W as r}from"./WideImage-BiJu-G0K.js";import{P as j}from"./PageHeaders-CwTmdHL4.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./globalClasses-BlHLKr18.js";import"./FooterLogoLinks-wi3yy42J.js";import"./useLinkContext-Bz4KtVj9.js";import"./Avatar-DmMiCzKE.js";import"./optionClasses-BoR2qQUj.js";import"./Button-D5JFhw15.js";import"./Icon-DDs_3nyd.js";import"./parseISO-CHcXfkHQ.js";import"./format-DUBvZ7C8.js";const A={title:"Examples/Layouts"},t=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})}),i=()=>e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),u={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(a,{children:e.jsxs(a.Primary,{children:[e.jsx(a.Logo,{title:"Web Services",link:"https://carleton.ca/webservices"}),e.jsx(a.Menu,{menu:h}),e.jsx(a.Aside,{menu:x})]})}),e.jsxs(m,{children:[e.jsx(s,{isGrey:!0,children:e.jsx(o,{children:e.jsx(o.Content,{headerType:"h1",title:"Grey and White Sections",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})})}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsxs(s,{children:[e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx(o,{children:e.jsx(o.Content,{headerType:"h2",title:"Grey and White Sections",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),e.jsx("h2",{children:"Outside Section H2"}),e.jsx(i,{}),e.jsx(r,{as:"section",isType:"dark",title:"Wide image with dark bg",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsx("h3",{children:"Outside Section H3"}),e.jsx(t,{}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(t,{})}),e.jsx(n.Content,{children:e.jsx(t,{})})]}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(t,{})}),e.jsx(n.Content,{children:e.jsx(t,{})})]}),e.jsxs(s,{isGrey:!0,children:[e.jsx(j,{as:"h1",header:"Large page header",size:"lg"}),e.jsx("h2",{children:"Inside Section H2"}),e.jsx(i,{}),e.jsx(r,{isType:"dark",title:"Wide image with dark bg",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsx(i,{}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(t,{})}),e.jsx(n.Content,{children:e.jsx(t,{})})]})]}),e.jsxs(s,{isGrey:!0,children:[e.jsx("h2",{children:"Inside Section H2"}),e.jsx(i,{}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(t,{})}),e.jsx(n.Content,{children:e.jsx(t,{})})]})]}),e.jsx(r,{as:"section",isType:"dark",title:"Wide image with dark bg",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsxs(s,{isGrey:!0,children:[e.jsx("h2",{children:"Inside Section H2"}),e.jsx(i,{})]}),e.jsxs(s,{children:[e.jsx("h2",{children:"Inside Section H2"}),e.jsx(i,{}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(t,{})}),e.jsx(n.Content,{children:e.jsx(t,{})})]})]}),e.jsxs(s,{children:[e.jsx("h2",{children:"Inside Section H2"}),e.jsx(i,{})]}),e.jsx(r,{as:"section",isType:"dark",title:"Wide image with dark bg",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."})}),e.jsxs(s,{children:[e.jsx("h2",{children:"Inside Section H2"}),e.jsx(i,{})]}),e.jsxs(s,{isGrey:!0,children:[e.jsx("h2",{children:"Inside Section H2"}),e.jsx(i,{})]}),e.jsx(i,{}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(t,{})}),e.jsx(n.Content,{children:e.jsx(t,{})})]}),e.jsxs(n,{cols:"2",children:[e.jsx(n.Content,{children:e.jsx(t,{})}),e.jsx(n.Content,{children:e.jsx(t,{})})]}),e.jsx("h2",{children:"Outside Section H2"}),e.jsx(i,{})]}),e.jsx(p,{})]})};var l,d,c;u.parameters={...u.parameters,docs:{...(l=u.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Primary>
+          <Nav.Logo title="Web Services" link="https://carleton.ca/webservices" />
+          <Nav.Menu menu={NavDataSingle} />
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Primary>
+      </Nav>
+
+      <Main>
+        <Section isGrey>
+          <TextImage>
+            <TextImage.Content headerType="h1" title="Grey and White Sections">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+        </Section>
+
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+        <p>
+          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+          in. Praesent quis ligula quis nulla malesuada tempor.
+        </p>
+
+        <Section>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <TextImage>
+            <TextImage.Content headerType="h2" title="Grey and White Sections">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+        </Section>
+
+        <h2>Outside Section H2</h2>
+        <DoublePara />
+
+        <WideImage as="section" isType="dark" title="Wide image with dark bg">
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus.
+          </p>
+        </WideImage>
+
+        <h3>Outside Section H3</h3>
+        <SinglePara />
+        <Column cols="2">
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+        </Column>
+        <Column cols="2">
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+        </Column>
+
+        <Section isGrey>
+          <PageHeaders as="h1" header="Large page header" size="lg" />
+          <h2>Inside Section H2</h2>
+          <DoublePara />
+          <WideImage isType="dark" title="Wide image with dark bg">
+            <p>
+              Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+              reprehenderit ducimus.
+            </p>
+          </WideImage>
+          <DoublePara />
+          <Column cols="2">
+            <Column.Content>
+              <SinglePara />
+            </Column.Content>
+            <Column.Content>
+              <SinglePara />
+            </Column.Content>
+          </Column>
+        </Section>
+
+        <Section isGrey>
+          <h2>Inside Section H2</h2>
+          <DoublePara />
+          <Column cols="2">
+            <Column.Content>
+              <SinglePara />
+            </Column.Content>
+            <Column.Content>
+              <SinglePara />
+            </Column.Content>
+          </Column>
+        </Section>
+
+        <WideImage as="section" isType="dark" title="Wide image with dark bg">
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus.
+          </p>
+        </WideImage>
+
+        <Section isGrey>
+          <h2>Inside Section H2</h2>
+          <DoublePara />
+        </Section>
+
+        <Section>
+          <h2>Inside Section H2</h2>
+          <DoublePara />
+          <Column cols="2">
+            <Column.Content>
+              <SinglePara />
+            </Column.Content>
+            <Column.Content>
+              <SinglePara />
+            </Column.Content>
+          </Column>
+        </Section>
+
+        <Section>
+          <h2>Inside Section H2</h2>
+          <DoublePara />
+        </Section>
+
+        <WideImage as="section" isType="dark" title="Wide image with dark bg">
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus.
+          </p>
+        </WideImage>
+
+        <Section>
+          <h2>Inside Section H2</h2>
+          <DoublePara />
+        </Section>
+
+        <Section isGrey>
+          <h2>Inside Section H2</h2>
+          <DoublePara />
+        </Section>
+        <DoublePara />
+
+        <Column cols="2">
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+        </Column>
+        <Column cols="2">
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+          <Column.Content>
+            <SinglePara />
+          </Column.Content>
+        </Column>
+
+        <h2>Outside Section H2</h2>
+        <DoublePara />
+      </Main>
+
+      <FooterStandard />
+    </>
+}`,...(c=(d=u.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const F=["WithSections"];export{u as WithSections,F as __namedExportsOrder,A as default};
