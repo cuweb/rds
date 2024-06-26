5cb7c9d6596266bbdbe7fcbe903ce845ffb91a0b
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-06-26 19:27:38 +0000

    Deploying to gh-pages from @ cuweb/rds@03a8fc414849c4bd29c3d22594a1e3015ca12c8e 🚀

---
+++ lib/components/Form/FieldArrayContainer/FieldArrayContainer.d.ts
@@ -0,0 +1,4 @@
+export interface FieldArrayContainerProps {
+    children: React.ReactNode;
+}
+export declare const FieldArrayContainer: ({ children }: FieldArrayContainerProps) => import("react/jsx-runtime").JSX.Element;
