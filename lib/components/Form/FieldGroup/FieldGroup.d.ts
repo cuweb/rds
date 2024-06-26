fd446502bf57117d1dc0459a7330347053d3b753
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-06-26 19:05:08 +0000

    Deploying to gh-pages from @ cuweb/rds@494a93a2da0dd95748dd6d7cce8149402eb7b260 🚀

--- lib/components/Form/FieldGroup/FieldGroup.d.ts
+++ lib/components/Form/FieldGroup/FieldGroup.d.ts
@@ -1,6 +1,7 @@
 export interface FieldGroupProps {
     children: React.ReactNode;
     cols?: 1 | 2 | 3 | 4 | '1/3' | '2/3';
+    alignment?: 'top' | 'bottom' | 'center';
 }
-export declare const FieldGroup: ({ children, cols }: FieldGroupProps) => import("react/jsx-runtime").JSX.Element;
+export declare const FieldGroup: ({ children, cols, alignment }: FieldGroupProps) => import("react/jsx-runtime").JSX.Element;
 export default FieldGroup;
