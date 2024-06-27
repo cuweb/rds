5b016a0dd5f45e1b22063f4978b7b643f2b4a0e6
ajanuszkiewicz <ajanuszkiewicz@users.noreply.github.com>
2024-06-27 13:24:59 +0000

    Deploying to gh-pages from @ cuweb/rds@2073ba6df2e36528cf1235a798894638ae366c1b 🚀

---
+++ lib/components/Embed/Embed.SoundCloud.d.ts
@@ -0,0 +1,8 @@
+export interface EmbedSoundCloudProps {
+    title: string;
+    url: string;
+}
+export declare const EmbedSoundCloud: {
+    ({ title, url }: EmbedSoundCloudProps): import("react/jsx-runtime").JSX.Element;
+    displayName: string;
+};
