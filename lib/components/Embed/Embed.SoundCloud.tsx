7b0c89b03ea130305499c158df8906be5d13ae39
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2024-06-27 09:22:12 -0400

    change: update based on feedback

--- lib/components/Embed/Embed.SoundCloud.tsx
+++ lib/components/Embed/Embed.SoundCloud.tsx
@@ -2,27 +2,12 @@ import { styles } from './Embed.Styles'
 export interface EmbedSoundCloudProps {
   title: string
   url: string
-  width?: string
-  autoPlay?: boolean
-  hideRelated?: boolean
-  showComments?: boolean
-  showUser?: boolean
-  showReposts?: boolean
-  color?: string
 }
 
 export const EmbedSoundCloud = ({ title, url }: EmbedSoundCloudProps) => {
-  const width = '100%'
-  const autoPlay = false
-  const hideRelated = false
-  const showComments = false
-  const showUser = true
-  const showReposts = false
-  const color = 'ff5500'
+  const src = `https://w.soundcloud.com/player/?url=${url}&amp;color='ff5500'&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true`
 
-  const src = `https://w.soundcloud.com/player/?url=${url}&amp;color=${color}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}&amp;visual=true`
-
-  return <iframe title={title} width={width} src={src} className={styles.iframe} />
+  return <iframe title={title} width="100%" src={src} className={styles.iframe} />
 }
 
 EmbedSoundCloud.displayName = 'Embed.SoundCloud'
