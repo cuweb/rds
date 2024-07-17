96fd66abb3aa126be233bb008934f243bdf9f4e8
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-07-17 21:56:57 +0000

    Deploying to gh-pages from @ cuweb/rds@bc2d0ec43901f36f4fa4940847086e6b9b5c42d2 🚀

--- lib/components/Form/FileUpload/FileUpload.d.ts
+++ lib/components/Form/FileUpload/FileUpload.d.ts
@@ -3,5 +3,6 @@ import { FieldComponentProps } from '../FormField/FormField';
 export interface FileUploadProps extends FieldComponentProps {
     onChange?: (File: FileList | File[] | null) => void;
     nopreview?: boolean;
+    preview?: string[];
 }
 export declare const FileUpload: ({ ...props }: FileUploadProps) => import("react/jsx-runtime").JSX.Element;
