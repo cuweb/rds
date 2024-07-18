249bdaa4ac8128d2dcb4fc92294bfabf99451c3f
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-07-18 14:30:25 +0000

    Deploying to gh-pages from @ cuweb/rds@c5df5689696aa03cd4393644eb0bc20dea09ef8d 🚀

--- lib/components/Form/Editor/nodes/InlineImageComponent.d.ts
+++ lib/components/Form/Editor/nodes/InlineImageComponent.d.ts
@@ -1,13 +1,9 @@
 import { NodeKey } from 'lexical';
-import { Position } from './InlineImageNode';
 
-export default function InlineImageComponent({ src, altText, nodeKey, width, height, showCaption, caption, position, }: {
+export default function InlineImageComponent({ src, altText, nodeKey, showCaption, caption, }: {
     altText: string;
     caption: string;
-    height: 'inherit' | number;
     nodeKey: NodeKey;
     showCaption: boolean;
     src: string;
-    width: 'inherit' | number;
-    position: Position;
 }): JSX.Element;
