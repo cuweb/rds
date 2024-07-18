32e64a3197c40bfe810f6d52c149d0116d98c29a
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-07-18 21:05:03 +0000

    Deploying to gh-pages from @ cuweb/rds@f70e8b4766b61ce85288efd43a379f480c93487d 🚀

--- lib/components/Form/Editor/plugins/InlineImagePlugin.d.ts
+++ lib/components/Form/Editor/plugins/InlineImagePlugin.d.ts
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from '../../../../../node_modules/react';
 import { LexicalCommand } from 'lexical';
 import { InlineImagePayload } from './../nodes/InlineImageNode';
 
@@ -6,5 +7,5 @@ export type InsertInlineImagePayload = Readonly<InlineImagePayload>;
 export declare const INSERT_INLINE_IMAGE_COMMAND: LexicalCommand<InlineImagePayload>;
 export default function InlineImagePlugin({ captionsEnabled, setCaptionsEnabled, }: {
     captionsEnabled?: boolean;
-    setCaptionsEnabled?: Dispatch<SetStateAction<boolean>>;
+    setCaptionsEnabled: Dispatch<SetStateAction<boolean>>;
 }): JSX.Element | null;
