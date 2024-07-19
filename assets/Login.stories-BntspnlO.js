39c842dcd19ec055bc49e34f033024834b9a24dc
troychaplin <troychaplin@users.noreply.github.com>
2024-07-19 15:29:53 +0000

    Deploying to gh-pages from @ cuweb/rds@8bfe11bdb62cbb53cb96e102c1b74fef66d7b0b8 🚀

---
+++ assets/Login.stories-BntspnlO.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{S as s}from"./Section-BvwVsgGu.js";import{M as n}from"./Main-B8uQ0PDb.js";import{F as m}from"./FooterDept-zvIRYwAI.js";import{N as r,a as p,b as c}from"./NavData-BNb7tmZ4.js";import{L as l}from"./Login-CPcyjOWp.js";import{F as d}from"./FooterStandard-Qr3Pr6Y8.js";import{F as u}from"./FooterCookie-zBmvFIM7.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./globalClasses-BlHLKr18.js";import"./Button-D5JFhw15.js";import"./Icon-DDs_3nyd.js";import"./optionClasses-BoR2qQUj.js";import"./useLinkContext-Bz4KtVj9.js";import"./Avatar-DmMiCzKE.js";import"./Alert-ihfMe_-R.js";import"./ShieldExclamationIcon-Dq6tIaUn.js";import"./ButtonGroup-CP3XHqUd.js";import"./FooterLogoLinks-wi3yy42J.js";const R={title:"Examples/Blocks"},o={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{children:e.jsxs(r.Primary,{children:[e.jsx(r.Logo,{title:"Web Services",link:"https://carleton.ca/webservices"}),e.jsx(r.Menu,{menu:p}),e.jsx(r.Aside,{menu:c})]})}),e.jsx(n,{children:e.jsx(s,{children:e.jsx(l,{isCenter:!0})})}),e.jsx(m,{buildingName:"Pigiarvik (ᐱᒋᐊᕐᕕᒃ)",deptName:"Information Technology Services",email:"noreply@carleton.ca",footerButtons:[{id:1,title:"Contact support",url:"#"},{id:2,title:"Recent alerts",url:"#"}],officeNumber:"400",phone:"613-520-2600"}),e.jsx(d,{}),e.jsx(u,{})]})};var t,i,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Primary>
+          <Nav.Logo title="Web Services" link="https://carleton.ca/webservices" />
+          <Nav.Menu menu={NavDataSingle} />
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Primary>
+      </Nav>
+
+      <Main>
+        <Section>
+          <Login isCenter />
+        </Section>
+      </Main>
+
+      <FooterDept buildingName="Pigiarvik (ᐱᒋᐊᕐᕕᒃ)" deptName="Information Technology Services" email="noreply@carleton.ca" footerButtons={[{
+      id: 1,
+      title: 'Contact support',
+      url: '#'
+    }, {
+      id: 2,
+      title: 'Recent alerts',
+      url: '#'
+    }]} officeNumber="400" phone="613-520-2600" />
+
+      <FooterStandard />
+      <FooterCookie />
+    </>
+}`,...(a=(i=o.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const w=["LoginPage"];export{o as LoginPage,w as __namedExportsOrder,R as default};
