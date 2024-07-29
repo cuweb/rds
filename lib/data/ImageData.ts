c4deea57b971c86e299872d85446eaa630bb0775
m-vojjala <58004039+m-vojjala@users.noreply.github.com>
2024-07-29 16:37:17 -0400

    Feature/wsdev 4008 image grid (#367)
    * change: created a new component for image grid
    
    * change: added images and content to the image grid
    
    * change: added click feature to the button
    
    * refactor: imagegrid to abstract a and img tags
    
    * change: image grid sub components and props
    
    * change: image width and height
    
    * add: changelog note
    
    ---------
    
    Co-authored-by: Troy Chaplin <troy.chaplin@gmail.com>

---
+++ lib/data/ImageData.ts
@@ -0,0 +1,33 @@
+export const imageData = [
+  {
+    id: 1,
+    image: 'https://picsum.photos/640/400',
+    link: '#',
+    alt: 'Image requirement',
+    span: '2',
+    title: 'Ottawa Bluefest 30',
+    content:
+      'Lorem ipsum odor amet, consectetuer adipiscing elit. Porta pulvinar consectetur faucibus fusce scelerisque nulla!',
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/640/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+  {
+    id: 3,
+    image: 'https://picsum.photos/640/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+  {
+    id: 4,
+    image: 'https://picsum.photos/640/400',
+    link: '#',
+    alt: 'Image requirement',
+    title: 'Ottawa Tulip Festival',
+    content:
+      'Lorem ipsum odor amet, consectetuer adipiscing elit. Porta pulvinar consectetur faucibus fusce scelerisque nulla!',
+  },
+]
