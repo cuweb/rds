3d834d9ea49bff3d942b0d00da95873f3a79db2b
m-vojjala <m-vojjala@users.noreply.github.com>
2024-07-30 19:05:03 +0000

    Deploying to gh-pages from @ cuweb/rds@d4825f7f4bcd2d3ec38b9c6e500f2cd0c455a9e1 🚀

---
+++ assets/Button.stories-SACYuwDu.js
@@ -0,0 +1,55 @@
+import{B as U}from"./Button-6qCVuKzM.js";import{r as d}from"./index-BwDkhjyp.js";import"./jsx-runtime-Nms4Y4qS.js";import"./Icon-DMbNrQi4.js";import"./optionClasses-BBJ5-s7M.js";import"./_commonjsHelpers-BosuxZz1.js";function V({title:p,titleId:g,...K},Q){return d.createElement("svg",Object.assign({xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",strokeWidth:1.5,stroke:"currentColor","aria-hidden":"true","data-slot":"icon",ref:Q,"aria-labelledby":g},K),p?d.createElement("title",{id:g},p):null,d.createElement("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"}))}const e=d.forwardRef(V),ar={title:"Components/Button",component:U,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}},parameters:{controls:{sort:"requiredFirst"}}},r={};r.args={title:"Primary Red",isFull:!1,onClick:()=>{alert("You clicked the button")}};const t={args:{title:"Light Grey Button",color:"grey"}},o={args:{title:"Dark Grey Button",color:"dark-grey"}},a={args:{isDisabled:!0,title:"Disabled Button"}},s={args:{title:"Full Width Button",isFull:!0}},n={args:{title:"Icon Button",icon:e}},i={args:{title:"Small Button",isSmall:!0}},l={args:{title:"Small Button w/Icon",isSmall:!0,icon:e}},c={args:{title:"Button with Aria Label",isSmall:!0,icon:e,ariaLabel:"label"}},u={args:{title:"Button with No Aria Label",isSmall:!0,icon:e}},m={args:{isSmall:!0,icon:e}};var B,S,h;r.parameters={...r.parameters,docs:{...(B=r.parameters)==null?void 0:B.docs,source:{originalSource:"{}",...(h=(S=r.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};var w,b,L;t.parameters={...t.parameters,docs:{...(w=t.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    title: 'Light Grey Button',
+    color: 'grey'
+  }
+}`,...(L=(b=t.parameters)==null?void 0:b.docs)==null?void 0:L.source}}};var y,A,D;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    title: 'Dark Grey Button',
+    color: 'dark-grey'
+  }
+}`,...(D=(A=o.parameters)==null?void 0:A.docs)==null?void 0:D.source}}};var k,I,W;a.parameters={...a.parameters,docs:{...(k=a.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    title: 'Disabled Button'
+  }
+}`,...(W=(I=a.parameters)==null?void 0:I.docs)==null?void 0:W.source}}};var f,C,F;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    title: 'Full Width Button',
+    isFull: true
+  }
+}`,...(F=(C=s.parameters)==null?void 0:C.docs)==null?void 0:F.source}}};var G,N,x;n.parameters={...n.parameters,docs:{...(G=n.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    title: 'Icon Button',
+    icon: ArrowDownCircleIcon
+  }
+}`,...(x=(N=n.parameters)==null?void 0:N.docs)==null?void 0:x.source}}};var E,v,P;i.parameters={...i.parameters,docs:{...(E=i.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    title: 'Small Button',
+    isSmall: true
+  }
+}`,...(P=(v=i.parameters)==null?void 0:v.docs)==null?void 0:P.source}}};var R,T,j;l.parameters={...l.parameters,docs:{...(R=l.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    title: 'Small Button w/Icon',
+    isSmall: true,
+    icon: ArrowDownCircleIcon
+  }
+}`,...(j=(T=l.parameters)==null?void 0:T.docs)==null?void 0:j.source}}};var H,O,_;c.parameters={...c.parameters,docs:{...(H=c.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    title: 'Button with Aria Label',
+    isSmall: true,
+    icon: ArrowDownCircleIcon,
+    ariaLabel: 'label'
+  }
+}`,...(_=(O=c.parameters)==null?void 0:O.docs)==null?void 0:_.source}}};var q,M,Y;u.parameters={...u.parameters,docs:{...(q=u.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    title: 'Button with No Aria Label',
+    isSmall: true,
+    icon: ArrowDownCircleIcon
+  }
+}`,...(Y=(M=u.parameters)==null?void 0:M.docs)==null?void 0:Y.source}}};var Z,z,J;m.parameters={...m.parameters,docs:{...(Z=m.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  args: {
+    isSmall: true,
+    icon: ArrowDownCircleIcon
+  }
+}`,...(J=(z=m.parameters)==null?void 0:z.docs)==null?void 0:J.source}}};const sr=["Primary","LightGrey","DarkGrey","Disabled","FullWidth","HasIcon","SmallButton","SmallButtonWithIcon","ButtonWithAriaLabel","ButtonWithNoAriaLabel","ButtonWithNoAriaLabelNoTitle"];export{c as ButtonWithAriaLabel,u as ButtonWithNoAriaLabel,m as ButtonWithNoAriaLabelNoTitle,o as DarkGrey,a as Disabled,s as FullWidth,n as HasIcon,t as LightGrey,r as Primary,i as SmallButton,l as SmallButtonWithIcon,sr as __namedExportsOrder,ar as default};
--- lib/components/Button/Button.d.ts
+++ lib/components/Button/Button.d.ts
@@ -1,6 +1,16 @@
 import { default as React } from '../../../node_modules/react';
 
-export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+export interface ButtonTitleProps extends React.ComponentPropsWithoutRef<'button'> {
+    title: string;
+    icon?: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, 'ref'>>;
+    color?: 'red' | 'grey' | 'dark-grey' | 'white';
+    type?: 'button' | 'submit' | 'reset';
+    isSmall?: boolean;
+    isFull?: boolean;
+    isDisabled?: boolean;
+    ariaLabel?: string;
+}
+export interface ButtonNoTitleProps extends React.ComponentPropsWithoutRef<'button'> {
     title?: string;
     icon?: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, 'ref'>>;
     color?: 'red' | 'grey' | 'dark-grey' | 'white';
@@ -8,5 +18,6 @@ export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
     isSmall?: boolean;
     isFull?: boolean;
     isDisabled?: boolean;
+    ariaLabel?: string;
 }
-export declare const Button: ({ color, title, icon, type, isSmall, isFull, isDisabled, ...rest }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Button: ({ color, title, icon, type, isSmall, isFull, isDisabled, ariaLabel, ...rest }: ButtonNoTitleProps | ButtonTitleProps) => import("react/jsx-runtime").JSX.Element;
