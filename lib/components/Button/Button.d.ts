df5db226e157680ea1dd9dcb5a27af67937c10cc
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-07-31 13:08:04 +0000

    Deploying to gh-pages from @ cuweb/rds@a8be7baf18ee9147be510329cc051cd65062971f 🚀

--- lib/components/Button/Button.d.ts
+++ lib/components/Button/Button.d.ts
@@ -1,23 +1,20 @@
 import { default as React } from '../../../node_modules/react';
 
-export interface ButtonTitleProps extends React.ComponentPropsWithoutRef<'button'> {
+export interface ButtonProp extends React.ComponentPropsWithoutRef<'button'> {
+    color?: 'red' | 'grey' | 'dark-grey' | 'white';
+    type?: 'button' | 'submit' | 'reset';
+    isSmall?: boolean;
+    isFull?: boolean;
+    isDisabled?: boolean;
+}
+export interface ButtonTitleProps extends ButtonProp {
     title: string;
     icon?: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, 'ref'>>;
-    color?: 'red' | 'grey' | 'dark-grey' | 'white';
-    type?: 'button' | 'submit' | 'reset';
-    isSmall?: boolean;
-    isFull?: boolean;
-    isDisabled?: boolean;
     ariaLabel?: string;
 }
-export interface ButtonNoTitleProps extends React.ComponentPropsWithoutRef<'button'> {
+export interface ButtonNoTitleProps extends ButtonProp {
     title?: string;
-    icon?: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, 'ref'>>;
-    color?: 'red' | 'grey' | 'dark-grey' | 'white';
-    type?: 'button' | 'submit' | 'reset';
-    isSmall?: boolean;
-    isFull?: boolean;
-    isDisabled?: boolean;
-    ariaLabel?: string;
+    icon: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, 'ref'>>;
+    ariaLabel: string;
 }
 export declare const Button: ({ color, title, icon, type, isSmall, isFull, isDisabled, ariaLabel, ...rest }: ButtonNoTitleProps | ButtonTitleProps) => import("react/jsx-runtime").JSX.Element;
