a3707d1eaa2b8be2bd7c49bc9a2e2038efef3249
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2024-08-02 12:56:05 -0400

    add: lexical editor validation with yup

--- lib/components/Form/Form.stories.tsx
+++ lib/components/Form/Form.stories.tsx
@@ -1,5 +1,5 @@
 import React, { MouseEventHandler, useMemo, useState } from 'react'
-import { composeStories, type Meta, type StoryObj } from '@storybook/react'
+import { type Meta, type StoryObj } from '@storybook/react'
 import { useFormik } from 'formik'
 import { FormikHelpers } from 'formik'
 import * as Yup from 'yup'
@@ -8,33 +8,11 @@ import { ButtonGroup } from '../ButtonGroup/ButtonGroup'
 import { Button } from '../Button/Button'
 import { AutoSuggestData } from './../../data/AutoSuggestData'
 
-interface LexicalEditorContent {
-  root: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    children: any[]
-  }
-}
-
-// Custom method to check if Lexical Editor content is not empty
-Yup.addMethod<Yup.MixedSchema<LexicalEditorContent | undefined>>(Yup.mixed, 'lexicalRequired', function (message) {
-  return this.test('lexicalRequired', message, function (value) {
-    const { path, createError } = this
-
-    if (!value) {
-      return createError({ path, message: message ?? 'This field is required' })
-    } else {
-      return true
-    }
-
-    // const isEmpty = value.root.children.length === 0
-
-    // console.log(isEmpty, 'isEmptyisEmpty')
-    // return isEmpty ? createError({ path, message: message ?? 'This field is required' }) : true
-  })
-})
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
 const meta: Meta<typeof Form> = {
   title: 'Components/Form',
+  component: Form,
   tags: ['autodocs'],
   parameters: {
     controls: {
@@ -46,8 +24,6 @@ const meta: Meta<typeof Form> = {
 export default meta
 
 type Story = StoryObj<typeof Form>
-
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
 export const Input: Story = () => {
   type IInput = {
@@ -155,12 +131,13 @@ export const Editor: Story = () => {
   const [editorError, setEditorError] = useState<boolean>(false)
 
   const EditorInitialValues = {
-    editor: '',
+    editor:
+      '<h2>H2 Heading</h2> <p>Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien. </p><p> Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id. </p><img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt=""> <ul> <li>Quis elit egestas venenatis mattis dignissim.</li><li> Cras cras lobortis vitae vivamus ultricies facilisis tempus. </li><li>Orci in sit morbi dignissim metus diam arcu pretium.</li></ul> <p> Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim. </p><h3>H3 Heading</h3> <p> Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam. </p><blockquote> <p> Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. </p></blockquote> <p> Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. </p><h4>H4 Heading</h4> <p> Purus morbi dignissim senectus mattis <a href="#">adipiscing</a> . Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam. </p><p> Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. </p><ol> <li>Quis elit egestas venenatis mattis dignissim.</li><li> Cras cras lobortis vitae vivamus ultricies facilisis tempus. </li><li>Orci in sit morbi dignissim metus diam arcu pretium.</li></ol> <p> Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. </p>',
     inputText: '',
   }
 
   const EditorValidationSchema = Yup.object().shape({
-    editor: Yup.mixed().lexicalRequired('Content is required'),
+    editor: Yup.mixed().required('Content is required'),
     inputText: Yup.string().required('The field is required'),
   })
 
@@ -169,15 +146,10 @@ export const Editor: Story = () => {
   const onSubmit = async (values: IEditor, actions: FormikHelpers<IEditor>) => {
     actions.setSubmitting(true)
     setEditorError(false)
-
-    // if (editorContent === null && requiredEditor == true) {
-    //   // setEditorError(true)
-    // } else {
     await sleep(1000)
-    alert(editorContent)
+    alert(JSON.stringify(values, null, 2))
     console.log(values, editorContent)
     actions.setSubmitting(false)
-    // }
   }
 
   const formikProps = useFormik({
@@ -187,9 +159,7 @@ export const Editor: Story = () => {
   })
 
   // const initialValue =
-  //   '<h2>H2 Heading</h2> <p>Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien. </p><p> Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id. </p><img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt=""> <ul> <li>Quis elit egestas venenatis mattis dignissim.</li><li> Cras cras lobortis vitae vivamus ultricies facilisis tempus. </li><li>Orci in sit morbi dignissim metus diam arcu pretium.</li></ul> <p> Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim. </p><h3>H3 Heading</h3> <p> Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam. </p><blockquote> <p> Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. </p></blockquote> <p> Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. </p><h4>H4 Heading</h4> <p> Purus morbi dignissim senectus mattis <a href="#">adipiscing</a> . Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam. </p><p> Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. </p><ol> <li>Quis elit egestas venenatis mattis dignissim.</li><li> Cras cras lobortis vitae vivamus ultricies facilisis tempus. </li><li>Orci in sit morbi dignissim metus diam arcu pretium.</li></ol> <p> Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. </p>'
-
-  const initialValue = ''
+  //   ''
 
   return (
     <Form formikProps={formikProps}>
@@ -198,7 +168,7 @@ export const Editor: Story = () => {
           label="Editor"
           name="editor"
           helper="Helper Text"
-          value={initialValue}
+          value={EditorInitialValues.editor}
           placeholder="Text goes here..."
           setEditorContent={setEditorContent}
           errorMessage={editorError ? 'Field is required' : ''}
