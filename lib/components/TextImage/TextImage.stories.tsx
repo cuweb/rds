ed64497d68832da92b682fadad9cf2d5a84a9dbb
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-05 18:23:53 -0400

    change: text image stories

--- lib/components/TextImage/TextImage.stories.tsx
+++ lib/components/TextImage/TextImage.stories.tsx
@@ -16,25 +16,37 @@ const meta: Meta<typeof TextImage> = {
 export default meta
 type Story = StoryObj<typeof TextImage>
 
-export const DefaultTemplate: Story = {
-  render: (args) => (
-    <TextImage {...args}>
-      <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266">
-        <p>
-          Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
-          reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
-          in. Praesent quis ligula quis nulla malesuada tempor.
-        </p>
-        <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
-          <Button title="Primary" />
-          <Button title="Secondary" color="grey" />
-        </div>
-      </TextImage.Content>
-    </TextImage>
-  ),
+export const Primary: Story = {
+  args: {
+    maxWidth: '5xl',
+    flexRow: 'lg',
+    flipX: false,
+    flipYsm: false,
+  },
+  render: (args) => {
+    return (
+      <TextImage {...args}>
+        <TextImage.Content title="Website and Application Development" imageUrl="https://picsum.photos/400/266">
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <div className="flex flex-wrap gap-4 mt-6 buttons md:gap-6 md:mt-10">
+            <Button title="Primary" />
+            <Button title="Secondary" color="grey" />
+          </div>
+        </TextImage.Content>
+      </TextImage>
+    )
+  },
 }
 
 export const HeaderOne: Story = {
+  args: {
+    ...Primary.args,
+    maxWidth: '7xl',
+  },
   render: (args) => (
     <TextImage maxWidth="7xl" {...args}>
       <TextImage.Content
@@ -57,8 +69,13 @@ export const HeaderOne: Story = {
 }
 
 export const ImageLeft: Story = {
+  args: {
+    ...Primary.args,
+    maxWidth: '7xl',
+    flipX: true,
+  },
   render: (args) => (
-    <TextImage flipX {...args}>
+    <TextImage {...args}>
       <TextImage.Content
         title="Website and Application Development"
         imageUrl="https://picsum.photos/400/266"
@@ -79,6 +96,9 @@ export const ImageLeft: Story = {
 }
 
 export const NoContent: Story = {
+  args: {
+    ...Primary.args,
+  },
   render: (args) => (
     <TextImage {...args}>
       <TextImage.Content title="Website and Application Development" />
@@ -87,6 +107,9 @@ export const NoContent: Story = {
 }
 
 export const NoImage: Story = {
+  args: {
+    ...Primary.args,
+  },
   render: (args) => (
     <TextImage {...args}>
       <TextImage.Content title="Website and Application Development">
