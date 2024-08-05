bf72bfb8a6f98cb471d39fa08d486d93376b7037
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-05 15:05:34 -0400

    refactor: listing stories

--- lib/components/Listing/Listing.stories.tsx
+++ lib/components/Listing/Listing.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
+import { StackedList } from '../../layouts/StackedList/StackedList'
 import { Listing } from './Listing'
-import { IconDataSingle as iconData } from '../../data/IconData'
-import { EventDataSingle as eventData } from '../../data/EventData'
-import { NewsDataSingle as newsData } from '../../data/NewsData'
-import { PeopleDataSingle as peopleData } from '../../data/PeopleData'
+
+import { NewsData } from '../../data/NewsData'
+import { EventData } from '../../data/EventData'
+import { IconData } from '../../data/IconData'
+import { PeopleData } from '../../data/PeopleData'
 
 const meta: Meta<typeof Listing> = {
   title: 'Components/Listing',
@@ -36,83 +38,270 @@ export const Default: Story = {
   ),
 }
 
-export const NewsListing: Story = {
+export const SingleNewsListing: Story = {
+  args: {},
   render: (args) => (
-    <Listing {...args}>
-      <Listing.Figure>
-        <img src={newsData.image} alt={newsData.alt} width="400" height="300" />
-      </Listing.Figure>
-      <Listing.Body>
-        <Listing.Header title={newsData.title} date={newsData.date} />
-        <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
-        <Listing.Footer>
-          <a href="https://carleton.ca/webservices" className="cu-button cu-button--red cu-button--small">
-            Read more
-          </a>
-        </Listing.Footer>
-      </Listing.Body>
-    </Listing>
+    <StackedList cols="1">
+      {NewsData.slice(0, 1).map(({ id, link, title, image, alt, date }) => (
+        <Listing key={id} {...args}>
+          <Listing.Figure>
+            <img src={image} alt={alt} width="400" height="300" />
+          </Listing.Figure>
+          <Listing.Body>
+            <Listing.Header title={title} date={date} />
+            <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                Read more
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
   ),
 }
 
-export const EventListItem: Story = {
+export const MultipleNewsListings: Story = {
+  args: {},
   render: (args) => (
-    <Listing {...args}>
-      <Listing.DateThumb startDate={eventData.startDate} endDate={eventData.endDate} />
-      <Listing.Body>
-        <Listing.Header title={eventData.title} />
-        <Listing.EventMeta
-          startDateTime={eventData.startDate}
-          endDateTime={eventData.endDate}
-          onCampus={eventData.on_campus}
-          onCampusBuilding={eventData.on_campus_building}
-          onCampusRoomNumber={eventData.on_campus_room_number}
-          eventAddress={eventData.event_address}
-        />
-        <Listing.Footer>
-          <a href="https://carleton.ca/webservices" className="cu-button cu-button--red cu-button--small">
-            Event details
-          </a>
-        </Listing.Footer>
-      </Listing.Body>
-    </Listing>
+    <StackedList cols="2">
+      {NewsData.slice(1, 5).map(({ id, link, title, image, alt, date }) => (
+        <Listing key={id} {...args}>
+          <Listing.Figure>
+            <img src={image} alt={alt} width="400" height="300" />
+          </Listing.Figure>
+          <Listing.Body>
+            <Listing.Header title={title} date={date} />
+            <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                Read more
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
   ),
 }
 
-export const IconListing: Story = {
+export const SingleEventListing: Story = {
+  args: {},
   render: (args) => (
-    <Listing {...args}>
-      <Listing.IconThumb icon={iconData.icon} />
-      <Listing.Body>
-        <Listing.Header title={iconData.title} />
-        <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
-        <Listing.Footer>
-          <a href="https://carleton.ca/webservices" className="cu-button cu-button--red cu-button--small">
-            Get informed
-          </a>
-        </Listing.Footer>
-      </Listing.Body>
-    </Listing>
+    <StackedList cols="1">
+      {EventData.slice(0, 1).map(
+        ({
+          id,
+          title,
+          link,
+          startDate,
+          endDate,
+          on_campus,
+          on_campus_building,
+          on_campus_room_number,
+          event_address,
+        }) => (
+          <Listing key={id} {...args}>
+            <Listing.DateThumb startDate={startDate} endDate={endDate} />
+            <Listing.Body>
+              <Listing.Header title={title} />
+              <Listing.EventMeta
+                startDateTime={startDate}
+                endDateTime={endDate}
+                onCampus={on_campus}
+                onCampusBuilding={on_campus_building}
+                onCampusRoomNumber={on_campus_room_number}
+                eventAddress={event_address}
+              />
+              <Listing.Footer>
+                <a href={link} className="cu-button cu-button--red cu-button--small">
+                  Event details
+                </a>
+              </Listing.Footer>
+            </Listing.Body>
+          </Listing>
+        ),
+      )}
+    </StackedList>
   ),
 }
 
-export const PeopleListing: Story = {
+export const MultipleEventListings: Story = {
+  args: {},
   render: (args) => (
-    <Listing {...args}>
-      <Listing.Figure isSquare>
-        <img src={peopleData.image} alt={peopleData.alt} width="280" height="280" />
-      </Listing.Figure>
-      <Listing.Body>
-        <Listing.Header title={`${peopleData.firstName} ${peopleData.lastName}`} />
-        <Listing.PeopleMeta jobTitle={peopleData.jobTitle} phone={peopleData.phone}>
-          <a href={`mailto:${peopleData.email}`}>{peopleData.email}</a>
-        </Listing.PeopleMeta>
-        <Listing.Footer>
-          <a href="https://carleton.ca/webservices" className="cu-button cu-button--red cu-button--small">
-            View profile
-          </a>
-        </Listing.Footer>
-      </Listing.Body>
-    </Listing>
+    <StackedList cols="2">
+      {EventData.slice(1, 5).map(
+        ({
+          id,
+          title,
+          link,
+          startDate,
+          endDate,
+          on_campus,
+          on_campus_building,
+          on_campus_room_number,
+          event_address,
+        }) => (
+          <Listing key={id} {...args}>
+            <Listing.DateThumb startDate={startDate} endDate={endDate} />
+            <Listing.Body>
+              <Listing.Header title={title} />
+              <Listing.EventMeta
+                startDateTime={startDate}
+                endDateTime={endDate}
+                onCampus={on_campus}
+                onCampusBuilding={on_campus_building}
+                onCampusRoomNumber={on_campus_room_number}
+                eventAddress={event_address}
+              />
+              <Listing.Footer>
+                <a href={link} className="cu-button cu-button--red cu-button--small">
+                  Event details
+                </a>
+              </Listing.Footer>
+            </Listing.Body>
+          </Listing>
+        ),
+      )}
+    </StackedList>
+  ),
+}
+
+export const SingleIconListing: Story = {
+  args: {},
+  render: (args) => (
+    <StackedList cols="1">
+      {IconData.slice(0, 1).map(({ id, link, title, icon }) => (
+        <Listing key={id} {...args}>
+          <Listing.IconThumb icon={icon} />
+          <Listing.Body>
+            <Listing.Header title={title} />
+            <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                Get informed
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
+  ),
+}
+
+export const MultipleIconListing: Story = {
+  args: {},
+  render: (args) => (
+    <StackedList cols="2">
+      {IconData.slice(1, 5).map(({ id, link, title, icon }) => (
+        <Listing key={id} {...args}>
+          <Listing.IconThumb icon={icon} />
+          <Listing.Body>
+            <Listing.Header title={title} />
+            <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                Get informed
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
+  ),
+}
+
+export const SinglePageListing: Story = {
+  args: {},
+  render: (args) => (
+    <StackedList cols="1">
+      {NewsData.slice(0, 1).map(({ id, link, title, date }) => (
+        <Listing key={id} {...args}>
+          <Listing.Body>
+            <Listing.Header title={title} date={date} datePrefix="Modified on " datePosition="bottom" />
+            <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                Read more
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
+  ),
+}
+
+export const MultiplePageListing: Story = {
+  args: {},
+  render: (args) => (
+    <StackedList cols="2">
+      {NewsData.slice(1, 5).map(({ id, link, title, date }) => (
+        <Listing key={id} {...args}>
+          <Listing.Body>
+            <Listing.Header title={title} date={date} datePrefix="Modified on " datePosition="bottom" />
+            <Listing.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                Read more
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
+  ),
+}
+
+export const SinglePeopleListing: Story = {
+  args: {},
+  render: (args) => (
+    <StackedList cols="1">
+      {PeopleData.slice(0, 1).map(({ id, link, image, alt, firstName, lastName, jobTitle, email, phone }) => (
+        <Listing key={id} {...args}>
+          <Listing.Figure isSquare>
+            <img src={image} alt={alt} width="280" height="280" />
+          </Listing.Figure>
+          <Listing.Body>
+            <Listing.Header title={`${firstName} ${lastName}`} />
+            <Listing.PeopleMeta jobTitle={jobTitle} phone={phone}>
+              <a href={`mailto:${email}`}>{email}</a>
+            </Listing.PeopleMeta>
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                View profile
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
+  ),
+}
+
+export const MultiplePeopleListing: Story = {
+  args: {},
+  render: (args) => (
+    <StackedList cols="2">
+      {PeopleData.slice(1, 5).map(({ id, link, image, alt, firstName, lastName, jobTitle, email, phone }) => (
+        <Listing key={id} {...args}>
+          <Listing.Figure isSquare>
+            <img src={image} alt={alt} width="280" height="280" />
+          </Listing.Figure>
+          <Listing.Body>
+            <Listing.Header title={`${firstName} ${lastName}`} />
+            <Listing.PeopleMeta jobTitle={jobTitle} phone={phone}>
+              <a href={`mailto:${email}`}>{email}</a>
+            </Listing.PeopleMeta>
+            <Listing.Footer>
+              <a href={link} className="cu-button cu-button--red cu-button--small">
+                View profile
+              </a>
+            </Listing.Footer>
+          </Listing.Body>
+        </Listing>
+      ))}
+    </StackedList>
   ),
 }
