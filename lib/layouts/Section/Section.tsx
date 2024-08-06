6d40098865c0b30ffe605bd8589b621687335976
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-05 22:00:37 -0400

    change: cleanup

--- lib/layouts/Section/Section.tsx
+++ lib/layouts/Section/Section.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { maxWidthClasses } from '../../utils/propClasses'
-import { layoutSpacing } from '../../utils/globalClasses'
+
+const layoutSpacing = {
+  main: 'px-8 py-6 md:py-10 md:px-10',
+  containerWhite: 'bg-white',
+  containerGrey: 'max-w-screen-2xl mx-auto bg-cu-black-50 rounded-xl px-6 py-5 md:px-14 md:py-12',
+}
 
 type maxWidthKeys = keyof typeof maxWidthClasses
 
