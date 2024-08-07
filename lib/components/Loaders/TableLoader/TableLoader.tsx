0d8ee6686370ac9856954f3b0b2882d88a9a3c81
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2024-08-07 17:41:12 -0400

    refactor: table loader and fix the key prop error

--- lib/components/Loaders/TableLoader/TableLoader.tsx
+++ lib/components/Loaders/TableLoader/TableLoader.tsx
@@ -8,21 +8,14 @@ interface TableLoaderProps {
 export const TableLoader: React.FC<TableLoaderProps> = ({ numRow = 5, numCol = 5 }) => {
   // Generate table rows with span elements for each column
   const rows = Array.from({ length: numRow }, (_, rowIndex) => (
-    <>
-      {rowIndex % 2 === 0 ? (
-        <div key={rowIndex} className="flex space-x-1">
-          {Array.from({ length: numCol }, (_, colIndex) => (
-            <span key={colIndex} className="animate-pulse grow h-12 bg-cu-black-100"></span>
-          ))}
-        </div>
-      ) : (
-        <div key={rowIndex} className="flex space-x-1">
-          {Array.from({ length: numCol }, (_, colIndex) => (
-            <span key={colIndex} className="animate-pulse grow h-12 bg-cu-black-50"></span>
-          ))}
-        </div>
-      )}
-    </>
+    <div key={rowIndex} className="flex space-x-1">
+      {Array.from({ length: numCol }, (_, colIndex) => (
+        <span
+          key={colIndex}
+          className={`animate-pulse grow h-12 ${rowIndex % 2 === 0 ? 'bg-cu-black-100' : 'bg-cu-black-50'}`}
+        ></span>
+      ))}
+    </div>
   ))
 
   return <div className="cu-loader-table cu-component rounded-md overflow-hidden grid gap-1">{rows}</div>
