3113edffc5af8f909b8553bc621ddf632ea25f7c
troychaplin <troychaplin@users.noreply.github.com>
2024-08-07 13:43:45 +0000

    Deploying to gh-pages from @ cuweb/rds@7fcb2f5b7b38329a5e3b568493c91660f191241d 🚀

---
+++ assets/Alert.stories-B-xTnyLW.js
@@ -0,0 +1,11 @@
+import{j as s}from"./jsx-runtime-Nms4Y4qS.js";import{M as d}from"./Main-CdpHQN4R.js";import{A as c}from"./Alert-BikTafhT.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ShieldExclamationIcon-Dq6tIaUn.js";const j={title:"Components/Alert",component:c,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},r={};r.args={title:"Alert title",type:"success",content:"Records have been updated.",size:"sm"};const e={args:{...r.args,size:"lg"},render:p=>s.jsx(d,{children:s.jsx(c,{...p})})};var t,o,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var n,m,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    size: 'lg'
+  },
+  render: args => {
+    return <Main>
+        <Alert {...args} />
+      </Main>;
+  }
+}`,...(i=(m=e.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const y=["Primary","TwoColumns"];export{r as Primary,e as TwoColumns,y as __namedExportsOrder,j as default};
