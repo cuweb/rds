5f7d3e0e7018af9afd06ab03ec0e65f46cc2208a
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-16 11:50:24 -0400

    refactor: wave graphic names and classes

--- lib/components/Footer/FooterBasic/FooterBasic.tsx
+++ lib/components/Footer/FooterBasic/FooterBasic.tsx
@@ -3,7 +3,7 @@ import { FooterLogoLinks } from '../FooterLogoLinks/FooterLogoLinks'
 export const FooterBasic = () => {
   return (
     <footer
-      className="mt-auto mb-0 pt-8 bg-bottom bg-cover cu-footerbasic not-prose bg-cu-black-900 bg-cu-waves-red sm:pt-12"
+      className="mt-auto mb-0 pt-8 bg-bottom bg-cover cu-footerbasic not-prose bg-cu-black-900 bg-cu-waves-footer-red sm:pt-12"
       aria-labelledby="footer-heading"
     >
       <h2 id="footer-heading" className="bg-white sr-only text-cu-black-900">
