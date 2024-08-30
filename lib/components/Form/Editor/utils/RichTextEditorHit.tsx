8f94b2e0bdbbf8101b752be5ac9cbc20de995ceb
Bhoomi-Pipalia <56330259+Bhoomi-Pipalia@users.noreply.github.com>
2024-08-30 15:36:45 -0400

    remove: lexical and AWS function (#389)
    * remove: lexical and AWS function
    
    * add: changelog
    
    * remove: editor story

--- lib/components/Form/Editor/utils/RichTextEditorHit.tsx
+++
@@ -1,5 +0,0 @@
-const RichTextEditorHit = ({ captionsEnabled, placeholder }: { captionsEnabled: boolean; placeholder: string }) => {
-  return captionsEnabled ? <span className="hint-text">{placeholder}</span> : null
-}
-
-export default RichTextEditorHit
--- lib/components/Form/Editor/utils/insertInlineImageCommand.ts
+++
@@ -1,5 +0,0 @@
-import { InlineImagePayload } from './../nodes/InlineImageNode'
-import { createCommand, LexicalCommand } from 'lexical'
-
-export const INSERT_INLINE_IMAGE_COMMAND: LexicalCommand<InlineImagePayload> =
-  createCommand('INSERT_INLINE_IMAGE_COMMAND')
