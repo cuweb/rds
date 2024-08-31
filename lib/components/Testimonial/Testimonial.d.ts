19a749802534c056f79437e4f990e1a1429e9233
troychaplin <troychaplin@users.noreply.github.com>
2024-08-31 14:27:57 +0000

    Deploying to gh-pages from @ cuweb/rds@25611a0d8c7972453e34ca1bf4cc024770466fcb 🚀

--- lib/components/Testimonial/Testimonial.d.ts
+++ lib/components/Testimonial/Testimonial.d.ts
@@ -1,11 +1,9 @@
 import { default as React } from '../../../node_modules/react';
 export interface TestimonialProps {
     children: React.ReactNode;
-    name: string;
     imageUrl: string;
     imageZoom?: number;
     focalPointX?: number;
     focalPointY?: number;
-    quoteType?: 'border' | 'quote';
 }
-export declare const Testimonial: ({ children, name, imageUrl, imageZoom, focalPointX, focalPointY, quoteType, }: TestimonialProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Testimonial: ({ children, imageUrl, imageZoom, focalPointX, focalPointY, }: TestimonialProps) => import("react/jsx-runtime").JSX.Element;
