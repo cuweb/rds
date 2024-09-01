063d5cb0810d8814eb6da41dd6e3b098bb887de3
troychaplin <troychaplin@users.noreply.github.com>
2024-09-01 13:48:17 +0000

    Deploying to gh-pages from @ cuweb/rds@d9fb9322709175322e63c740e29562a6bdac8129 🚀

--- lib/components/FullBanner/FullBanner.d.ts
+++ lib/components/FullBanner/FullBanner.d.ts
@@ -5,11 +5,12 @@ export interface FullBannerProps {
     children?: React.ReactNode;
     as?: 'section' | 'div';
     title: string;
+    headerType?: 'h1' | 'h2';
     image?: string;
     focalPointX?: number;
     focalPointY?: number;
     justify?: justifyContentKeys;
     opacity?: (typeof opacityValues)[number];
 }
-export declare const FullBanner: ({ children, as, title, image, focalPointX, focalPointY, opacity, justify, }: FullBannerProps) => import("react/jsx-runtime").JSX.Element;
+export declare const FullBanner: ({ children, as, title, headerType, image, focalPointX, focalPointY, opacity, justify, }: FullBannerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
