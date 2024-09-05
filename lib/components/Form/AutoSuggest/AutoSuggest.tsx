ef0f2c0624c7d43296adf2a1c7b8cfe40fafb016
Ish <ishdeep.711@gmail.com>
2024-09-05 12:00:44 -0400

    change: auto suggestion reset fix

--- lib/components/Form/AutoSuggest/AutoSuggest.tsx
+++ lib/components/Form/AutoSuggest/AutoSuggest.tsx
@@ -2,7 +2,7 @@ import { useField, useFormikContext } from 'formik'
 import Select from 'react-select'
 import { FieldComponentProps } from '../FormField/FormField'
 import useErrorClass from '../UseError'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 interface Option {
   value: string
@@ -15,32 +15,28 @@ export interface AutoSuggestProps extends FieldComponentProps {
 }
 
 export const AutoSuggest = ({ ...props }: AutoSuggestProps) => {
-  const { name, options, disabled = false, ...rest } = props;
-  const [field, meta] = useField(name);
-  const { setFieldValue } = useFormikContext<unknown>();
-  const errorClass = useErrorClass(name);
+  const { name, options, disabled = false, ...rest } = props
+  const [field, meta] = useField(name)
+  const { setFieldValue } = useFormikContext<unknown>()
+  const errorClass = useErrorClass(name)
 
+  const [selectedOption, setSelectedOption] = useState<Option | null>(null)
 
-
-  const [selectedOption, setSelectedOption] = useState<Option | null>(null);
-
-  
   useEffect(() => {
-    const initialOption = options?.find((option) =>   option.value === field.value)
-    
+    const initialOption = options?.find((option) => option.value === field.value)
+
     setSelectedOption(initialOption || null)
   }, [field.value, options])
 
-  console.log("selectedOption", selectedOption)
-  
-    useEffect(() => {
+  console.log('selectedOption', selectedOption)
+
+  useEffect(() => {
     if (meta.touched && !meta.value) {
       setFieldValue(name, '')
       setSelectedOption(null)
     }
-    }, [meta.touched, meta.value])
-  
-  
+  }, [meta.touched, meta.value])
+
   // value={options?.find((option) => option.value === field.value)}
 
   return (
@@ -49,7 +45,10 @@ export const AutoSuggest = ({ ...props }: AutoSuggestProps) => {
       inputId={name}
       options={options}
       value={selectedOption}
-      onChange={(option: Option | null) => { setFieldValue(name, option?.value); setSelectedOption(option) }}
+      onChange={(option: Option | null) => {
+        setFieldValue(name, option?.value)
+        setSelectedOption(option)
+      }}
       onBlur={field.onBlur}
       isSearchable={true}
       isClearable={true}
