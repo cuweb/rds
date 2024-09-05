6fe49eb2a22d74ad0c124b9e64b1894dff78f136
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-09-05 20:17:06 +0000

    Deploying to gh-pages from @ cuweb/rds@dc597648df2ebab993f1ac49b6a3712416405bb0 🚀

---
+++ assets/Figure.stories-B5yzBbex.js
@@ -0,0 +1,102 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{M as P}from"./Main-CJV9MMXx.js";import{S as f}from"./Section-CO2ge5vF.js";import{F as t}from"./Figure-C9uxvYMD.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DpwMMZfN.js";import"./iframe-CGHlEIyM.js";import"../sb-preview/runtime.js";import"./index-Dk74W0Oi.js";import"./propClasses-Im8HJTiC.js";const U={title:"Components/Figure",component:t,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},s={args:{caption:"",size:"full",align:"none",rounded:"none",noMobile:!1},render:i=>e.jsxs(f,{children:[e.jsx(t,{...i,children:e.jsx("img",{src:"https://picsum.photos/id/15/1600/900",alt:"Sample for figure stories file",width:"400",height:"266"})}),i.align!=="none"&&e.jsxs("div",{children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit. Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero."}),e.jsx("p",{children:"Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit."})]})]})},u={args:{...s.args,size:"md",align:"right",caption:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at aliquet lorem, id lobortis eros. Maecenas erat nibh, imperdiet vel fermentum ut, posuere faucibus dolor."},render:i=>e.jsx(P,{children:e.jsxs(f,{children:[e.jsx(t,{...i,children:e.jsx("img",{src:"https://picsum.photos/id/15/1600/900",alt:"Sample for figure stories file",width:"400",height:"266"})}),e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit. Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero."}),e.jsx("p",{children:"Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit."})]})})},a={args:{...s.args},render:i=>e.jsx(t,{...i,children:e.jsx(t.Video,{source:"https://www.youtube.com/watch?v=gtEJtKwUGiU"})})},r={args:{...s.args,size:"md",align:"right",caption:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at aliquet lorem, id lobortis eros."},render:i=>e.jsxs(e.Fragment,{children:[e.jsx(t,{...i,children:e.jsx(t.Video,{source:"https://www.youtube.com/watch?v=gtEJtKwUGiU"})}),e.jsxs("div",{className:"cu-prose",children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit. Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero."}),e.jsx("p",{children:"Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit."})]})]})};var l,o,m;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    caption: '',
+    size: 'full',
+    align: 'none',
+    rounded: 'none',
+    noMobile: false
+  },
+  render: args => {
+    return <Section>
+        <Figure {...args}>
+          <img src="https://picsum.photos/id/15/1600/900" alt="Sample for figure stories file" width="400" height="266" />
+        </Figure>
+        {args.align !== 'none' && <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+              egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada
+              metus, et tristique dolor. Suspendisse vestibulum hendrerit. Aliquam blandit tellus odio, nec commodo est
+              efficitur sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit
+              dui sed quam. Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero.
+            </p>
+            <p>
+              Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+              lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+              tempus. Phasellus in egestas sapien ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et
+              semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit.
+            </p>
+          </div>}
+      </Section>;
+  }
+}`,...(m=(o=s.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};var n,d,c;u.parameters={...u.parameters,docs:{...(n=u.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    size: 'md',
+    align: 'right',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at aliquet lorem, id lobortis eros. Maecenas erat nibh, imperdiet vel fermentum ut, posuere faucibus dolor.'
+  },
+  render: args => {
+    return <Main>
+        <Section>
+          <Figure {...args}>
+            <img src="https://picsum.photos/id/15/1600/900" alt="Sample for figure stories file" width="400" height="266" />
+          </Figure>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit. Aliquam blandit tellus odio, nec commodo est efficitur
+            sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam.
+            Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero.
+          </p>
+          <p>
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et
+            semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+        </Section>
+      </Main>;
+  }
+}`,...(c=(d=u.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var p,g,q;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args
+  },
+  render: args => {
+    return <Figure {...args}>
+        <Figure.Video source="https://www.youtube.com/watch?v=gtEJtKwUGiU" />
+      </Figure>;
+  }
+}`,...(q=(g=a.parameters)==null?void 0:g.docs)==null?void 0:q.source}}};var v,h,b;r.parameters={...r.parameters,docs:{...(v=r.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    size: 'md',
+    align: 'right',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at aliquet lorem, id lobortis eros.'
+  },
+  render: args => {
+    return <>
+        <Figure {...args}>
+          <Figure.Video source="https://www.youtube.com/watch?v=gtEJtKwUGiU" />
+        </Figure>
+
+        <div className="cu-prose">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue turpis, feugiat vitae viverra in,
+            egestas vitae nulla. Quisque auctor ultrices mauris, et semper urna aliquam quis. Duis sed malesuada metus,
+            et tristique dolor. Suspendisse vestibulum hendrerit. Aliquam blandit tellus odio, nec commodo est efficitur
+            sit amet. Proin molestie, risus in mollis laoreet, lectus dui egestas augue, eu maximus velit dui sed quam.
+            Pellentesque iaculis suscipit libero gravida tempus. Phasellus in egestas sapien ac libero.
+          </p>
+          <p>
+            Aliquam blandit tellus odio, nec commodo est efficitur sit amet. Proin molestie, risus in mollis laoreet,
+            lectus dui egestas augue, eu maximus velit dui sed quam. Pellentesque iaculis suscipit libero gravida
+            tempus. Phasellus in egestas sapien ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aliquam augue turpis, feugiat vitae viverra in, egestas vitae nulla. Quisque auctor ultrices mauris, et
+            semper urna aliquam quis. Duis sed malesuada metus, et tristique dolor. Suspendisse vestibulum hendrerit.
+          </p>
+        </div>
+      </>;
+  }
+}`,...(b=(h=r.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};const V=["Primary","WithCaption","Video","VideoAlignCaption"];export{s as Primary,a as Video,r as VideoAlignCaption,u as WithCaption,V as __namedExportsOrder,U as default};
