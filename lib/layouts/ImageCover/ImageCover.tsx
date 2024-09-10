b034a86b012b32b576bcc6c6e0f6c6fc3e075a64
Troy Chaplin <troy.chaplin@gmail.com>
2024-09-10 15:41:01 -0400

    fix: image cover wave image

--- lib/layouts/ImageCover/ImageCover.tsx
+++ lib/layouts/ImageCover/ImageCover.tsx
@@ -33,30 +33,21 @@ export const ImageCover = ({
     transform: `scale(1.${imageZoom})`,
   }
 
-  // const bgImgClass =
-  //   'bg-[url("https://cu-production.s3.amazonaws.com/rds/assets/graphics/cu-waves-repeating-bottom-red.svg")] bg-[length:200px] md:bg-[length:300px] xl:bg-[length:400px] bg-repeat-x bg-bottom'
-
-  const bgImgClassNew =
-    'bg-[url("https://cu-production.s3.amazonaws.com/rds/assets/graphics/cu-waves-repeating-bottom-red.svg")] bg-repeat-x bg-bottom'
-
-  // const bgImgClass =
-  //   'bg-cu-waves-repeating-bottom-red bg-[length:200px] md:bg-[length:300px] xl:bg-[length:400px] bg-repeat-x bg-bottom'
+  const bgImgClass =
+    'bg-cu-waves-repeating-bottom-red bg-[length:200px] md:bg-[length:300px] xl:bg-[length:400px] bg-repeat-x bg-bottom'
 
   return (
     <SectionComponent
-      className={`cu-imagecover relative  bg-cover bg-no-repeat cu-no-browsers-edge not-contained`}
+      className={`cu-imagecover bg-cover bg-no-repeat cu-no-browsers-edge not-contained`}
       style={mainBgImage}
     >
-      {/* <div className={`absolute top-0 bottom-0 z-10 w-full h-full bg-orange`} /> */}
-
       <div
-        className={`cu-imagecover-content cu-prose cu-prose-dark cu-prose-first-last relative ${childWidth} ${childPadding} mx-auto z-30 bg-white/80 ${bgImgClassNew}`}
+        className={`cu-imagecover-content cu-prose cu-prose-dark cu-prose-first-last ${childWidth} ${childPadding} mx-auto bg-white/80 `}
       >
         {children}
       </div>
 
-      {/* <div className="absolute z-10 w-full h-full bg-white/85 bg-cu-waves-repeating-bottom-red bg-[length:200px] md:bg-[length:300px] xl:bg-[length:400px] bg-repeat-x bg-bottom" /> */}
-      {/* <div className="absolute z-10 top-0 left-0 w-full h-full bg-white/85 bg-cu-waves-repeating-bottom-red bg-[length:195px] md:bg-[length:319px] lg:bg-[length:402px] bg-repeat-x bg-bottom" /> */}
+      <div className={`absolute bottom-0 h-full w-full ${bgImgClass} -mb-1`} />
     </SectionComponent>
   )
 }
