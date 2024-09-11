157c2c533ee7e7bdea9d27670c0db4b0753b9e7b
troychaplin <troychaplin@users.noreply.github.com>
2024-09-11 18:43:21 +0000

    Deploying to gh-pages from @ cuweb/rds@07f45cdd79b1b4533e6a19837f4195bb6a49d091 🚀

--- lib/layouts/ImageCover/ImageCover.d.ts
+++ lib/layouts/ImageCover/ImageCover.d.ts
@@ -1,14 +1,16 @@
 import { default as React } from '../../../node_modules/react';
 import { maxWidthClasses } from '../../utils/propClasses';
 type maxWidthKeys = keyof typeof maxWidthClasses;
+declare const opacityValues: number[];
 export interface ImageCoverProps {
     children?: React.ReactNode;
     as?: 'section' | 'div';
     maxWidth?: maxWidthKeys;
     image: string;
     imageZoom?: number;
+    opacity?: (typeof opacityValues)[number];
     focalPointX?: string;
     focalPointY?: string;
 }
-export declare const ImageCover: ({ children, as, maxWidth, image, imageZoom, focalPointX, focalPointY, }: ImageCoverProps) => import("react/jsx-runtime").JSX.Element;
+export declare const ImageCover: ({ children, as, maxWidth, image, imageZoom, opacity, focalPointX, focalPointY, }: ImageCoverProps) => import("react/jsx-runtime").JSX.Element;
 export {};
