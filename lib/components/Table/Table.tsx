ebadc6913e74b25896b2b31fc86c2b43cb7c69ff
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2024-09-11 14:17:46 -0400

    add: custom column option to table

--- lib/components/Table/Table.tsx
+++ lib/components/Table/Table.tsx
@@ -16,6 +16,7 @@ export interface TableProps {
     [k: string]: string | number | JSX.Element
   }[]
   columns: ColumnDefinitionType[]
+  colgroup?: number[]
   hasStripes?: boolean
   noWordBreak?: boolean
   range?: number[]
@@ -25,6 +26,7 @@ export interface TableProps {
 export const Table = ({
   data,
   columns,
+  colgroup,
   hasStripes = false,
   noWordBreak = false,
   range = [1, -1],
@@ -48,6 +50,13 @@ export const Table = ({
   return (
     <div className={`cu-table cu-component not-prose overflow-x-auto rounded-lg shadow-lg`}>
       <table className="min-w-full table-auto cu-table">
+        {colgroup && (
+          <colgroup>
+            {colgroup.map((width, index) => (
+              <col key={`col-${index}`} style={{ width: `${width}%` }}></col>
+            ))}
+          </colgroup>
+        )}
         <TableHeader columns={columns} noWordBreak={noWordBreak} sortData={setTableData} />
         <TableRows
           data={tableData}
