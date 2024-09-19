94579fa398d2554d348a0b78e9d86947403c9520
troychaplin <15524465+troychaplin@users.noreply.github.com>
2024-09-19 10:37:35 +0000

    Deploying to gh-pages from @ cuweb/rds@e081e51988d4eff1a5647a06d14652ac8bce0c1b 🚀

--- lib/components/Table/Table.d.ts
+++ lib/components/Table/Table.d.ts
@@ -11,11 +11,11 @@ export interface TableProps {
     data: {
         [k: string]: string | number | JSX.Element;
     }[];
-    colgroup?: number[];
     columns: ColumnDefinitionType[];
+    colgroup?: number[];
     hasStripes?: boolean;
     noWordBreak?: boolean;
     range?: number[];
     enableRowHeader?: boolean;
 }
-export declare const Table: ({ data, colgroup, columns, hasStripes, noWordBreak, range, enableRowHeader, }: TableProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Table: ({ data, columns, colgroup, hasStripes, noWordBreak, range, enableRowHeader, }: TableProps) => import("react/jsx-runtime").JSX.Element;
