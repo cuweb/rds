d2461ebe59a13f20acabde13f1de9c7e6ed9b4dc
troychaplin <troychaplin@users.noreply.github.com>
2024-09-19 10:38:30 +0000

    Deploying to gh-pages from @ cuweb/rds@163871e4e814417c53c6c197f5eeac6317c6e89a 🚀

---
+++ assets/ImageGrid.stories-Ct0C8eel.js
@@ -0,0 +1,98 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{S as s}from"./Section-BA8VpE3g.js";import{M as n}from"./Main-CJV9MMXx.js";import{F as o}from"./FooterStandard-B4w2ktcd.js";import{N as i,a as m}from"./NavData-DDa_KIlN.js";import{T as u}from"./TextImage-C30QtQK4.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FooterLogoLinks-wi3yy42J.js";import"./useLinkContext-Bz4KtVj9.js";import"./Avatar-DmMiCzKE.js";import"./ButtonGroup-CnxC-BVQ.js";import"./propClasses-Im8HJTiC.js";const A={title:"Prototypes/Concepts"},t={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(i,{children:e.jsxs(i.Primary,{children:[e.jsx(i.Logo,{title:"Web Services",link:"https://carleton.ca/webservices"}),e.jsx(i.Aside,{menu:m})]})}),e.jsx(n,{children:e.jsxs(s,{children:[e.jsx(u,{children:e.jsx(u.Content,{headerType:"h1",title:"News Listing",children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})})}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx(s,{as:"div",children:e.jsxs("div",{className:"grid grid-cols-4 gap-5",children:[e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')] col-span-2 row-span-2"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"}),e.jsx("div",{className:"aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"})]})}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus."}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."})]})}),e.jsx(o,{})]})};var a,r,l;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Primary>
+          <Nav.Logo title="Web Services" link="https://carleton.ca/webservices" />
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Primary>
+      </Nav>
+
+      <Main>
+        <Section>
+          <TextImage>
+            <TextImage.Content headerType="h1" title="News Listing">
+              <p>
+                Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+                reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores
+                accusamus in. Praesent quis ligula quis nulla malesuada tempor.
+              </p>
+            </TextImage.Content>
+          </TextImage>
+
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+
+          <Section as="div">
+            <div className="grid grid-cols-4 gap-5">
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')] col-span-2 row-span-2"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+              <div className="aspect-[3/4] bg-green-500 bg-cover bg-center bg-[url('https://picsum.photos/id/431/600/460')]"></div>
+            </div>
+          </Section>
+
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor.
+          </p>
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+          <p>
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Nobis voluptatem dolorum et eum doloremque cupiditate
+            velit. Praesentium architecto a distinctio aut reprehenderit ducimus.
+          </p>
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+        </Section>
+      </Main>
+
+      <FooterStandard />
+    </>
+}`,...(l=(r=t.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const M=["GridImages"];export{t as GridImages,M as __namedExportsOrder,A as default};
--- lib/components/Table/Table.d.ts
+++ lib/components/Table/Table.d.ts
@@ -11,11 +11,11 @@ export interface TableProps {
     data: {
         [k: string]: string | number | JSX.Element;
     }[];
-    columns: ColumnDefinitionType[];
     colgroup?: number[];
+    columns: ColumnDefinitionType[];
     hasStripes?: boolean;
     noWordBreak?: boolean;
     range?: number[];
     enableRowHeader?: boolean;
 }
-export declare const Table: ({ data, columns, colgroup, hasStripes, noWordBreak, range, enableRowHeader, }: TableProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Table: ({ data, colgroup, columns, hasStripes, noWordBreak, range, enableRowHeader, }: TableProps) => import("react/jsx-runtime").JSX.Element;
