49dffd82c06a9de6cd48ac1412e8eb01204dc9b4
troychaplin <troychaplin@users.noreply.github.com>
2024-09-23 20:07:24 +0000

    Deploying to gh-pages from @ cuweb/rds@298fc747d803a9340bc844099d3f395d9bc9d94f 🚀

--- assets/Badge.stories-Bs37r5I_.js
+++
@@ -1,9 +0,0 @@
-import{B as t}from"./Badge-CcCXuzfr.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useLinkContext-Bz4KtVj9.js";import"./propClasses-DxtanR7D.js";const i={title:"Components/Badge",component:t,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},r={args:{text:"Badge Example",link:"",rounded:"full",color:"grey",noWordBreak:!1}};var e,o,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    text: 'Badge Example',
-    link: '',
-    rounded: 'full',
-    color: 'grey',
-    noWordBreak: false
-  }
-}`,...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const c=["Primary"];export{r as Primary,c as __namedExportsOrder,i as default};
--- lib/components/Badge/Badge.d.ts
+++ lib/components/Badge/Badge.d.ts
@@ -4,7 +4,7 @@ export interface BadgeProps {
     text: string;
     link?: string;
     rounded?: borderRadiusKeys;
-    color?: 'grey' | 'white' | 'black60' | 'white60' | 'green' | 'red' | 'yellow' | 'blue' | 'purple';
+    color?: 'grey' | 'white' | 'black80' | 'white80' | 'green' | 'red' | 'yellow' | 'blue' | 'purple';
     noWordBreak?: boolean;
 }
 export declare const Badge: ({ text, link, rounded, color, noWordBreak }: BadgeProps) => import("react/jsx-runtime").JSX.Element;
