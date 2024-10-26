cdc630cdd38ce8a962e23633be3fb65f989cfffd
troychaplin <troychaplin@users.noreply.github.com>
2024-10-26 13:44:09 +0000

    Deploying to gh-pages from @ cuweb/rds@66a164fd8faac56a13396735ea599e519cf294c8 🚀

---
+++ assets/NewsBadges.stories-DOttXZAW.js
@@ -0,0 +1,78 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{S as s}from"./Section-BA8VpE3g.js";import{M as l}from"./Main-CJV9MMXx.js";import{F as d}from"./FooterStandard-B4CWoQhG.js";import{N as a}from"./Nav-JKqsZN2Y.js";import{C as m}from"./Column-BXgQPcQc.js";import{T as r}from"./TextMedia-Be2xK1l3.js";import{B as c}from"./BadgeGroup-De9U_dWS.js";import{B as p}from"./Badge-BizDSC7S.js";import{C as t}from"./Card-CgDw_n16.js";import{N as h}from"./NavData-EgWmcfkt.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FooterLogoLinks-FVafPX33.js";import"./useLinkContext-Bz4KtVj9.js";import"./Avatar-DmMiCzKE.js";import"./ButtonGroup-02ebpWXM.js";import"./propClasses-C1Ju-ucV.js";import"./PageHeader-PuVLkLqK.js";import"./parseISO-B7ihSCno.js";import"./format-IKGLl-Aq.js";import"./getDate-B4BU6gpK.js";import"./parse-Dkc84rVP.js";import"./index-D0eS3BJV.js";import"./iframe-vlC5hiVo.js";import"../sb-preview/runtime.js";import"./index-Dk74W0Oi.js";const O={title:"Prototypes/Concepts"},i={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(a,{children:e.jsxs(a.Primary,{children:[e.jsx(a.Logo,{title:"Web Services",link:"https://carleton.ca/webservices"}),e.jsx(a.Aside,{menu:h})]})}),e.jsx(l,{children:e.jsxs(s,{children:[e.jsx(r,{children:e.jsx(r.Content,{headerType:"h1",title:"News Listing"})}),e.jsx("p",{children:"Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna. Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx(s,{children:e.jsxs(m,{cols:"3",children:[e.jsxs(t,{children:[e.jsxs(t.Figure,{children:[e.jsx(c,{gap:"2",isAbsolute:!0,top:10,left:10,children:e.jsx(p,{color:"black80",text:"News",rounded:"lg"})}),e.jsx("img",{alt:"Image alt text",height:"300",src:"https://picsum.photos/id/15/600/400",width:"400"})]}),e.jsx(t.Header,{date:"2022-12-23 10:27:31",title:"End-of-Year Reflections and Resolutions for Our Web Services Team"}),e.jsx(t.Body,{children:e.jsx(t.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(t.Footer,{children:e.jsx("a",{className:"cu-button cu-button--red cu-button--small",href:"#",children:"Read more"})})]}),e.jsxs(t,{children:[e.jsx(t.Figure,{children:e.jsx("img",{alt:"Image alt text",height:"300",src:"https://picsum.photos/id/42/600/400",width:"400"})}),e.jsx(t.Header,{date:"2022-12-14 10:27:31",title:"How to Write for the Web"}),e.jsx(t.Body,{children:e.jsx(t.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(t.Footer,{children:e.jsx("a",{className:"cu-button cu-button--red cu-button--small",href:"#",children:"Read more"})})]}),e.jsxs(t,{children:[e.jsx(t.Figure,{children:e.jsx("img",{alt:"Image alt text",height:"300",src:"https://picsum.photos/id/40/600/400",width:"400"})}),e.jsx(t.Header,{date:"2022-11-25 10:27:31",title:"Captivating Captions: Why We Use Captions on Videos"}),e.jsx(t.Body,{children:e.jsx(t.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(t.Footer,{children:e.jsx("a",{className:"cu-button cu-button--red cu-button--small",href:"#",children:"Read more"})})]})]})})]})}),e.jsx(d,{})]})};var o,n,u;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Primary>
+          <Nav.Logo title="Web Services" link="https://carleton.ca/webservices" />
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Primary>
+      </Nav>
+
+      <Main>
+        <Section>
+          <TextMedia>
+            <TextMedia.Content headerType="h1" title="News Listing" />
+          </TextMedia>
+
+          <p>
+            Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+            Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+            reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus
+            in. Praesent quis ligula quis nulla malesuada tempor. Etiam quis interdum nisi, et malesuada lectus. Aliquam
+            luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna.
+          </p>
+
+          <Section>
+            <Column cols="3">
+              <Card>
+                <Card.Figure>
+                  <BadgeGroup gap="2" isAbsolute top={10} left={10}>
+                    <Badge color="black80" text="News" rounded="lg" />
+                  </BadgeGroup>
+                  <img alt="Image alt text" height="300" src="https://picsum.photos/id/15/600/400" width="400" />
+                </Card.Figure>
+                <Card.Header date="2022-12-23 10:27:31" title="End-of-Year Reflections and Resolutions for Our Web Services Team" />
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+                </Card.Body>
+                <Card.Footer>
+                  <a className="cu-button cu-button--red cu-button--small" href="#">
+                    Read more
+                  </a>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Figure>
+                  <img alt="Image alt text" height="300" src="https://picsum.photos/id/42/600/400" width="400" />
+                </Card.Figure>
+                <Card.Header date="2022-12-14 10:27:31" title="How to Write for the Web" />
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+                </Card.Body>
+                <Card.Footer>
+                  <a className="cu-button cu-button--red cu-button--small" href="#">
+                    Read more
+                  </a>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Figure>
+                  <img alt="Image alt text" height="300" src="https://picsum.photos/id/40/600/400" width="400" />
+                </Card.Figure>
+                <Card.Header date="2022-11-25 10:27:31" title="Captivating Captions: Why We Use Captions on Videos" />
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+                </Card.Body>
+                <Card.Footer>
+                  <a className="cu-button cu-button--red cu-button--small" href="#">
+                    Read more
+                  </a>
+                </Card.Footer>
+              </Card>
+            </Column>
+          </Section>
+        </Section>
+      </Main>
+
+      <FooterStandard />
+    </>
+}`,...(u=(n=i.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};const D=["NewsBadges"];export{i as NewsBadges,D as __namedExportsOrder,O as default};
