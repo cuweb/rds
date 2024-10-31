8e70cce7e1369123cacd40db4b9823c7a4558342
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2024-10-31 10:50:55 -0400

    Feature/wsdev 4814 slider (#437)
    * add: slider block and slick
    
    * change: image slider block
    
    * add: slider component
    
    * add: slider image component
    
    * add: slider to main export file
    
    * add: swiper to slider
    
    * add: responsive slider view options in slider
    
    * add: style updates to pagination and arrow
    
    * remove: pagination from slider prop
    
    * add: center mode true to slider
    
    * add: remove pagination and ahusted next/prev button on slider
    
    * add: slider updates with auto slides
    
    * fix: slider image script and style
    
    * refactor: slider to image slider
    
    * refactor: slider to image slider
    
    * add: banner image
    
    ---------
    
    Co-authored-by: Bhoomi Pipalia <bhoomipipalia.15@gmail.com>

---
+++ lib/data/SliderData.ts
@@ -0,0 +1,69 @@
+import { aspectRatioClasses } from '../utils/propClasses'
+
+type aspectRatioKeys = keyof typeof aspectRatioClasses
+
+type IsliderData = {
+  id: number
+  image: string
+  alt: string
+  link?: string
+  title?: string
+  focalPointX?: number
+  focalPointY?: number
+  aspectRatio?: aspectRatioKeys
+}
+
+export const sliderData: IsliderData[] = [
+  {
+    id: 1,
+    image: 'https://picsum.photos/id/531/600/400',
+    link: '#test',
+    alt: 'Image requirement',
+    title: 'Image Title',
+    focalPointX: 0,
+    focalPointY: 10,
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/id/626/600/400',
+    link: '#',
+    alt: 'Image requirement',
+    aspectRatio: 'landscape',
+  },
+  {
+    id: 3,
+    image: 'https://picsum.photos/id/381/600/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+  {
+    id: 4,
+    image: 'https://picsum.photos/id/691/600/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+  {
+    id: 5,
+    image: 'https://picsum.photos/id/163/600/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+  {
+    id: 6,
+    image: 'https://picsum.photos/id/482/600/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+  {
+    id: 7,
+    image: 'https://picsum.photos/id/111/600/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+  {
+    id: 8,
+    image: 'https://picsum.photos/id/888/600/400',
+    link: '#',
+    alt: 'Image requirement',
+  },
+]
