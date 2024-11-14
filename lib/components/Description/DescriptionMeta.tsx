2b2cbcaa94beab7d27e7448726d18fb980b76844
Troy Chaplin <troy.chaplin@gmail.com>
2024-11-14 10:35:23 -0500

    add: spacing prop to desc

--- lib/components/Description/DescriptionMeta.tsx
+++ lib/components/Description/DescriptionMeta.tsx
@@ -4,13 +4,15 @@ export interface DescriptionMetaProps {
   term?: string
   children: React.ReactNode
   useColumns?: boolean
+  spacing?: 'default' | 'tight'
 }
 
-export const DescriptionMeta = ({ term, children, useColumns }: DescriptionMetaProps) => {
+export const DescriptionMeta = ({ term, children, useColumns, spacing = 'default' }: DescriptionMetaProps) => {
   const columns = useColumns ? 'md:grid-cols-left-260 gap-2 md:gap-6' : 'grid-row gap-2'
+  const spaceType = spacing === 'default' ? 'py-4 md:py-6' : 'py-2.5 md:py-3.5'
 
   return (
-    <div className={`${styles.base} ${styles.baseMeta} ${columns}`}>
+    <div className={`${styles.base} ${styles.baseMeta} ${columns} ${spaceType}`}>
       <dt className={`${styles.term}`}>{term}</dt>
       <dd className={`${styles.desc}`}>{children}</dd>
     </div>
