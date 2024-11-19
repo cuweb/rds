0843b27cc8160dc5deeab6f4baa21e55f7e50a30
Ish <Ishdeep.711@gmail.com>
2024-11-19 11:08:27 -0500

    change: updating the auto suggest options (#444)
    * change: updating the auto suggest options
    
    * change: updating the value for auto suggest
    
    * change: updating the value for auto suggest
    
    ---------
    
    Co-authored-by: Ishdeep Singh <ishdeepsingh@Ishdeeps-MacBook-Pro.local>

--- lib/components/Form/AutoSuggest/AutoSuggest.tsx
+++ lib/components/Form/AutoSuggest/AutoSuggest.tsx
@@ -12,28 +12,23 @@ interface Option {
 export interface AutoSuggestProps extends FieldComponentProps {
   disabled?: boolean
   options?: Option[]
+  onChange?: (option: string | null | undefined) => void
 }
 
 export const AutoSuggest = ({ ...props }: AutoSuggestProps) => {
   const { name, options, disabled = false, ...rest } = props
-  const [field, meta] = useField(name)
+  const [, meta] = useField(name)
   const { setFieldValue } = useFormikContext<unknown>()
+
   const errorClass = useErrorClass(name)
 
   const [selectedOption, setSelectedOption] = useState<Option | null>(null)
 
-  useEffect(() => {
-    const initialOption = options?.find((option) => option.value === field.value)
-
-    setSelectedOption(initialOption || null)
-  }, [field.value, options])
+  const optionValue = options?.find((option) => option.value === meta?.value)
 
   useEffect(() => {
-    if (meta.touched && !meta.value) {
-      setFieldValue(name, '')
-      setSelectedOption(null)
-    }
-  }, [meta.touched, meta.value, name, setFieldValue])
+    setSelectedOption(optionValue || null)
+  }, [optionValue])
 
   return (
     <Select
@@ -44,8 +39,10 @@ export const AutoSuggest = ({ ...props }: AutoSuggestProps) => {
       onChange={(option: Option | null) => {
         setFieldValue(name, option?.value)
         setSelectedOption(option)
+        if (rest?.onChange) {
+          rest?.onChange(option?.value)
+        }
       }}
-      onBlur={field.onBlur}
       isSearchable={true}
       isClearable={true}
       className={errorClass + ` cu-autosuggest`}
