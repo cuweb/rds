0d2c6d0f35b723d6fb6ec98de9b59c271899fd46
Singh-Ish <Singh-Ish@users.noreply.github.com>
2024-11-19 16:09:41 +0000

    Deploying to gh-pages from @ cuweb/rds@0843b27cc8160dc5deeab6f4baa21e55f7e50a30 🚀

--- lib/components/Form/AutoSuggest/AutoSuggest.d.ts
+++ lib/components/Form/AutoSuggest/AutoSuggest.d.ts
@@ -6,6 +6,7 @@ interface Option {
 export interface AutoSuggestProps extends FieldComponentProps {
     disabled?: boolean;
     options?: Option[];
+    onChange?: (option: string | null | undefined) => void;
 }
 export declare const AutoSuggest: ({ ...props }: AutoSuggestProps) => import("react/jsx-runtime").JSX.Element;
 export {};
