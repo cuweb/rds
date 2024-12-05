fd2b53d5eb961c76f8449eef83b66aa9677698ee
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2024-12-05 16:26:14 -0500

    add: nospace to the slider slide

--- lib/components/Slider/Slide.tsx
+++ lib/components/Slider/Slide.tsx
@@ -1,11 +1,8 @@
 export interface SlideProps {
   children: React.ReactNode
+  noSpace?: boolean
 }
 
-export const Slide = ({ children }: SlideProps) => {
-  return (
-    <div className="slider__slide">
-      <div className="px-3">{children}</div>
-    </div>
-  )
+export const Slide = ({ children, noSpace }: SlideProps) => {
+  return <div className="slider__slide">{noSpace ? children : <div className="px-3">{children}</div>}</div>
 }
