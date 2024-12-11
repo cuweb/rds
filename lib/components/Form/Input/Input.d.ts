4a71bf6f51f2c1b00b899edf14f72f30028c5f35
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2024-12-11 13:18:33 +0000

    Deploying to gh-pages from @ cuweb/rds@407bb359c7c75b6692c9f90a95aade31081cef73 🚀

--- lib/components/Form/Input/Input.d.ts
+++ lib/components/Form/Input/Input.d.ts
@@ -2,5 +2,6 @@ import { FieldComponentProps } from '../FormField/FormField';
 export interface InputProps extends FieldComponentProps {
     hasPrefix?: React.ReactNode;
     hasSuffix?: React.ReactNode;
+    disabled?: boolean;
 }
 export declare const Input: ({ ...props }: InputProps) => import("react/jsx-runtime").JSX.Element;
