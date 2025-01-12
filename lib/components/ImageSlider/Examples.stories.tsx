d5c4f559f833cae41c3ec7de8a4d134085069dcf
Troy Chaplin <troy.chaplin@gmail.com>
2025-01-12 07:56:51 -0500

    change: image grid and slider examples

---
+++ lib/components/ImageSlider/Examples.stories.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Section } from '../../layouts/Section/Section'
+import { Main } from '../../layouts/Main/Main'
+import { FooterStandard } from '../Footer/FooterStandard/FooterStandard'
+import { Nav } from '../Nav/Nav'
+import { NavAsideData } from '../../data/NavData'
+import { ImageSlider } from './ImageSlider'
+import { PageHeader } from '../PageHeader/PageHeader'
+
+const meta: Meta = {
+  title: 'Components/Image Slider/Examples',
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export default meta
+type Story = StoryObj
+
+const SinglePara = () => {
+  return (
+    <>
+      <p>
+        Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut
+        reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in.
+        Praesent quis ligula quis nulla malesuada tempor.
+      </p>
+    </>
+  )
+}
+
+export const LayoutExample: Story = {
+  render: () => (
+    <>
+      <Nav>
+        <Nav.Primary>
+          <Nav.Logo title="Raven Design System" link="/" />
+          {/* <Nav.Menu menu={NavDataSingle} /> */}
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Primary>
+      </Nav>
+
+      <Main maxWidth="5xl">
+        <Section maxWidth="7xl">
+          <PageHeader
+            as="h1"
+            header="Image Slider Layouts"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis."
+            size="lg"
+          />
+          <ImageSlider slidesPerViewDesktop={3} slidesPerViewMobile={1} slidesPerViewTablet={2}>
+            <ImageSlider.Item
+              focalPointX={0}
+              focalPointY={10}
+              imageUrl="https://picsum.photos/id/531/600/400"
+              link="#test"
+              title="Image Title"
+            />
+            <ImageSlider.Item aspectRatio="landscape" imageUrl="https://picsum.photos/id/626/600/400" link="#" />
+            <ImageSlider.Item imageUrl="https://picsum.photos/id/381/600/400" link="#" />
+            <ImageSlider.Item imageUrl="https://picsum.photos/id/691/600/400" link="#" />
+            <ImageSlider.Item imageUrl="https://picsum.photos/id/163/600/400" link="#" />
+            <ImageSlider.Item imageUrl="https://picsum.photos/id/482/600/400" link="#" />
+            <ImageSlider.Item imageUrl="https://picsum.photos/id/111/600/400" link="#" />
+            <ImageSlider.Item imageUrl="https://picsum.photos/id/888/600/400" link="#" />
+          </ImageSlider>
+        </Section>
+
+        <h2>Header Two</h2>
+        <SinglePara />
+        <SinglePara />
+      </Main>
+
+      <FooterStandard />
+    </>
+  ),
+}
--- lib/components/ImageSlider/ImageSlider.stories.tsx
+++ lib/components/ImageSlider/ImageSlider.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { ImageSlider } from './ImageSlider'
 import { sliderData } from '../../data/SliderData'
+import { Main } from '../../layouts/Main/Main'
 
 const meta: Meta<typeof ImageSlider> = {
   title: 'Components/Image Slider',
@@ -25,22 +26,24 @@ export const Primary: Story = {
   },
   render: (args) => {
     return (
-      <ImageSlider {...args}>
-        {sliderData.map((image, index) => {
-          const { image: imageUrl, title, link, focalPointX, focalPointY, aspectRatio } = image
-          return (
-            <ImageSlider.Item
-              key={index}
-              imageUrl={imageUrl}
-              title={title}
-              link={link}
-              focalPointX={focalPointX}
-              focalPointY={focalPointY}
-              aspectRatio={aspectRatio}
-            />
-          )
-        })}
-      </ImageSlider>
+      <Main>
+        <ImageSlider {...args}>
+          {sliderData.map((image, index) => {
+            const { image: imageUrl, title, link, focalPointX, focalPointY, aspectRatio } = image
+            return (
+              <ImageSlider.Item
+                key={index}
+                imageUrl={imageUrl}
+                title={title}
+                link={link}
+                focalPointX={focalPointX}
+                focalPointY={focalPointY}
+                aspectRatio={aspectRatio}
+              />
+            )
+          })}
+        </ImageSlider>
+      </Main>
     )
   },
 }
