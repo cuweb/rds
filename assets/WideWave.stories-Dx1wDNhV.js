c065da8383098ccbea20c65300763c7388229930
Singh-Ish <Singh-Ish@users.noreply.github.com>
2025-01-15 15:11:26 +0000

    Deploying to gh-pages from @ cuweb/rds@d57a4cea85a357a715a1e201a4208b93c53767ff 🚀

---
+++ assets/WideWave.stories-Dx1wDNhV.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-DmnAw2-i.js";import{W as o}from"./WideWave-CU62eVJE.js";import{M as x}from"./Main-U5ImXc02.js";import{T as t}from"./TextMedia-hxtAS-5U.js";import{B as f}from"./ButtonGroup-7ziFSMqo.js";import"./index-BTEhrml1.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PageHeader-vkxoGiL-.js";import"./useLinkContext-BVlaw0aI.js";import"./parseISO-BQ4LyrE0.js";import"./format-Dt71a7ij.js";import"./isSameDay-Dpgs3O9f.js";import"./propClasses-BhZSwrl0.js";const y={title:"Layouts/Wide Wave",component:o,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},i={};i.args={children:"",as:"section",wave:"red",hasRaven:!1};const a={args:{...i.args},render:s=>e.jsx(x,{children:e.jsx(o,{...s,children:e.jsxs(t,{maxWidth:"7xl",children:[e.jsxs(t.Content,{title:"Red Wave with Text & Media",width:60,hasUnderline:!0,isWhite:!0,children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non placerat massa. Morbi ornare maximus ultricies. Nunc tempus nisl non ullamcorper sagittis. Suspendisse imperdiet vulputate tortor a dignissim. Praesent quam felis, laoreet id odio vitae, fringilla auctor lacus. Cras est libero, faucibus sit amet tristique vitae, auctor a justo. Fusce molestie, justo fermentum vehicula efficitur, leo ligula eleifend ante, in commodo diam turpis in ante."}),e.jsx(f,{children:e.jsx("a",{href:"#",className:"cu-button cu-button--white",children:"Read more"})})]}),e.jsx(t.Aside,{align:"center",border:"sm",borderColor:"white",children:e.jsx("img",{src:"https://picsum.photos/id/431/600/460",alt:"Presentation only",width:"600",height:"460"})})]})})})},r={args:{...i.args,wave:"black",hasRaven:!0},render:s=>e.jsx(x,{children:e.jsxs(o,{...s,children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non placerat massa. Morbi ornare maximus ultricies. Nunc tempus nisl non ullamcorper sagittis. Suspendisse imperdiet vulputate tortor a dignissim. Praesent quam felis, laoreet id odio vitae, fringilla auctor lacus. Cras est libero, faucibus sit amet tristique vitae, auctor a justo. Fusce molestie, justo fermentum vehicula efficitur, leo ligula eleifend ante, in commodo diam turpis in ante."}),e.jsxs(t,{maxWidth:"7xl",reverse:!0,children:[e.jsxs(t.Content,{title:"Red Wave with Text & Media",width:60,hasUnderline:!0,isWhite:!0,children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non placerat massa. Morbi ornare maximus ultricies. Nunc tempus nisl non ullamcorper sagittis. Suspendisse imperdiet vulputate tortor a dignissim. Praesent quam felis, laoreet id odio vitae, fringilla auctor lacus. Cras est libero, faucibus sit amet tristique vitae, auctor a justo. Fusce molestie, justo fermentum vehicula efficitur, leo ligula eleifend ante, in commodo diam turpis in ante."}),e.jsx(f,{children:e.jsx("a",{href:"#",className:"cu-button cu-button--white",children:"Read more"})})]}),e.jsx(t.Aside,{align:"center",border:"sm",borderColor:"white",children:e.jsx("img",{src:"https://picsum.photos/id/431/600/460",alt:"Presentation only",width:"600",height:"460"})})]})]})})};var n,u,l;i.parameters={...i.parameters,docs:{...(n=i.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(l=(u=i.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var m,d,c;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args
+  },
+  render: args => <Main>
+      <WideWave {...args}>
+        <TextMedia maxWidth="7xl">
+          <TextMedia.Content title="Red Wave with Text & Media" width={60} hasUnderline isWhite>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non placerat massa. Morbi ornare maximus
+              ultricies. Nunc tempus nisl non ullamcorper sagittis. Suspendisse imperdiet vulputate tortor a dignissim.
+              Praesent quam felis, laoreet id odio vitae, fringilla auctor lacus. Cras est libero, faucibus sit amet
+              tristique vitae, auctor a justo. Fusce molestie, justo fermentum vehicula efficitur, leo ligula eleifend
+              ante, in commodo diam turpis in ante.
+            </p>
+            <ButtonGroup>
+              <a href="#" className="cu-button cu-button--white">
+                Read more
+              </a>
+            </ButtonGroup>
+          </TextMedia.Content>
+          <TextMedia.Aside align="center" border="sm" borderColor="white">
+            <img src="https://picsum.photos/id/431/600/460" alt="Presentation only" width="600" height="460" />
+          </TextMedia.Aside>
+        </TextMedia>
+      </WideWave>
+    </Main>
+}`,...(c=(d=a.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var p,h,g;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    wave: 'black',
+    hasRaven: true
+  },
+  render: args => <Main>
+      <WideWave {...args}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non placerat massa. Morbi ornare maximus
+          ultricies. Nunc tempus nisl non ullamcorper sagittis. Suspendisse imperdiet vulputate tortor a dignissim.
+          Praesent quam felis, laoreet id odio vitae, fringilla auctor lacus. Cras est libero, faucibus sit amet
+          tristique vitae, auctor a justo. Fusce molestie, justo fermentum vehicula efficitur, leo ligula eleifend ante,
+          in commodo diam turpis in ante.
+        </p>
+        <TextMedia maxWidth="7xl" reverse>
+          <TextMedia.Content title="Red Wave with Text & Media" width={60} hasUnderline isWhite>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non placerat massa. Morbi ornare maximus
+              ultricies. Nunc tempus nisl non ullamcorper sagittis. Suspendisse imperdiet vulputate tortor a dignissim.
+              Praesent quam felis, laoreet id odio vitae, fringilla auctor lacus. Cras est libero, faucibus sit amet
+              tristique vitae, auctor a justo. Fusce molestie, justo fermentum vehicula efficitur, leo ligula eleifend
+              ante, in commodo diam turpis in ante.
+            </p>
+            <ButtonGroup>
+              <a href="#" className="cu-button cu-button--white">
+                Read more
+              </a>
+            </ButtonGroup>
+          </TextMedia.Content>
+          <TextMedia.Aside align="center" border="sm" borderColor="white">
+            <img src="https://picsum.photos/id/431/600/460" alt="Presentation only" width="600" height="460" />
+          </TextMedia.Aside>
+        </TextMedia>
+      </WideWave>
+    </Main>
+}`,...(g=(h=r.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const S=["Primary","WithChildren","BlackWave"];export{r as BlackWave,i as Primary,a as WithChildren,S as __namedExportsOrder,y as default};
