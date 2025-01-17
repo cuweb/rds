d37c8ddcd3eef856422b7973f7fc4fc5d406ed30
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2025-01-17 12:07:44 -0500

    Wsdev 5117 form stories (#462)
    * add: add form example
    
    * add: add foap and employee info form example
    
    * add: add foap and employee info form example
    
    * add: add more example
    
    * add: add more example

--- lib/components/Form/Examples.stories.tsx
+++ lib/components/Form/Examples.stories.tsx
@@ -39,14 +39,51 @@ export const LayoutExamples: Story = {
   render: () => {
     type IInput = {
       inputText: string
+      faculty: string
+      department: string
+      position: string
+      startDate: string
+      endDate: string
+      program: string
+      account: string
+      radio: string
+      checkbox: string[]
+      bannerIds: { employeeID: string; firstName: string; lastName: string }[]
     }
 
     const InputInitialValues = {
       inputText: '',
+      faculty: '',
+      department: '',
+      position: '',
+      startDate: '',
+      endDate: '',
+      program: '',
+      account: '',
+      radio: '',
+      checkbox: [],
+      bannerIds: [],
     }
 
     const InputValidationSchema = Yup.object().shape({
       inputText: Yup.string().required('The field is required'),
+      faculty: Yup.string().required('Please select a faculty'),
+      department: Yup.string().required('Please select a department'),
+      position: Yup.string().required('Please select a position'),
+      startDate: Yup.date().required('Start date is required'),
+      endDate: Yup.date()
+        .required('End date is required')
+        .min(Yup.ref('startDate'), 'End date must be after start date'),
+      program: Yup.string(),
+      account: Yup.string().required('Please select an Account'),
+      employeeSearch: Yup.string()
+        .matches(/^[0-9]{9}$/, 'Employee Id  must be exactly 9 digits')
+        .nullable(),
+      radio: Yup.string().required('Please select the Label'),
+      checkbox: Yup.array()
+        .of(Yup.string())
+        .min(1, 'Please select the acknowledge statement')
+        .required('Please select the acknowledge statement'),
     })
 
     const selectOptions = [
@@ -62,6 +99,30 @@ export const LayoutExamples: Story = {
         label: 'Option 3',
         value: 'option3',
       },
+    ]
+
+    const dataFacultyList = [
+      { value: 'Science', label: 'Faculty of Science' },
+      { value: 'Engineering', label: 'Faculty of Engineering' },
+      { value: 'Arts', label: 'Faculty of Arts' },
+    ]
+
+    const dataDepartmentList = [
+      { value: 'History', label: 'History' },
+      { value: 'Philosophy', label: 'Philosophy' },
+      { value: 'Literature', label: 'Literature' },
+    ]
+
+    const dataPositionList = [
+      { value: 'Manager', label: 'Manager' },
+      { value: 'Developer', label: 'Developer' },
+      { value: 'Analyst', label: 'Analyst' },
+    ]
+
+    const FOAPAccountData = [
+      { value: '1001', label: 'General Fund - 1001' },
+      { value: '2002', label: 'Research Fund - 2002' },
+      { value: '3003', label: 'Development Fund - 3003' },
     ]
 
     const onSubmit = async (values: IInput, actions: FormikHelpers<IInput>) => {
@@ -94,39 +155,75 @@ export const LayoutExamples: Story = {
         <Main maxWidth="5xl">
           <PageHeader
             as="h1"
-            header="Form Example Layout"
+            header="Form Example"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis."
             size="lg"
           />
 
           <Form formikProps={formikProps}>
-            <Section>
-              <PageHeader
-                header="Section Heading"
-                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis."
-                size="md"
-              />
-              <Form.FieldGroup cols={2}>
+            <Main>
+              <PageHeader header="Drop Down" content="Drop down example" size="md" />
+              <Form.FieldGroup cols={3}>
+                <Form.FieldControl control="select" label="Faculty" name="faculty" options={dataFacultyList} required />
                 <Form.FieldControl
-                  control="text"
-                  label="Label"
-                  name="inputText"
+                  control="select"
+                  label="Department"
+                  name="department"
+                  options={dataDepartmentList}
                   required
-                  helper="Helper Text"
-                  helperpostop
-                  disabled={formikProps.isSubmitting}
                 />
                 <Form.FieldControl
-                  control="text"
-                  label="Label"
-                  name="inputText"
+                  control="select"
+                  label="Position"
+                  name="position"
+                  options={dataPositionList}
                   required
-                  helper="Helper Text"
-                  helperpostop
-                  disabled={formikProps.isSubmitting}
                 />
               </Form.FieldGroup>
+            </Main>
 
+            <Main>
+              <Form.FieldArrayContainer>
+                <PageHeader header="FOAP sample field" content="Auto Suggest and Disabled Field example" size="md" />
+                <Form.FieldGroup cols={2}>
+                  <Form.FieldControl
+                    control="autosuggest"
+                    label="Account"
+                    name="account"
+                    options={FOAPAccountData}
+                    onChange={(selectedOption) => {
+                      const accountValue = selectedOption?.value || selectedOption
+                      formikProps.setFieldValue('account', accountValue)
+
+                      const selectedAccount = FOAPAccountData.find((acc) => acc.value === accountValue)
+                      formikProps.setFieldValue('program', selectedAccount ? selectedAccount.value : '')
+                    }}
+                  />
+
+                  <Form.FieldControl
+                    control="text"
+                    label="Program"
+                    name="program"
+                    type="text"
+                    placeholder="code"
+                    disabled
+                    value={formikProps.values.program}
+                  />
+                </Form.FieldGroup>
+              </Form.FieldArrayContainer>
+            </Main>
+
+            <Main>
+              <PageHeader header="Date Selection" content="Date selection example" size="md" />
+              <Form.FieldGroup cols={2}>
+                <Form.FieldControl control="datetime" label="Start Date" name="startDate" required />
+
+                <Form.FieldControl control="datetime" label="End Date" name="endDate" required />
+              </Form.FieldGroup>
+            </Main>
+
+            <Main>
+              <PageHeader header="Text" content="Text example" size="md" />
               <Form.FieldGroup>
                 <Form.FieldControl
                   control="text"
@@ -147,14 +244,68 @@ export const LayoutExamples: Story = {
                   disabled={formikProps.isSubmitting}
                 />
               </Form.FieldGroup>
-            </Section>
+            </Main>
 
-            <Section>
-              <PageHeader
-                header="Section Heading"
-                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis."
-                size="md"
+            <Main>
+              <PageHeader header="Employee Info" content="Manage employee information" size="md" />
+
+              <FieldArray
+                name="bannerIds"
+                render={({ push, remove }) => (
+                  <>
+                    <Form.FieldGroup cols={2}>
+                      <Form.FieldControl
+                        control="text"
+                        name="employeeSearch"
+                        label="Employee ID"
+                        placeholder="Enter Employee ID"
+                        disabled={formikProps.isSubmitting}
+                      />
+                      <ButtonGroup>
+                        <Button
+                          title="+ Add Employee"
+                          type="button"
+                          onClick={() => {
+                            const mockEmployeeData = [
+                              { employeeID: '123456789', firstName: 'John', lastName: 'Doe' },
+                              { employeeID: '987654321', firstName: 'Jane', lastName: 'Smith' },
+                            ]
+
+                            const randomEmployee = mockEmployeeData[Math.floor(Math.random() * mockEmployeeData.length)]
+                            push(randomEmployee)
+                          }}
+                          disabled={formikProps.isSubmitting}
+                        />
+                      </ButtonGroup>
+                    </Form.FieldGroup>
+
+                    <ErrorMessage name="employeeSearch">{(error) => <Form.Error>{error}</Form.Error>}</ErrorMessage>
+
+                    {formikProps.values.bannerIds.length > 0 && (
+                      <Form.FieldGroup>
+                        <Table
+                          columns={[
+                            { header: 'Employee ID', key: 'employeeID' },
+                            { header: 'First Name', key: 'firstName' },
+                            { header: 'Last Name', key: 'lastName' },
+                            { header: 'Action', key: 'remove' },
+                          ]}
+                          data={formikProps.values.bannerIds.map((emp, index) => ({
+                            ...emp,
+                            remove: <Button title="Remove" type="button" color="red" onClick={() => remove(index)} />,
+                          }))}
+                          hasShadow
+                          hasStripes
+                        />
+                      </Form.FieldGroup>
+                    )}
+                  </>
+                )}
               />
+            </Main>
+
+            <Main>
+              <PageHeader header="Selection" content="Radio and Checkbox" size="md" />
               <Form.FieldGroup>
                 <Form.FieldControl
                   control="radio"
@@ -185,7 +336,7 @@ export const LayoutExamples: Story = {
                   disabled={formikProps.isSubmitting}
                 />
               </Form.FieldGroup>
-            </Section>
+            </Main>
 
             <ButtonGroup>
               <Button title="Submit" type="submit" />
