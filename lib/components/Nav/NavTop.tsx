e60a76cf69ea256322dc7dd808aa75fa6e328c08
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2025-01-21 15:20:40 -0500

    add: breakout maxwidth class and put the todos

--- lib/components/Nav/NavTop.tsx
+++ lib/components/Nav/NavTop.tsx
@@ -1,8 +1,10 @@
 import { PropsWithChildren } from 'react'
 
 export const NavTop = ({ children }: PropsWithChildren) => {
+  // TODO GLOBAL: break out of main width
+  const $breakoutMaxWidth = `w-screen max-w-screen-2xl mx-auto`
   return (
-    <div className="cu-nav__top w-full max-w-screen-2xl mx-auto flex flex-col sm:flex-row gap-x-8 sm:flex-nowrap items-center">
+    <div className={`cu-nav__top ${$breakoutMaxWidth} flex flex-col sm:flex-row gap-x-8 sm:flex-nowrap items-center`}>
       {children}
     </div>
   )
