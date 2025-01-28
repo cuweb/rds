ba424a8e06a2a5a60fad57fdbebf759dff77ba1e
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2025-01-28 12:45:07 -0500

    add: add hubspot embed

--- lib/components/Embed/Embed.HubSpot.tsx
+++ lib/components/Embed/Embed.HubSpot.tsx
@@ -9,11 +9,8 @@ export const EmbedHubSpot = ({ title, url }: EmbedHubSpotProps) => {
       title={title}
       src={url}
       className="cu-embed-iframe"
-      allow="autoplay *; fullscreen *; encrypted-media *"
-      referrerPolicy="no-referrer-when-downgrade"
-      sandbox="allow-same-origin allow-scripts"
-      style={{ width: '100%', height: '500px', border: 'none' }}
-    ></iframe>
+      style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
+    />
   )
 }
 
