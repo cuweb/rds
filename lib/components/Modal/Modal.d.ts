c42772edb052c998aa24b1c8310f0383204aee5b
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2025-01-29 12:57:01 +0000

    Deploying to gh-pages from @ cuweb/rds@b8d9ba6aebda5ab85983ee56b0b90270c8b8a229 🚀

--- lib/components/Modal/Modal.d.ts
+++ lib/components/Modal/Modal.d.ts
@@ -11,6 +11,9 @@ export interface ModalProps {
     isOpen: boolean;
     alignTop?: boolean;
     setIsOpen: (k: boolean) => void;
+    hideCloseButton?: boolean;
+    preventOutsideClick?: boolean;
+    onCloseOutsideClick?: () => void;
 }
-export declare const Modal: ({ children, content, isOpen, maxWidth, noProse, alignTop, ariaLabel, ariaDescription, setIsOpen, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Modal: ({ children, content, isOpen, maxWidth, noProse, alignTop, ariaLabel, ariaDescription, setIsOpen, hideCloseButton, preventOutsideClick, onCloseOutsideClick, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
 export {};
