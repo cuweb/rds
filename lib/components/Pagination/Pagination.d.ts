5b7d2c87ee401963d634bd231ed5cf3d2a6b89c4
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2025-02-12 20:12:14 +0000

    Deploying to gh-pages from @ cuweb/rds@616de3393b2e2e5c0041e85814f9bb40cf0b5deb 🚀

--- lib/components/Pagination/Pagination.d.ts
+++ lib/components/Pagination/Pagination.d.ts
@@ -6,5 +6,6 @@ export interface PaginationProps {
     pageSize: number;
     callback: (k: [number?, number?]) => void;
     callbackPage: (page: number) => void;
+    currentPageNumber?: number;
 }
-export declare const Pagination: ({ hasBorder, hasSpacing, totalCount, siblingCount, pageSize, callback, callbackPage, }: PaginationProps) => import("react/jsx-runtime").JSX.Element | null;
+export declare const Pagination: ({ hasBorder, hasSpacing, totalCount, siblingCount, pageSize, callback, callbackPage, currentPageNumber, }: PaginationProps) => import("react/jsx-runtime").JSX.Element | null;
