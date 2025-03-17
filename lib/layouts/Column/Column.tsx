aebfff9591ee92ef4c5267197e806ce7897932c0
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2025-03-17 10:51:10 -0400

    Feature/wsdev 5351 list calendar layout (#490)
    * add: add event calendar layout
    
    * add: add column with order-1 and order-none
    
    * add: add column with order-1 and order-none
    
    * change: column
    
    ---------
    
    Co-authored-by: Troy Chaplin <troy.chaplin@gmail.com>

--- lib/layouts/Column/Column.tsx
+++ lib/layouts/Column/Column.tsx
@@ -7,15 +7,17 @@ type gridColumnKeys = keyof typeof gridColumnClasses
 export interface ColumnProps {
   children: React.ReactNode
   cols?: gridColumnKeys
+  reverse?: boolean
 }
 
-export const ColumnWrapper = ({ children, cols = '1' }: ColumnProps) => {
-  // TODO GLOBAL: margin-y
+export const ColumnWrapper = ({ children, cols = '1', reverse = false }: ColumnProps) => {
   const $componentMarginY = `my-6 md:my-10 first:mt-0`
 
   return (
     <div className={`cu-column mx-auto grid gap-6 md:gap-10 ${gridColumnClasses[cols]} ${$componentMarginY}`}>
-      {children}
+      {React.Children.map(children, (child, index) => (
+        <ColumnContent reverse={reverse && index === 0}>{child}</ColumnContent>
+      ))}
     </div>
   )
 }
--- lib/layouts/Column/ColumnContent.tsx
+++ lib/layouts/Column/ColumnContent.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export interface ColumnContentProps {
   children: React.ReactNode
+  reverse?: boolean
 }
 
-export const ColumnContent = ({ children }: ColumnContentProps) => {
-  return <div className="cu-column-content">{children}</div>
+export const ColumnContent = ({ children, reverse = false }: ColumnContentProps) => {
+  const columnOrder = reverse ? 'order-1' : 'order-none'
+
+  return <div className={`cu-column-content ${columnOrder}`}>{children}</div>
 }
 
 ColumnContent.displayName = 'Column.Content'
