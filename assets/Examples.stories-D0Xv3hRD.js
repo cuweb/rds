836a2a1e8d350b9f1a94b0c6db3ed1072b699d04
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-03-24 18:33:59 +0000

    Deploying to gh-pages from @ cuweb/rds@28e4fedb0e270f2ae0a9fcca35f1dffdd1b5ed85 🚀

---
+++ assets/Examples.stories-D0Xv3hRD.js
@@ -0,0 +1,86 @@
+import{j as a}from"./jsx-runtime-Cf8x2fCZ.js";import{S as p}from"./Section-B-CCKZVd.js";import{M as b}from"./Main-BqxOGoMS.js";import{C as d}from"./Column-Dlks7Fvl.js";import{F as N}from"./FooterStandard-Did9kEDy.js";import{N as o}from"./Nav-D9xZ8HkV.js";import{N as P}from"./NavData-EgWmcfkt.js";import{C as e}from"./Card-CoN5pTQ3.js";import{P as u}from"./PageHeader-dKVTA0_m.js";import{N as c}from"./NewsData-rz4fZ2B1.js";import{P as m}from"./Pagination-B4614Syn.js";import"./index-yBjzXJbu.js";import"./propClasses-BYrQ2lXe.js";import"./FooterLogoLinks-tMRqyGnd.js";import"./useLinkContext-NG2HLdE9.js";import"./index-7sdMIo0z.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Avatar-R8Sfo5dY.js";import"./ButtonGroup-CVkrxlV-.js";import"./parse-BKaVvLnB.js";import"./format-CoDaAjhx.js";import"./getDate-D8TjdU5O.js";import"./isSameDay-D4TUG8v2.js";import"./iframe-DVs4o-DJ.js";import"./index-Chjiymov.js";import"./parseISO-53XxuXUY.js";const Q={title:"Components/Pagination/Examples",parameters:{layout:"fullscreen"}},s=()=>a.jsx(a.Fragment,{children:a.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})}),l={render:()=>a.jsxs(a.Fragment,{children:[a.jsx(o,{children:a.jsxs(o.Top,{children:[a.jsx(o.Logo,{title:"Raven Design System",link:"/"}),a.jsx(o.Aside,{children:a.jsx(o.Buttons,{menu:P})})]})}),a.jsxs(b,{maxWidth:"5xl",children:[a.jsxs(p,{maxWidth:"7xl",children:[a.jsx(u,{as:"h1",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis.",header:"Card Grid Layouts",size:"lg"}),a.jsx(d,{cols:"4",children:c.slice(0,8).map(({id:t,link:n,title:i,image:r,alt:C,date:j})=>a.jsxs(e,{children:[a.jsx(e.Figure,{children:a.jsx("img",{src:r,alt:C,width:"400",height:"300"})}),a.jsx(e.Header,{title:i,date:j}),a.jsx(e.Footer,{children:a.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"Read more"})})]},t))}),a.jsx(m,{callback:()=>{},callbackPage:()=>{},pageSize:5,siblingCount:1,totalCount:48})]}),a.jsx(s,{}),a.jsx(s,{}),a.jsx(u,{header:"Card Grid Layouts",size:"md"}),a.jsx(d,{cols:"3",children:c.slice(0,6).map(({id:t,link:n,title:i,date:r})=>a.jsxs(e,{children:[a.jsx(e.Header,{title:i,date:r}),a.jsx(e.Footer,{children:a.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"Read more"})})]},t))}),a.jsx(m,{callback:()=>{},callbackPage:()=>{},pageSize:5,siblingCount:1,totalCount:48}),a.jsx(s,{}),a.jsx(s,{}),a.jsxs(p,{maxWidth:"7xl",isGrey:!0,children:[a.jsx(u,{header:"Card Grid Layouts",size:"md"}),a.jsx(d,{cols:"4",children:c.slice(0,8).map(({id:t,link:n,title:i,date:r})=>a.jsxs(e,{children:[a.jsx(e.Header,{title:i,date:r}),a.jsx(e.Footer,{children:a.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"Read more"})})]},t))}),a.jsx(m,{callback:()=>{},callbackPage:()=>{},pageSize:5,siblingCount:1,totalCount:48})]})]}),a.jsx(N,{})]})};var x,g,h;l.parameters={...l.parameters,docs:{...(x=l.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Top>
+          <Nav.Logo title="Raven Design System" link="/" />
+          {/* <Nav.Menu menu={NavDataSingle} /> */}
+          <Nav.Aside>
+            <Nav.Buttons menu={NavButtonsData} />
+          </Nav.Aside>
+        </Nav.Top>
+      </Nav>
+
+      <Main maxWidth="5xl">
+        <Section maxWidth="7xl">
+          <PageHeader as="h1" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis." header="Card Grid Layouts" size="lg" />
+          <Column cols="4">
+            {NewsData.slice(0, 8).map(({
+            id,
+            link,
+            title,
+            image,
+            alt,
+            date
+          }) => <Card key={id}>
+                <Card.Figure>
+                  <img src={image} alt={alt} width="400" height="300" />
+                </Card.Figure>
+                <Card.Header title={title} date={date} />
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    Read more
+                  </a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+          <Pagination callback={() => {}} callbackPage={() => {}} pageSize={5} siblingCount={1} totalCount={48} />
+        </Section>
+
+        <SinglePara />
+        <SinglePara />
+
+        <PageHeader header="Card Grid Layouts" size="md" />
+        <Column cols="3">
+          {NewsData.slice(0, 6).map(({
+          id,
+          link,
+          title,
+          date
+        }) => <Card key={id}>
+              <Card.Header title={title} date={date} />
+              <Card.Footer>
+                <a href={link} className="cu-button cu-button--red cu-button--small">
+                  Read more
+                </a>
+              </Card.Footer>
+            </Card>)}
+        </Column>
+        <Pagination callback={() => {}} callbackPage={() => {}} pageSize={5} siblingCount={1} totalCount={48} />
+
+        <SinglePara />
+        <SinglePara />
+
+        <Section maxWidth="7xl" isGrey>
+          <PageHeader header="Card Grid Layouts" size="md" />
+          <Column cols="4">
+            {NewsData.slice(0, 8).map(({
+            id,
+            link,
+            title,
+            date
+          }) => <Card key={id}>
+                <Card.Header title={title} date={date} />
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    Read more
+                  </a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+          <Pagination callback={() => {}} callbackPage={() => {}} pageSize={5} siblingCount={1} totalCount={48} />
+        </Section>
+      </Main>
+
+      <FooterStandard />
+    </>
+}`,...(h=(g=l.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};const V=["LayoutExamples"];export{l as LayoutExamples,V as __namedExportsOrder,Q as default};
