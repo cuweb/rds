3876d6aa1a72377552921c025fa1a1704dc575b9
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2025-03-25 13:37:53 -0400

    add: video load script

--- lib/components/FullBanner/script.ts
+++ lib/components/FullBanner/script.ts
@@ -7,7 +7,7 @@ const VideoControls = (): void => {
   }
 
   videos.forEach((video) => {
-    video.play()
+    video.load()
 
     // Check if the video has already been initialized
     if (video.dataset.initialized === 'true') {
