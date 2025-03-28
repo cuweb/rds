97862d5c745cb0359bf41254a17ffa86b78300eb
Mounika Vojjala <mounikavojjala@gmail.com>
2025-03-28 12:17:49 -0400

    change: removed page header and replaced with div

--- lib/components/Carleton360/Carleton360.tsx
+++ lib/components/Carleton360/Carleton360.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PageHeader } from '../PageHeader/PageHeader'
+import Carleton360Logo from './logo'
 
 export interface Carleton360Props {
   children?: React.ReactNode
@@ -11,17 +11,11 @@ export const Carleton360 = ({ children }: Carleton360Props) => {
     <div
       className={`text-center relative flex items-center justify-center mx-auto px-8 overflow-hidden md:px-16 rounded-xl not-prose ${$componentMarginY} pt-10 pb-12  z-[2] max-w-4xl w-full  flex-col gap-2 bg-cu-black-50 cu-zero-first-last`}
     >
-      <PageHeader header={''} as={'h2'} size={'md'} isWhite={false} isCenter noUnderline>
-        <img
-          className="mb-4 h-12 justify-center mx-auto"
-          src={'https://cdn.carleton.ca/rds/assets/cu-logos/cu-logo-carleton360.svg'}
-          alt={'Logo of Carleton 360'}
-        />
-        <p className="text-md text-gray-600 text-center mb-6 mx-auto ">
-          Get started in Carleton 360 to receive tailored information on our programs, student services and community.
-        </p>
-        {children}
-      </PageHeader>
+      <Carleton360Logo />
+      <p className="text-md text-gray-600 text-center mx-auto ">
+        Get started in Carleton 360 to receive tailored information on our programs, student services and community.
+      </p>
+      {children}
     </div>
   )
 }
