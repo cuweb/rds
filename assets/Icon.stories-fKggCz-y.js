387972dcf6bad160691df7b498adb4389b0ca82c
troychaplin <troychaplin@users.noreply.github.com>
2025-04-01 17:01:23 +0000

    Deploying to gh-pages from @ cuweb/rds@08018c14a0eff04190f90f4e205b6d439db6b4ca 🚀

---
+++ assets/Icon.stories-fKggCz-y.js
@@ -0,0 +1,9 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{I as t}from"./Icon-Cq1ojZpE.js";import{F as c}from"./CheckCircleIcon-CyPy8vXU.js";import"./index-yBjzXJbu.js";import"./propClasses-BYrQ2lXe.js";import"./index-DDmFEvuO.js";import"./_commonjsHelpers-D6-XlEtG.js";const G={title:"Components/Icon",component:t,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},e={render:r=>n.jsx(t,{...r,"aria-hidden":!0})};e.args={icon:c};const a={render:r=>n.jsx(t,{...r,"aria-hidden":!0})};a.args={icon:c,color:"red"};const o={render:r=>n.jsx(t,{...r,"aria-hidden":!0})};o.args={icon:c,color:"grey"};const s={render:r=>n.jsx(t,{...r,"aria-hidden":!0})};s.args={icon:c,color:"white"};var d,i,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: args => <Icon {...args} aria-hidden={true} />
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,u,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => <Icon {...args} aria-hidden={true} />
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var l,h,x;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => <Icon {...args} aria-hidden={true} />
+}`,...(x=(h=o.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var f,I,j;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => <Icon {...args} aria-hidden={true} />
+}`,...(j=(I=s.parameters)==null?void 0:I.docs)==null?void 0:j.source}}};const W=["Default","Red","Grey","White"];export{e as Default,o as Grey,a as Red,s as White,W as __namedExportsOrder,G as default};
---
+++ lib/components/Carleton360/Carleton360.d.ts
@@ -0,0 +1,5 @@
+import { default as React } from '../../../node_modules/react';
+export interface Carleton360Props {
+    children?: React.ReactNode;
+}
+export declare const Carleton360: ({ children }: Carleton360Props) => import("react/jsx-runtime").JSX.Element;
