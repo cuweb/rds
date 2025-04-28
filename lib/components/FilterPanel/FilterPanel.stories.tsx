d086c55d7524d29d332c1c74ec9acf65dec8ead3
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2025-04-28 15:51:44 -0400

    add: filter panel data

--- lib/components/FilterPanel/FilterPanel.stories.tsx
+++ lib/components/FilterPanel/FilterPanel.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { FilterPanel } from './FilterPanel'
 import { Main } from '../../layouts/Main/Main'
+import { FilterPanelData } from '../../data/FilterData'
 
 const meta: Meta<typeof FilterPanel> = {
   title: 'Components/FilterPanel',
@@ -17,50 +18,11 @@ const meta: Meta<typeof FilterPanel> = {
 export default meta
 type Story = StoryObj<typeof FilterPanel>
 
-const sortOptions = [
-  { label: 'Newest to oldest', value: 'newest', selected: true },
-  { label: 'Oldest to newest', value: 'oldest' },
-  { label: 'Most popular', value: 'popular' },
-]
-
-const filterOptions = [
-  {
-    id: 'audience',
-    name: 'Audience',
-    options: [
-      { value: 'staff', label: 'Staff', checked: false },
-      {
-        value: 'student',
-        label: 'Student',
-        checked: false,
-      },
-    ],
-  },
-  {
-    id: 'tags',
-    name: 'Tags',
-    options: [
-      { value: 'general', label: 'General', checked: false },
-      {
-        value: 'online/virtual',
-        label: 'Online/Virtual',
-        checked: true,
-      },
-      { value: 'finances', label: 'Finances', checked: false },
-      {
-        value: 'healthyworkplace',
-        label: 'Healthy Workplace',
-        checked: false,
-      },
-    ],
-  },
-]
-
 export const Primary: Story = {
   render: () => (
     <Main>
       <FilterPanel>
-        <FilterPanel.Top sortOptions={sortOptions} filterOptions={filterOptions} />
+        <FilterPanel.Top sortOptions={FilterPanelData.sortOptions} filterOptions={FilterPanelData.filters} />
         <FilterPanel.Bottom />
       </FilterPanel>
     </Main>
--- lib/data/FilterData.ts
+++ lib/data/FilterData.ts
@@ -39,3 +39,43 @@ export const FilterData = {
     },
   ],
 }
+
+export const FilterPanelData = {
+  sortOptions: [
+    { label: 'Newest to oldest', value: 'newest', selected: true },
+    { label: 'Oldest to newest', value: 'oldest' },
+    { label: 'Most popular', value: 'popular' },
+  ],
+  filters: [
+    {
+      id: 'audience',
+      name: 'Audience',
+      options: [
+        { value: 'staff', label: 'Staff', checked: false },
+        {
+          value: 'student',
+          label: 'Student',
+          checked: false,
+        },
+      ],
+    },
+    {
+      id: 'tags',
+      name: 'Tags',
+      options: [
+        { value: 'general', label: 'General', checked: false },
+        {
+          value: 'online/virtual',
+          label: 'Online/Virtual',
+          checked: true,
+        },
+        { value: 'finances', label: 'Finances', checked: false },
+        {
+          value: 'healthyworkplace',
+          label: 'Healthy Workplace',
+          checked: false,
+        },
+      ],
+    },
+  ],
+}
