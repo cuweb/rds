ae731742465b35bad0fe5007ff3c2d0d39a53a08
Singh-Ish <Ishdeep.711@gmail.com>
2025-05-05 14:21:10 +0000

    Deploying to gh-pages from @ cuweb/rds@cad147e8e335b0b3a6ba47a053574165fc024eca 🚀

---
+++ assets/ImageCover.stories-BSPGPz_a.js
@@ -0,0 +1,67 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{I as r}from"./ImageCover-PDrgCkB6.js";import{M as g}from"./Main-BqxOGoMS.js";import{S as u}from"./Section-CKcqyDpo.js";import{C as f}from"./Column-D9RQLqlL.js";import{P as C}from"./PageHeader-DmLaumFu.js";import{C as t}from"./Card-n6flBtQu.js";import{E as n}from"./Embed-sccDNtji.js";import"./index-yBjzXJbu.js";import"./propClasses-Dlt7SDjn.js";import"./parseISO-Z6eZh0wm.js";import"./format-BDD0as5c.js";import"./isSameDay-C9OCNg_L.js";import"./parse-CXxdJvFO.js";import"./getDate-DcT4OotK.js";import"./MapPinIcon-DKh9k5f_.js";import"./index-DDmFEvuO.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./iframe-nfRKxtQM.js";import"./index-Chjiymov.js";import"./Figure-GVCf42Z2.js";const j=[{id:1,title:"Undergradaute Admissions",link:"#",image:"/data-images/attend-1-thumb.jpg",badge:"News",alt:"Image alt text",excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",date:"2022-12-23 10:27:31"},{id:2,title:"Gradaute Admissions",link:"#",image:"/data-images/attend-2-thumb.jpg",badge:"Research",alt:"Image alt text",excerpt:"Proin placerat non nisl sed ullamcorper ultrices.",date:"2022-12-14 10:27:31"},{id:3,title:"Continuin Studies",link:"#",image:"/data-images/attend-3-thumb.jpg",badge:"Story",alt:"Image alt text",excerpt:"Donec dolor sapien, aliquet eu nibh vel, laoreet aliquam arcu.",date:"2022-11-25 10:27:31"},{id:4,title:"Professional Development & Executive Education",link:"#",image:"/data-images/attend-4-thumb.jpg",badge:"Success",alt:"Image alt text",excerpt:"Proin placerat non nisl sed ullamcorper diam a lorem.",date:"2022-12-14 10:27:31"}],T={title:"Layouts/Image Cover",component:r,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},i={render:s=>e.jsx(g,{children:e.jsxs(r,{...s,children:[e.jsxs("p",{children:[" ","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris arcu, vehicula sed leo ut, finibus gravida massa. Nunc euismod risus porta tellus dignissim posuere. Proin quis commodo nisl. In commodo laoreet urna, eget lobortis nulla cursus ut. Cras sagittis velit in ligula volutpat, id tempus felis dignissim. Nullam a massa vehicula, placerat nulla nec, gravida sem. Mauris ex ligula, hendrerit et tristique non, sodales in turpis. Proin tincidunt ante vehicula lorem auctor ullamcorper. Vestibulum vulputate lectus ante, et cursus risus auctor sed. Quisque justo lacus, dapibus eu eros quis, suscipit elementum nisl. Praesent nunc augue, lacinia eget interdum at, commodo eleifend velit. Morbi pretium consectetur libero quis aliquet. Aliquam vitae nulla ac quam auctor efficitur sit amet eget massa. Donec porta nunc et sapien sodales, et viverra orci tincidunt. Duis nisl ex, imperdiet eu leo eget, tempus vestibulum leo. Sed viverra, eros non aliquet ultrices, mauris arcu gravida augue, sit amet placerat est leo et augue."," "]}),e.jsxs("p",{children:[" ","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris arcu, vehicula sed leo ut, finibus gravida massa. Nunc euismod risus porta tellus dignissim posuere. Proin quis commodo nisl. In commodo laoreet urna, eget lobortis nulla cursus ut. Cras sagittis velit in ligula volutpat, id tempus felis dignissim. Nullam a massa vehicula, placerat nulla nec, gravida sem. Mauris ex ligula, hendrerit et tristique non, sodales in turpis. Proin tincidunt ante vehicula lorem auctor ullamcorper. Vestibulum vulputate lectus ante, et cursus risus auctor sed. Quisque justo lacus, dapibus eu eros quis, suscipit elementum nisl. Praesent nunc augue, lacinia eget interdum at, commodo eleifend velit. Morbi pretium consectetur libero quis aliquet. Aliquam vitae nulla ac quam auctor efficitur sit amet eget massa. Donec porta nunc et sapien sodales, et viverra orci tincidunt. Duis nisl ex, imperdiet eu leo eget, tempus vestibulum leo. Sed viverra, eros non aliquet ultrices, mauris arcu gravida augue, sit amet placerat est leo et augue."," "]})]})})};i.args={children:"",as:"section",maxWidth:"5xl",image:"tory",imageZoom:0,opacity:85,focalPointX:"50",focalPointY:"50"};const a={args:{...i.args},render:s=>e.jsx(g,{children:e.jsxs(r,{...s,children:[e.jsxs(u,{as:"div",maxWidth:"7xl",children:[e.jsx(C,{as:"h2",header:"Attend Carleton",size:"md"}),e.jsx(f,{cols:"4",maxWidth:"7xl",children:j.slice(0,4).map(({id:v,link:h,title:x,image:b,alt:q})=>e.jsxs(t,{noHover:!0,children:[e.jsx(t.Figure,{children:e.jsx("img",{src:b,alt:q,width:"200",height:"133"})}),e.jsx(t.Header,{title:x}),e.jsx(t.Footer,{children:e.jsx("a",{href:h,className:"cu-button cu-button--red cu-button--small",children:"More info"})})]},v))})]}),e.jsx(u,{as:"div",maxWidth:"7xl",children:e.jsx(n,{maxWidth:"7xl",children:e.jsx(n.YouTube,{title:"test",url:"https://www.youtube.com/watch?v=ZISHr6sPNCE"})})})]})})};var o,l,m;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => <Main>
+      <ImageCover {...args}>
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris arcu, vehicula sed leo ut, finibus
+          gravida massa. Nunc euismod risus porta tellus dignissim posuere. Proin quis commodo nisl. In commodo laoreet
+          urna, eget lobortis nulla cursus ut. Cras sagittis velit in ligula volutpat, id tempus felis dignissim. Nullam
+          a massa vehicula, placerat nulla nec, gravida sem. Mauris ex ligula, hendrerit et tristique non, sodales in
+          turpis. Proin tincidunt ante vehicula lorem auctor ullamcorper. Vestibulum vulputate lectus ante, et cursus
+          risus auctor sed. Quisque justo lacus, dapibus eu eros quis, suscipit elementum nisl. Praesent nunc augue,
+          lacinia eget interdum at, commodo eleifend velit. Morbi pretium consectetur libero quis aliquet. Aliquam vitae
+          nulla ac quam auctor efficitur sit amet eget massa. Donec porta nunc et sapien sodales, et viverra orci
+          tincidunt. Duis nisl ex, imperdiet eu leo eget, tempus vestibulum leo. Sed viverra, eros non aliquet ultrices,
+          mauris arcu gravida augue, sit amet placerat est leo et augue.{' '}
+        </p>
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris arcu, vehicula sed leo ut, finibus
+          gravida massa. Nunc euismod risus porta tellus dignissim posuere. Proin quis commodo nisl. In commodo laoreet
+          urna, eget lobortis nulla cursus ut. Cras sagittis velit in ligula volutpat, id tempus felis dignissim. Nullam
+          a massa vehicula, placerat nulla nec, gravida sem. Mauris ex ligula, hendrerit et tristique non, sodales in
+          turpis. Proin tincidunt ante vehicula lorem auctor ullamcorper. Vestibulum vulputate lectus ante, et cursus
+          risus auctor sed. Quisque justo lacus, dapibus eu eros quis, suscipit elementum nisl. Praesent nunc augue,
+          lacinia eget interdum at, commodo eleifend velit. Morbi pretium consectetur libero quis aliquet. Aliquam vitae
+          nulla ac quam auctor efficitur sit amet eget massa. Donec porta nunc et sapien sodales, et viverra orci
+          tincidunt. Duis nisl ex, imperdiet eu leo eget, tempus vestibulum leo. Sed viverra, eros non aliquet ultrices,
+          mauris arcu gravida augue, sit amet placerat est leo et augue.{' '}
+        </p>
+      </ImageCover>
+    </Main>
+}`,...(m=(l=i.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var c,d,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args
+  },
+  render: args => <Main>
+      <ImageCover {...args}>
+        <Section as="div" maxWidth="7xl">
+          <PageHeader as="h2" header="Attend Carleton" size="md" />
+          <Column cols="4" maxWidth="7xl">
+            {PromoData.slice(0, 4).map(({
+            id,
+            link,
+            title,
+            image,
+            alt
+          }) => <Card key={id} noHover>
+                <Card.Figure>
+                  <img src={image} alt={alt} width="200" height="133" />
+                </Card.Figure>
+                <Card.Header title={title} />
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    More info
+                  </a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+        </Section>
+        <Section as="div" maxWidth="7xl">
+          <Embed maxWidth="7xl">
+            <Embed.YouTube title="test" url="https://www.youtube.com/watch?v=ZISHr6sPNCE" />
+          </Embed>
+        </Section>
+      </ImageCover>
+    </Main>
+}`,...(p=(d=a.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const _=["Primary","WithChildren"];export{i as Primary,a as WithChildren,_ as __namedExportsOrder,T as default};
