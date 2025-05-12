2917d523d7d20c16e8c9f8d692ae22f41a69c493
Bhoomi-Pipalia <Bhoomi-Pipalia@users.noreply.github.com>
2025-05-12 15:48:10 +0000

    Deploying to gh-pages from @ cuweb/rds@c1f44db0ab8e6f1fd7f0bbe9494ce10864cc297b 🚀

---
+++ assets/FilterPanel.stories-ChrHpZln.js
@@ -0,0 +1,8 @@
+import{j as r}from"./jsx-runtime-Cf8x2fCZ.js";import{F as e}from"./FilterPanel-AFkBSfIl.js";import{M as i}from"./Main-BqxOGoMS.js";import{F as o}from"./FilterData-D1ec_lJy.js";import"./index-yBjzXJbu.js";import"./index-DDmFEvuO.js";import"./_commonjsHelpers-D6-XlEtG.js";const f={title:"Components/FilterPanel",component:e,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},t={render:()=>r.jsx(i,{children:r.jsxs(e,{children:[r.jsx(e.Top,{sortOptions:o.sortOptions,filterOptions:o.filters}),r.jsx(e.Bottom,{})]})})};var s,a,n;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <Main>
+      <FilterPanel>
+        <FilterPanel.Top sortOptions={FilterPanelData.sortOptions} filterOptions={FilterPanelData.filters} />
+        <FilterPanel.Bottom />
+      </FilterPanel>
+    </Main>
+}`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const x=["Primary"];export{t as Primary,x as __namedExportsOrder,f as default};
