0eed1937bf7cb6c5b99a2382e3802b37ad9724bf
Bhoomi Pipalia <bhoomipipalia.15@gmail.com>
2025-06-03 11:12:08 -0400

    fix: spacing

--- lib/components/Splash/SplashContent.tsx
+++ lib/components/Splash/SplashContent.tsx
@@ -13,18 +13,18 @@ export const SplashContent = ({ eyebrow, header, buttons, children }: SplashCont
   const LinkComponent = useLinkContext()
 
   const paddingX = 'px-4 sm:px-24'
-  const paddingY = 'py-10 sm:pt-20 sm:pb-5'
+  const paddingY = 'py-14 sm:pt-20 sm:pb-5'
 
   return (
     <div className={`relative z-20 h-full flex flex-col items-center gap-12 sm:gap-20 ${paddingX} ${paddingY}`}>
       <LinkComponent href="https://goravens.carleton.ca" className="inline-block">
         <img
-          className="w-24 h-auto"
+          className="h-auto w-24 sm:w-auto"
           src="https://cu-production.s3.amazonaws.com/rds/assets/ravens-logos/ravens-logo-wordmark.svg"
           alt="Carleton"
         />
       </LinkComponent>
-      <div className={`w-full h-full flex flex-col gap-4 items-center justify-between`}>
+      <div className={`w-full h-full flex flex-col gap-4 items-center justify-between sm:justify-start`}>
         {header && <PageHeader eyebrow={eyebrow} header={header} isWhite isCenter noUnderline></PageHeader>}
         {buttons && (
           <div className={`flex flex-col mt-auto mb-0 ${children ? 'sm:hidden' : ''}`}>
