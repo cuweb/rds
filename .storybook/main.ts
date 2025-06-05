93df0d62161f75f67fa5f673312d6c95ba3baef7
Ish <Ishdeep.711@gmail.com>
2025-06-05 09:37:58 -0400

    change: updating the vide confog globals and removing webpack for storbook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-styling',
+    '@storybook/addon-themes',
   ],
   framework: {
     name: '@storybook/react-vite',
