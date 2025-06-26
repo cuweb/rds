26a8f4822bda1f13804b0c8ba6bf996c6259307b
Ish <Ishdeep.711@gmail.com>
2025-06-26 11:33:49 -0400

    change: reverting the react 19 to 18 in packagejson

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,7 +14,6 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-styling',
-    '@storybook/addon-themes',
   ],
   framework: {
     name: '@storybook/react-vite',
