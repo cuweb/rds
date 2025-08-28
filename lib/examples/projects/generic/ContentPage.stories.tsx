9962f7a07f6171901ea6fea2c8f7b7ee9bd5c47c
Troy Chaplin <troy.chaplin@gmail.com>
2025-08-28 09:57:54 -0400

    add: server html page example

---
+++ lib/examples/projects/generic/ContentPage.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Main } from '../../../layouts/Main/Main'
+import { Nav } from '../../../components/Nav/Nav'
+import { Section } from '../../../layouts/Section/Section'
+import { TextMedia } from '../../../components/TextMedia/TextMedia'
+
+const meta: Meta = {
+  title: 'Prototypes/Generic',
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export default meta
+type Story = StoryObj
+
+export const ANERROR: Story = {
+  render: () => {
+    return (
+      <>
+        <Nav>
+          <Nav.Top>
+            <Nav.Logo />
+          </Nav.Top>
+        </Nav>
+
+        <Main>
+          <Section>
+            <TextMedia>
+              <TextMedia.Content headerType="h1" title="Invalid request " width={100} isLight>
+                <p>
+                  The request was invalid. If you have concerns, please contact ITS at{' '}
+                  <a href="https://carleton.ca/its">https://carleton.ca/its</a>
+                </p>
+              </TextMedia.Content>
+            </TextMedia>
+          </Section>
+        </Main>
+      </>
+    )
+  },
+}
