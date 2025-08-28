58caaa9f28b666893c3e9fbb4980a443115f15de
HanqingZhou <HanqingZhou@users.noreply.github.com>
2025-08-28 18:48:25 +0000

    Deploying to gh-pages from @ cuweb/rds@cb58b1701575742da291ac3f4919445692b21dc1 🚀

---
+++ assets/Examples.stories-evJKtIvQ.js
@@ -0,0 +1,284 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{S as u}from"./Section-CKcqyDpo.js";import{M as D}from"./Main-BqxOGoMS.js";import{P as b}from"./PageHeader-CIeaA_hc.js";import{C as o}from"./Column-Bhqy_FO7.js";import{F as S}from"./FooterStandard-CssxRqDU.js";import{N as p}from"./Nav-Dg0sZsUG.js";import{N as B}from"./NavData-DGjf4Von.js";import{C as a}from"./Card-BSAtFqyg.js";import{N as x}from"./NewsData-DpAyc1JF.js";import{E as v}from"./EventData-DQzocGgI.js";import{I as F,P as H}from"./PeopleData-oav3_Z5q.js";import{V as T,S as P}from"./VideoData-Csafh9oY.js";import"./index-yBjzXJbu.js";import"./parseISO-Z6eZh0wm.js";import"./format-BDD0as5c.js";import"./isSameDay-C9OCNg_L.js";import"./propClasses-CJUCiil5.js";import"./FooterLogoLinks-CPWKd5Ho.js";import"./useLinkContext-B51pPRuk.js";import"./index-DDmFEvuO.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Avatar-R8Sfo5dY.js";import"./ButtonGroup-Bw2xFjvQ.js";import"./Icon-Cdu77a2g.js";import"./parse-CXxdJvFO.js";import"./getDate-DcT4OotK.js";import"./iframe-D4q14bN-.js";import"./index-Chjiymov.js";const se={title:"Components/Card/Examples",parameters:{layout:"fullscreen"}},s=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})}),C={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(p,{children:e.jsxs(p.Top,{children:[e.jsx(p.Logo,{title:"Raven Design System",link:"/"}),e.jsx(p.Aside,{children:e.jsx(p.Buttons,{menu:B})})]})}),e.jsxs(D,{maxWidth:"5xl",children:[e.jsxs(u,{children:[e.jsx(b,{as:"h1",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis.",header:"Card Grid Layouts",size:"lg"}),e.jsxs(o,{cols:"3",children:[x.slice(0,3).map(({id:t,title:n,image:i,alt:r})=>e.jsxs(a,{isGrey:!0,hasWave:!0,children:[e.jsx(a.ImageThumb,{children:e.jsx("img",{src:i,alt:r,width:"200",height:"133"})}),e.jsx(a.Header,{title:n}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})})]},t)),x.slice(0,3).map(({id:t,title:n,icon:i})=>e.jsxs(a,{isGrey:!0,hasWave:!0,children:[e.jsx(a.IconThumb,{icon:i}),e.jsx(a.Header,{title:n}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})})]},t))]})]}),e.jsx("h2",{children:"Header Two"}),e.jsx(s,{}),e.jsx(s,{}),e.jsxs(u,{children:[e.jsx(b,{as:"h1",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis.",header:"Card Grid Layouts",size:"lg"}),e.jsx(o,{cols:"3",children:x.slice(0,3).map(({id:t,link:n,title:i,image:r,alt:d,date:l})=>e.jsxs(a,{children:[e.jsx(a.Figure,{children:e.jsx("img",{src:r,alt:d,width:"400",height:"300"})}),e.jsx(a.Header,{title:i,date:l}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"Read more"})})]},t))})]}),e.jsx("h2",{children:"Header Two"}),e.jsx(s,{}),e.jsx(s,{}),e.jsx(b,{header:"Event Cards",size:"md"}),e.jsx(o,{cols:"3",children:v.slice(0,3).map(({id:t,title:n,link:i,image:r,alt:d,startDate:l,endDate:m,on_campus:c,on_campus_building:h,on_campus_room_number:j,event_address:g})=>e.jsxs(a,{children:[e.jsx(a.Figure,{children:e.jsx("img",{src:r,alt:d,width:400,height:175})}),e.jsx(a.DateThumb,{startDate:l,endDate:m}),e.jsx(a.Header,{title:n}),e.jsx(a.Body,{children:e.jsx(a.EventMeta,{startDateTime:l,endDateTime:m,onCampus:c,onCampusBuilding:h,onCampusRoomNumber:j,eventAddress:g})}),e.jsx(a.Footer,{children:e.jsx("a",{href:i,className:"cu-button cu-button--red cu-button--small",children:"More info"})})]},t))}),e.jsx(o,{cols:"3",children:v.slice(0,3).map(({id:t,title:n,link:i,image:r,alt:d,startDate:l,endDate:m,on_campus:c,on_campus_building:h,on_campus_room_number:j,event_address:g})=>e.jsxs(a,{children:[e.jsx(a.Figure,{children:e.jsx("img",{src:r,alt:d,width:400,height:175})}),e.jsx(a.DateThumb,{startDate:l,endDate:m}),e.jsx(a.Header,{title:n}),e.jsx(a.Body,{children:e.jsx(a.EventMeta,{startDateTime:l,endDateTime:m,onCampus:c,onCampusBuilding:h,onCampusRoomNumber:j,eventAddress:g})}),e.jsx(a.Footer,{children:e.jsx("a",{href:i,className:"cu-button cu-button--red cu-button--small",children:"More info"})})]},t))}),e.jsx(s,{}),e.jsx(s,{}),e.jsx(o,{cols:"3",children:x.slice(0,3).map(({id:t,link:n,title:i,image:r,alt:d})=>e.jsxs(a,{noHover:!0,children:[e.jsx(a.ImageThumb,{children:e.jsx("img",{src:r,alt:d,width:"200",height:"133"})}),e.jsx(a.Header,{title:i}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"Get informed"})})]},t))}),e.jsx(u,{maxWidth:"7xl",isGrey:!0,children:e.jsx(o,{cols:"4",children:F.slice(0,4).map(({id:t,link:n,title:i,icon:r})=>e.jsxs(a,{children:[e.jsx(a.IconThumb,{icon:r}),e.jsx(a.Header,{title:i}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"More info"})})]},t))})}),e.jsxs(u,{children:[e.jsx("h2",{children:"White Background"}),e.jsx(s,{}),e.jsx(s,{})]}),e.jsx("h2",{children:"Header Two"}),e.jsx(s,{}),e.jsx(s,{}),e.jsx(o,{cols:"3",children:x.slice(0,3).map(({id:t,link:n,title:i,date:r})=>e.jsxs(a,{children:[e.jsx(a.Header,{title:i,date:r,datePrefix:"Modified on ",datePosition:"bottom"}),e.jsx(a.Body,{children:e.jsx(a.Excerpt,{text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris."})}),e.jsx(a.Footer,{children:e.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"Read more"})})]},t))}),e.jsx(u,{maxWidth:"7xl",children:e.jsx(o,{cols:"3",children:H.slice(0,3).map(({id:t,link:n,image:i,alt:r,firstName:d,lastName:l,jobTitle:m,email:c,phone:h})=>e.jsxs(a,{isCenter:!0,children:[e.jsx(a.Figure,{isRound:!0,children:e.jsx("img",{src:i,alt:r,width:280,height:280})}),e.jsx(a.Header,{title:`${d} ${l}`}),e.jsx(a.Body,{children:e.jsx(a.PeopleMeta,{jobTitle:m,phone:h,children:e.jsx("a",{href:`mailto:${c}`,children:c})})}),e.jsx(a.Footer,{children:e.jsx("a",{href:n,className:"cu-button cu-button--red cu-button--small",children:"More info"})})]},t))})}),e.jsxs(u,{children:[e.jsx("h2",{children:"White Background"}),e.jsx(s,{}),e.jsx(s,{})]}),e.jsx(o,{cols:"3",children:T.slice(0,3).map(({id:t,source:n,title:i})=>e.jsxs(a,{noHover:!0,children:[e.jsx(a.Video,{source:n}),e.jsx(a.Header,{title:i})]},t))}),e.jsx("h2",{children:"Header Two"}),e.jsx(s,{}),e.jsx(s,{}),e.jsx(u,{maxWidth:"full",isGrey:!0,children:e.jsx(o,{cols:"4",children:P.slice(0,4).map(({id:t,stat:n,desc:i})=>e.jsx(a,{leftBorder:!0,noHover:!0,children:e.jsx(a.Stats,{stat:n,desc:i})},t))})})]}),e.jsx(S,{})]})};var N,f,y;C.parameters={...C.parameters,docs:{...(N=C.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav>
+        <Nav.Top>
+          <Nav.Logo title="Raven Design System" link="/" />
+          {/* <Nav.Menu menu={NavDataSingle} /> */}
+          <Nav.Aside>
+            <Nav.Buttons menu={NavButtonsData} />
+          </Nav.Aside>
+        </Nav.Top>
+      </Nav>
+
+      <Main maxWidth="5xl">
+        <Section>
+          <PageHeader as="h1" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis." header="Card Grid Layouts" size="lg" />
+          <Column cols="3">
+            {NewsData.slice(0, 3).map(({
+            id,
+            title,
+            image,
+            alt
+          }) => <Card key={id} isGrey hasWave>
+                <Card.ImageThumb>
+                  <img src={image} alt={alt} width="200" height="133" />
+                </Card.ImageThumb>
+                <Card.Header title={title} />
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+                </Card.Body>
+              </Card>)}
+            {NewsData.slice(0, 3).map(({
+            id,
+            title,
+            icon
+          }) => <Card key={id} isGrey hasWave>
+                <Card.IconThumb icon={icon} />
+                <Card.Header title={title} />
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+                </Card.Body>
+              </Card>)}
+          </Column>
+        </Section>
+
+        <h2>Header Two</h2>
+        <SinglePara />
+        <SinglePara />
+
+        <Section>
+          <PageHeader as="h1" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat euismod. Ut ut diam dapibus nisi ullamcorper sollicitudin id vitae turpis." header="Card Grid Layouts" size="lg" />
+          <Column cols="3">
+            {NewsData.slice(0, 3).map(({
+            id,
+            link,
+            title,
+            image,
+            alt,
+            date
+          }) => <Card key={id}>
+                <Card.Figure>
+                  <img src={image} alt={alt} width="400" height="300" />
+                </Card.Figure>
+                <Card.Header title={title} date={date} />
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+                </Card.Body>
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    Read more
+                  </a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+        </Section>
+
+        <h2>Header Two</h2>
+        <SinglePara />
+        <SinglePara />
+
+        <PageHeader header="Event Cards" size="md" />
+        <Column cols="3">
+          {EventData.slice(0, 3).map(({
+          id,
+          title,
+          link,
+          image,
+          alt,
+          startDate,
+          endDate,
+          on_campus,
+          on_campus_building,
+          on_campus_room_number,
+          event_address
+        }) => <Card key={id}>
+                <Card.Figure>
+                  <img src={image} alt={alt} width={400} height={175} />
+                </Card.Figure>
+                <Card.DateThumb startDate={startDate} endDate={endDate} />
+                <Card.Header title={title} />
+                <Card.Body>
+                  <Card.EventMeta startDateTime={startDate} endDateTime={endDate} onCampus={on_campus} onCampusBuilding={on_campus_building} onCampusRoomNumber={on_campus_room_number} eventAddress={event_address} />
+                </Card.Body>
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    More info
+                  </a>
+                </Card.Footer>
+              </Card>)}
+        </Column>
+        <Column cols="3">
+          {EventData.slice(0, 3).map(({
+          id,
+          title,
+          link,
+          image,
+          alt,
+          startDate,
+          endDate,
+          on_campus,
+          on_campus_building,
+          on_campus_room_number,
+          event_address
+        }) => <Card key={id}>
+                <Card.Figure>
+                  <img src={image} alt={alt} width={400} height={175} />
+                </Card.Figure>
+                <Card.DateThumb startDate={startDate} endDate={endDate} />
+                <Card.Header title={title} />
+                <Card.Body>
+                  <Card.EventMeta startDateTime={startDate} endDateTime={endDate} onCampus={on_campus} onCampusBuilding={on_campus_building} onCampusRoomNumber={on_campus_room_number} eventAddress={event_address} />
+                </Card.Body>
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    More info
+                  </a>
+                </Card.Footer>
+              </Card>)}
+        </Column>
+
+        <SinglePara />
+        <SinglePara />
+
+        <Column cols="3">
+          {NewsData.slice(0, 3).map(({
+          id,
+          link,
+          title,
+          image,
+          alt
+        }) => <Card key={id} noHover>
+              <Card.ImageThumb>
+                <img src={image} alt={alt} width="200" height="133" />
+              </Card.ImageThumb>
+              <Card.Header title={title} />
+              <Card.Body>
+                <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+              </Card.Body>
+              <Card.Footer>
+                <a href={link} className="cu-button cu-button--red cu-button--small">
+                  Get informed
+                </a>
+              </Card.Footer>
+            </Card>)}
+        </Column>
+
+        <Section maxWidth="7xl" isGrey>
+          <Column cols="4">
+            {IconData.slice(0, 4).map(({
+            id,
+            link,
+            title,
+            icon
+          }) => <Card key={id}>
+                <Card.IconThumb icon={icon} />
+                <Card.Header title={title} />
+                <Card.Body>
+                  <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst." />
+                </Card.Body>
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    More info
+                  </a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+        </Section>
+
+        <Section>
+          <h2>White Background</h2>
+          <SinglePara />
+          <SinglePara />
+        </Section>
+
+        <h2>Header Two</h2>
+        <SinglePara />
+        <SinglePara />
+
+        <Column cols="3">
+          {NewsData.slice(0, 3).map(({
+          id,
+          link,
+          title,
+          date
+        }) => <Card key={id}>
+              <Card.Header title={title} date={date} datePrefix="Modified on " datePosition="bottom" />
+              <Card.Body>
+                <Card.Excerpt text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra laoreet lobortis. In hac habitasse platea dictumst. Nulla porta posuere est, aliquam mollis mi accumsan id. Morbi mauris ex, gravida eu sodales sed, tempor vel mauris." />
+              </Card.Body>
+              <Card.Footer>
+                <a href={link} className="cu-button cu-button--red cu-button--small">
+                  Read more
+                </a>
+              </Card.Footer>
+            </Card>)}
+        </Column>
+
+        <Section maxWidth="7xl">
+          <Column cols="3">
+            {PeopleData.slice(0, 3).map(({
+            id,
+            link,
+            image,
+            alt,
+            firstName,
+            lastName,
+            jobTitle,
+            email,
+            phone
+          }) => <Card key={id} isCenter>
+                <Card.Figure isRound>
+                  <img src={image} alt={alt} width={280} height={280} />
+                </Card.Figure>
+                <Card.Header title={\`\${firstName} \${lastName}\`} />
+                <Card.Body>
+                  <Card.PeopleMeta jobTitle={jobTitle} phone={phone}>
+                    <a href={\`mailto:\${email}\`}>{email}</a>
+                  </Card.PeopleMeta>
+                </Card.Body>
+                <Card.Footer>
+                  <a href={link} className="cu-button cu-button--red cu-button--small">
+                    More info
+                  </a>
+                </Card.Footer>
+              </Card>)}
+          </Column>
+        </Section>
+
+        <Section>
+          <h2>White Background</h2>
+          <SinglePara />
+          <SinglePara />
+        </Section>
+
+        <Column cols="3">
+          {VideoData.slice(0, 3).map(({
+          id,
+          source,
+          title
+        }) => <Card key={id} noHover>
+              <Card.Video source={source} />
+              <Card.Header title={title} />
+            </Card>)}
+        </Column>
+
+        <h2>Header Two</h2>
+        <SinglePara />
+        <SinglePara />
+
+        <Section maxWidth="full" isGrey>
+          <Column cols="4">
+            {StatData.slice(0, 4).map(({
+            id,
+            stat,
+            desc
+          }) => <Card key={id} leftBorder noHover>
+                <Card.Stats stat={stat} desc={desc} />
+              </Card>)}
+          </Column>
+        </Section>
+      </Main>
+
+      <FooterStandard />
+    </>
+}`,...(y=(f=C.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};const oe=["LayoutExamples"];export{C as LayoutExamples,oe as __namedExportsOrder,se as default};
