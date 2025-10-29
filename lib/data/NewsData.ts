1e36994b4aad73709caf5ac15601b362d3ac4c09
m-vojjala <58004039+m-vojjala@users.noreply.github.com>
2025-10-29 08:33:55 -0400

    change: added time to read to news listing (#536)
    * change: added time to read to news listing
    
    * change: updated changelog

--- lib/data/NewsData.ts
+++ lib/data/NewsData.ts
@@ -30,6 +30,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has launched a new AI research lab to advance the field of artificial intelligence and machine learning. The lab will focus on cutting-edge research and collaboration with industry partners.',
     date: '2023-09-15 09:00:00',
+    readTime: '5',
     tags: {
       category: [
         {
@@ -50,6 +51,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has introduced a new scholarship program aimed at supporting underrepresented students. The program will provide financial assistance and mentorship opportunities to help students succeed.',
     date: '2023-09-10 10:00:00',
+    readTime: '4',
     tags: {
       category: [
         {
@@ -70,6 +72,7 @@ export const NewsData = [
     excerpt:
       'Researchers at Carleton University have published groundbreaking findings on the impact of climate change on Arctic ecosystems. The study highlights the urgent need for action to mitigate climate change effects.',
     date: '2023-09-05 11:00:00',
+    readTime: '6',
     tags: {
       category: [
         {
@@ -90,6 +93,7 @@ export const NewsData = [
     excerpt:
       'Carleton University is proud to announce record graduation rates for the 2022 academic year. The university continues to support student success through innovative programs and dedicated faculty.',
     date: '2023-09-09 12:00:00',
+    readTime: '3',
     tags: {
       category: [
         {
@@ -110,6 +114,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has launched a new interdisciplinary program in Environmental Studies. The program aims to equip students with the knowledge and skills to address complex environmental challenges.',
     date: '2022-12-25 13:00:00',
+    readTime: '5',
     tags: {
       category: [
         {
@@ -130,6 +135,7 @@ export const NewsData = [
     excerpt:
       'A team of researchers at Carleton University has developed a promising new treatment for cancer. The treatment has shown significant results in preclinical trials and offers hope for patients worldwide.',
     date: '2022-12-20 14:00:00',
+    readTime: '7',
     tags: {
       category: [
         {
@@ -150,6 +156,7 @@ export const NewsData = [
     excerpt:
       'Carleton University recently hosted an international conference on cybersecurity, bringing together experts from around the world to discuss the latest trends and challenges in the field.',
     date: '2022-12-15 15:00:00',
+    readTime: '6',
     tags: {
       category: [
         {
@@ -170,6 +177,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has formed a new partnership with local businesses to boost innovation and economic growth. The collaboration will focus on developing new technologies and supporting startups.',
     date: '2022-12-10 16:00:00',
+    readTime: '4',
     tags: {
       category: [
         {
@@ -190,6 +198,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has received a significant grant to support research in renewable energy. The funding will be used to develop new technologies and promote sustainable energy solutions.',
     date: '2022-12-05 17:00:00',
+    readTime: '5',
     tags: {
       category: [
         {
@@ -210,6 +219,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has launched a new mental health initiative to support students, faculty, and staff. The program includes counseling services, workshops, and resources to promote mental well-being.',
     date: '2022-12-09 18:00:00',
+    readTime: '4',
     tags: {
       category: [
         {
@@ -230,6 +240,7 @@ export const NewsData = [
     excerpt:
       'Carleton University is celebrating its 75th anniversary with a series of events and activities. The university reflects on its rich history and looks forward to a bright future.',
     date: '2022-11-25 19:00:00',
+    readTime: '3',
     tags: {
       category: [
         {
@@ -250,6 +261,7 @@ export const NewsData = [
     excerpt:
       'Carleton University is expanding its online learning programs to provide more flexible and accessible education options. The new programs will cater to a diverse range of students and professionals.',
     date: '2022-11-20 20:00:00',
+    readTime: '4',
     tags: {
       category: [
         {
@@ -270,6 +282,7 @@ export const NewsData = [
     excerpt:
       'Researchers at Carleton University are studying the impact of social media on mental health. The study aims to understand the effects of social media usage and develop strategies to promote healthy online behavior.',
     date: '2022-11-15 21:00:00',
+    readTime: '6',
     tags: {
       category: [
         {
@@ -290,6 +303,7 @@ export const NewsData = [
     excerpt:
       'Carleton University recently hosted its annual science fair, showcasing innovative projects and research by students and faculty. The event attracted participants from across the region.',
     date: '2022-11-10 22:00:00',
+    readTime: '5',
     tags: {
       category: [
         {
@@ -310,6 +324,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has launched a new business incubator to support startups and entrepreneurs. The incubator will provide resources, mentorship, and networking opportunities to help businesses grow.',
     date: '2022-11-05 23:00:00',
+    readTime: '4',
     tags: {
       category: [
         {
@@ -330,6 +345,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has been recognized for its sustainability efforts with a prestigious award. The university continues to implement green initiatives and promote environmental responsibility.',
     date: '2022-11-09 08:00:00',
+    readTime: '3',
     tags: {
       category: [
         {
@@ -350,6 +366,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has partnered with international institutions to collaborate on research projects. The partnership aims to advance knowledge and address global challenges through joint efforts.',
     date: '2022-10-25 09:00:00',
+    readTime: '7',
     tags: {
       category: [
         {
@@ -370,6 +387,7 @@ export const NewsData = [
     excerpt:
       'Carleton University has launched a new program focused on arts and culture. The program aims to foster creativity and provide students with opportunities to explore various forms of artistic expression.',
     date: '2022-10-20 10:00:00',
+    readTime: '5',
     tags: {
       category: [
         {
@@ -390,6 +408,7 @@ export const NewsData = [
     excerpt:
       'Carleton University recently hosted a workshop on digital literacy, aimed at equipping participants with essential skills for navigating the digital world. The workshop covered topics such as online safety and effective communication.',
     date: '2022-10-15 11:00:00',
+    readTime: '4',
     tags: {
       category: [
         {
@@ -410,6 +429,7 @@ export const NewsData = [
     excerpt:
       'Carleton University is celebrating the achievements of its students in research. The university recognizes the hard work and dedication of students who have made significant contributions to various fields of study.',
     date: '2022-10-10 12:00:00',
+    readTime: '6',
     tags: {
       category: [
         {
