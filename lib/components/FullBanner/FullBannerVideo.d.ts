b2959a61f85e34e53b2c9c92fa67778272696bf9
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-10-29 18:33:58 +0000

    Deploying to gh-pages from @ cuweb/rds@d8daf85df01d9af343719b953fe9608d0e545057 🚀

--- lib/components/FullBanner/FullBannerVideo.d.ts
+++ lib/components/FullBanner/FullBannerVideo.d.ts
@@ -1,7 +1,9 @@
+import { VideoBannerItem } from '../../hooks/useVideoBanner';
 import { videoBanner } from '../../utils/json-lists.js';
 type VideoNameKeys = (typeof videoBanner)[number]['name'];
 export interface FullBannerVideoProps {
     videoName?: VideoNameKeys;
+    videoBannerList?: VideoBannerItem[];
 }
 export declare const FullBannerVideo: React.FC<FullBannerVideoProps>;
 export {};
--- lib/hooks/useVideoBanner.d.ts
+++ lib/hooks/useVideoBanner.d.ts
@@ -1,9 +1,16 @@
 import { videoBanner, videoSplash } from '../utils/json-lists.js';
+export interface VideoBannerItem {
+    title: string;
+    name: string;
+    description: string;
+    [key: string]: unknown;
+}
 type VideoNameKeys = (typeof videoBanner)[number]['name'] | (typeof videoSplash)[number]['name'];
 export interface VideoBannerProps {
     videoName?: VideoNameKeys;
     videoType?: 'banner' | 'splash';
     showPlayPauseButton?: boolean;
+    videoBannerList?: VideoBannerItem[];
 }
 export declare const VideoBanner: ({ videoName, videoType, showPlayPauseButton, }: VideoBannerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
