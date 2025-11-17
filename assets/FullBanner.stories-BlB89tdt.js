eb5dfdbc2216d8d540138714bd4d7a7d4975e80f
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-11-17 17:25:46 +0000

    Deploying to gh-pages from @ cuweb/rds@cadccaa4585a6531ecf50a338e72c6c6f33f077c 🚀

---
+++ assets/FullBanner.stories-BlB89tdt.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{F as o}from"./FullBanner-DYjN0Yz3.js";import{B as d}from"./ButtonGroup-Bw2xFjvQ.js";import{B as r}from"./Button-DbTEXKsr.js";import"./index-yBjzXJbu.js";import"./propClasses-CJUCiil5.js";import"./PageHeader-DdzypWj8.js";import"./parseISO-Z6eZh0wm.js";import"./format-BDD0as5c.js";import"./isSameDay-C9OCNg_L.js";import"./useVideoBanner-jeBAW2Vy.js";import"./index-DDmFEvuO.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Icon-BcjPle5o.js";const C={title:"Components/Full Banner",component:o,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},n={args:{},render:s=>e.jsxs(o,{...s,children:[e.jsx(o.Image,{children:e.jsx("img",{src:"https://picsum.photos/id/20/1920/840",alt:"Top down view of a desk"})}),e.jsxs(o.Content,{title:"The nostalgia is real.",headerType:"h1",children:[e.jsx("p",{children:"The launch of the iPod revolutionized how the world consumes music on the move! The launch of the iPod revolutionized how the world consumes music on the move!"}),e.jsxs(d,{children:[e.jsx(r,{title:"Apply Now"}),e.jsx(r,{color:"grey",title:"Request Information"})]})]})]})},t={args:{},render:s=>e.jsxs(o,{...s,children:[e.jsx(o.Video,{videoName:"campus-2025"}),e.jsxs(o.Content,{title:"The nostalgia is real.",headerType:"h1",children:[e.jsx("p",{children:"The launch of the iPod revolutionized how the world consumes music on the move! The launch of the iPod revolutionized how the world consumes music on the move!"}),e.jsxs(d,{children:[e.jsx(r,{title:"Apply Now"}),e.jsx(r,{color:"grey",title:"Request Information"})]})]})]})};var i,l,a;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <FullBanner {...args}>
+        <FullBanner.Image>
+          <img src="https://picsum.photos/id/20/1920/840" alt="Top down view of a desk" />
+        </FullBanner.Image>
+        <FullBanner.Content title="The nostalgia is real." headerType="h1">
+          <p>
+            The launch of the iPod revolutionized how the world consumes music on the move! The launch of the iPod
+            revolutionized how the world consumes music on the move!
+          </p>
+          <ButtonGroup>
+            <Button title="Apply Now" />
+            <Button color="grey" title="Request Information" />
+          </ButtonGroup>
+        </FullBanner.Content>
+      </FullBanner>;
+  }
+}`,...(a=(l=n.parameters)==null?void 0:l.docs)==null?void 0:a.source}}};var u,h,m;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <FullBanner {...args}>
+        <FullBanner.Video videoName="campus-2025" />
+        <FullBanner.Content title="The nostalgia is real." headerType="h1">
+          <p>
+            The launch of the iPod revolutionized how the world consumes music on the move! The launch of the iPod
+            revolutionized how the world consumes music on the move!
+          </p>
+          <ButtonGroup>
+            <Button title="Apply Now" />
+            <Button color="grey" title="Request Information" />
+          </ButtonGroup>
+        </FullBanner.Content>
+      </FullBanner>;
+  }
+}`,...(m=(h=t.parameters)==null?void 0:h.docs)==null?void 0:m.source}}};const I=["Primary","Video"];export{n as Primary,t as Video,I as __namedExportsOrder,C as default};
