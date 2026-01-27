0b889edf827c324a0334d3573df3a09e0ce3fee8
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2026-01-27 15:27:05 -0500

    change: table font size (#553)

--- lib/components/Table/Table.Styles.ts
+++ lib/components/Table/Table.Styles.ts
@@ -1,7 +1,7 @@
 export const styles = {
-  tableGlobal: `p-3 md:p-4 text-left align-text-top`,
-  tableHeaderRow: `font-semibold text-sm md:text-base`,
-  tableBodyRow: `text-sm md:text-base`,
+  tableGlobal: `p-2 md:py-3.5 md:px-2.5 text-left align-text-top`,
+  tableHeaderRow: `font-semibold text-sm`,
+  tableBodyRow: `text-sm`,
   borders: `border-b border-cu-black-100 last:border-none`,
   striped: `odd:bg-white even:bg-cu-black-25`,
 }
