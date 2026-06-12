227231dea4f7253fa497dc8a0968599c1a926a0b
Mounika Vojjala <mounikavojjala@gmail.com>
2026-06-12 11:02:58 -0400

    change: added social icons,acknowledgment to the footer

--- lib/components/Footer/FooterFutureFunder/FooterFutureFunder.tsx
+++ lib/components/Footer/FooterFutureFunder/FooterFutureFunder.tsx
@@ -1,10 +1,8 @@
 import { FooterLogoLinks } from '../FooterLogoLinks/FooterLogoLinks'
 import { FooterFutureFunderLinks as futureFunderData } from '../../../data/FooterStandardData'
 import { useLinkContext } from '../../LinkProvider/useLinkContext'
-
-const styles = {
-  linkHeaders: `text-base font-bold text-white mt-8`,
-}
+import { FooterSocialIcons } from '../FooterSocialIcons'
+import { footerStyles as styles } from '../footerStyles'
 
 export const FooterFutureFunder = () => {
   const LinkComponent = useLinkContext()
@@ -17,6 +15,13 @@ export const FooterFutureFunder = () => {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
+
+      <div className="flex-col pb-8 md:mx-auto md:mb-8 md:border-b md:block md:max-w-7xl md:border-cu-black-700">
+        <p className="text-sm md:text-base text-center font-medium text-white">
+          Carleton University acknowledges the location of its campus on the traditional, unceded territories of the
+          Algonquin Anishinàbeg nation
+        </p>
+      </div>
 
       <div className="flex-col hidden md:pb-8 md:mx-auto md:mb-8 md:border-b md:block md:max-w-7xl md:border-cu-black-700">
         <div id="footer-links" className="flex justify-between w-full gap-8">
@@ -89,6 +94,26 @@ export const FooterFutureFunder = () => {
       </div>
 
       <div className="flex flex-col mx-auto max-w-7xl">
+        <div className="flex flex-col-reverse items-center justify-between gap-2 mb-12 text-base lg:gap-4 lg:text-sm lg:items-start lg:flex-row text-cu-black-300">
+          <p>
+            Contact us by{' '}
+            <LinkComponent
+              href="tel:+1-613-520-2600"
+              className="font-semibold text-white underline underline-offset-auto hover:text-cu-red"
+            >
+              phone
+            </LinkComponent>{' '}
+            or{' '}
+            <LinkComponent
+              href="https://carleton.ca/about/contact/"
+              className="font-semibold text-white underline underline-offset-auto hover:text-cu-red"
+            >
+              email
+            </LinkComponent>
+          </p>
+          <p className="text-center">1125 Colonel By Drive, Ottawa, ON, K1S 5B6, Canada</p>
+          <FooterSocialIcons hoverColor="hover:text-white" />
+        </div>
         <FooterLogoLinks />
       </div>
     </footer>
---
+++ lib/components/Footer/footerStyles.ts
@@ -0,0 +1,3 @@
+export const footerStyles = {
+  linkHeaders: `text-base font-bold text-white mt-8`,
+}
